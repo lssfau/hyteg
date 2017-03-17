5666ad4a4660c4491b1bdded3f22f6b5f40e9f32
dthoennes <dominik.thoennes@fau.de>
2017-03-17 12:32:47 +0100

    added p2 face interpolate function

--- include/p2functionspace/p2face.hpp
+++ include/p2functionspace/p2face.hpp
@@ -32,7 +32,7 @@ inline void free(Face& face, size_t memory_id, size_t minLevel, size_t maxLevel)
 
 inline void interpolate(Face& face, size_t memory_id, std::function<double(const hhg::Point3D&)>& expr, size_t level)
 {
-  size_t rowsize = levelinfo::num_microvertices_per_edge(level);
+  size_t rowsize = levelinfo::num_microvertices_per_edge(level) + levelinfo::num_microedges_per_edge(level);;
   Point3D x, x0;
 
   if (face.edge_orientation[0] == 1)
@@ -47,22 +47,22 @@ inline void interpolate(Face& face, size_t memory_id, std::function<double(const
   Point3D d0 = face.edge_orientation[0] * face.edges[0]->direction / (rowsize-1);
   Point3D d2 = -face.edge_orientation[2] * face.edges[2]->direction / (rowsize-1);
 
-  size_t mr_c = 1 + rowsize;
+  size_t mr_c = 2 + rowsize;
   size_t inner_rowsize = rowsize;
 
-  for (size_t i = 0; i < rowsize-3; ++i)
+  for (size_t i = 0; i < rowsize-5; ++i)
   {
     x = x0;
-    x += (i+1) * d2 + d0;
+    x += (i+1) * d2 *2 + d0 * 2;
 
-    for (size_t j = 0; j < inner_rowsize-3; ++j)
+    for (size_t j = 0; j < inner_rowsize-5; ++j)
     {
       face.data[memory_id][level-2][mr_c] = expr(x);
       x += d0;
       mr_c += 1;
     }
 
-    mr_c += 2;
+    mr_c += 4;
     inner_rowsize -= 1;
   }
 }
