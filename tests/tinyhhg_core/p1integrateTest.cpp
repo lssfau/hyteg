d57c518c3016bd7dd78b3c8ba0f956f027643bd1
dthoennes <dominik.thoennes@fau.de>
2017-04-13 15:10:47 +0200

    started adding tests

---
+++ tests/tinyhhg_core/p1integrateTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by thoennes on 13.04.17.
+//
+
+#include <tinyhhg_core/tinyhhg.hpp>
+#include <core/debug/TestSubsystem.h>
+
+int main (int argc, char ** argv )
+{
+  hhg::Mesh mesh("./tri_1el.msh");
+
+
+}
