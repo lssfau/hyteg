ed0ffbe693f1616fc93ddb3a64f22c7da607bc95
dthoennes <dominik.thoennes@fau.de>
2017-05-22 10:40:42 +0200

    added fmg test

--- tests/tinyhhg_core/p1integrateTest.cpp
+++ tests/tinyhhg_core/p1integrateTest.cpp
@@ -2,9 +2,12 @@
 // Created by thoennes on 13.04.17.
 //
 
-#include <tinyhhg_core/tinyhhg.hpp>
+#include "tinyhhg_core/tinyhhg.hpp"
 #include <core/debug/TestSubsystem.h>
 #include <core/mpi/MPIManager.h>
+#include "core/DataTypes.h"
+
+using walberla::real_t;
 
 int main (int argc, char ** argv )
 {
@@ -18,7 +21,7 @@ int main (int argc, char ** argv )
 
   hhg::P1Function x("x", mesh, minLevel, maxLevel);
 
-  std::function<double(const hhg::Point3D&)> exact = [](const hhg::Point3D&) { return 13; };
+  std::function<double(const hhg::Point3D&)> exact = [](const hhg::Point3D&) { return 13.0; };
 
   auto faceZero = mesh.faces[0];
   hhg::P1Face::interpolate(faceZero,0,exact,maxLevel);
@@ -27,30 +30,28 @@ int main (int argc, char ** argv )
   size_t length = hhg::levelinfo::num_microvertices_per_edge(maxLevel);
   for(size_t i = 0; i < totalPoints; ++i){
     if(hhg::P1Face::is_boundary(i,length)) {
-      WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i], 0.0
-      ,  "i was " << i << " totalPoints was "<< totalPoints)
+      WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i],0.0)
     } else {
-      WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i], 13.0, "i was " << i)
+      WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i], 13.0)
     }
-
   }
 
 
 
-  // auto edgeZero = mesh.edges[0];
-  // hhg::P1Edge::interpolate(edgeZero,0,exact,maxLevel);
-
-  // for(size_t i = 1; i < (micro_per_e-1); ++i){
-
-  //   WALBERLA_LOG_INFO()
-  // }
-
-  // for(size_t i = 1; i < (micro_per_e-1); ++i){
-  //   WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i], 13.0,
-  //                              "index was " << i)
-  // }
-  // WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][0], 0.0)
-  // WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][micro_per_e-1], 0.0)
+//   auto edgeZero = mesh.edges[0];
+//   hhg::P1Edge::interpolate(edgeZero,0,exact,maxLevel);
+//
+//   for(size_t i = 1; i < (micro_per_e-1); ++i){
+//
+//     WALBERLA_LOG_INFO()
+//   }
+//
+//   for(size_t i = 1; i < (micro_per_e-1); ++i){
+//     WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][i], 13.0,
+//                                "index was " << i)
+//   }
+//   WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][0], 0.0)
+//   WALBERLA_CHECK_FLOAT_EQUAL(mesh.faces[0].data[0][maxLevel - 2][micro_per_e-1], 0.0)
 
 
   return 0;
