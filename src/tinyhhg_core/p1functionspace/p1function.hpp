238ef2457ae994bf1aab377ac1ca11896c067a2d
Daniel Drzisga <sersmicro@gmail.com>
2017-05-23 00:04:51 +0200

    fixed serial builds

--- src/tinyhhg_core/p1functionspace/p1function.hpp
+++ src/tinyhhg_core/p1functionspace/p1function.hpp
@@ -267,10 +267,14 @@ public:
       }
     }
 
+#ifdef WALBERLA_BUILD_WITH_MPI
     double sp_g = 0.0;
     MPI_Allreduce(&sp_l, &sp_g, 1, walberla::MPITrait< double >::type(), MPI_SUM, MPI_COMM_WORLD);
 
     return sp_g;
+#else // WALBERLA_BUILD_WITH_MPI
+    return sp_l;
+#endif
   }
 
   void prolongate(size_t level, DoFType flag = All)
