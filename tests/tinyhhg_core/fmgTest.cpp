c7be51f0b7d53b3283caef082a995a52543d76ce
Daniel Drzisga <sersmicro@gmail.com>
2017-05-24 16:25:25 +0200

    trying to fix clang error

--- tests/tinyhhg_core/fmgTest.cpp
+++ tests/tinyhhg_core/fmgTest.cpp
@@ -9,6 +9,50 @@ using walberla::uint_t;
 using walberla::uint_c;
 using walberla::real_t;
 
+template<class O, class F, class CSolver>
+void cscycle(size_t level, size_t minLevel, CSolver& csolver, O& A, F& x, F&ax, F& b, F& r, F& tmp, real_t coarse_tolerance, size_t coarse_maxiter, size_t nu_pre, size_t nu_post)
+{
+  std::function<real_t(const hhg::Point3D&)> zero  = [](const hhg::Point3D&) { return 0.0; };
+
+  if (level == minLevel)
+  {
+    // fmt::printf("Coarse solve...\n");
+    csolver.solve(A, x, b, r, minLevel, coarse_tolerance, coarse_maxiter, hhg::Inner, false);
+  }
+  else
+  {
+    // fmt::printf("Level %d...\n", level);
+
+    // pre-smooth
+    for (size_t i = 0; i < nu_pre; ++i)
+    {
+      A.smooth_gs(x, b, level, hhg::Inner);
+    }
+
+    A.apply(x, ax, level, hhg::Inner);
+    r.assign({1.0, -1.0}, { &b, &ax }, level, hhg::Inner);
+
+    // restrict
+    r.restrict(level, hhg::Inner);
+    b.assign({1.0}, { &r }, level - 1, hhg::Inner);
+
+    x.interpolate(zero, level-1);
+
+    cscycle(level-1, minLevel, csolver, A, x, ax, b, r, tmp, coarse_tolerance, coarse_maxiter, nu_pre, nu_post);
+
+    // prolongate
+    tmp.assign({1.0}, { &x }, level, hhg::Inner);
+    x.prolongate(level-1, hhg::Inner);
+    x.add({1.0}, { &tmp }, level, hhg::Inner);
+
+    // post-smooth
+    for (size_t i = 0; i < nu_post; ++i)
+    {
+      A.smooth_gs(x, b, level, hhg::Inner);
+    }
+  }
+}
+
 int main(int argc, char* argv[])
 {
   LIKWID_MARKER_INIT;
@@ -58,7 +102,6 @@ int main(int argc, char* argv[])
 
   std::function<real_t(const hhg::Point3D&)> exact = [](const hhg::Point3D& xx) { return xx[0]*xx[0] - xx[1]*xx[1]; };
   std::function<real_t(const hhg::Point3D&)> rhs   = [](const hhg::Point3D&) { return 0.0; };
-  std::function<real_t(const hhg::Point3D&)> zero  = [](const hhg::Point3D&) { return 0.0; };
   std::function<real_t(const hhg::Point3D&)> ones  = [](const hhg::Point3D&) { return 1.0; };
 
   x.interpolate(exact, maxLevel, hhg::DirichletBoundary);
@@ -67,7 +110,7 @@ int main(int argc, char* argv[])
   tmp.interpolate(ones, maxLevel);
   real_t npoints = tmp.dot(tmp, maxLevel);
 
-  auto solver = hhg::CGSolver<hhg::P1Function, hhg::P1LaplaceOperator>(mesh, minLevel, minLevel);
+  auto csolver = hhg::CGSolver<hhg::P1Function, hhg::P1LaplaceOperator>(mesh, minLevel, minLevel);
 
   if (rk == 0)
   {
@@ -92,54 +135,11 @@ int main(int argc, char* argv[])
     WALBERLA_LOG_INFO_ON_ROOT(fmt::format("{:3d}   {:e}  {:e}  {:e}  {:e}", 0, begin_res, rel_res, begin_res/abs_res_old, discr_l2_err));
   }
 
-  std::function<void(size_t)> cscycle;
-
-  cscycle = [&](size_t level)
-  {
-    if (level == minLevel)
-    {
-      // fmt::printf("Coarse solve...\n");
-      solver.solve(A, x, b, r, minLevel, coarse_tolerance, coarse_maxiter, hhg::Inner, false);
-    }
-    else
-    {
-      // fmt::printf("Level %d...\n", level);
-
-      // pre-smooth
-      for (size_t i = 0; i < nu_pre; ++i)
-      {
-        A.smooth_gs(x, b, level, hhg::Inner);
-      }
-
-      A.apply(x, ax, level, hhg::Inner);
-      r.assign({1.0, -1.0}, { &b, &ax }, level, hhg::Inner);
-
-      // restrict
-      r.restrict(level, hhg::Inner);
-      b.assign({1.0}, { &r }, level - 1, hhg::Inner);
-
-      x.interpolate(zero, level-1);
-
-      cscycle(level-1);
-
-      // prolongate
-      tmp.assign({1.0}, { &x }, level, hhg::Inner);
-      x.prolongate(level-1, hhg::Inner);
-      x.add({1.0}, { &tmp }, level, hhg::Inner);
-
-      // post-smooth
-      for (size_t i = 0; i < nu_post; ++i)
-      {
-        A.smooth_gs(x, b, level, hhg::Inner);
-      }
-    }
-  };
-
   LIKWID_MARKER_START("Compute");
   size_t i = 0;
   for (; i < outer; ++i)
   {
-    cscycle(maxLevel);
+    cscycle(maxLevel, minLevel, csolver, A, x, ax, b, r, tmp, coarse_tolerance, coarse_maxiter, nu_pre, nu_post);
     A.apply(x, ax, maxLevel, hhg::Inner);
     r.assign({1.0, -1.0}, { &b, &ax }, maxLevel, hhg::Inner);
     real_t abs_res = std::sqrt(r.dot(r, maxLevel, hhg::Inner));
