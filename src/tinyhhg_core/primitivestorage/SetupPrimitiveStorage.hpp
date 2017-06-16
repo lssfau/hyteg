0d0834b1f5f44f6dea01cd6c9ac103a5f41efbfa
Nils Kohl <nils.kohl@fau.de>
2017-06-16 16:41:56 +0200

    some typedefs for convenience

--- src/tinyhhg_core/primitivestorage/SetupPrimitiveStorage.hpp
+++ src/tinyhhg_core/primitivestorage/SetupPrimitiveStorage.hpp
@@ -20,7 +20,7 @@ using walberla::uint_t;
 using walberla::real_t;
 
 /// \brief Contains information about a mesh
-/// \author Daniel Drzisga (drzisga@ma.tum.de)
+/// \author Daniel Drzisga (drzisga@ma.tum.de) \n
 ///         Nils Kohl (nils.kohl@fau.de)
 ///
 /// The \ref MeshInfo class is used to store information about meshes that are
@@ -32,22 +32,29 @@ class MeshInfo
 {
 public:
 
+  /// Vertices: ( ID, coordinate )
+  typedef std::map< uint_t, Point3D >                                   VertexContainer;
+  /// Edges: ( ( VertexID_0, VertexID_1 ), DoFType )
+  typedef std::set< std::pair< std::pair< uint_t, uint_t >, DoFType > > EdgeContainer;
+  /// Faces: ( VertexID_0, VertexID_1, VertexID_2 )
+  typedef std::set< std::tuple< uint_t, uint_t, uint_t > >              FaceContainer;
+
   /// Construct a MeshInfo from a file in Gmsh format
   static MeshInfo fromGmshFile( const std::string & meshFileName );
 
   /// Returns vertices of the mesh
-  /// \return map that maps ( vertex ID -> vertex coordinate )
-  const std::map< uint_t, Point3D > & getVertices() const { return vertices_; };
+  /// \return Map that maps ( vertex ID -> vertex coordinate )
+  const VertexContainer & getVertices() const { return vertices_; };
 
   /// Returns edges of the mesh
-  /// \return set of a pair of \n
+  /// \return Set of a pair of \n
   ///         - a pair of vertex indices (matching indices from the vertices from \ref getVertices \n
   ///         - and the corresponding \ref DoFType
-  const std::set< std::pair< std::pair< uint_t, uint_t >, DoFType > > & getEdges() const { return edges_; };
+  const EdgeContainer & getEdges() const { return edges_; };
 
   /// Returns faces of the mesh
-  /// \return set of 3-tuples of vertex indices from the vertices from \ref getVertices
-  const std::set< std::tuple< uint_t, uint_t, uint_t > > & getFaces() const { return faces_; }
+  /// \return Set of 3-tuples of vertex indices from the vertices from \ref getVertices
+  const FaceContainer & getFaces() const { return faces_; }
 
 private:
 
@@ -56,14 +63,9 @@ private:
   /// Adds edge in ascending index order and performs checks
   void addEdge( uint_t idx0, uint_t idx1, DoFType dofType );
 
-  /// Vertices: ( ID, coordinate )
-  std::map< uint_t, Point3D > vertices_;
-
-  /// Edges: ( ( VertexID_0, VertexID_1 ), DoFType )
-  std::set< std::pair< std::pair< uint_t, uint_t >, DoFType > > edges_;
-
-  /// Faces: ( VertexID_0 )
-  std::set< std::tuple< uint_t, uint_t, uint_t > > faces_;
+  VertexContainer vertices_;
+  EdgeContainer edges_;
+  FaceContainer faces_;
 
 };
 
