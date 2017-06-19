7c0792b0b7e59666a1c356171a14755def375996
Nils Kohl <nils.kohl@fau.de>
2017-06-19 18:51:14 +0200

    moving storage constructor to cpp file

--- src/tinyhhg_core/primitivestorage/PrimitiveStorage.cpp
+++ src/tinyhhg_core/primitivestorage/PrimitiveStorage.cpp
@@ -14,6 +14,53 @@ namespace hhg {
 
 using walberla::uint_t;
 
+PrimitiveStorage::PrimitiveStorage( const SetupPrimitiveStorage & setupStorage ) :
+  primitiveDataHandlers_( 0 )
+{
+  std::map< PrimitiveID::IDType, SetupVertex* > setupVertices;
+  std::map< PrimitiveID::IDType, SetupEdge* >   setupEdges;
+  std::map< PrimitiveID::IDType, SetupFace* >   setupFaces;
+
+  setupStorage.getVertices( setupVertices );
+  setupStorage.getEdges( setupEdges );
+  setupStorage.getFaces( setupFaces );
+
+  uint_t processRank = walberla::mpi::MPIManager::instance()->rank();
+
+  for ( auto it = setupVertices.begin(); it != setupVertices.end(); it++  )
+  {
+    if ( processRank == it->second->getTargetRank() )
+    {
+      vertices_[ it->first ] = new Vertex( it->first, it->second->getCoordinates() );
+    }
+  }
+
+  for ( auto it = setupEdges.begin(); it != setupEdges.end(); it++ )
+  {
+    if ( processRank == it->second->getTargetRank() )
+    {
+      PrimitiveID edgeID = it->first;
+      DoFType edgeType = it->second->getDoFType();
+      Vertex* edgeVertex0 = vertices_[ it->second->getVertexID0().getID() ];
+      Vertex* edgeVertex1 = vertices_[ it->second->getVertexID1().getID() ];
+      edges_[ edgeID.getID() ] = new Edge( edgeID.getID(), edgeType, edgeVertex0, edgeVertex1 );
+    }
+  }
+
+  for ( auto it = setupFaces.begin(); it != setupFaces.end(); it++ )
+  {
+    if ( processRank == it->second->getTargetRank() )
+    {
+      PrimitiveID faceID = it->first;
+      Edge* faceEdges[3];
+      faceEdges[0] = edges_[ it->second->getEdgeID0().getID() ];
+      faceEdges[1] = edges_[ it->second->getEdgeID1().getID() ];
+      faceEdges[2] = edges_[ it->second->getEdgeID2().getID() ];
+      faces_[ faceID.getID() ] = new Face( faceID.getID(), faceEdges );
+    }
+  }
+}
+
 
 PrimitiveID PrimitiveStorage::addVertex()
 {
