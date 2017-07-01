5807f4ee2e49739ab41083eb24ee53afcc5ff59d
Dominik Thoennes <dom.thoennes@googlemail.com>
2017-07-01 10:25:53 +0200

    Added more funcitons to Packinfo class

--- src/tinyhhg_core/communication/PackInfo.hpp
+++ src/tinyhhg_core/communication/PackInfo.hpp
@@ -1,5 +1,7 @@
-
 #pragma once
+
+#include <tinyhhg_core/primitives/edge.hpp>
+#include "tinyhhg_core/primitiveid.hpp"
 
 namespace hhg {
 /// namespace containing function for communication between primitives
@@ -17,10 +19,45 @@ public:
 
     virtual ~PackInfo() {}
 
-    //virtual void packDataEdgeToFace        ( const Edge * sender,   const PrimitiveID & receiver, walberla::mpi::SendBuffer & buffer ) = 0;
-    //virtual void unpackDataEdgeToFace      (       Face * receiver, const PrimitiveID & sender  , walberla::mpi::RecvBuffer & buffer ) = 0;
+/// @name Vertex to Edge
+///@{
 
-    //virtual void communicateLocalEdgeToFace( const Edge * sender, Face * receiver ) = 0;
+/// pack data from Vertex into SendBuffer for Edge
+  virtual void packVertexForEdge(const Vertex *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+/// unpack data into Vertex from RecvBuffer from Edge
+  virtual void unpackVertexFromEdge(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+/// transfer data locally (meaning both are on the same MPI Process) from Vertex to Edge
+  virtual void communicateLocalVertexToEdge(const Vertex *sender, Edge *receiver) = 0;
+  ///@}
+
+/// @name Edge to Vertex
+///@{
+  virtual void packEdgeForVertex(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+
+  virtual void unpackVertexFromEdge(Vertex *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+
+  virtual void communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiver) = 0;
+///@}
+
+/// @name Edge to Face
+///@{
+
+  virtual void packEdgeForFace(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+
+  virtual void unpackFaceFromEdge(Face *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+
+  virtual void communicateLocalEdgeToFace(const Edge *sender, Face *receiver) = 0;
+///@}
+
+/// @name Face to Edge
+///@{
+
+  virtual void packFaceForEdge(const Face *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+
+  virtual void unpackEdgeFromFace(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+
+  virtual void communicateLocalFaceToEdge(const Face *sender, Edge *receiver) = 0;
+///@}
 
 };
 
