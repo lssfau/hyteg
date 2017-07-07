9782480610410d1d5a678e6fb5c91800ffa36926
Nils Kohl <nilskohlabc@gmail.com>
2017-07-07 12:44:32 +0200

    using existing meshfile in storage test since there seem to be race conditions when generating it on the fly

--- tests/tinyhhg_core/PrimitiveStorageTest.cpp
+++ tests/tinyhhg_core/PrimitiveStorageTest.cpp
@@ -7,55 +7,11 @@
 
 namespace hhg {
 
-std::string getExampleMeshFileContent()
-{
-  std::string content =
-  "$MeshFormat\n"
-  "2.2 0 8\n"
-  "$EndMeshFormat\n"
-  "$Nodes\n"
-  "4\n"
-  "1 0 0 0\n"
-  "2 1 0 0\n"
-  "3 0 1 0\n"
-  "4 1 1 0\n"
-  "$EndNodes\n"
-  "$Elements\n"
-  "9\n"
-  "1 15 2 1 1 1\n"
-  "2 15 2 1 2 2\n"
-  "3 15 2 1 3 3\n"
-  "4 1 2 1 1 1 2\n"
-  "5 1 2 1 2 2 4\n"
-  "6 1 2 1 2 4 3\n"
-  "7 1 2 1 3 3 1\n"
-  "8 2 2 0 5 2 4 1\n"
-  "9 2 2 0 5 1 4 3\n"
-  "$EndElements\n";
-
-  return content;
-}
-
-void writeTestMeshFile( const std::string & meshFileName )
-{
-  WALBERLA_ROOT_SECTION()
-  {
-    std::string meshFileContent = getExampleMeshFileContent();
-    std::ofstream file( meshFileName );
-    file << meshFileContent;
-    file.close();
-  }
-}
-
 static void testPrimitiveStorage()
 {
   uint_t rank = uint_c( walberla::mpi::MPIManager::instance()->rank() );
 
-  std::string meshFileName = "./tmpMeshFile.msh";
-  writeTestMeshFile( meshFileName );
-
-  // For mesh file
-  WALBERLA_MPI_BARRIER();
+  std::string meshFileName = "../../data/meshes/tri_2el.msh";
 
   MeshInfo meshInfo = MeshInfo::fromGmshFile( meshFileName );
   SetupPrimitiveStorage setupStorage( meshInfo, uint_c ( walberla::mpi::MPIManager::instance()->numProcesses() ) );
