da58d55a9e9497b3ee0b3db684014d46bc784ee9
dthoennes <dominik.thoennes@fau.de>
2017-07-13 16:33:38 +0200

    improvement on BubblePackTest

--- tests/tinyhhg_core/P1BubblePackTest.cpp
+++ tests/tinyhhg_core/P1BubblePackTest.cpp
@@ -9,13 +9,13 @@ class FaceP1BubbleFunctionMemoryDataHandling : public OnlyInitializeDataHandling
 public:
     FaceP1BubbleFunctionMemory * initialize(const Face * const face) const
     {
-      //face->memory.push_back(new FaceP1BubbleFunctionMemory());
       WALBERLA_UNUSED(face);
+      FaceP1BubbleFunctionMemory *mem = new FaceP1BubbleFunctionMemory();
       for (size_t level = 1; level <= 5; ++level)
       {
-        P1Bubble::getFaceFunctionMemory(*face, 0)->addlevel(level);
+        mem->addlevel(level);
       }
-      return P1Bubble::getFaceFunctionMemory(*face, 0);
+      return mem;
     }
 };
 
@@ -53,6 +53,8 @@ int main (int argc, char** argv) {
 
   const uint_t maxLevel = 3;
 
+  size_t v_perEdge = hhg::levelinfo::num_microvertices_per_edge(maxLevel);
+
   hhg::MeshInfo meshInfo = MeshInfo::fromGmshFile( "../../data/meshes/tri_2el.msh" );
   hhg::SetupPrimitiveStorage setupStorage( meshInfo, 1  );
 
@@ -68,27 +70,75 @@ int main (int argc, char** argv) {
   hhg::PrimitiveStorage::PrimitiveMap primitveMap;
   storage->getPrimitives( primitveMap);
 
-  FaceP1BubbleFunctionMemory *data = storage->beginFaces()->second->getData(faceDataID);
-  WALBERLA_UNUSED(data);
+  //FaceP1BubbleFunctionMemory *face0data = storage->beginFaces()->second->getData(faceDataID);
+  real_t *face0data = storage.get()->beginFaces()->second->getData(faceDataID)->data[maxLevel].get();
+  std::cout << "First Face ID is: " << storage.get()->beginFaces()->second->getID().getID() << std::endl;
+  real_t *face1data = (++storage.get()->beginFaces())->second->getData(faceDataID)->data[maxLevel].get();
 
+  uint_t edgeId = 1;
+  for(auto it = storage->beginEdges(); it != storage->endEdges(); ++it){
+    for(uint_t i = 0; i < v_perEdge; ++i){
+      it->second->getData(edgeDataID)->data[maxLevel].get()[i] = 2 + (edgeId * 0.1);
+    }
+    edgeId++;
+
+    if(it->second->getNumHigherDimNeighbors() == 2){
+      EdgeP1BubbleFunctionMemory *middleEdgeData = it->second->getData(edgeDataID);
+      std::cout << "Middle Edge ID is: " << it->second->getID().getID() << std::endl;
+    }
+  }
+  real_t counter = 1;
+  for(uint_t i = 0; i < hhg::levelinfo::num_microvertices_per_face(maxLevel);++i){
+    face0data[i] = 1.1;
+  }
   P1BubblePackInfo *packInfo = new P1BubblePackInfo(maxLevel,vertexDataID,edgeDataID,faceDataID,storage);
-
+//  for(auto it = storage->beginEdges(); it != storage->endEdges(); ++it){
+//
+//    //packInfo->packEdgeForFace(*it,)
+//  }
 
 
   std::function<real_t(const hhg::Point3D&)> eight = [](const hhg::Point3D&) { return 8; };
   std::function<real_t(const hhg::Point3D&)> nine = [](const hhg::Point3D&) { return 9; };
-
-  hhg::Face *face0 = storage->beginFaces()->second;
 //
-  walberla::mpi::SendBuffer sb;
-  packInfo->packVertexForEdge(storage->beginVertices()->second,storage->beginEdges()->first,sb);
+//  hhg::Face *face0 = storage->beginFaces()->second;
+//
+//  walberla::mpi::SendBuffer sb;
+//  packInfo->packEdgeForFace(storage->getEdge(7),10, sb);
 //  hhg::P1BubbleVertex::packData(level,*edge.v0,0,sb);
-//  hhg::P1BubbleVertex::packData(level,*edge.v1,0,sb);
+// hhg::P1BubbleVertex::packData(level,*edge.v1,0,sb);
 //  walberla::mpi::RecvBuffer rb(sb);
-//  unpackVertexData(level,edge,memory_id,rb,*edge.v0);
+//  packInfo->unpackFaceFromEdge(storage->getFace(10),7,rb);
 
-  hhg::P1BubbleFace::interpolate(*face0,0,eight,maxLevel);
+  for(auto it = storage->beginEdges(); it != storage->endEdges(); ++it){
+    walberla::mpi::SendBuffer sb;
+    for(auto jt = it->second->beginHigherDimNeighbors(); jt != it->second->endHigherDimNeighbors(); ++jt){
+      packInfo->packEdgeForFace(it->second,jt->first,sb);
+      walberla::mpi::RecvBuffer rb(sb);
+      packInfo->unpackFaceFromEdge(storage->getFace(jt->first),it->first,rb);
+    }
+  }
 
-  WALBERLA_UNUSED(face0);
+  //hhg::P1BubbleFace::interpolate(*face0,0,eight,maxLevel);
 
+
+
+  std::cout << "++++ Face 0 Vertex Dofs: ++++" << std::endl;
+  for(size_t i = 0; i < v_perEdge; ++i){
+    for(size_t j = 0; j < v_perEdge - i; ++j) {
+      //std::cout << face0mem[CoordsVertex::index<maxLevel>(i, j, CoordsVertex::VERTEX_C)] << " ";
+      fmt::print("{0:.2f}  ",face0data[hhg::P1BubbleFace::CoordsVertex::index<maxLevel>(i, j, hhg::P1BubbleFace::CoordsVertex::VERTEX_C)]);
+      //std::cout << face0data[hhg::P1BubbleFace::CoordsVertex::index<maxLevel>(i, j, hhg::P1BubbleFace::CoordsVertex::VERTEX_C)];
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "++++ Face 1 Vertex Dofs: ++++" << std::endl;
+  for(size_t i = 0; i < v_perEdge; ++i){
+    for(size_t j = 0; j < v_perEdge - i; ++j) {
+      //std::cout << face0mem[CoordsVertex::index<maxLevel>(i, j, CoordsVertex::VERTEX_C)] << " ";
+      fmt::print("{0:.2f}  ",face1data[hhg::P1BubbleFace::CoordsVertex::index<maxLevel>(i, j, hhg::P1BubbleFace::CoordsVertex::VERTEX_C)]);
+      //std::cout << face0data[hhg::P1BubbleFace::CoordsVertex::index<maxLevel>(i, j, hhg::P1BubbleFace::CoordsVertex::VERTEX_C)];
+    }
+    std::cout << std::endl;
+  }
 }
