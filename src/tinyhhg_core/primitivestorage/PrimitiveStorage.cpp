658eb227acaa5d3a41de9aceb241daa888e20af9
Nils Kohl <nils.kohl@fau.de>
2017-07-18 17:22:57 +0200

    adding neighborhood to PrimitiveStorage

--- src/tinyhhg_core/primitivestorage/PrimitiveStorage.cpp
+++ src/tinyhhg_core/primitivestorage/PrimitiveStorage.cpp
@@ -23,7 +23,7 @@ PrimitiveStorage::PrimitiveStorage( const uint_t & rank, const SetupPrimitiveSto
   {
     if ( rank_ == setupStorage.getTargetRank( it->first ) )
     {
-      vertices_[ it->first ] = new Vertex( *this, setupStorage, it->first );
+      vertices_[ it->first ] = new Vertex( *it->second );
     }
   }
 
@@ -31,7 +31,7 @@ PrimitiveStorage::PrimitiveStorage( const uint_t & rank, const SetupPrimitiveSto
   {
     if ( rank_ == setupStorage.getTargetRank( it->first ) )
     {
-      edges_[ it->first ] = new Edge( *this, setupStorage, it->first );
+      edges_[ it->first ] = new Edge( *it->second );
     }
   }
 
@@ -39,9 +39,117 @@ PrimitiveStorage::PrimitiveStorage( const uint_t & rank, const SetupPrimitiveSto
   {
     if ( rank_ == setupStorage.getTargetRank( it->first ) )
     {
-      faces_[ it->first ] = new Face( *this, setupStorage, it->first );
+      faces_[ it->first ] = new Face( *it->second );
     }
   }
+
+  // Neighborhood
+
+  for ( const auto & it : vertices_ )
+  {
+    Vertex * vertex = it.second;
+
+    for ( const auto & neighborVertexID : vertex->neighborVertices() )
+    {
+      const Vertex * neighborVertex = setupStorage.getVertex( neighborVertexID );
+      if ( !vertexExistsLocally( neighborVertexID ) && !vertexExistsInNeighborhood( neighborVertexID ) )
+      {
+        neighborVertices_[ neighborVertexID.getID() ] = new Vertex( *neighborVertex );
+        neighborRanks_[ neighborVertexID.getID() ] = setupStorage.getTargetRank( neighborVertexID.getID() );
+      }
+    }
+
+    for ( const auto & neighborEdgeID : vertex->neighborEdges() )
+    {
+      const Edge * neighborEdge = setupStorage.getEdge( neighborEdgeID );
+      if ( !edgeExistsLocally( neighborEdgeID ) && !edgeExistsInNeighborhood( neighborEdgeID ) )
+      {
+        neighborEdges_[ neighborEdgeID.getID() ] = new Edge( *neighborEdge );
+        neighborRanks_[ neighborEdgeID.getID() ] = setupStorage.getTargetRank( neighborEdgeID.getID() );
+      }
+    }
+
+    for ( const auto & neighborFaceID : vertex->neighborFaces() )
+    {
+      const Face * neighborFace = setupStorage.getFace( neighborFaceID );
+      if ( !faceExistsLocally( neighborFaceID ) && !faceExistsInNeighborhood( neighborFaceID ) )
+      {
+        neighborFaces_[ neighborFaceID.getID() ] = new Face( *neighborFace );
+        neighborRanks_[ neighborFaceID.getID() ] = setupStorage.getTargetRank( neighborFaceID.getID() );
+      }
+    }
+  }
+
+  for ( const auto & it : edges_ )
+  {
+    Edge * edge = it.second;
+
+    for ( const auto & neighborVertexID : edge->neighborVertices() )
+    {
+      const Vertex * neighborVertex = setupStorage.getVertex( neighborVertexID );
+      if ( !vertexExistsLocally( neighborVertexID ) && !vertexExistsInNeighborhood( neighborVertexID ) )
+      {
+        neighborVertices_[ neighborVertexID.getID() ] = new Vertex( *neighborVertex );
+        neighborRanks_[ neighborVertexID.getID() ] = setupStorage.getTargetRank( neighborVertexID.getID() );
+      }
+    }
+
+    for ( const auto & neighborEdgeID : edge->neighborEdges() )
+    {
+      const Edge * neighborEdge = setupStorage.getEdge( neighborEdgeID );
+      if ( !edgeExistsLocally( neighborEdgeID ) && !edgeExistsInNeighborhood( neighborEdgeID ) )
+      {
+        neighborEdges_[ neighborEdgeID.getID() ] = new Edge( *neighborEdge );
+        neighborRanks_[ neighborEdgeID.getID() ] = setupStorage.getTargetRank( neighborEdgeID.getID() );
+      }
+    }
+
+    for ( const auto & neighborFaceID : edge->neighborFaces() )
+    {
+      const Face * neighborFace = setupStorage.getFace( neighborFaceID );
+      if ( !faceExistsLocally( neighborFaceID ) && !faceExistsInNeighborhood( neighborFaceID ) )
+      {
+        neighborFaces_[ neighborFaceID.getID() ] = new Face( *neighborFace );
+        neighborRanks_[ neighborFaceID.getID() ] = setupStorage.getTargetRank( neighborFaceID.getID() );
+      }
+    }
+  }
+
+  for ( const auto & it : faces_ )
+  {
+    Face * face = it.second;
+
+    for ( const auto & neighborVertexID : face->neighborVertices() )
+    {
+      const Vertex * neighborVertex = setupStorage.getVertex( neighborVertexID );
+      if ( !vertexExistsLocally( neighborVertexID ) && !vertexExistsInNeighborhood( neighborVertexID ) )
+      {
+        neighborVertices_[ neighborVertexID.getID() ] = new Vertex( *neighborVertex );
+        neighborRanks_[ neighborVertexID.getID() ] = setupStorage.getTargetRank( neighborVertexID.getID() );
+      }
+    }
+
+    for ( const auto & neighborEdgeID : face->neighborEdges() )
+    {
+      const Edge * neighborEdge = setupStorage.getEdge( neighborEdgeID );
+      if ( !edgeExistsLocally( neighborEdgeID ) && !edgeExistsInNeighborhood( neighborEdgeID ) )
+      {
+        neighborEdges_[ neighborEdgeID.getID() ] = new Edge( *neighborEdge );
+        neighborRanks_[ neighborEdgeID.getID() ] = setupStorage.getTargetRank( neighborEdgeID.getID() );
+      }
+    }
+
+    for ( const auto & neighborFaceID : face->neighborFaces() )
+    {
+      const Face * neighborFace = setupStorage.getFace( neighborFaceID );
+      if ( !faceExistsLocally( neighborFaceID ) && !faceExistsInNeighborhood( neighborFaceID ) )
+      {
+        neighborFaces_[ neighborFaceID.getID() ] = new Face( *neighborFace );
+        neighborRanks_[ neighborFaceID.getID() ] = setupStorage.getTargetRank( neighborFaceID.getID() );
+      }
+    }
+  }
+
 
 #ifndef NDEBUG
   checkConsistency();
@@ -70,7 +178,7 @@ const Primitive* PrimitiveStorage::getPrimitive( const PrimitiveID & id ) const
   if ( vertexExistsLocally( id ) ) return getVertex( id );
   if (   edgeExistsLocally( id ) ) return   getEdge( id );
   if (   faceExistsLocally( id ) ) return   getFace( id );
-  return NULL;
+  return nullptr;
 }
 
 Primitive* PrimitiveStorage::getPrimitive( const PrimitiveID & id )
@@ -81,7 +189,30 @@ Primitive* PrimitiveStorage::getPrimitive( const PrimitiveID & id )
   if ( vertexExistsLocally( id ) ) return getVertex( id );
   if (   edgeExistsLocally( id ) ) return   getEdge( id );
   if (   faceExistsLocally( id ) ) return   getFace( id );
-  return NULL;
+  return nullptr;
+}
+
+
+const Primitive* PrimitiveStorage::getNeighborPrimitive( const PrimitiveID & id ) const
+{
+  WALBERLA_ASSERT_LESS_EQUAL(   neighborVertices_.count( id.getID() )
+                              +    neighborEdges_.count( id.getID() )
+                              +    neighborFaces_.count( id.getID() ), uint_c( 1 ) );
+  if ( vertexExistsInNeighborhood( id ) ) return getNeighborVertex( id );
+  if (   edgeExistsInNeighborhood( id ) ) return   getNeighborEdge( id );
+  if (   faceExistsInNeighborhood( id ) ) return   getNeighborFace( id );
+  return nullptr;
+}
+
+Primitive* PrimitiveStorage::getNeighborPrimitive( const PrimitiveID & id )
+{
+  WALBERLA_ASSERT_LESS_EQUAL(   neighborVertices_.count( id.getID() )
+                              +    neighborEdges_.count( id.getID() )
+                              +    neighborFaces_.count( id.getID() ), uint_c( 1 ) );
+  if ( vertexExistsInNeighborhood( id ) ) return getNeighborVertex( id );
+  if (   edgeExistsInNeighborhood( id ) ) return   getNeighborEdge( id );
+  if (   faceExistsInNeighborhood( id ) ) return   getNeighborFace( id );
+  return nullptr;
 }
 
 
@@ -137,6 +268,27 @@ void PrimitiveStorage::checkConsistency()
     WALBERLA_CHECK_EQUAL( it->second->getNumLowerDimNeighbors(), 3 );
   }
 
+  // 4. Number of data handlers of neighbor primitives is zero
+  // 5. PrimitiveIDs of neighbor maps match IDs of neighbor Primitives
+  // 6. Neighborhood of Primitives
+  for ( auto it = neighborVertices_.begin(); it != neighborVertices_.end(); it++ )
+  {
+    WALBERLA_CHECK_GREATER_EQUAL( 0, it->second->getNumberOfDataEntries() );
+    WALBERLA_CHECK_EQUAL( it->first, it->second->getID().getID() );
+    WALBERLA_CHECK_EQUAL( it->second->getNumLowerDimNeighbors(), 0 );
+  }
+  for ( auto it = neighborEdges_.begin(); it != neighborEdges_.end(); it++ )
+  {
+    WALBERLA_CHECK_GREATER_EQUAL( 0, it->second->getNumberOfDataEntries() );
+    WALBERLA_CHECK_EQUAL( it->first, it->second->getID().getID() );
+    WALBERLA_CHECK_EQUAL( it->second->getNumLowerDimNeighbors(), 2 );
+  }
+  for ( auto it = neighborFaces_.begin(); it != neighborFaces_.end(); it++ )
+  {
+    WALBERLA_CHECK_GREATER_EQUAL( 0, it->second->getNumberOfDataEntries() );
+    WALBERLA_CHECK_EQUAL( it->first, it->second->getID().getID() );
+    WALBERLA_CHECK_EQUAL( it->second->getNumLowerDimNeighbors(), 3 );
+  }
 
 
 }
