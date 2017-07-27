f0491678a348e18f6daa638bda065a8f2bfe8402
Nils Kohl <nils.kohl@fau.de>
2017-07-27 18:20:03 +0200

    commented out some not compiling function calls, removed const from vector template type, removed default function parameters

--- src/tinyhhg_core/p1functionspace/P1Function.cpp
+++ src/tinyhhg_core/p1functionspace/P1Function.cpp
@@ -22,7 +22,7 @@ P1Function::~P1Function()
     //TODO implement!
 }
 
-void P1Function::interpolate(std::function<real_t(const hhg::Point3D&)>& expr, uint_t level, DoFType flag = All)
+void P1Function::interpolate(std::function<real_t(const hhg::Point3D&)>& expr, uint_t level, DoFType flag)
 {
     for (auto& it : storage_->getVertices()) {
         Vertex& vertex = *it.second;
@@ -49,19 +49,19 @@ void P1Function::interpolate(std::function<real_t(const hhg::Point3D&)>& expr, u
         Face& face = *it.second;
 
         if (testFlag(face.type, flag)) {
-            P1Face::interpolate(face, faceDataID_, expr, level);
+            // P1Face::interpolate(face, faceDataID_, expr, level);
         }
     }
 
     communicators_[level]->endCommunication<Edge, Face>();
 }
 
-void P1Function::assign(const std::vector<walberla::real_t> scalars, const std::vector<P1Function*> functions, size_t level, DoFType flag = All)
+void P1Function::assign(const std::vector<walberla::real_t> scalars, const std::vector<P1Function*> functions, size_t level, DoFType flag)
 {
     // Collect all source IDs in a vector
-    std::vector<const PrimitiveDataID<VertexP1FunctionMemory, Vertex>> srcVertexIDs(functions.size());
-    std::vector<const PrimitiveDataID<EdgeP1FunctionMemory, Edge>> srcEdgeIDs(functions.size());
-    std::vector<const PrimitiveDataID<FaceP1FunctionMemory, Face>> srcFaceIDs(functions.size());
+    std::vector<PrimitiveDataID<VertexP1FunctionMemory, Vertex>> srcVertexIDs(functions.size());
+    std::vector<PrimitiveDataID<EdgeP1FunctionMemory, Edge>> srcEdgeIDs(functions.size());
+    std::vector<PrimitiveDataID<FaceP1FunctionMemory, Face>> srcFaceIDs(functions.size());
 
     for (auto& function : functions)
     {
@@ -74,7 +74,7 @@ void P1Function::assign(const std::vector<walberla::real_t> scalars, const std::
         Vertex& vertex = *it.second;
 
         if (testFlag(vertex.type, flag)) {
-            P1Vertex::assign(vertex, scalars, srcVertexIDs, vertexDataID_, level);
+            //P1Vertex::assign(vertex, scalars, srcVertexIDs, vertexDataID_, level);
         }
     }
 
@@ -84,7 +84,7 @@ void P1Function::assign(const std::vector<walberla::real_t> scalars, const std::
         Edge& edge = *it.second;
 
         if (testFlag(edge.type, flag)) {
-            P1Edge::assign(edge, scalars, srcEdgeIDs, edgeDataID_, level);
+            //P1Edge::assign(edge, scalars, srcEdgeIDs, edgeDataID_, level);
         }
     }
 
@@ -95,32 +95,32 @@ void P1Function::assign(const std::vector<walberla::real_t> scalars, const std::
         Face& face = *it.second;
 
         if (testFlag(face.type, flag)) {
-            P1Face::assign(level, face, scalars, srcFaceIDs, faceDataID_);
+            //P1Face::assign(level, face, scalars, srcFaceIDs, faceDataID_);
         }
     }
 
     communicators_[level]->endCommunication<Edge, Face>();
 }
 
-void P1Function::add(const std::vector<walberla::real_t> scalars, const std::vector<P1Function*> functions, size_t level, DoFType flag = All)
+void P1Function::add(const std::vector<walberla::real_t> scalars, const std::vector<P1Function*> functions, size_t level, DoFType flag)
 {
 
 }
 
-real_t P1Function::dot(P1Function& rhs, size_t level, DoFType flag = All)
+real_t P1Function::dot(P1Function& rhs, size_t level, DoFType flag)
 {
 
 }
 
-void P1Function::prolongate(size_t level, DoFType flag = All){
+void P1Function::prolongate(size_t level, DoFType flag){
 
 }
 
-void P1Function::prolongateQuadratic(size_t level, DoFType flag = All){
+void P1Function::prolongateQuadratic(size_t level, DoFType flag){
 
 }
 
-void P1Function::restrict(size_t level, DoFType flag = All)
+void P1Function::restrict(size_t level, DoFType flag)
 {
 
 }
