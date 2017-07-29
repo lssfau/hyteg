ebd64d83237970d8f8da09c106e0a018abfbc764
Daniel Drzisga <sersmicro@gmail.com>
2017-07-29 09:50:45 +0200

    fixed bug in localcommunication; this fixes stabilized stokes

--- src/tinyhhg_core/p1functionspace/P1PackInfo.cpp
+++ src/tinyhhg_core/p1functionspace/P1PackInfo.cpp
@@ -79,7 +79,7 @@ void P1PackInfo::communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiv
   uint_t edgeIdOnVertex = receiver->edge_index(sender->getID());
   //the last element would be the vertex itself so we have to send the next one
   if(vertexIdOnEdge == 0){
-    uint_t idx = edge_index(level_,0,VERTEX_C);
+    uint_t idx = edge_index(level_,1,VERTEX_C);
     vertexData[edgeIdOnVertex+1] = edgeData[idx];
   } else if(vertexIdOnEdge == 1){
     uint_t idx = edge_index(level_,levelinfo::num_microvertices_per_edge(level_)-2,VERTEX_C);
