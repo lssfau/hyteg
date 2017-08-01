5d9064d0f145451aaffcdc81837b53b2109d9848
Nils Kohl <nils.kohl@fau.de>
2017-08-01 15:59:45 +0200

    adding timer to operator class

--- src/tinyhhg_core/operator.hpp
+++ src/tinyhhg_core/operator.hpp
@@ -1,7 +1,8 @@
 
 #pragma once
 
-#include <core/all.h>
+#include <core/DataTypes.h>
+#include <core/timing/TimingTree.h>
 
 namespace hhg
 {
@@ -23,6 +24,8 @@ public:
 
   void smooth_gs( DestinationFunction& dst, SourceFunction& rhs, size_t level, DoFType flag );
 
+  void enableTiming( const std::shared_ptr< walberla::WcTimingTree > & timingTree ) { timingTree_ = timingTree; }
+
  protected:
 
   virtual void apply_impl( SourceFunction& src, DestinationFunction& dst, size_t level, DoFType flag, UpdateType updateType = Replace ) = 0;
@@ -31,19 +34,49 @@ public:
   const std::shared_ptr< PrimitiveStorage > storage_;
   const uint_t minLevel_;
   const uint_t maxLevel_;
+
+ private:
+
+  std::shared_ptr< walberla::WcTimingTree > timingTree_;
+
+  void startTiming( const std::string & timerString )
+  {
+    if ( timingTree_ )
+    {
+      timingTree_->start( "Operator" );
+      timingTree_->start( timerString );
+    }
+  }
+
+  void stopTiming ( const std::string & timerString )
+  {
+    if ( timingTree_ )
+    {
+      timingTree_->stop( timerString );
+      timingTree_->stop( "Operator" );
+    }
+  }
 };
 
 
 template< typename SourceFunction, typename DestinationFunction >
 void Operator< SourceFunction, DestinationFunction  >::apply( SourceFunction& src, DestinationFunction& dst, size_t level, DoFType flag, UpdateType updateType )
 {
+  startTiming( "Apply" );
+
   apply_impl( src, dst, level, flag, updateType );
+
+  stopTiming( "Apply" );
 }
 
 template< typename SourceFunction, typename DestinationFunction >
 void Operator< SourceFunction, DestinationFunction  >::smooth_gs( DestinationFunction& dst, SourceFunction& rhs, size_t level, DoFType flag )
 {
+  startTiming( "Smooth GS" );
+
   smooth_gs_impl( dst, rhs, level, flag );
+
+  stopTiming( "Smooth GS" );
 }
 
 }
