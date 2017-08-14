be622acf378c3238de2a90dd997d6fe090b3e52d
Nils Kohl <nils.kohl@fau.de>
2017-08-14 15:47:25 +0200

    implemented stupid greedy load balancer to test output (maybe useful, maybe not)

--- src/tinyhhg_core/primitivestorage/loadbalancing/SimpleBalancer.hpp
+++ src/tinyhhg_core/primitivestorage/loadbalancing/SimpleBalancer.hpp
@@ -32,7 +32,7 @@ private:
 };
 
 
-/// \brief Loadbalanding callback for \ref SetupPrimitiveStorage that locates all blocks on root
+/// \brief Load balanding callback for \ref SetupPrimitiveStorage that locates all blocks on root
 /// \author Nils Kohl (nils.kohl@fau.de)
 class AllBlocksOnRoot : public AllBlocksOnOneRank
 {
@@ -41,8 +41,8 @@ public:
 };
 
 
-/// \brief Loadbalanding callback for \ref SetupPrimitiveStorage that distributed all primitives in a round robin fashion
-/// \autho Nils Kohl (nils.kohl@fau.de)
+/// \brief Load balancing callback for \ref SetupPrimitiveStorage that distributed all primitives in a round robin fashion
+/// \author Nils Kohl (nils.kohl@fau.de)
 class RoundRobin
 {
 public:
@@ -61,6 +61,91 @@ public:
 
     return std::min( currentRank + 1, storage.getNumberOfProcesses() );
   }
+};
+
+
+/// \brief Load balancing callback for \ref SetupPrimitiveStorage that distributes all primitives in a greedy fashion.
+///
+/// This balancer ignores the type of the primitive (vertex, edge, ...) and therefore does not result in a distribution
+/// that balances the number of the primitives per process if they are distinguished by type.
+/// It's also pretty slow.
+class GreedyIgnoringPrimitiveType
+{
+public:
+
+  uint_t operator()( SetupPrimitiveStorage & storage, const memory_t & perProcessMemoryLimit ) const
+  {
+    const uint_t numProcesses = uint_c( walberla::mpi::MPIManager::instance()->numProcesses() );
+    const uint_t maxPrimitives = storage.getNumberOfPrimitives() / numProcesses;
+
+    // Set all target ranks to zero
+    SetupPrimitiveStorage::PrimitiveMap primitives;
+    storage.getSetupPrimitives( primitives );
+
+    PrimitiveID       startPrimitiveID;
+    const Primitive * startPrimitive;
+
+    for ( const auto & it : primitives )
+    {
+      startPrimitiveID = it.first;
+      startPrimitive   = it.second.get();
+      storage.setTargetRank( it.first, 0 );
+    }
+
+    // Main algorithm
+    const Primitive * currentPrimitive = startPrimitive;
+    std::vector< PrimitiveID > visited;
+
+    for ( uint_t rank = 1; rank < numProcesses; rank++ )
+    {
+      uint_t currentNumPrimitives = 0;
+
+      while ( true )
+      {
+        visited.push_back( currentPrimitive->getID() );
+
+        if ( storage.getTargetRank( currentPrimitive->getID().getID() ) == 0 )
+        {
+          storage.setTargetRank( currentPrimitive->getID().getID(), rank );
+          currentNumPrimitives++;
+
+          if ( currentNumPrimitives >= maxPrimitives )
+          {
+            break;
+          }
+        }
+
+        // Choose next primitive
+        const Primitive * nextPrimitive;
+        for ( const auto & it : primitives )
+        {
+          PrimitiveID id = it.first;
+          if ( std::find( visited.begin(), visited.end(), id ) == visited.end() )
+          {
+            nextPrimitive = storage.getPrimitive( id );
+            break;
+          }
+        }
+
+        std::vector< PrimitiveID > neighbors;
+        currentPrimitive->getNeighborPrimitives( neighbors );
+
+        for ( const auto & neighborID : neighbors )
+        {
+          if ( std::find( visited.begin(), visited.end(), neighborID ) == visited.end() )
+          {
+            nextPrimitive = storage.getPrimitive( neighborID );
+            break;
+          }
+        }
+
+        currentPrimitive = nextPrimitive;
+      }
+    }
+
+    return 0;
+  }
+
 };
 
 
