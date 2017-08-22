4859210d36dd2bf7954382bfc281b21994d65156
Nils Kohl <nils.kohl@fau.de>
2017-08-22 15:38:59 +0200

    added new FunctionMemory class, purpose is to unify the Memory classes to avoid massive code duplication

---
+++ src/tinyhhg_core/FunctionMemory.hpp
@@ -0,0 +1,144 @@
+
+#include "tinyhhg_core/support.hpp"
+#include "tinyhhg_core/primitivedata/PrimitiveDataHandling.hpp"
+
+#include <core/DataTypes.h>
+#include <core/mpi/SendBuffer.h>
+#include <core/mpi/RecvBuffer.h>
+
+#include <map>
+#include <memory>
+
+#pragma once
+
+namespace hhg {
+
+using walberla::uint_t;
+using walberla::real_t;
+using walberla::mpi::SendBuffer;
+using walberla::mpi::RecvBuffer;
+
+class FunctionMemory
+{
+public:
+
+  /// Constructs memory for a function
+  /// \param numDeps number of higher dimensional neighbor primitives
+  FunctionMemory( const uint_t & numDeps = 0 ) : num_deps_( numDeps ) {}
+
+  virtual ~FunctionMemory(){}
+
+  /// Returns the length of the allocated array for a certain level
+  virtual uint_t getSize( uint_t level ) const = 0;
+
+  /// Allocates an array for a certain level
+  /// Uses the virtual member \ref getSize() to determine the length of the array
+  inline std::unique_ptr< real_t[] > & addlevel( uint_t level, uint_t num_deps = 0 )
+  {
+    if (data.count(level)>0)
+    {
+      WALBERLA_LOG_WARNING("Level already exists.");
+    }
+    else
+    {
+      num_deps_ = num_deps;
+      data[level] = hhg::make_unique<real_t[]>(getSize(level));
+    }
+    return data[level];
+  }
+
+  /// Serializes the allocated data to a send buffer
+  inline void serialize( SendBuffer & sendBuffer ) const
+  {
+    for ( const auto & it : data )
+    {
+      uint_t level = it.first;
+      const real_t * dataPtr = it.second.get();
+      sendBuffer << level;
+      for ( uint_t idx = 0; idx < getSize( level ); idx++ )
+      {
+        sendBuffer << dataPtr[ idx ];
+      }
+    }
+  }
+
+  /// Deserializes the allocated data from a recv buffer
+  inline void deserialize( RecvBuffer & recvBuffer )
+  {
+    data.clear();
+    while ( !recvBuffer.isEmpty() )
+    {
+      uint_t level;
+      recvBuffer >> level;
+      data[ level ] = hhg::make_unique< real_t[] >( getSize( level ) );
+      for ( uint_t idx = 0; idx < getSize( level ); idx++ )
+      {
+        recvBuffer >> data[ level ][ idx ];
+      }
+    }
+  }
+
+  /// Maps a level to the respective allocated data
+  std::map< uint_t, std::unique_ptr< real_t[] > > data;
+
+protected:
+
+  uint_t num_deps_;
+
+};
+
+}
+
+namespace walberla {
+namespace mpi {
+
+template< typename T,  // Element type of SendBuffer
+          typename G > // Growth policy of SendBuffer
+inline mpi::GenericSendBuffer<T,G> & operator<<( mpi::GenericSendBuffer<T,G> & buffer, const hhg::FunctionMemory & functionMemory )
+{
+   functionMemory.serialize( buffer );
+   return buffer;
+}
+
+template< typename T > // Element type  of RecvBuffer
+inline mpi::GenericRecvBuffer<T>& operator>>( mpi::GenericRecvBuffer<T> & buffer, hhg::FunctionMemory & functionMemory )
+{
+   functionMemory.deserialize( buffer );
+   return buffer;
+}
+
+template<>
+struct BufferSizeTrait< hhg::FunctionMemory > { static const bool constantSize = false; };
+
+} // namespace mpi
+} // namespace walberla
+
+
+namespace hhg {
+
+
+template< typename DataType, typename PrimitiveType >
+class FunctionMemoryDataHandling : public PrimitiveDataHandling< DataType, PrimitiveType >
+{
+public:
+  static_assert( std::is_base_of< FunctionMemory, DataType >::value,
+                 "FunctionMemoryDataHandling can only be used for subclasses of FunctionMemory!" );
+
+  virtual ~FunctionMemoryDataHandling() {}
+
+  virtual void serialize( const PrimitiveType * const primitive, const PrimitiveDataID< DataType, PrimitiveType > & id, SendBuffer & buffer ) const
+  {
+    DataType * data = primitive->getData( id );
+    buffer << *data;
+  }
+
+  virtual void deserialize( const PrimitiveType * const primitive, const PrimitiveDataID< DataType, PrimitiveType > & id, RecvBuffer & buffer ) const
+  {
+    DataType * data = primitive->getData( id );
+    buffer >> *data;
+  }
+
+};
+
+
+}
