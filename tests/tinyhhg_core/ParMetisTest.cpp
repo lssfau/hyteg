4cad1e04030d956c92d1750ef329c5242e0d000a
Nils Kohl <nils.kohl@fau.de>
2017-08-24 16:58:07 +0200

    first version of the parmetis loadbalancer test [skip CI]

---
+++ tests/tinyhhg_core/ParMetisTest.cpp
@@ -0,0 +1,47 @@
+
+#include "core/Environment.h"
+#include "core/debug/CheckFunctions.h"
+#include "core/debug/TestSubsystem.h"
+#include "core/mpi/SendBuffer.h"
+#include "core/mpi/RecvBuffer.h"
+#include "tinyhhg_core/tinyhhg.hpp"
+
+namespace hhg {
+
+
+static void testParMetis()
+{
+  uint_t numProcesses = uint_c( walberla::mpi::MPIManager::instance()->numProcesses() );
+
+  const std::string meshFileName = "../../data/meshes/quad_4el.msh";
+
+  MeshInfo meshInfo = MeshInfo::fromGmshFile( meshFileName );
+  SetupPrimitiveStorage setupStorage( meshInfo, uint_c ( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+
+  loadbalancing::roundRobin( setupStorage );
+
+  std::shared_ptr< PrimitiveStorage > storage( new PrimitiveStorage( setupStorage ) );
+
+  writeDomainPartitioningVTK( storage, "../../output/", "domain_partitioning_after_setup_load_balancing" );
+
+  loadbalancing::distributed::parmetis( *storage );
+
+  writeDomainPartitioningVTK( storage, "../../output/", "domain_partitioning_after_distributed_load_balancing" );
+
+}
+
+} // namespace hhg
+
+
+int main( int argc, char* argv[] )
+{
+  walberla::debug::enterTestMode();
+
+  walberla::Environment walberlaEnv(argc, argv);
+  walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
+  walberla::MPIManager::instance()->useWorldComm();
+  walberla::debug::enterTestMode();
+  hhg::testParMetis();
+
+  return EXIT_SUCCESS;
+}
