f936b44dd8540daf78ecfd6b76860c30efb793b6
Nils Kohl <nils.kohl@fau.de>
2017-08-30 14:26:34 +0200

    initial version of the cell class (as basic as possible)

---
+++ src/tinyhhg_core/primitives/Cell.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include "tinyhhg_core/primitives/Primitive.hpp"
+
+namespace hhg {
+
+class Cell : public Primitive
+{
+public:
+
+  Cell( const PrimitiveID                & primitiveID,
+        const std::vector< PrimitiveID > & vertexIDs,
+        const std::vector< PrimitiveID > & edgeIDs,
+        const std::vector< PrimitiveID > & faceIDs );
+
+  virtual void getLowerDimNeighbors ( std::vector< PrimitiveID > & lowerDimNeighbors )  const { getNeighborFaces( lowerDimNeighbors ); }
+  virtual void getHigherDimNeighbors( std::vector< PrimitiveID > & higherDimNeighbors ) const { higherDimNeighbors.clear(); }
+
+  virtual const std::vector< PrimitiveID > & getLowerDimNeighbors()  const { return neighborFaces(); }
+  virtual const std::vector< PrimitiveID > & getHigherDimNeighbors() const { WALBERLA_ASSERT_EQUAL( getNumNeighborCells(), 0 ); return neighborCells(); }
+
+  virtual uint_t getNumLowerDimNeighbors()  const { return getNumNeighborFaces(); }
+  virtual uint_t getNumHigherDimNeighbors() const { return 0; }
+
+};
+
+}
