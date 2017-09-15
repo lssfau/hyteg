4f4fa6496856825fd87b8f2c12bff874abab5269
Daniel Drzisga <drzisga@ma.tum.de>
2017-09-15 10:02:49 +0200

    untested implementation of P1Vertex variable coefficients application

--- src/tinyhhg_core/p1functionspace/P1Vertex.hpp
+++ src/tinyhhg_core/p1functionspace/P1Vertex.hpp
@@ -73,6 +73,52 @@ inline void apply(Vertex &vertex,
   }
 }
 
+/// Apply function in the case of a coefficient
+inline void applyCoefficient(Vertex &vertex,
+                             const PrimitiveDataID<VertexP1LocalMatrixMemory, Vertex> &operatorId,
+                             const PrimitiveDataID<VertexP1FunctionMemory, Vertex> &srcId,
+                             const PrimitiveDataID<VertexP1FunctionMemory, Vertex> &dstId,
+                             const PrimitiveDataID<VertexP1FunctionMemory, Vertex> &coeffId,
+                             uint_t level,
+                             UpdateType update) {
+  auto &localMatrices = vertex.getData(operatorId)->data[level];
+  auto &src = vertex.getData(srcId)->data[level];
+  auto &dst = vertex.getData(dstId)->data[level];
+  auto &coeff = vertex.getData(coeffId)->data[level];
+
+  if (update == Replace) {
+    dst[0] = real_t(0);
+  }
+
+  for (auto& faceId : vertex.neighborFaces()) {
+    real_t meanCoefficient = coeff[0];
+
+    Face* face = storage_->getFace(faceId);
+
+    uint_t v_i = face->vertex_index(vertex.getID());
+
+    std::vector<PrimitiveID> adj_edges = face->adjacent_edges(vertex.getID());
+
+    for (auto &edgeId : adj_edges) {
+      uint_t edge_idx = vertex.edge_index(edgeId) + 1;
+      meanCoefficient += coeff[edge_idx];
+    }
+
+    meanCoefficient *= 1.0/3.0;
+
+    // iterate over adjacent edges
+    for (auto &edgeId : adj_edges) {
+      uint_t edge_idx = vertex.edge_index(edgeId) + 1;
+      Edge *edge = storage_->getEdge(edgeId);
+      PrimitiveID vertex_j = edge->get_opposite_vertex(vertex.getID());
+
+      uint_t v_j = face->vertex_index(vertex_j);
+
+      dst[0] += meanCoefficient * (local_stiffness(v_i, v_j) * src[edge_idx] + local_stiffness(v_i, v_i) * src[0]);
+    }
+  }
+}
+
 inline void smooth_gs(Vertex &vertex, const PrimitiveDataID<VertexP1StencilMemory, Vertex> &operatorId,
                       const PrimitiveDataID<VertexP1FunctionMemory, Vertex> &dstId,
                       const PrimitiveDataID<VertexP1FunctionMemory, Vertex> &rhsId, size_t level) {
