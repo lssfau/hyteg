1249ea0ac098c56a94afe4dd2215aa7638562363
dthoennes <dominik.thoennes@fau.de>
2017-09-18 17:15:00 +0200

    added P1Edge indexIterator

--- src/tinyhhg_core/p1functionspace/P1EdgeIndex.hpp
+++ src/tinyhhg_core/p1functionspace/P1EdgeIndex.hpp
@@ -73,5 +73,89 @@ constexpr inline uint_t index(uint_t pos, DirVertex dir) {
 SPECIALIZE(uint_t, index, edge_index)
 
 }//namespace EdgeCoordsVertex
+
+/// Iterator to get the indices for vertex data on the edge and the adjacent faces
+/// Be aware that the iterator does not handle the orientation meaning the indices are in
+/// the same order as they are in memory which can be different to the order in the face
+class edgeIndexIterator : public std::iterator< std::forward_iterator_tag, walberla::uint_t >
+{
+public:
+  /*!
+   * @brief begin iterator
+   * @param positon -1 to access data on the edge; 0-x to access data on the corresponding face (position in neighborFaces vector)
+   * @param level multigrid level
+   */
+  inline edgeIndexIterator(int position ,uint_t level);
+  /*!
+   * @brief end iterator
+   */
+  inline edgeIndexIterator();
+
+  inline edgeIndexIterator& operator++();
+  inline edgeIndexIterator operator++(int);
+  inline walberla::uint_t operator*() const;
+  inline bool operator==(const edgeIndexIterator& other) const;
+  inline bool operator!=(const edgeIndexIterator& other) const;
+
+
+private:
+  uint_t idx_;
+  uint_t counter_;
+  uint_t verticesOnEdge_;
+  uint_t totalPoints_;
+  int position_;
+  bool ended_;
+};
+
+edgeIndexIterator::edgeIndexIterator(int position, uint_t level)
+  : idx_(0),
+    counter_(0),
+    verticesOnEdge_(hhg::levelinfo::num_microvertices_per_edge(level)),
+    totalPoints_(hhg::levelinfo::num_microvertices_per_edge(level)),
+    position_(position),
+    ended_(false)
+{
+  if(position_ != -1){
+    idx_ = verticesOnEdge_ + (position_ * (verticesOnEdge_ - 1));
+    //there is on point less on faces than on the edge so we have to decrement totalPoints
+    totalPoints_--;
+  }
+}
+
+edgeIndexIterator &edgeIndexIterator::operator++()
+{
+  idx_ ++;
+  counter_++;
+  if(counter_ == totalPoints_) ended_ = true;
+  return *this;
+}
+
+edgeIndexIterator edgeIndexIterator::operator++(int)
+{
+  edgeIndexIterator tmp(*this);
+  operator++();
+  return tmp;
+}
+
+walberla::uint_t edgeIndexIterator::operator*() const {
+  return walberla::uint_c(idx_);
+}
+
+bool edgeIndexIterator::operator==(const edgeIndexIterator &other) const {
+  if (ended_ || other.ended_)
+  {
+    return (ended_ == other.ended_);
+  }
+  return (idx_ == other.idx_);
+}
+
+bool edgeIndexIterator::operator!=(const edgeIndexIterator &other) const {
+  return !(*this == other);
+}
+
+edgeIndexIterator::edgeIndexIterator()
+  :ended_(true)
+{}
+
 }//namespace P1Edge
 }//namespace hhg
