5f813f52b68fc64ea2180d35469c01bf4d680cdd
dthoennes <dominik.thoennes@fau.de>
2017-09-29 17:37:56 +0200

    progress [skip ci]

--- src/tinyhhg_core/dgfunctionspace/DgFaceIndex.hpp
+++ src/tinyhhg_core/dgfunctionspace/DgFaceIndex.hpp
@@ -8,13 +8,14 @@ namespace DgFace{
 
 using walberla::uint_t;
 
+template<size_t Level>
 constexpr inline uint_t indexDGcellFromVertex( const uint_t col, const uint_t row, const stencilDirection dir){
-  BubbleFace::FaceCoordsVertex::index(col,row,dir);
+  BubbleFace::FaceCoordsVertex::index<Level>(col,row,dir);
 }
 
-
+template<size_t Level>
 constexpr inline uint_t indexDGcellFromGrayDGCell( const uint_t col, const uint_t row, const stencilDirection dir){
-  BubbleFace::FaceCoordsVertex::index(col-1,row);
+  BubbleFace::FaceCoordsVertex::index<Level>(col-1,row);
 }
 
 
