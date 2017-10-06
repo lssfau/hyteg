f5913a47e3d7e023aaa7b58183a46df4bf605349
Nils Kohl <nils.kohl@fau.de>
2017-10-06 18:08:02 +0200

    progress on edge dof index function for macro face (from vertex)

--- src/tinyhhg_core/edgedofspace/EdgeDoFOnMacroFaceIndex.hpp
+++ src/tinyhhg_core/edgedofspace/EdgeDoFOnMacroFaceIndex.hpp
@@ -16,26 +16,64 @@ constexpr inline uint_t indexFromVertex( const uint_t col, const uint_t row, con
   const uint_t numMicroVerticesPerEdge = levelinfo::num_microvertices_per_edge( Level );
   const uint_t numMicroEdgesPerEdge    = levelinfo::num_microedges_per_edge( Level );
 
-  const uint_t maxRowVertexIndex = numMicroVerticesPerEdge - col;
-  const uint_t maxColVertexIndex = numMicroVerticesPerEdge - row;
+  const uint_t numMicroVerticesInRow      = numMicroVerticesPerEdge - row;
+  const uint_t numMicroEdgesInRow         = numMicroEdgesPerEdge - row;
+  const uint_t numMicroEdgesInRowBelow    = numMicroEdgesPerEdge - (row - 1);
+
+  const uint_t sumZeroToRow = ( ( row + 1 ) * row ) / 2;
+
+  const uint_t numHorizontalEdgeDoFsBelowRow = row * numMicroVerticesPerEdge - sumZeroToRow;
+
+  const uint_t numOverallHorizontalEdgeDoFs = (( numMicroEdgesPerEdge * numMicroEdgesPerEdge ) - numMicroEdgesPerEdge ) / 2 + numMicroEdgesPerEdge;
+  const uint_t numOverallDiagonalEdgeDoFs   = numOverallHorizontalEdgeDoFs;
+  const uint_t numOverallVerticalEdgeDoFs   = numOverallHorizontalEdgeDoFs;
 
   WALBERLA_ASSERT( col + row < numMicroVerticesPerEdge );
-  WALBERLA_ASSERT( row <= maxRowVertexIndex );
-  WALBERLA_ASSERT( col <= maxColVertexIndex );
 
   switch ( dir )
   {
   case sD::EDGE_HO_E:
-    WALBERLA_ASSERT( row < maxRowVertexIndex );
-    WALBERLA_ASSERT( col < maxColVertexIndex );
-    return col + row * numMicroVerticesPerEdge - ( ( row + 1 ) * row ) / 2;
+  {
+    WALBERLA_ASSERT( col + row < numMicroVerticesPerEdge - 1 );
+    const uint_t result = col + numHorizontalEdgeDoFsBelowRow;
+    WALBERLA_ASSERT( result < numOverallHorizontalEdgeDoFs );
+    return result;
     break;
+  }
   case sD::EDGE_HO_W:
-    WALBERLA_ASSERT( row <  maxRowVertexIndex );
-    WALBERLA_ASSERT( col <= maxColVertexIndex );
+  {
     WALBERLA_ASSERT( col >  0 );
-    return col - 1 + row * numMicroVerticesPerEdge - ( ( row + 1 ) * row ) / 2;
+    const uint_t result = col - 1 + numHorizontalEdgeDoFsBelowRow;
+    WALBERLA_ASSERT( result < numOverallHorizontalEdgeDoFs );
+    return result;
     break;
+  }
+  case sD::EDGE_HO_NW:
+  {
+    WALBERLA_ASSERT( col > 0 );
+    WALBERLA_ASSERT( col + row < numMicroVerticesPerEdge - 1 );
+    const uint_t result = col - 1 + numHorizontalEdgeDoFsBelowRow + numMicroEdgesInRow;
+    WALBERLA_ASSERT( result < numOverallHorizontalEdgeDoFs );
+    return result;
+    break;
+  }
+  case sD::EDGE_HO_SE:
+  {
+    WALBERLA_ASSERT( row > 0 );
+    const uint_t result = col + numHorizontalEdgeDoFsBelowRow - numMicroEdgesInRowBelow;
+    WALBERLA_ASSERT( result < numOverallHorizontalEdgeDoFs );
+    return result;
+    break;
+  }
+  case sD::EDGE_VE_N:
+  {
+    WALBERLA_ASSERT( col + row < numMicroVerticesPerEdge - 1 );
+    const uint_t result = numOverallHorizontalEdgeDoFs + numOverallDiagonalEdgeDoFs + col + numHorizontalEdgeDoFsBelowRow;
+    WALBERLA_ASSERT( result >= numOverallHorizontalEdgeDoFs + numOverallDiagonalEdgeDoFs );
+    WALBERLA_ASSERT( result <  numOverallHorizontalEdgeDoFs + numOverallDiagonalEdgeDoFs + numOverallVerticalEdgeDoFs );
+    return result;
+    break;
+  }
   default:
     WALBERLA_ASSERT( false );
     break;
