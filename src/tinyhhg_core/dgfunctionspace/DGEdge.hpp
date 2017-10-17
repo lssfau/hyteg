7bfecf4ec5d7e80d4db22d0378403692f1a819b0
dthoennes <dominik.thoennes@fau.de>
2017-10-17 15:34:06 +0200

    progess on Edge interpolate [skip ci]

--- src/tinyhhg_core/dgfunctionspace/DGEdge.hpp
+++ src/tinyhhg_core/dgfunctionspace/DGEdge.hpp
@@ -41,18 +41,20 @@ inline void interpolateTmpl(Edge &edge,
   Point3D x0 = edge.getCoordinates()[0];
   Point3D dx = edge.getDirection()/(walberla::real_c(rowsize - 1));
 
-  Point3D face0d0 = (storage.get()->getFace(edge.neighborFaces()[0].getID())->getCoordinates()[1] -
-                     storage.get()->getFace(edge.neighborFaces()[0].getID())->getCoordinates()[0])/
-                    (walberla::real_c(rowsize - 1));
-  Point3D face0d2 = (storage.get()->getFace(edge.neighborFaces()[0].getID())->getCoordinates()[1] -
-                     storage.get()->getFace(edge.neighborFaces()[0].getID())->getCoordinates()[0])/
+
+  Face* face0 = storage->getFace(edge.neighborFaces()[0].getID());
+  uint_t outerVertexOnFace0 = face0->vertex_index(face0->get_vertex_opposite_to_edge(edge.getID()));
+
+  Face* face1 = storage->getFace(edge.neighborFaces()[0].getID());
+  uint_t outerVertexOnFace1 = face1->vertex_index(face1->get_vertex_opposite_to_edge(edge.getID()));
+
+
+  Point3D face0d0 = dx;
+  Point3D face0d2 = (face0->getCoordinates()[outerVertexOnFace0] - x0) /
                     (walberla::real_c(rowsize - 1));
 
-  Point3D face1d0 = (storage.get()->getFace(edge.neighborFaces()[1].getID())->getCoordinates()[1] -
-                     storage.get()->getFace(edge.neighborFaces()[1].getID())->getCoordinates()[0])/
-                    (walberla::real_c(rowsize - 1));
-  Point3D face1d2 = (storage.get()->getFace(edge.neighborFaces()[1].getID())->getCoordinates()[1] -
-                     storage.get()->getFace(edge.neighborFaces()[1].getID())->getCoordinates()[0])/
+  Point3D face1d0 = dx;
+  Point3D face1d2 = (face1->getCoordinates()[outerVertexOnFace1] - x0)/
                     (walberla::real_c(rowsize - 1));
 
   // gray south cells
