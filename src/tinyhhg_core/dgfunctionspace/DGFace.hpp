lssfau/hyteg

commit 6d02767da2dd24da2d1eeba58d35ddc5a0b33bcb

enabled timing in geophysics_plume

src/tinyhhg_core/dgfunctionspace/DGFace.hpp
@@ -192,10 +192,10 @@ inline void assignTmpl(Face &face,
 
       auto cellIndex = BubbleFace::indexFaceFromBlueFace<Level>(i, j, stencilDirection::CELL_BLUE_C);
 
-      ValueType tmp = scalars[0]*face.getData(srcIds[0])->getPointer( Level )[cellIndex];
+      ValueType tmp = scalars[0]*srcPtr[0][cellIndex];
 
       for (uint_t k = 1; k < srcIds.size(); ++k) {
-        tmp += scalars[k]*face.getData(srcIds[k])->getPointer( Level )[cellIndex];
+        tmp += scalars[k]*srcPtr[0][cellIndex];
       }
 
       dst[cellIndex] = tmp;

apps/cg_bubble.cpp
@@ -1,6 +1,5 @@
 #include <core/timing/Timer.h>
 #include <tinyhhg_core/tinyhhg.hpp>
-#include <fmt/format.h>
 
 using walberla::real_t;
 using walberla::uint_t;
@@ -21,11 +20,12 @@ int main(int argc, char* argv[])
 
   hhg::loadbalancing::roundRobin( setupStorage );
 
-  size_t minLevel = 2;
-  size_t maxLevel = 2;
-  size_t maxiter = 10000;
+  const size_t minLevel = 2;
+  const size_t maxLevel = 2;
+  const size_t maxiter = 10000;
 
   std::shared_ptr<PrimitiveStorage> storage = std::make_shared<PrimitiveStorage>(setupStorage);
+
 
   hhg::P1BubbleFunction<real_t> r("r", storage, minLevel, maxLevel);
   hhg::P1BubbleFunction<real_t> f("f", storage, minLevel, maxLevel);

apps/geophysics_plume.cpp
@@ -16,6 +16,10 @@ int main(int argc, char* argv[])
   walberla::MPIManager::instance()->initializeMPI( &argc, &argv );
   walberla::MPIManager::instance()->useWorldComm();
 
+  std::shared_ptr< walberla::WcTimingTree > timingTree( new walberla::WcTimingTree() );
+
+  timingTree->start("Global");
+
   std::string meshFileName = "../data/meshes/annulus.msh";
 
   hhg::MeshInfo meshInfo = hhg::MeshInfo::fromGmshFile( meshFileName );
@@ -26,7 +30,7 @@ int main(int argc, char* argv[])
   const uint_t minLevel = 2;
   const uint_t maxLevel = 3;
   const uint_t solverMaxiter = 100;
-  const uint_t timesteps = 500000;
+  const uint_t timesteps = 5000;
   real_t dt = 0.75 * std::pow(2.0, -walberla::real_c(maxLevel));
 
   std::function<real_t(const hhg::Point3D&,const std::vector<real_t>&)> initialConcentration = [dt](const hhg::Point3D& x,const std::vector<real_t>&) {
@@ -46,6 +50,8 @@ int main(int argc, char* argv[])
   };
 
   std::shared_ptr<hhg::PrimitiveStorage> storage = std::make_shared<hhg::PrimitiveStorage>(setupStorage);
+
+  storage->enableGlobalTiming(timingTree);
 
   // Setting up Functions
   auto c_old = std::make_shared<hhg::DGFunction<real_t>>("c", storage, minLevel, maxLevel);
@@ -103,9 +109,10 @@ int main(int argc, char* argv[])
         WALBERLA_LOG_DEVEL("[Uzawa] residuum: " << std::scientific << std::sqrt(r->dot(*r, maxLevel, hhg::Inner | hhg::NeumannBoundary)));
       }
 
-
+      timingTree->start("VTK");
       hhg::VTKWriter<hhg::P1Function<real_t>, hhg::DGFunction<real_t >>({&u->u, &u->v, &u->p, &f->u, &f->v}, {c_old.get()}, maxLevel,
                                                                         "../output", fmt::format("plume-{:0>6}", t));
+      timingTree->stop("VTK");
     }
 
     N.apply(*c_old, *c, maxLevel, hhg::Inner, Replace);
@@ -113,6 +120,9 @@ int main(int argc, char* argv[])
 
     c_old.swap(c);
   }
+
+  timingTree->stop("Global");
+  WALBERLA_LOG_INFO_ON_ROOT(timingTree->getReduced());
 
   return EXIT_SUCCESS;
 }

src/tinyhhg_core/Function.hpp
@@ -129,8 +129,6 @@ protected:
 
   std::shared_ptr< walberla::WcTimingTree > timingTree_;
 
-private:
-
   void startTiming( const std::string & timerString )
   {
     if ( timingTree_ )

src/tinyhhg_core/dgfunctionspace/DGFunction.hpp
@@ -109,6 +109,8 @@ void DGFunction< ValueType >::interpolate(std::function<ValueType(const Point3D 
                                           uint_t level,
                                           DoFType flag) {
 
+  this->startTiming( "Interpolate");
+
   // Collect all source IDs in a vector
   std::vector<PrimitiveDataID<FunctionMemory< ValueType >, Vertex>> srcVertexIDs;
   std::vector<PrimitiveDataID<FunctionMemory< ValueType >, Edge>>   srcEdgeIDs;
@@ -151,6 +153,9 @@ void DGFunction< ValueType >::interpolate(std::function<ValueType(const Point3D 
   }
 
   communicators_[level]->template endCommunication<Edge, Face>();
+
+  this->stopTiming( "Interpolate");
+
 }
 
 template< typename ValueType >

src/tinyhhg_core/p1functionspace/P1Face.hpp
@@ -582,10 +582,12 @@ inline void enumerateTmpl(Face &face, const PrimitiveDataID<FaceP1FunctionMemory
 
   uint_t mr = 1 + rowsize;
 
+  ValueType* dstPtr = face.getData(dstId)->getPointer( Level );
+
   for (uint_t i = 0; i < rowsize - 3; ++i) {
     for (uint_t j = 0; j < inner_rowsize - 3; ++j) {
 
-      face.getData(dstId)->getPointer( Level )[mr] = static_cast<ValueType>(num);
+      dstPtr[mr] = static_cast<ValueType>(num);
       num++;
 
       mr += 1;

src/tinyhhg_core/p1functionspace/P1PackInfo.hpp
@@ -18,29 +18,29 @@ public:
              std::weak_ptr<PrimitiveStorage> storage)
     : communication::DoFSpacePackInfo< ValueType >(level,dataIDVertex,dataIDEdge,dataIDFace,storage)
   {}
-  virtual void packVertexForEdge(const Vertex *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
+  void packVertexForEdge(const Vertex *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
 
-  virtual void unpackEdgeFromVertex(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
+  void unpackEdgeFromVertex(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
 
-  virtual void communicateLocalVertexToEdge(const Vertex *sender, Edge *receiver) override;
+  void communicateLocalVertexToEdge(const Vertex *sender, Edge *receiver) override;
 
-  virtual void packEdgeForVertex(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
+  void packEdgeForVertex(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
 
-  virtual void unpackVertexFromEdge(Vertex *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
+  void unpackVertexFromEdge(Vertex *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
 
-  virtual void communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiver) override;
+  void communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiver) override;
 
-  virtual void packEdgeForFace(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
+  void packEdgeForFace(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
 
-  virtual void unpackFaceFromEdge(Face *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
+  void unpackFaceFromEdge(Face *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
 
-  virtual void communicateLocalEdgeToFace(const Edge *sender, Face *receiver) override;
+  void communicateLocalEdgeToFace(const Edge *sender, Face *receiver) override;
 
-  virtual void packFaceForEdge(const Face *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
+  void packFaceForEdge(const Face *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) override;
 
-  virtual void unpackEdgeFromFace(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
+  void unpackEdgeFromFace(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) override;
 
-  virtual void communicateLocalFaceToEdge(const Face *sender, Edge *receiver) override;
+  void communicateLocalFaceToEdge(const Face *sender, Edge *receiver) override;
 
 private:
   using communication::DoFSpacePackInfo< ValueType >::level_;

src/tinyhhg_core/primitivestorage/PrimitiveStorage.hpp
@@ -213,9 +213,18 @@ public:
   void getNeighboringRanks( std::set< uint_t >                 & neighboringRanks ) const;
   void getNeighboringRanks( std::set< walberla::mpi::MPIRank > & neighboringRanks ) const;
 
+  /// enables timing in all methods of the function spaces
   inline void enableGlobalTiming(){
     if(!timingTree_) {
       timingTree_ = std::make_shared<walberla::WcTimingTree>();
+    }
+  }
+
+  /// enables timing in all methods of the function spaces
+  /// and adds them to an existing timing tree
+  inline void enableGlobalTiming(const std::shared_ptr< walberla::WcTimingTree > & timingTree){
+    if(!timingTree_) {
+      timingTree_ = timingTree;
     }
   }
 

commit f3ac5b7cdd581938fd5c4f2fd346f98ac86881e4

fixed bug introduced in 6d02767da2dd24da2d1eeba58d35ddc5a0b33bcb

src/tinyhhg_core/dgfunctionspace/DGFace.hpp
@@ -195,7 +195,7 @@ inline void assignTmpl(Face &face,
       ValueType tmp = scalars[0]*srcPtr[0][cellIndex];
 
       for (uint_t k = 1; k < srcIds.size(); ++k) {
-        tmp += scalars[k]*srcPtr[0][cellIndex];
+        tmp += scalars[k]*srcPtr[k][cellIndex];
       }
 
       dst[cellIndex] = tmp;