57e96ce1da6ea7cdd2a659694522ebd36f195f7e
Nils Kohl <nils.kohl@fau.de>
2017-10-27 13:39:05 +0200

    added edge dof function skeleton

---
+++ src/tinyhhg_core/edgedofspace/EdgeDoFFunction.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "tinyhhg_core/Function.hpp"
+#include "tinyhhg_core/types/pointnd.hpp"
+
+#include "tinyhhg_core/primitives/Vertex.hpp"
+#include "tinyhhg_core/primitives/Edge.hpp"
+#include "tinyhhg_core/primitives/Face.hpp"
+
+#include "tinyhhg_core/communication/BufferedCommunication.hpp"
+
+#include "tinyhhg_core/edgedofspace/EdgeDoFMemory.hpp"
+
+namespace hhg {
+
+template< typename ValueType >
+class EdgeDoFFunction : public Function< EdgeDoFFunction< ValueType > >
+{
+public:
+
+  EdgeDoFFunction( const std::string & name, const std::shared_ptr< PrimitiveStorage > & storage, const uint_t & minLevel, const uint_t & maxLevel ) :
+      Function< EdgeDoFFunction< ValueType > >( name, storage, minLevel, maxLevel )
+  {
+  }
+
+private:
+
+    using Function< EdgeDoFFunction< ValueType > >::storage_;
+    using Function< EdgeDoFFunction< ValueType > >::communicators_;
+
+    /// Interpolates a given expression to a P1Function
+    inline void
+    interpolate_impl( std::function< ValueType( const Point3D& ) > & expr,
+                      uint_t level, DoFType flag = All );
+
+    inline void
+    assign_impl( const std::vector< ValueType > scalars, const std::vector< EdgeDoFFunction< ValueType >* > functions,
+                 uint_t level, DoFType flag = All );
+
+    inline void
+    add_impl( const std::vector< ValueType > scalars, const std::vector< EdgeDoFFunction< ValueType >* > functions,
+              uint_t level, DoFType flag = All );
+
+    inline real_t
+    dot_impl( EdgeDoFFunction< ValueType >& rhs, uint_t level, DoFType flag = All );
+
+    inline void
+    prolongate_impl( uint_t sourceLevel, DoFType flag = All );
+
+    inline void
+    prolongateQuadratic_impl( uint_t sourceLevel, DoFType flag = All );
+
+    inline void
+    restrict_impl( uint_t sourceLevel, DoFType flag = All );
+
+    inline void
+    enumerate_impl( uint_t level, uint_t& num );
+
+    PrimitiveDataID< FunctionMemory< ValueType >, Vertex > vertexDataID_;
+    PrimitiveDataID< FunctionMemory< ValueType >, Edge > edgeDataID_;
+    PrimitiveDataID< FunctionMemory< ValueType >, Face > faceDataID_;
+};
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::interpolate_impl(std::function< ValueType(const hhg::Point3D&) > & expr, uint_t level, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::assign_impl(const std::vector<ValueType> scalars, const std::vector<EdgeDoFFunction< ValueType >*> functions, size_t level, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::add_impl(const std::vector<ValueType> scalars, const std::vector<EdgeDoFFunction< ValueType >*> functions, size_t level, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline real_t EdgeDoFFunction< ValueType >::dot_impl(EdgeDoFFunction< ValueType >& rhs, size_t level, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::prolongate_impl(size_t sourceLevel, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::prolongateQuadratic_impl(size_t sourceLevel, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::restrict_impl(size_t sourceLevel, DoFType flag)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+template< typename ValueType >
+inline void EdgeDoFFunction< ValueType >::enumerate_impl(uint_t level, uint_t& num)
+{
+  WALBERLA_ASSERT( "To be implemented..." );
+}
+
+}
