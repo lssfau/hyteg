cee98f47665eeb1c884eae1bbea7b4ec1bc7a465
Nils Kohl <nils.kohl@fau.de>
2017-10-27 13:24:15 +0200

    added edge dof memory size functions

---
+++ src/tinyhhg_core/edgedofspace/EdgeDoFMemory.hpp
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "core/DataTypes.h"
+
+namespace hhg {
+
+using walberla::uint_t;
+
+inline uint_t EdgeDoFMacroVertexFunctionMemorySize( const uint_t & level, const uint_t & numDependencies )
+{
+  WALBERLA_UNUSED( level );
+  return 2 * numDependencies;
+}
+
+inline uint_t EdgeDoFMacroEdgeFunctionMemorySize( const uint_t & level, const uint_t & numDependencies )
+{
+  return levelinfo::num_microedges_per_edge( level ) + numDependencies * ( 3 * ( levelinfo::num_microedges_per_edge( level ) ) - 1 );
+}
+
+inline uint_t EdgeDoFMacroFaceFunctionMemorySize( const uint_t & level, const uint_t & numDependencies )
+{
+  WALBERLA_UNUSED( numDependencies );
+  return 3 * ( ( ( levelinfo::num_microedges_per_edge( level ) + 1 ) * levelinfo::num_microedges_per_edge( level ) ) / 2 );
+}
+
+}
