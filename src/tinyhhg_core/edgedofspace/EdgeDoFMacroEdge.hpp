344682061b8e8d30e8618e5ed96a38bd55044040
Dominik Thoennes <dominik.thoennes@fau.de>
2017-11-04 09:04:34 +0100

    added File for edge dof macro edge

---
+++ src/tinyhhg_core/edgedofspace/EdgeDoFMacroEdge.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#include "tinyhhg_core/primitives/Face.hpp"
+#include "tinyhhg_core/levelinfo.hpp"
+#include "tinyhhg_core/macros.hpp"
+#include "tinyhhg_core/edgedofspace/EdgeDoFMemory.hpp"
+#include "tinyhhg_core/indexing/EdgeDoFIndexing.hpp"
+
+namespace hhg {
+namespace edgedof {
+namespace macroedge {
+
+
+} ///namespace macroedge
+} ///namespace edgedof
+} ///namespace hhg
--- tests/tinyhhg_core/edgedofspace/EdgeDoFEnumerateTest.cpp
+++ tests/tinyhhg_core/edgedofspace/EdgeDoFEnumerateTest.cpp
@@ -8,57 +8,34 @@ using namespace hhg;
 
 using walberla::real_t;
 
-void checkComm(std::string meshfile,const uint_t level, bool bufferComm = false){
+template<uint_t Level>
+void checkComm(std::string meshfile, bool bufferComm = false){
 
   MeshInfo meshInfo = MeshInfo::fromGmshFile(meshfile);
   SetupPrimitiveStorage setupStorage(meshInfo, uint_c(walberla::mpi::MPIManager::instance()->numProcesses()));
   std::shared_ptr<PrimitiveStorage> storage = std::make_shared<PrimitiveStorage>(setupStorage);
 
-  hhg::EdgeDoFFunction< uint_t > x("x", storage, level, level);
+  hhg::EdgeDoFFunction< uint_t > x("x", storage, Level, Level);
   if(bufferComm) {
-    x.getCommunicator(level).get()->setLocalCommunicationMode(communication::BufferedCommunicator::BUFFERED_MPI);
+    x.getCommunicator(Level).get()->setLocalCommunicationMode(communication::BufferedCommunicator::BUFFERED_MPI);
   }
 
   size_t num = 0;
   uint_t check = 0;
   uint_t sum = 0;
-  x.enumerate(level,num);
 
-  uint_t totalDoFs = (hhg::levelinfo::num_microfaces_per_face(level) * storage->getNumberOfLocalFaces());
+  //x.enumerate(Level,num);
+
+  uint_t totalDoFs = (hhg::levelinfo::num_microfaces_per_face(Level) * storage->getNumberOfLocalFaces());
   uint_t expectedSum = (totalDoFs * (totalDoFs + 1))/2;
 
-//  for (auto &vertexIt : storage->getVertices()){
-//    Vertex &vertex = *vertexIt.second;
-//    uint_t *vertexData = vertex.getData(x.getVertexDataID())->getPointer(level);
-//    for(uint_t i = 0; i < vertex.getNumNeighborFaces(); ++i){
-//      WALBERLA_CHECK_EQUAL(vertexData[i*2],check);
-//      sum += check;
-//      check++;
-//    }
-//  }
-//
-//
-//
-//  for (auto &edgeIt : storage->getEdges()) {
-//    Edge &edge = *edgeIt.second;
-//    uint_t *edgeData = edge.getData(x.getEdgeDataID())->getPointer(level);
-//    uint_t FaceDoFonFace = hhg::levelinfo::num_microvertices_per_edge(level) * 2 - 3 ;
-//    //this only works with the linear default layout; can be changed to use index function
-//    for(uint_t i = 0; i < edge.getNumHigherDimNeighbors(); ++i){
-//      uint_t start = FaceDoFonFace * i;
-//      for(uint_t j = 2; j < hhg::levelinfo::num_microvertices_per_edge(level) * 2 - 5; j += 2){
-//        WALBERLA_CHECK_EQUAL(edgeData[start + j],check);
-//        sum += check;
-//        check++;
-//      }
-//    }
-//  }
 
   for ( auto &faceIt : storage->getFaces() ) {
+    hhg::edgedof::macroface::enumerateTmpl< uint_t,Level >(*faceIt.second,x.getFaceDataID(),num);
     size_t idxCounter = 0;
     Face &face = *faceIt.second;
-    uint_t *faceData = face.getData(x.getFaceDataID())->getPointer(level);
-    uint_t rowsize = levelinfo::num_microedges_per_edge(level);
+    uint_t *faceData = face.getData(x.getFaceDataID())->getPointer(Level);
+    uint_t rowsize = levelinfo::num_microedges_per_edge(Level);
     uint_t inner_rowsize = rowsize;
     for(uint_t i = 0; i < rowsize ; ++i){
       for(uint_t j = 0; j <  inner_rowsize  ; ++j){
@@ -74,11 +51,11 @@ void checkComm(std::string meshfile,const uint_t level, bool bufferComm = false)
       }
       --inner_rowsize;
     }
-    rowsize = levelinfo::num_microedges_per_edge(level);
+    rowsize = levelinfo::num_microedges_per_edge(Level);
     inner_rowsize = rowsize;
     for(uint_t i = 0; i < rowsize ; ++i){
       for(uint_t j = 0; j <  inner_rowsize  ; ++j){
-        if((j + i) == (levelinfo::num_microedges_per_edge( level ) - 1)) {
+        if((j + i) == (levelinfo::num_microedges_per_edge( Level ) - 1)) {
           WALBERLA_CHECK_EQUAL(faceData[idxCounter], 0, "idxCounter was: " << idxCounter);
           ++idxCounter;
         } else {
@@ -90,7 +67,7 @@ void checkComm(std::string meshfile,const uint_t level, bool bufferComm = false)
       }
       --inner_rowsize;
     }
-    rowsize = levelinfo::num_microedges_per_edge(level);
+    rowsize = levelinfo::num_microedges_per_edge(Level);
     inner_rowsize = rowsize;
     for(uint_t i = 0; i < rowsize ; ++i){
       for(uint_t j = 0; j <  inner_rowsize  ; ++j){
@@ -108,6 +85,21 @@ void checkComm(std::string meshfile,const uint_t level, bool bufferComm = false)
     }
   }
 
+  //  for (auto &edgeIt : storage->getEdges()) {
+//    Edge &edge = *edgeIt.second;
+//    uint_t *edgeData = edge.getData(x.getEdgeDataID())->getPointer(Level);
+//    uint_t FaceDoFonFace = hhg::Levelinfo::num_microvertices_per_edge(Level) * 2 - 3 ;
+//    //this only works with the linear default layout; can be changed to use index function
+//    for(uint_t i = 0; i < edge.getNumHigherDimNeighbors(); ++i){
+//      uint_t start = FaceDoFonFace * i;
+//      for(uint_t j = 2; j < hhg::Levelinfo::num_microvertices_per_edge(Level) * 2 - 5; j += 2){
+//        WALBERLA_CHECK_EQUAL(edgeData[start + j],check);
+//        sum += check;
+//        check++;
+//      }
+//    }
+//  }
+
   --check;
   //WALBERLA_CHECK_EQUAL(check,totalDoFs);
   //WALBERLA_CHECK_EQUAL(sum,expectedSum);
@@ -121,7 +113,9 @@ int main (int argc, char ** argv ) {
   walberla::MPIManager::instance()->useWorldComm();
   walberla::debug::enterTestMode();
 
-  checkComm("../../data/meshes/tri_1el.msh",3);
+  const uint_t Level = 3;
+
+  checkComm< Level > ("../../data/meshes/tri_1el.msh");
 
 }
 
