d16656d629fd1744c5c1f997709f0b8de03fb1e3
Nils Kohl <nils.kohl@fau.de>
2017-12-13 10:29:10 +0100

    making pack info methods const in base class

--- src/tinyhhg_core/communication/PackInfo.hpp
+++ src/tinyhhg_core/communication/PackInfo.hpp
@@ -22,115 +22,115 @@ public:
   /// @name Vertex to Edge
   ///@{
   /// pack data from Vertex into SendBuffer for Edge
-  virtual void packVertexForEdge(const Vertex *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+  virtual void packVertexForEdge(const Vertex *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) const = 0;
   /// unpack data into Edge from RecvBuffer from Vertex
-  virtual void unpackEdgeFromVertex(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+  virtual void unpackEdgeFromVertex(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) const = 0;
   /// transfer data locally (meaning both are on the same MPI Process) from Vertex to Edge
-  virtual void communicateLocalVertexToEdge(const Vertex *sender, Edge *receiver) = 0;
+  virtual void communicateLocalVertexToEdge(const Vertex *sender, Edge *receiver) const = 0;
   ///@}
 
   /// @name Edge to Vertex
   ///@{
-  virtual void packEdgeForVertex(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+  virtual void packEdgeForVertex(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) const = 0;
 
-  virtual void unpackVertexFromEdge(Vertex *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+  virtual void unpackVertexFromEdge(Vertex *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) const = 0;
 
-  virtual void communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiver) = 0;
+  virtual void communicateLocalEdgeToVertex(const Edge *sender, Vertex *receiver) const = 0;
   ///@}
 
   /// @name Edge to Face
   ///@{
-  virtual void packEdgeForFace(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+  virtual void packEdgeForFace(const Edge *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) const = 0;
 
-  virtual void unpackFaceFromEdge(Face *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+  virtual void unpackFaceFromEdge(Face *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) const = 0;
 
-  virtual void communicateLocalEdgeToFace(const Edge *sender, Face *receiver) = 0;
+  virtual void communicateLocalEdgeToFace(const Edge *sender, Face *receiver) const = 0;
   ///@}
 
   /// @name Face to Edge
   ///@{
-  virtual void packFaceForEdge(const Face *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) = 0;
+  virtual void packFaceForEdge(const Face *sender, const PrimitiveID &receiver, walberla::mpi::SendBuffer &buffer) const = 0;
 
-  virtual void unpackEdgeFromFace(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) = 0;
+  virtual void unpackEdgeFromFace(Edge *receiver, const PrimitiveID &sender, walberla::mpi::RecvBuffer &buffer) const = 0;
 
-  virtual void communicateLocalFaceToEdge(const Face *sender, Edge *receiver) = 0;
+  virtual void communicateLocalFaceToEdge(const Face *sender, Edge *receiver) const = 0;
   ///@}
 
   /// @name Generic communication methods
   ///@{
   template< typename SenderType, typename ReceiverType >
-  void pack  ( const SenderType * sender,     const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
+  void pack  ( const SenderType * sender,     const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) const { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
 
   template< typename SenderType, typename ReceiverType >
-  void unpack(       ReceiverType * receiver, const PrimitiveID & senderID,   walberla::mpi::RecvBuffer & recvBuffer ) { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
+  void unpack(       ReceiverType * receiver, const PrimitiveID & senderID,   walberla::mpi::RecvBuffer & recvBuffer ) const { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
 
   template< typename SenderType, typename ReceiverType >
-  void communicateLocal( const SenderType * sender, ReceiverType * receiver ) { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
+  void communicateLocal( const SenderType * sender, ReceiverType * receiver ) const { static_assert( sizeof( SenderType ) == 0 /* always false */, "Invalid primitive type" ); }
   ///@}
 
 };
 
 template<>
-inline void PackInfo::pack< Vertex, Edge > ( const Vertex * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer )
+inline void PackInfo::pack< Vertex, Edge > ( const Vertex * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) const
 {
   packVertexForEdge( sender, receiverID, sendBuffer );
 }
 template<>
-inline void PackInfo::pack< Edge, Vertex > ( const Edge * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer )
+inline void PackInfo::pack< Edge, Vertex > ( const Edge * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) const
 {
   packEdgeForVertex( sender, receiverID, sendBuffer );
 }
 template<>
-inline void PackInfo::pack< Edge, Face > ( const Edge * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer )
+inline void PackInfo::pack< Edge, Face > ( const Edge * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) const
 {
   packEdgeForFace( sender, receiverID, sendBuffer );
 }
 template<>
-inline void PackInfo::pack< Face, Edge > ( const Face * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer )
+inline void PackInfo::pack< Face, Edge > ( const Face * sender, const PrimitiveID & receiverID, walberla::mpi::SendBuffer & sendBuffer ) const
 {
   packFaceForEdge( sender, receiverID, sendBuffer );
 }
 
 
 template<>
-inline void PackInfo::unpack< Vertex, Edge > ( Edge * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer )
+inline void PackInfo::unpack< Vertex, Edge > ( Edge * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer ) const
 {
   unpackEdgeFromVertex( receiver, senderID, recvBuffer );
 }
 template<>
-inline void PackInfo::unpack< Edge, Vertex > ( Vertex * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer )
+inline void PackInfo::unpack< Edge, Vertex > ( Vertex * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer ) const
 {
   unpackVertexFromEdge( receiver, senderID, recvBuffer );
 }
 template<>
-inline void PackInfo::unpack< Edge, Face > ( Face * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer )
+inline void PackInfo::unpack< Edge, Face > ( Face * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer ) const
 {
   unpackFaceFromEdge( receiver, senderID, recvBuffer );
 }
 template<>
-inline void PackInfo::unpack< Face, Edge > ( Edge * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer )
+inline void PackInfo::unpack< Face, Edge > ( Edge * receiver, const PrimitiveID & senderID, walberla::mpi::RecvBuffer & recvBuffer ) const
 {
   unpackEdgeFromFace( receiver, senderID, recvBuffer );
 }
 
 
 template<>
-inline void PackInfo::communicateLocal< Vertex, Edge >( const Vertex * sender, Edge * receiver )
+inline void PackInfo::communicateLocal< Vertex, Edge >( const Vertex * sender, Edge * receiver ) const
 {
   communicateLocalVertexToEdge( sender, receiver );
 }
 template<>
-inline void PackInfo::communicateLocal< Edge, Vertex >( const Edge * sender, Vertex * receiver )
+inline void PackInfo::communicateLocal< Edge, Vertex >( const Edge * sender, Vertex * receiver ) const
 {
   communicateLocalEdgeToVertex( sender, receiver );
 }
 template<>
-inline void PackInfo::communicateLocal< Edge, Face >( const Edge * sender, Face * receiver )
+inline void PackInfo::communicateLocal< Edge, Face >( const Edge * sender, Face * receiver ) const
 {
   communicateLocalEdgeToFace( sender, receiver );
 }
 template<>
-inline void PackInfo::communicateLocal< Face, Edge >( const Face * sender, Edge * receiver )
+inline void PackInfo::communicateLocal< Face, Edge >( const Face * sender, Edge * receiver ) const
 {
   communicateLocalFaceToEdge( sender, receiver );
 }
