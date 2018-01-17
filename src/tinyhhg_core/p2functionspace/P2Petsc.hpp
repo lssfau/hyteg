279c435bb18864f0dd4fcc4aaf565e5e2df5d909
Daniel Drzisga <drzisga@ma.tum.de>
2018-01-17 11:10:35 +0100

    added EdgeDofPetsc

--- src/tinyhhg_core/p2functionspace/P2Petsc.hpp
+++ src/tinyhhg_core/p2functionspace/P2Petsc.hpp
@@ -5,6 +5,7 @@
 #include <tinyhhg_core/p1functionspace/P1Petsc.hpp>
 #include <tinyhhg_core/mixedoperators/EdgeDoFToVertexDoFOperator/EdgeDoFToVertexDoFPetsc.hpp>
 #include <tinyhhg_core/mixedoperators/VertexDoFToEdgeDoFOperator/VertexDoFToEdgeDoFPetsc.hpp>
+#include <tinyhhg_core/edgedofspace/EdgeDoFPetsc.hpp>
 
 namespace hhg {
 namespace petsc {
@@ -21,7 +22,7 @@ inline void createMatrix(OperatorType &opr,
   createMatrix(opr.getVertexToVertexOpr(), *src.getVertexDoFFunction(), *dst.getVertexDoFFunction(), mat, level, flag);
   EdgeDoFToVertexDoF::createMatrix(opr.getEdgeToVertexOpr(), *src.getEdgeDoFFunction(), *dst.getVertexDoFFunction(), mat, level, flag);
   VertexDoFToEdgeDoF::createMatrix(opr.getVertexToEdgeOpr(), *src.getVertexDoFFunction(), *dst.getEdgeDoFFunction(), mat, level, flag);
-//  createMatrix(opr.getEdgeToEdgeOpr(), *src.getEdgeDoFFunction(), *dst.getEdgeDoFFunction(), mat, level, flag);
+  EdgeDoF::createMatrix(opr.getEdgeToEdgeOpr(), *src.getEdgeDoFFunction(), *dst.getEdgeDoFFunction(), mat, level, flag);
 
 }
 
