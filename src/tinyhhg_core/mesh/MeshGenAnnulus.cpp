2a64d48b5fe32eee561a28aff86918dc05ead31e
Marcus Mohr <marcus.mohr@lmu.de>
2018-01-18 17:23:31 +0100

    Implements inline mesh generation requested in issue #42
    This commit adds the following three public methods to the MeshInfo class:
    - meshRectangle()
    - meshAnnulus() [for partial annuli]
    - meshAnnulus() [for a full/closed annulus]
    The implementation of the methods is put into the new files
    - MeshGenRectangle.cpp
    - MeshGenAnnulus.cpp
    - MeshGenFromGmshFile.cpp
    where the latter contains the source code for MeshInfo::fromGmshFile().
    So MeshInfo.hpp/.cpp only contain source code for generic methods that
    are useful for all variants.
    
    The commit adds a new auxilliary method MeshInfo::deriveEdges(). This
    allows to generate the edges of the mesh automatically, once the faces
    and vertices have been constructed. It extracts the information from the
    faces and sets the DoFType of an edge based on that of its two vertices.
    
    The doxygen documentation of the class includes a description of the
    new methods and what different flavours of mesh generation are supported.
    For this the commit adds:
    - figures of sample meshes in @tinyhhg_SOURCE_DIR@/doc/ExtraPics
    - @tinyhhg_SOURCE_DIR@/doc/ExtraPics to Doxygen's IMAGE_PATH.
    
    A new app called show_mesh is also added. It demonstrates the usage of
    the new inline mesh generators and allows to output the resulting meshes
    as VTU files for visualisation. It can also be used to import, export and
    then visualise a Gmsh file.
    
    **NOTE:**
    The inline mesh generators currently set all vertex and edge primitives on
    the domain boundary to DoFType DirichletBoundary and those inside the
    domain, and of course all face primitives, to DoFType Inner.

---
+++ src/tinyhhg_core/mesh/MeshGenAnnulus.cpp
@@ -0,0 +1,140 @@
+
+#include "tinyhhg_core/mesh/MeshInfo.hpp"
+
+#include "core/logging/Logging.h"
+#include "core/debug/CheckFunctions.h"
+#include "core/debug/Debug.h"
+
+#include <array>
+#include <vector>
+
+namespace hhg {
+
+MeshInfo MeshInfo::meshAnnulus( const real_t rhoMin, const real_t rhoMax,
+                                const real_t phiLeft, const real_t phiRight,
+                                const meshFlavour flavour, uint_t nTan, uint_t nRad )
+{
+
+  WALBERLA_ASSERT_LESS( rhoMin, rhoMax );
+  WALBERLA_ASSERT_LESS( phiLeft, phiRight );
+  WALBERLA_ASSERT_GREATER( rhoMin, 1e-8 );
+  WALBERLA_ASSERT_GREATER( nTan, 0 );
+  WALBERLA_ASSERT_GREATER( nRad, 0 );
+
+  // mesh partial annulus in polar coordinates
+  MeshInfo meshInfo = MeshInfo::meshRectangle( Point2D( { rhoMin, phiLeft  } ),
+                                               Point2D( { rhoMax, phiRight } ),
+                                               flavour, nRad, nTan );
+
+  // map vertex coordinates to cartesian domain
+  Point3D node;
+  node[2] = 0.0;
+  DoFType dofType;
+  real_t rho, phi;
+  for ( size_t id = 0; id < meshInfo.vertices_.size(); ++id )
+    {
+      node = meshInfo.vertices_[id].getCoordinates();
+      dofType = meshInfo.vertices_[id].getDoFType();
+      rho = node[0];
+      phi = node[1];
+      node[0] = rho*std::cos(phi);
+      node[1] = rho*std::sin(phi);
+      meshInfo.vertices_[id] = MeshInfo::Vertex( id, Point3D( node ), dofType );
+    }
+
+  return meshInfo;
+
+}
+
+
+MeshInfo MeshInfo::meshAnnulus( const real_t rhoMin, const real_t rhoMax, uint_t nTan, uint_t nRad )
+{
+
+  WALBERLA_ASSERT_LESS( rhoMin, rhoMax );
+  WALBERLA_ASSERT_GREATER( rhoMin, 1e-8 );
+  WALBERLA_ASSERT_GREATER( nTan, 0 );
+  WALBERLA_ASSERT_GREATER( nRad, 0 );
+
+  MeshInfo meshInfo;
+
+  // -------------------
+  //  generate vertices
+  // -------------------
+  IDType id = 0;
+  std::map< std::array<uint_t,2>, IDType > indices2id;
+
+  std::array<real_t,3> node;
+  node[2] = (real_t)0.0;
+
+  real_t deltaPhi = 2.0 * M_PI / (real_t)nTan;
+  real_t deltaRho = (rhoMax - rhoMin) / (real_t)nRad;
+
+  for ( uint_t i = 0; i < nTan; ++i )
+    {
+      // inner boundary
+      node[0] = rhoMin * std::cos( (real_t)i * deltaPhi );
+      node[1] = rhoMin * std::sin( (real_t)i * deltaPhi );
+      meshInfo.vertices_[id] = MeshInfo::Vertex( id, Point3D( node ), DirichletBoundary );
+      indices2id.insert( { {i,0}, id } );
+      ++id;
+
+      // interior nodes
+      for ( uint_t j = 1; j < nRad; ++j )
+        {
+          node[0] = (rhoMin + (real_t)j * deltaRho) * std::cos( (real_t)i * deltaPhi );
+          node[1] = (rhoMin + (real_t)j * deltaRho) * std::sin( (real_t)i * deltaPhi );
+          meshInfo.vertices_[id] = MeshInfo::Vertex( id, Point3D( node ), Inner );
+          indices2id.insert( { {i,j}, id } );
+          ++id;
+        }
+
+      // outer boundary
+      node[0] = rhoMax * std::cos( (real_t)i * deltaPhi );
+      node[1] = rhoMax * std::sin( (real_t)i * deltaPhi );
+      meshInfo.vertices_[id] = MeshInfo::Vertex( id, Point3D( node ), DirichletBoundary );
+      indices2id.insert( { {i,nRad}, id } );
+      ++id;
+    }
+
+  // map node indices to linear id
+  auto getIDX = [ indices2id ] ( uint_t ii, uint_t jj ) -> IDType
+    {
+      std::map< std::array<uint_t,2>, IDType >::const_iterator found = indices2id.find( { ii, jj } );
+      WALBERLA_CHECK( found != indices2id.end(), "Could not map tupled to index!" );
+      return found->second;
+    };
+
+  // --------------------
+  //  generate triangles
+  // --------------------
+  real_t midPhi = 0.0;
+  real_t midRho = 0.0;
+  for ( uint_t i = 0; i < nTan; ++i )
+    {
+      for ( uint_t j = 0; j < nRad; ++j )
+        {
+
+          // add new central vertex
+          midPhi = ( (real_t)i + 0.5 ) * deltaPhi;
+          midRho = rhoMin + ( (real_t)j + 0.5 ) * deltaRho;
+          node[0] = midRho * std::cos( midPhi );
+          node[1] = midRho * std::sin( midPhi );
+          meshInfo.vertices_[id] = MeshInfo::Vertex( id, Point3D( node ), Inner );
+
+          // add four sub-triangles of cell
+          meshInfo.addFace( Face( { getIDX( ( i )     ,  j  ), id, getIDX( (i+1)%nTan,  j  ) }, Inner ) );
+          meshInfo.addFace( Face( { getIDX( (i+1)%nTan,  j  ), id, getIDX( (i+1)%nTan, j+1 ) }, Inner ) );
+          meshInfo.addFace( Face( { getIDX( (i+1)%nTan, j+1 ), id, getIDX( ( i )     , j+1 ) }, Inner ) );
+          meshInfo.addFace( Face( { getIDX( ( i )     , j+1 ), id, getIDX( ( i )     ,  j  ) }, Inner ) );
+          ++id;
+        }
+    }
+
+  // generate edges from faces
+  meshInfo.deriveEdges();
+
+  // done
+  return meshInfo;
+}
+
+} // namespace hhg
