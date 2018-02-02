3091682d3eb7b3cef2793c11b30ee7c81de6c57e
Daniel Drzisga <sersmicro@gmail.com>
2018-02-02 18:54:17 +0100

    added alias for matrices of real_t type

--- src/tinyhhg_core/types/matrix.hpp
+++ src/tinyhhg_core/types/matrix.hpp
@@ -115,6 +115,7 @@ inline std::ostream& operator<<(std::ostream &os, const Matrix<T, M, N> &matrix)
   return os;
 }
 
+template<uint_t M, uint_t N> using Matrixr = Matrix<real_t, M, N>;
 typedef Matrix<real_t, 3, 3> Matrix3r;
 typedef Matrix<real_t, 6, 6> Matrix6r;
 
