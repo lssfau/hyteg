7389d6b597ad18f0c64d53b16173ba2eb103fe5c
Daniel Drzisga <drzisga@ma.tum.de>
2018-02-07 10:55:46 +0100

    added gmg_P2_h_refinement app for testing pure P2 multigrid

---
+++ apps/gmg_P2_h_refinement.cpp
@@ -0,0 +1,153 @@
+#include <core/timing/Timer.h>
+#include <tinyhhg_core/tinyhhg.hpp>
+#include <core/Environment.h>
+#include <core/config/Config.h>
+
+using walberla::real_t;
+using walberla::uint_t;
+using walberla::uint_c;
+
+using namespace hhg;
+
+int main(int argc, char* argv[])
+{
+
+  walberla::Environment walberlaEnv(argc, argv);
+  walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
+  walberla::MPIManager::instance()->useWorldComm();
+
+  walberla::shared_ptr<walberla::config::Config> cfg(new walberla::config::Config);
+  cfg->readParameterFile("../data/param/gmg_P2.prm");
+  walberla::Config::BlockHandle parameters = cfg->getOneBlock("Parameters");
+
+  const uint_t minLevel = parameters.getParameter<uint_t>("minLevel");
+  const uint_t maxLevel = parameters.getParameter<uint_t>("maxLevel");
+  const uint_t max_outer_iter =  parameters.getParameter<uint_t>("max_outer_iter");
+  const uint_t max_cg_iter =  parameters.getParameter<uint_t>("max_cg_iter");
+  const real_t mg_tolerance = parameters.getParameter<real_t>("mg_tolerance");
+  const real_t coarse_tolerance = parameters.getParameter<real_t>("coarse_tolerance");
+
+
+  MeshInfo meshInfo = MeshInfo::fromGmshFile( parameters.getParameter<std::string>("mesh") );
+  SetupPrimitiveStorage setupStorage( meshInfo, uint_c ( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+
+  hhg::loadbalancing::roundRobin( setupStorage );
+
+  std::shared_ptr< walberla::WcTimingTree > timingTree( new walberla::WcTimingTree() );
+  std::shared_ptr<PrimitiveStorage> storage = std::make_shared<PrimitiveStorage>(setupStorage, timingTree);
+
+  hhg::P2Function< real_t > r("r", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > f("f", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > u("u", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > Lu("Lu", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > tmp("tmp", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > u_exact("u_exact", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > err("err", storage, maxLevel, maxLevel);
+  hhg::P2Function< real_t > npoints_helper("npoints_helper", storage, maxLevel, maxLevel);
+
+  std::function<real_t(const hhg::Point3D&)> exact = [](const hhg::Point3D& x) { return sin(x[0])*sinh(x[1]); };
+  std::function<real_t(const hhg::Point3D&)> rhs = [](const hhg::Point3D&) { return 0; };
+  std::function<real_t(const hhg::Point3D&)> zero  = [](const hhg::Point3D&) { return 0.0; };
+  std::function<real_t(const hhg::Point3D&)> ones  = [](const hhg::Point3D&) { return 1.0; };
+
+  WALBERLA_LOG_INFO_ON_ROOT("Interpolating u");
+  u.interpolate(exact, maxLevel, hhg::DirichletBoundary);
+
+  WALBERLA_LOG_INFO_ON_ROOT("Interpolating exact function");
+  u_exact.interpolate(exact, maxLevel);
+//  WALBERLA_LOG_INFO_ON_ROOT("Interpolating and integrating rhs");
+//  npoints_helper.interpolate(rhs, maxLevel);
+//  M.apply(npoints_helper, f, maxLevel, hhg::All);
+
+  WALBERLA_LOG_INFO_ON_ROOT("Setting up stiffness operator");
+  auto start = walberla::timing::getWcTime();
+  hhg::P2ConstantLaplaceOperator L(storage, maxLevel, maxLevel);
+  auto end = walberla::timing::getWcTime();
+  real_t setupTime = end - start;
+
+  npoints_helper.interpolate(ones, maxLevel);
+  real_t npoints = npoints_helper.dot(npoints_helper, maxLevel);
+
+  typedef hhg::CGSolver<hhg::P2Function<real_t>, hhg::P2ConstantLaplaceOperator> CoarseSolver;
+  auto coarseLaplaceSolver = std::make_shared<CoarseSolver>(storage, minLevel, minLevel);
+  typedef GMultigridSolver<hhg::P2Function<real_t>, hhg::P2ConstantLaplaceOperator, CoarseSolver> LaplaceSover;
+  LaplaceSover laplaceSolver(storage, coarseLaplaceSolver, minLevel, maxLevel);
+
+  WALBERLA_LOG_INFO_ON_ROOT("Starting V cycles");
+  WALBERLA_LOG_INFO_ON_ROOT(hhg::format("%6s|%10s|%10s|%10s|%10s|%10s","iter","abs_res","rel_res","conv","L2-error","Time"));
+
+  real_t rel_res = 1.0;
+
+  L.apply(u, Lu, maxLevel, hhg::Inner);
+  r.assign({1.0, -1.0}, {&f, &Lu}, maxLevel, hhg::Inner);
+
+  real_t begin_res = std::sqrt(r.dot(r, maxLevel, hhg::Inner));
+  real_t abs_res_old = begin_res;
+
+  err.assign({1.0, -1.0}, {&u, &u_exact}, maxLevel);
+  real_t discr_l2_err = std::sqrt(err.dot(err, maxLevel) / npoints);
+
+  //WALBERLA_LOG_INFO_ON_ROOT(fmt::format("{:3d}   {:e}  {:e}  {:e}  {:e}  -", 0, begin_res, rel_res, begin_res/abs_res_old, discr_l2_err));
+  WALBERLA_LOG_INFO_ON_ROOT(hhg::format("%6d|%10.3e|%10.3e|%10.3e|%10.3e|%10.3e", 0, begin_res, rel_res, begin_res/abs_res_old, discr_l2_err,0))
+
+  real_t solveTime = real_c(0.0);
+  real_t averageConvergenceRate = real_c(0.0);
+  const uint_t convergenceStartIter = 3;
+
+  uint_t i = 0;
+  for (; i < max_outer_iter; ++i)
+  {
+    start = walberla::timing::getWcTime();
+
+    // Apply P2 geometric multigrid solver
+    laplaceSolver.solve(L, u, f, r, maxLevel, coarse_tolerance, max_cg_iter, hhg::Inner, LaplaceSover::CycleType::VCYCLE, false);
+
+    end = walberla::timing::getWcTime();
+
+
+    L.apply(u, Lu, maxLevel, hhg::Inner);
+    r.assign({1.0, -1.0}, { &f, &Lu }, maxLevel, hhg::Inner);
+    real_t abs_res = std::sqrt(r.dot(r, maxLevel, hhg::Inner));
+    rel_res = abs_res / begin_res;
+    err.assign({1.0, -1.0}, { &u, &u_exact }, maxLevel);
+    discr_l2_err = std::sqrt(err.dot(err, maxLevel) / npoints);
+
+    //WALBERLA_LOG_INFO_ON_ROOT(fmt::format("{:3d}   {:e}  {:e}  {:e}  {:e}  {:e}", i+1, abs_res, rel_res, abs_res/abs_res_old, discr_l2_err, end-start));
+    WALBERLA_LOG_INFO_ON_ROOT(hhg::format("%6d|%10.3e|%10.3e|%10.3e|%10.3e|%10.3e", i+1, abs_res, rel_res, abs_res/abs_res_old, discr_l2_err,end - start))
+    solveTime += end-start;
+
+    if (i >= convergenceStartIter) {
+      averageConvergenceRate += abs_res/abs_res_old;
+    }
+
+    abs_res_old = abs_res;
+
+    if (rel_res < mg_tolerance)
+    {
+      break;
+    }
+  }
+
+  WALBERLA_LOG_INFO_ON_ROOT("Setup time: " << std::defaultfloat << setupTime);
+  WALBERLA_LOG_INFO_ON_ROOT("Solve time " << std::defaultfloat << solveTime);
+  WALBERLA_LOG_INFO_ON_ROOT("Time to solution: " << std::defaultfloat << setupTime + solveTime);
+  WALBERLA_LOG_INFO_ON_ROOT("Avg. convergence rate: " << std::scientific << averageConvergenceRate / real_c(i-convergenceStartIter));
+  WALBERLA_LOG_INFO_ON_ROOT("L^2 error: " << std::scientific << discr_l2_err);
+  WALBERLA_LOG_INFO_ON_ROOT("DoFs: " << (uint_t) npoints);
+
+  if (parameters.getParameter<bool>("vtkOutput")) {
+    VTKOutput vtkOutput("../output", "gmg_P2_h_refinement");
+    vtkOutput.add(&u);
+    vtkOutput.add(&u_exact);
+    vtkOutput.add(&f);
+    vtkOutput.add(&r);
+    vtkOutput.add(&err);
+    vtkOutput.add(&npoints_helper);
+    vtkOutput.write(maxLevel);
+  }
+
+  walberla::WcTimingTree tt = timingTree->getReduced();
+  WALBERLA_LOG_INFO_ON_ROOT( tt );
+
+  return 0;
+}
