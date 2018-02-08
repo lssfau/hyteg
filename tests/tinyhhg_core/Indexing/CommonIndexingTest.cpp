734b7a965835f3ad8d620b08b9216d1e82315d87
Nils Kohl <nils.kohl@fau.de>
2018-02-08 17:44:31 +0100

    First test cases for macro-cell border iterator

--- tests/tinyhhg_core/Indexing/CommonIndexingTest.cpp
+++ tests/tinyhhg_core/Indexing/CommonIndexingTest.cpp
@@ -56,6 +56,27 @@ static void testCellIterator( const std::vector< std::array< uint_t, 3 > > & exp
   }
 }
 
+static void testCellBorderIterator( const std::vector< std::array< uint_t, 3 > > & expectedValues, const uint_t & width, const std::array< uint_t, 3 > & vertices )
+{
+  std::vector< std::array< uint_t, 3 > > iteratorResult;
+
+  uint_t cnt = 0;
+  for ( const auto & it : indexing::CellBorderIterator( width, vertices ) )
+  {
+    iteratorResult.push_back( {{ it.col(), it.row(), it.dep() }} );
+  }
+
+  WALBERLA_CHECK_EQUAL( iteratorResult.size(), expectedValues.size() );
+
+  for ( uint_t i = 0; i < iteratorResult.size(); i++ )
+  {
+    WALBERLA_LOG_DEVEL( "cnt = " << cnt << " ( " << iteratorResult[i][0] << ", " << iteratorResult[i][1] << ", " << iteratorResult[i][2]  << " )" );
+    cnt++;
+    WALBERLA_CHECK_EQUAL( iteratorResult[i][0], expectedValues[i][0] );
+    WALBERLA_CHECK_EQUAL( iteratorResult[i][1], expectedValues[i][1] );
+    WALBERLA_CHECK_EQUAL( iteratorResult[i][2], expectedValues[i][2] );
+  }
+}
 static void testCommonIndexing()
 {
   using walberla::uint_t;
@@ -147,6 +168,14 @@ static void testCommonIndexing()
 
   testCellIterator( std::vector< std::array< uint_t, 3 > >( {{ {{ 1, 1, 1 }} }} ), 5, 1 );
   testCellIterator( std::vector< std::array< uint_t, 3 > >( {{ {{ 2, 2, 2 }} }} ), 9, 2 );
+
+  testCellBorderIterator( std::vector< std::array< uint_t, 3 > >( {{
+    {{ 0, 0, 0 }}, {{ 0, 1, 0 }}, {{ 0, 2, 0 }}, {{ 0, 3, 0 }}, {{ 0, 4, 0 }}, {{ 1, 0, 0 }}, {{ 1, 1, 0 }}, {{ 1, 2, 0 }}, {{ 1, 3, 0 }}, {{ 2, 0, 0 }}, {{ 2, 1, 0 }}, {{ 2, 2, 0 }}, {{ 3, 0, 0 }}, {{ 3, 1, 0 }}, {{ 4, 0, 0 }}
+  }} ), 5, std::array< uint_t, 3 >( {{ 0, 2, 1}} ) );
+
+  testCellBorderIterator( std::vector< std::array< uint_t, 3 > >( {{
+    {{ 0, 0, 0 }}, {{ 0, 1, 0 }}, {{ 0, 2, 0 }}, {{ 0, 3, 0 }}, {{ 0, 4, 0 }}, {{ 0, 5, 0 }}, {{ 0, 6, 0 }}, {{ 0, 7, 0 }}, {{ 0, 8, 0 }}, {{ 1, 0, 0 }}, {{ 1, 1, 0 }}, {{ 1, 2, 0 }}, {{ 1, 3, 0 }}, {{ 1, 4, 0 }}, {{ 1, 5, 0 }}, {{ 1, 6, 0 }}, {{ 1, 7, 0 }}, {{ 2, 0, 0 }}, {{ 2, 1, 0 }}, {{ 2, 2, 0 }}, {{ 2, 3, 0 }}, {{ 2, 4, 0 }}, {{ 2, 5, 0 }}, {{ 2, 6, 0 }}, {{ 3, 0, 0 }}, {{ 3, 1, 0 }}, {{ 3, 2, 0 }}, {{ 3, 3, 0 }}, {{ 3, 4, 0 }}, {{ 3, 5, 0 }}, {{ 4, 0, 0 }}, {{ 4, 1, 0 }}, {{ 4, 2, 0 }}, {{ 4, 3, 0 }}, {{ 4, 4, 0 }}, {{ 5, 0, 0 }}, {{ 5, 1, 0 }}, {{ 5, 2, 0 }}, {{ 5, 3, 0 }}, {{ 6, 0, 0 }}, {{ 6, 1, 0 }}, {{ 6, 2, 0 }}, {{ 7, 0, 0 }}, {{ 7, 1, 0 }}, {{ 8, 0, 0 }}
+  }} ), 9, std::array< uint_t, 3 >( {{ 0, 2, 1 }} ) );
 }
 
 } // namespace hhg
