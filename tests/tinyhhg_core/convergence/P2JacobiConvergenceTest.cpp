138642b8b30ab42b8f089d0a2cb852d318ae2ca8
Dominik Thoennes <dominik.thoennes@fau.de>
2018-03-23 08:07:32 +0100

    added more output to JacboiConvergenceTest

--- tests/tinyhhg_core/convergence/P2JacobiConvergenceTest.cpp
+++ tests/tinyhhg_core/convergence/P2JacobiConvergenceTest.cpp
@@ -38,11 +38,19 @@ int main(int argc, char* argv[])
 
 
 
-  std::function<real_t(const hhg::Point3D&)> exactFunction = [](const hhg::Point3D& x) { return sin(x[0])*sinh(x[1]); };
-  //std::function<real_t(const hhg::Point3D&)> zeros = [](const hhg::Point3D&) { return 0; };
+  std::function<real_t(const hhg::Point3D&)> exactFunction = [](const hhg::Point3D& x) { return 50.0; }; //sin(x[0])*sinh(x[1]); };
+  //std::function<real_t(const hhg::Point3D&)> zeros = [](const hhg::Point3D&) { return 0.0; };
   std::function<real_t(const hhg::Point3D&)> ones  = [](const hhg::Point3D&) { return 1.0; };
+  std::function<real_t(const hhg::Point3D&)> random  = [](const hhg::Point3D&) { return real_c(rand()); };
+//  std::function<real_t(const hhg::Point3D&)> exactFunction2 = [](const hhg::Point3D& x) { return x[0]; };
+//
+//  p2function.interpolate(random, level, hhg::Inner);
+//  helperFun.interpolate(random, level, hhg::Inner);
 
+  //p2function.getVertexDoFFunction()->interpolate(exactFunction, level, hhg::Inner);
+  //helperFun.getVertexDoFFunction()->interpolate(exactFunction, level, hhg::Inner);
   p2function.interpolate(exactFunction, level, hhg::DirichletBoundary);
+  helperFun.interpolate(exactFunction, level, hhg::DirichletBoundary);
   p2Exact.interpolate(exactFunction, level);
 
   real_t begin_res, abs_res_old, rel_res, abs_res = 0;
@@ -54,20 +62,37 @@ int main(int argc, char* argv[])
   begin_res = std::sqrt(residuum.dot(residuum, level, hhg::Inner));
   abs_res_old = begin_res;
 
+
+  //if (parameters.getParameter<bool>("vtkOutput")) {
+    VTKOutput vtkOutput( "../../output", "gs_P2" );
+    vtkOutput.add( &p2function );
+    vtkOutput.add( &p2Exact );
+    vtkOutput.add( &rhs );
+    vtkOutput.add( &residuum );
+    vtkOutput.add( &error );
+    vtkOutput.add( &helperFun );
+  //}
+
+  //auto faceDataPtr = storage->getFace(PrimitiveID(5))->getData(p2function.getVertexDoFFunction()->getFaceDataID())->getPointer(level );
+
+  auto face0 = storage->getFace(PrimitiveID(6));
+  hhg::vertexdof::macroface::printFunctionMemory< real_t >(level,*face0, p2function.getVertexDoFFunction()->getFaceDataID());
+  hhg::edgedof::macroface::printFunctionMemory< real_t >(level,*face0, p2function.getEdgeDoFFunction()->getFaceDataID());
   WALBERLA_LOG_INFO_ON_ROOT(hhg::format("%6d|%10.3e|%10.3e|%10.3e", 0, begin_res, rel_res, begin_res/abs_res_old))
   walberla::WcTimer timer;
   for(uint_t i = 0; i < maxiter; ++i) {
-    helperFun.assign({1.0},{&p2function},level,hhg::All);
+
+    helperFun.assign({1.0},{&p2function},level,hhg::Inner);
+    //vtkOutput.write( level, i );
     L.smooth_jac(p2function, rhs, helperFun, level, hhg::Inner);
+    hhg::vertexdof::macroface::printFunctionMemory< real_t > (level,*face0, p2function.getVertexDoFFunction()->getFaceDataID());
+    hhg::edgedof::macroface::printFunctionMemory< real_t >(level,*face0, p2function.getEdgeDoFFunction()->getFaceDataID());
     L.apply(p2function, Lu, level, hhg::Inner);
     residuum.assign({1.0, -1.0}, { &rhs, &Lu }, level, hhg::Inner);
     abs_res = std::sqrt(residuum.dot(residuum, level, hhg::Inner));
     rel_res = abs_res / begin_res;
     WALBERLA_LOG_INFO_ON_ROOT(hhg::format("%6d|%10.3e|%10.3e|%10.3e", i+1, abs_res, rel_res, abs_res/abs_res_old))
-    if(i%5 == 0)
-    {
-      //WALBERLA_CHECK_LESS(abs_res, abs_res_old);
-    }
+    //WALBERLA_CHECK_LESS(abs_res,abs_res_old);
     abs_res_old = abs_res;
   }
   timer.end();
@@ -82,16 +107,6 @@ int main(int argc, char* argv[])
 
   WALBERLA_LOG_INFO_ON_ROOT("discrete L2 error = " << discr_l2_err);
 
-  if (parameters.getParameter<bool>("vtkOutput")) {
-    VTKOutput vtkOutput( "../../output", "gs_P2" );
-    vtkOutput.add( &p2function );
-    vtkOutput.add( &p2Exact );
-    vtkOutput.add( &rhs );
-    vtkOutput.add( &residuum );
-    vtkOutput.add( &error );
-    vtkOutput.add( &helperFun );
-    vtkOutput.write( level );
-  }
 
   if (parameters.getParameter<bool>("printTiming")) {
     walberla::WcTimingTree tt = timingTree->getReduced();
