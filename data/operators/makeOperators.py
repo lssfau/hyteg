0937642b96eda832fb4defe7979f7b164b9f758f
Marcus Mohr <marcus.mohr@lmu.de>
2018-03-28 14:36:29 +0200

    Resolves issue #60
    The commit resolves issue #60, i.e. it enhances our python script to also
    generate the C++ header files for operators with mixed finite element pairs
    automatically from generic UFC form files.
    
    The changes are rather unintrusive. We now only distinguish in the script
    and the form files between elementTest and elementTrial for the test and
    trial spaces. The name and order for both elements is given in the elemList
    of the script.
    
    Due to the rather irregular placing of generated C++ files for mixed forms
    in the source tree, we now need to specify their placement in the outDir
    list.

--- data/operators/makeOperators.py
+++ data/operators/makeOperators.py
@@ -1,26 +1,52 @@
 #! /usr/bin/python3
 import subprocess as sproc
 
-# Set list of elements for which to compile forms
+# Set list of elements and pairs for which to compile forms
+#
+# 0: marker
+# 1: name of element for test space
+# 2: order of element for test space
+# 3: name of element for trial space
+# 4: order of element for trial space
+#
 elemList = list()
-elemList.append( [ "p1", "Lagrange", 1 ] )
-elemList.append( [ "p2", "Lagrange", 2 ] )
-elemList.append( [ "bubble", "Bubble", 3 ] )
+elemList.append( [ "p1", "Lagrange", 1, "Lagrange", 1 ] )
+elemList.append( [ "p2", "Lagrange", 2, "Lagrange", 2 ] )
+elemList.append( [ "bubble", "Bubble", 3, "Bubble", 3 ] )
+elemList.append( [ "bubble_to_p1", "Bubble", 3, "Lagrange", 1 ] )
+elemList.append( [ "p1_to_bubble", "Lagrange", 1, "Bubble", 3 ] )
+elemList.append( [ "p1_to_p2", "Lagrange", 1, "Lagrange", 2 ] )
+elemList.append( [ "p2_to_p1", "Lagrange", 2, "Lagrange", 1 ] )
 
-# For each element set the forms to compile
+# For each element and pair set the forms to compile
 forms = {}
 forms[ "p1" ] = [ "diffusion", "div", "divt", "mass", "pspg", "stokes_epsilon" ]
 forms[ "p2" ] = [ "diffusion", "div", "divt" ]
 forms[ "bubble" ] = [ "diffusion" ]
+forms[ "bubble_to_p1" ] = [ "divt" ]
+forms[ "p1_to_bubble" ] = [ "div" ]
+forms[ "p1_to_p2" ] = [ "divt" ]
+forms[ "p2_to_p1" ] = [ "div" ]
+
+# For each element and pair set the directory into which we place the
+# generated C++ header files (relative to tinyHHGsrc below)
+outDir = {}
+outDir[ "p1" ] = [ "p1functionspace/generated" ]
+outDir[ "p2" ] = [ "p2functionspace/generated" ]
+outDir[ "bubble" ] = [ "bubblefunctionspace/generated" ]
+outDir[ "bubble_to_p1" ] = [ "mixedoperators/BubbleToP1/generated" ]
+outDir[ "p1_to_bubble" ] = [ "mixedoperators/P1ToBubble/generated" ]
+outDir[ "p1_to_p2" ] = [ "mixedoperators/generated" ]
+outDir[ "p2_to_p1" ] = [ "mixedoperators/generated" ]
+
+# Set output directory
+tinyHHGsrc = "../../src/tinyhhg_core"
 
 # Decide whether script should be talkative
 beVerbose = True
 
 # Set options for invocation of FFC
 ffcOpts = ""
-
-# Set output directory
-tinyHHGsrc = "../../../src/tinyhhg_core"
 
 # Set to false to not remove temporary files
 cleanTemporaries = True
@@ -51,7 +77,7 @@ def getFFCFileName( elem, form ):
 def getHeaderFileName( elem, form ):
     return elem + "_" + form + ".h"
 
-def readFormFile( elem, form ):
+def readFormFile( form ):
     fname = getFormFileName( form )
     fh = getFileHandle( fname, "r" )
     formDescr = fh.read()
@@ -60,12 +86,14 @@ def readFormFile( elem, form ):
 def genFFCFile( eDescr, form ):
 
     # Disassemble element description
-    eName  = eDescr[0]
-    eType  = eDescr[1]
-    eOrder = eDescr[2]
+    eName       = eDescr[0]
+    eTypeTest   = eDescr[1]
+    eOrderTest  = eDescr[2]
+    eTypeTrial  = eDescr[3]
+    eOrderTrial = eDescr[4]
   
     # Read (template) file with form description
-    formDescr = readFormFile( eName, form )
+    formDescr = readFormFile( form )
 
     # Open output file
     ofName = getFFCFileName( eName, form )
@@ -75,9 +103,14 @@ def genFFCFile( eDescr, form ):
     comment = "# generate code with ffc " + ofName + "\n"
     of.write( comment )
 
-    # Assemble UFL element description
-    eLine = "element = FiniteElement( \"" + eType + "\", \"triangle\", "
-    eLine += str(eOrder) + " )\n"
+    # Assemble UFL element description (test space)
+    eLine = "testElement = FiniteElement( \"" + eTypeTest
+    eLine += "\", \"triangle\", " + str(eOrderTest) + " )\n"
+    of.write( eLine )
+
+    # Assemble UFL element description (trial space)
+    eLine = "trialElement = FiniteElement( \"" + eTypeTrial
+    eLine += "\", \"triangle\", " + str(eOrderTrial) + " )\n"
     of.write( eLine )
 
     # Finish script by adding form description
@@ -95,8 +128,9 @@ def fixIncludeAndWriteOperatorFile( eDescr, form ):
     # Determine name of operator file (input)
     # and destination for operator file (output)
     inFileName  = getHeaderFileName( eName, form )
-    outFileName = tinyHHGsrc + "/" + eName + "functionspace/generated/"
-    outFileName += inFileName
+    # outFileName = tinyHHGsrc + "/" + eName + "functionspace/generated/"
+    # outFileName += inFileName
+    outFileName = tinyHHGsrc + "/" + outDir[ eName ][0] + "/" + inFileName
 
     # Read in header file
     with open( inFileName, "r" ) as inFile :
@@ -122,8 +156,11 @@ for elem in elemList:
 
     # Be verbose
     eName = elem[0]
-    logMsg( " Generating forms for element type '" + eName + "'" )
-
+    if elem[1] == elem[3] and elem[2] == elem[4]:
+        logMsg( " Generating forms for element type '" + eName + "'" )
+    else:
+        logMsg( " Generating forms for mixed element pair '" + eName + "'" )
+        
     for form in forms[ eName ]:
         logMsg( " -> current form: " + form )
         ffcFileName = genFFCFile( elem, form )
