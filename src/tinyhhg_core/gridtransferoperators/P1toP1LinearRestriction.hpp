231b4a873451463483dc2b0d760e8cdaeaa8f3b7
Nils Kohl <nils.kohl@fau.de>
2018-06-07 21:31:43 +0200

    Bugfix in new linear P1 restrict operator

--- src/tinyhhg_core/gridtransferoperators/P1toP1LinearRestriction.hpp
+++ src/tinyhhg_core/gridtransferoperators/P1toP1LinearRestriction.hpp
@@ -15,64 +15,62 @@ public:
       const auto storage = function.getStorage();
       const auto boundaryCondition = function.getBoundaryCondition();
 
+      // start pulling vertex halos
+      function.startCommunication< Edge, Vertex >( sourceLevel );
+
+      // start pulling edge halos
+      function.startCommunication< Face, Edge >( sourceLevel );
+
+      // end pulling vertex halos
+      function.endCommunication< Edge, Vertex >( sourceLevel );
+
+      for ( const auto & it : storage->getVertices())
+      {
+        const Vertex & vertex = *it.second;
+        const auto srcData = vertex.getData( function.getVertexDataID())->getPointer( sourceLevel );
+        auto dstData = vertex.getData( function.getVertexDataID())->getPointer( destinationLevel );
+
+        if ( testFlag( boundaryCondition.getBoundaryType( vertex.getMeshBoundaryFlag()), flag ))
+        {
+          restrictMacroVertex( srcData, dstData, sourceLevel, vertex.getNumNeighborEdges() );
+        }
+      }
+
+      function.startCommunication< Vertex, Edge >( destinationLevel );
+
+      // end pulling edge halos
+      function.endCommunication< Face, Edge >( sourceLevel );
+
+      for ( const auto & it : storage->getEdges())
+      {
+        const Edge & edge = *it.second;
+        const auto srcData = edge.getData( function.getEdgeDataID())->getPointer( sourceLevel );
+        auto dstData = edge.getData( function.getEdgeDataID())->getPointer( destinationLevel );
+
+        if ( testFlag( boundaryCondition.getBoundaryType( edge.getMeshBoundaryFlag()), flag ))
+        {
+          restrictMacroEdge( srcData, dstData, sourceLevel, edge.getNumNeighborFaces() );
+        }
+      }
+
+      function.endCommunication< Vertex, Edge >( destinationLevel );
+
+      function.startCommunication< Edge, Face >( destinationLevel );
+
       for ( const auto & it : storage->getFaces())
       {
-        // start pulling vertex halos
-        function.startCommunication< Edge, Vertex >( sourceLevel );
+        const Face & face = *it.second;
+        const auto srcData = face.getData( function.getFaceDataID())->getPointer( sourceLevel );
+        auto dstData = face.getData( function.getFaceDataID())->getPointer( destinationLevel );
 
-        // start pulling edge halos
-        function.startCommunication< Face, Edge >( sourceLevel );
-
-        // end pulling vertex halos
-        function.endCommunication< Edge, Vertex >( sourceLevel );
-
-        for ( const auto & it : storage->getVertices())
+        if ( testFlag( boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag()), flag ))
         {
-          const Vertex & vertex = *it.second;
-          const auto srcData = vertex.getData( function.getVertexDataID())->getPointer( sourceLevel );
-          auto dstData = vertex.getData( function.getVertexDataID())->getPointer( destinationLevel );
-
-          if ( testFlag( boundaryCondition.getBoundaryType( vertex.getMeshBoundaryFlag()), flag ))
-          {
-            restrictMacroVertex( srcData, dstData, sourceLevel, vertex.getNumNeighborEdges() );
-          }
+          restrictMacroFace( srcData, dstData, sourceLevel, 0 );
         }
-
-        function.startCommunication< Vertex, Edge >( destinationLevel );
-
-        // end pulling edge halos
-        function.endCommunication< Face, Edge >( sourceLevel );
-
-        for ( const auto & it : storage->getEdges())
-        {
-          const Edge & edge = *it.second;
-          const auto srcData = edge.getData( function.getEdgeDataID())->getPointer( sourceLevel );
-          auto dstData = edge.getData( function.getEdgeDataID())->getPointer( destinationLevel );
-
-          if ( testFlag( boundaryCondition.getBoundaryType( edge.getMeshBoundaryFlag()), flag ))
-          {
-            restrictMacroEdge( srcData, dstData, sourceLevel, edge.getNumNeighborFaces() );
-          }
-        }
-
-        function.endCommunication< Vertex, Edge >( destinationLevel );
-
-        function.startCommunication< Edge, Face >( destinationLevel );
-
-        for ( const auto & it : storage->getFaces())
-        {
-          const Face & face = *it.second;
-          const auto srcData = face.getData( function.getFaceDataID())->getPointer( sourceLevel );
-          auto dstData = face.getData( function.getFaceDataID())->getPointer( destinationLevel );
-
-          if ( testFlag( boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag()), flag ))
-          {
-            restrictMacroFace( srcData, dstData, sourceLevel, 0 );
-          }
-        }
-
-        function.endCommunication< Edge, Face >( destinationLevel );
       }
+
+      function.endCommunication< Edge, Face >( destinationLevel );
+
     }
 
 private:
