e9dd0e2772d078b3a3ec40de6d1249bc2d21dc83
Dominik Thoennes <dominik.thoennes@fau.de>
2018-06-27 15:04:40 +0200

    rename BlockLaplace to StokesLaplace; move app into tests

---
+++ src/tinyhhg_core/composites/P1StokesBlockLaplaceOperator.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "tinyhhg_core/composites/P1StokesFunction.hpp"
+#include "tinyhhg_core/p1functionspace/P1ConstantOperator.hpp"
+
+namespace hhg {
+
+class P1StokesBlockLaplaceOperator
+{
+ public:
+   P1StokesBlockLaplaceOperator( const std::shared_ptr< PrimitiveStorage >& storage, size_t minLevel, size_t maxLevel )
+   : A( storage, minLevel, maxLevel )
+   {}
+
+   void apply( P1StokesFunction< real_t >& src,
+               P1StokesFunction< real_t >& dst,
+               size_t                      level,
+               DoFType                     flag,
+               UpdateType                  updateType )
+   {
+      A.apply( src.u, dst.u, level, flag, updateType );
+      A.apply( src.v, dst.v, level, flag, updateType );
+      A.apply( src.p, dst.p, level, flag, updateType );
+   }
+
+   P1ConstantLaplaceOperator A;
+};
+
+} // namespace hhg
+
+#endif //TINYHHG_P1BLOCKLAPLACEOPERATOR_HPP
