5cb54c2ff35aaf3cdebe9043b028041308569743
Daniel Drzisga <sersmicro@gmail.com>
2018-08-22 11:02:45 +0200

    added uzawa relaxation parameter to constructor and param file; improved taylor-hood convergence rate

--- src/tinyhhg_core/solvers/UzawaSolver.hpp
+++ src/tinyhhg_core/solvers/UzawaSolver.hpp
@@ -27,15 +27,17 @@ public:
               const uint_t & maxLevel,
               const uint_t & numberOfPreSmoothingSteps,
               const uint_t & numberOfPostSmoothingSteps,
-              const uint_t & smoothingStepIncrement )
+              const uint_t & smoothingStepIncrement,
+              const real_t & relaxParam = walberla::real_c(0.3) )
     : coarseGridSolver_( coarseGridSolver ), restrictionOperator_( restrictionOperator ),
       prolongationOperator_( prolongationOperator ),
-      pressureMass_( storage, minLevel, maxLevel ),
+      pspg_( storage, minLevel, maxLevel ),
       minLevel_(minLevel), maxLevel_(maxLevel),
       nuPre_( numberOfPreSmoothingSteps ), nuPost_( numberOfPostSmoothingSteps ),
       nuAdd_( smoothingStepIncrement ),
       ax_("uzw_ax", storage, minLevel, maxLevel), tmp_("uzw_tmp", storage, minLevel, maxLevel),
-      hasGlobalCells_( storage->hasGlobalCells() )
+      hasGlobalCells_( storage->hasGlobalCells() ),
+      relaxParam_( relaxParam )
   {
     zero_ = [](const hhg::Point3D&) { return 0.0; };
   }
@@ -68,7 +70,7 @@ public:
       restrictionOperator_( r, level, flag );
 
       b.assign({1.0}, { &r }, level - 1, flag);
-      vertexdof::projectMean(b.p, ax_.p, level-1);
+//      vertexdof::projectMean(b.p, ax_.p, level-1);
 
       x.interpolate(zero_, level-1);
 
@@ -131,7 +133,7 @@ private:
 
     r.p.assign({1.0, -1.0}, {&b.p, &r.p}, level, flag);
 
-    A.pspg.smooth_sor(x.p, r.p, 0.3, level, flag);
+    A.pspg.smooth_sor(x.p, r.p, relaxParam_, level, flag);
   }
 
   // Tensor variant
@@ -152,7 +154,7 @@ private:
 
     r.p.assign({1.0, -1.0}, {&b.p, &r.p}, level, flag);
 
-    A.pspg.smooth_sor(x.p, r.p, 0.3, level, flag);
+    A.pspg.smooth_sor(x.p, r.p, relaxParam_, level, flag);
   }
 
   // Block-Laplace variant without stabilization
@@ -166,11 +168,28 @@ private:
     r.v.assign({1.0, -1.0}, {&b.v, &r.v}, level, flag);
     A.A.smooth_gs(x.v, r.v, level, flag);
 
-    A.div_x.apply(x.u, r.p, level, flag, Replace);
-    A.div_y.apply(x.v, r.p, level, flag, Add);
+    A.divT_x.apply(x.p, r.u, level, flag, Replace);
+    r.u.assign({1.0, -1.0}, {&b.u, &r.u}, level, flag);
+    A.A.smooth_gs(x.u, r.u, level, flag);
 
-    pressureMass_.apply(r.p, ax_.p, level, flag);
-    x.p.add( {1.0}, {&ax_.p}, level, flag);
+    A.divT_y.apply(x.p, r.v, level, flag, Replace);
+    r.v.assign({1.0, -1.0}, {&b.v, &r.v}, level, flag);
+    A.A.smooth_gs(x.v, r.v, level, flag);
+
+    A.div_x.apply(x.u, r.p, level, flag | DirichletBoundary, Replace);
+    A.div_y.apply(x.v, r.p, level, flag | DirichletBoundary, Add);
+    r.p.assign({1.0, -1.0}, {&b.p, &r.p}, level, flag | DirichletBoundary);
+
+//    invPressureMass_.apply(r.p, tmp_.p, level, flag | DirichletBoundary);
+//    x.p.add({-relaxParam_}, {&tmp_.p}, level, flag | DirichletBoundary);
+
+//    tmp_.p.interpolate(0.0, level);
+//    pressureMass_.smooth_sor(tmp_.p, r.p, relaxParam_, level, flag | DirichletBoundary);
+//    x.p.add({-1.0}, {&tmp_.p}, level, flag | DirichletBoundary);
+
+    tmp_.p.interpolate(0.0, level);
+    pspg_.smooth_sor(tmp_.p, r.p, relaxParam_, level, flag | DirichletBoundary);
+    x.p.add({1.0}, {&tmp_.p}, level, flag | DirichletBoundary);
   }
 
   uint_t nuPre_;
@@ -180,11 +199,13 @@ private:
   uint_t minLevel_;
   uint_t maxLevel_;
 
+  real_t relaxParam_;
+
   CoarseGridSolver coarseGridSolver_;
   RestrictionOperator restrictionOperator_;
   ProlongationOperator prolongationOperator_;
 
-  P1LumpedInvMassOperator pressureMass_;
+  P1PSPGOperator pspg_;
 
   F ax_;
   F tmp_;
