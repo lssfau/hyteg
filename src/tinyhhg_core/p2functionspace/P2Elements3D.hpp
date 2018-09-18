2aa2050559cbda17251fa2faa201f2eeede65ca6
Nils Kohl <nils.kohl@fau.de>
2018-09-18 11:57:14 +0200

    Documentation for functions that return neighborhood of micro-vertices and -edges.

--- src/tinyhhg_core/p2functionspace/P2Elements3D.hpp
+++ src/tinyhhg_core/p2functionspace/P2Elements3D.hpp
@@ -10,7 +10,14 @@ namespace hhg {
 namespace P2Elements {
 namespace P2Elements3D {
 
-
+/// \brief Returns the index offsets to the neighboring edge unknowns of a micro-vertex.
+///
+/// Given a logical index I of a micro-vertex, this function returns the
+/// index offsets that have to be added to I to reach the absolute logical indices of the
+/// neighboring edgedofs of the specified orientation.
+///
+/// \param edgeDoFOrientation the orientation of the edges that shall be reached from the vertexdof
+/// \return a set that contains all index offsets to neighboring edgedofs
 inline std::set< indexing::IndexIncrement > getAllEdgeDoFNeighborsFromVertexDoFInMacroCell( const edgedof::EdgeDoFOrientation & edgeDoFOrientation )
 {
   std::set< indexing::IndexIncrement > neighborEdgeDoFs;
@@ -40,6 +47,15 @@ inline std::set< indexing::IndexIncrement > getAllEdgeDoFNeighborsFromVertexDoFI
 }
 
 
+/// \brief Returns the index offsets to the neighboring edge unknowns of a micro-edge.
+///
+/// Given a logical index I and an orientation of the corresponding edgedof (== centerOrientation),
+/// this function returns the index offsets that have to be added to I to reach the absolute logical indices of the
+/// neighboring edgedofs of a certain orientation (== leafOrientation).
+///
+/// \param centerOrientation the orientation of the edge from which the neighbor edges shall be reached
+/// \param leafOrientation the orientation of the edges that shall be reached
+/// \return a set that contains all index offsets to neighboring edgedofs with leafOrientation
 inline std::set< indexing::IndexIncrement > getAllEdgeDoFNeighborsFromEdgeDoFInMacroCell( const edgedof::EdgeDoFOrientation & centerOrientation,
                                                                                           const edgedof::EdgeDoFOrientation & leafOrientation )
 {
@@ -79,7 +95,14 @@ inline std::set< indexing::IndexIncrement > getAllEdgeDoFNeighborsFromEdgeDoFInM
 }
 
 
-
+/// \brief Returns the index offsets to the neighboring vertex unknowns of a micro-edge.
+///
+/// Given a logical index I of a micro-edge and its orientation, this function returns the
+/// index offsets that have to be added to I to reach the absolute logical indices of the
+/// neighboring vertexdofs.
+///
+/// \param centerOrientation the orientation of the edges from which the neighbor vertices shall be reached
+/// \return a set that contains all index offsets to neighboring vertexdofs
 inline std::set< indexing::IndexIncrement > getAllVertexDoFNeighborsFromEdgeDoFInMacroCell( const edgedof::EdgeDoFOrientation & centerOrientation )
 {
   std::set< indexing::IndexIncrement > neighborVertexDoFs;
