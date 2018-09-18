1289ac8dbb51c38bd230aab5c741c394606c69a7
Nils Kohl <nils.kohl@fau.de>
2018-09-18 13:36:53 +0200

    Implemented vertex to edge apply

--- src/tinyhhg_core/mixedoperators/VertexDoFToEdgeDoFOperator/VertexDoFToEdgeDoFApply.hpp
+++ src/tinyhhg_core/mixedoperators/VertexDoFToEdgeDoFOperator/VertexDoFToEdgeDoFApply.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
-#include <tinyhhg_core/edgedofspace/EdgeDoFIndexing.hpp>
+#include "tinyhhg_core/edgedofspace/EdgeDoFIndexing.hpp"
 #include "tinyhhg_core/FunctionMemory.hpp"
 #include "tinyhhg_core/StencilMemory.hpp"
 #include "tinyhhg_core/p1functionspace/VertexDoFIndexing.hpp"
 #include "tinyhhg_core/primitives/all.hpp"
 #include "tinyhhg_core/Levelinfo.hpp"
+#include "tinyhhg_core/p2functionspace/P2Elements3D.hpp"
 
 namespace hhg{
 namespace VertexDoFToEdgeDoF{
@@ -106,9 +107,88 @@ inline void applyFace(const uint_t & Level, Face &face,
       }
     }
   }
-
 }
 
+
+inline void applyCell(const uint_t & Level, Cell & cell,
+                      const PrimitiveDataID<StencilMemory < real_t >, Cell> &operatorId,
+                      const PrimitiveDataID<FunctionMemory< real_t >, Cell> &srcId,
+                      const PrimitiveDataID<FunctionMemory< real_t >, Cell> &dstId,
+                      UpdateType update){
+
+  real_t * opr_data = cell.getData(operatorId)->getPointer( Level );
+  real_t * src      = cell.getData(srcId)->getPointer( Level );
+  real_t * dst      = cell.getData(dstId)->getPointer( Level );
+
+  for ( const auto & it : hhg::edgedof::macrocell::Iterator( Level, 0 ) )
+  {
+    std::vector< edgedof::EdgeDoFOrientation > innerOrientations;
+
+    if ( edgedof::macrocell::isInnerXEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::X );
+    if ( edgedof::macrocell::isInnerYEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::Y );
+    if ( edgedof::macrocell::isInnerZEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::Z );
+    if ( edgedof::macrocell::isInnerXYEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::XY );
+    if ( edgedof::macrocell::isInnerXZEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::XZ );
+    if ( edgedof::macrocell::isInnerYZEdgeDoF( Level, it ) )
+      innerOrientations.push_back( edgedof::EdgeDoFOrientation::YZ );
+
+    for ( const auto & centerOrientation : innerOrientations )
+    {
+      real_t tmp = 0.0;
+
+      const auto vertexDoFNeighbors = P2Elements::P2Elements3D::getAllVertexDoFNeighborsFromEdgeDoFInMacroCell( centerOrientation );
+      for ( const auto & neighbor : vertexDoFNeighbors )
+      {
+        const uint_t stencilIdx  = vertexdof::stencilIndexFromEdge3D( vertexdof::stencilDirectionFromLogicalOffset( neighbor ), centerOrientation );
+        const auto   srcIdx      = it + neighbor;
+        const auto   srcArrayIdx = vertexdof::macrocell::index( Level, srcIdx.x(), srcIdx.y(), srcIdx.z() );
+        tmp += opr_data[stencilIdx] * src[srcArrayIdx];
+      }
+
+      const auto dstArrayIdx = edgedof::macrocell::index( Level, it.x(), it.y(), it.z(), centerOrientation );
+
+      if ( update == Replace )
+      {
+        dst[dstArrayIdx] = tmp;
+      }
+      else if ( update == Add )
+      {
+        dst[dstArrayIdx] += tmp;
+      }
+    }
+  }
+
+  for ( const auto & it : edgedof::macrocell::IteratorXYZ( Level, 0 ) )
+  {
+    real_t tmp = 0.0;
+    const auto centerOrientation = edgedof::EdgeDoFOrientation::XYZ;
+
+    const auto vertexDoFNeighbors = P2Elements::P2Elements3D::getAllVertexDoFNeighborsFromEdgeDoFInMacroCell( centerOrientation );
+    for ( const auto & neighbor : vertexDoFNeighbors )
+    {
+      const uint_t stencilIdx  = vertexdof::stencilIndexFromEdge3D( vertexdof::stencilDirectionFromLogicalOffset( neighbor ), centerOrientation );
+      const auto   srcIdx      = it + neighbor;
+      const auto   srcArrayIdx = vertexdof::macrocell::index( Level, srcIdx.x(), srcIdx.y(), srcIdx.z() );
+      tmp += opr_data[stencilIdx] * src[srcArrayIdx];
+    }
+
+    const auto dstArrayIdx = edgedof::macrocell::index( Level, it.x(), it.y(), it.z(), centerOrientation );
+
+    if ( update == Replace )
+    {
+      dst[dstArrayIdx] = tmp;
+    }
+    else if ( update == Add )
+    {
+      dst[dstArrayIdx] += tmp;
+    }
+  }
+}
 
 
 } // VertexDoFToEdgeDoFOperator
