de39e37bab6575f0ac06a86dfa4dde544578d0d7
Dominik Thoennes <dominik.thoennes@fau.de>
2018-10-15 15:18:24 +0200

    move sync to cpp

--- src/tinyhhg_core/communication/Syncing.hpp
+++ src/tinyhhg_core/communication/Syncing.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "tinyhhg_core/Function.hpp"
-#include "tinyhhg_core/p2functionspace/P2Function.hpp"
-#include "tinyhhg_core/primitives/all.hpp"
+#include "core/DataTypes.h"
+
+template < typename funcType >
+class P2Function;
 
 namespace hhg {
 namespace communication {
@@ -10,23 +11,10 @@ namespace communication {
 using walberla::uint_t;
 
 template < typename funcType >
-void syncFunctionBetweenPrimitives( const funcType& function, const uint_t level )
-{
-   function.template communicate< Vertex, Edge >( level );
-   function.template communicate< Edge, Face >( level );
-   function.template communicate< Face, Cell >( level );
-
-   function.template communicate< Cell, Face >( level );
-   function.template communicate< Face, Edge >( level );
-   function.template communicate< Edge, Vertex >( level );
-}
+void syncFunctionBetweenPrimitives( const funcType& function, const uint_t& level );
 
 template < typename ValueType >
-void syncP2FunctionBetweenPrimitives( const P2Function< ValueType >& function, const uint_t level )
-{
-   syncFunctionBetweenPrimitives< hhg::vertexdof::VertexDoFFunction< ValueType > >( *function.getVertexDoFFunction(), level );
-   syncFunctionBetweenPrimitives< hhg::EdgeDoFFunction< ValueType > >( *function.getEdgeDoFFunction(), level );
-}
+void syncP2FunctionBetweenPrimitives( const P2Function< ValueType >& function, const uint_t& level );
 
 } // namespace communication
 } // namespace hhg
