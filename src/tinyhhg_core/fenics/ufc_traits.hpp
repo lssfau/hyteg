89c915f72597f9f39f6170b4f102aaccc70920e5
Dominik Thoennes <dominik.thoennes@fau.de>
2018-10-16 15:41:56 +0200

    moving fenics to source file [skip ci] WIP

--- src/tinyhhg_core/fenics/ufc_traits.hpp
+++ src/tinyhhg_core/fenics/ufc_traits.hpp
@@ -1,29 +1,6 @@
-
 #pragma once
 
 #include "tinyhhg_core/types/matrix.hpp"
-
-#ifdef _MSC_VER
-#pragma warning( push, 0 )
-#endif
-
-#include "tinyhhg_core/fenics/fenics.hpp"
-#include "tinyhhg_core/p1functionspace/generated/p1_diffusion.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_div.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_divt.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_mass.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_pspg.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_stokes_epsilon.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_diffusion.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_div_tet.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_divt_tet.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_mass.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_pspg_tet.h"
-#include "tinyhhg_core/p2functionspace/generated/p2_tet_diffusion.h"
-
-#ifdef _MSC_VER
-#pragma warning( pop )
-#endif
 
 namespace hhg {
 namespace fenics {
@@ -31,39 +8,6 @@ namespace fenics {
 template< typename UFCOperator >
 struct UFCTrait;
 
-#define HHG_CREATE_UFCOPERATOR_SPECIALIZATION( UFC_TYPE, LOCAL_STIFFNESS_MATRIX_TYPE ) \
-   template<> \
-   struct UFCTrait< UFC_TYPE > \
-   { \
-      typedef LOCAL_STIFFNESS_MATRIX_TYPE LocalStiffnessMatrix_T; \
-   }
 
-
-// P1 / Tet traits
-
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_diffusion_cell_integral_0_otherwise, Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_div_tet_cell_integral_0_otherwise,   Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_div_tet_cell_integral_1_otherwise,   Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_div_tet_cell_integral_2_otherwise,   Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_divt_tet_cell_integral_0_otherwise,  Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_divt_tet_cell_integral_1_otherwise,  Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_divt_tet_cell_integral_2_otherwise,  Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_mass_cell_integral_0_otherwise,      Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p1_tet_pspg_tet_cell_integral_0_otherwise,  Matrix4r );
-
-
-// P2 / Tet traits
-
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( p2_tet_diffusion_cell_integral_0_otherwise, Matrix10r );
-
-
-// Other
-
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( UndefinedAssembly,  Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( NoAssemble,         Matrix4r );
-HHG_CREATE_UFCOPERATOR_SPECIALIZATION( Dummy10x10Assembly, Matrix10r );
-
-
-#undef HHG_CREATE_UFCOPERATOR_SPECIALIZATION
 }
 }
--- src/tinyhhg_core/p1functionspace/P1ConstantOperator.hpp
+++ src/tinyhhg_core/p1functionspace/P1ConstantOperator.hpp
@@ -1,28 +1,8 @@
 #pragma once
 
-#ifdef _MSC_VER
-#pragma warning( push, 0 )
-#endif
-
-#include "tinyhhg_core/fenics/fenics.hpp"
-#include "tinyhhg_core/p1functionspace/generated/p1_diffusion.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_div.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_divt.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_mass.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_pspg.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_stokes_epsilon.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_diffusion.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_div_tet.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_divt_tet.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_mass.h"
-#include "tinyhhg_core/p1functionspace/generated/p1_tet_pspg_tet.h"
-
-#ifdef _MSC_VER
-#pragma warning( pop )
-#endif
-
 #include <array>
 
+#include "tinyhhg_core/fenics/fenics.hpp"
 #include "tinyhhg_core/HHGDefinitions.hpp"
 #include "tinyhhg_core/Operator.hpp"
 #include "tinyhhg_core/p1functionspace/VertexDoFIndexing.hpp"
@@ -798,6 +778,28 @@ class P1ConstantOperator : public Operator< P1Function< real_t >, P1Function< re
       }
    }
 };
+
+class p1_diffusion_cell_integral_0_otherwise;
+class p1_tet_diffusion_cell_integral_0_otherwise;
+class p1_stokes_epsilon_cell_integral_0_otherwise;
+class p1_stokes_epsilon_cell_integral_1_otherwise;
+class p1_stokes_epsilon_cell_integral_2_otherwise;
+class p1_stokes_epsilon_cell_integral_3_otherwise;
+class p1_div_cell_integral_0_otherwise;
+class p1_tet_div_tet_cell_integral_0_otherwise;
+class p1_div_cell_integral_1_otherwise;
+class p1_tet_div_tet_cell_integral_1_otherwise;
+class p1_tet_div_tet_cell_integral_2_otherwise;
+
+class p1_divt_cell_integral_0_otherwise;
+class p1_tet_divt_tet_cell_integral_0_otherwise;
+class p1_divt_cell_integral_1_otherwise;
+class p1_tet_divt_tet_cell_integral_1_otherwise;
+class p1_tet_divt_tet_cell_integral_2_otherwise;
+class p1_mass_cell_integral_0_otherwise;
+class p1_tet_mass_cell_integral_0_otherwise;
+class p1_pspg_cell_integral_0_otherwise;
+class p1_tet_pspg_tet_cell_integral_0_otherwise;
 
 typedef P1ConstantOperator< fenics::NoAssemble, fenics::NoAssemble > P1ZeroOperator;
 
