c7a40a1a72e7e9638c28f05335e14221eab42746
Nils Kohl <nils.kohl@fau.de>
2018-10-24 23:38:18 +0200

    Added Point3D conversion functions to convert between cartesian and spherical coordinates

--- src/tinyhhg_core/Math.hpp
+++ src/tinyhhg_core/Math.hpp
@@ -44,6 +44,29 @@ constexpr uint_t binomialCoefficient(uint_t n, uint_t k)
   return out;
 }
 
+/// Returns the given point from cartesian coordinates to spherical coordinates.
+/// (x, y, z) -> (r, theta, phi) / (radius, inclination, azimuth)
+inline Point3D toSpherical( const Point3D & pointInCartesianCoordinates )
+{
+   const real_t r = pointInCartesianCoordinates.norm();
+   const real_t theta = pointInCartesianCoordinates[2] / r;
+   const real_t phi = std::atan2( pointInCartesianCoordinates[1], pointInCartesianCoordinates[0] );
+   return Point3D( { r, theta, phi } );
+}
+
+/// Returns the given point from spherical coordinates to cartesian coordinates.
+/// (r, theta, phi) / (radius, inclination, azimuth) -> (x, y, z)
+inline Point3D toCartesian( const Point3D & pointInSphericalCoordinates )
+{
+    const real_t r     = pointInSphericalCoordinates[0];
+    const real_t theta = pointInSphericalCoordinates[1];
+    const real_t phi   = pointInSphericalCoordinates[2];
+    const real_t x = r * std::sin( theta ) * std::cos( phi );
+    const real_t y = r * std::sin( theta ) * std::sin( phi );
+    const real_t z = r * std::cos( theta );
+    return Point3D( { x, y, z } );
+}
+
 } // namespace math
 } // namespace hhg
 
