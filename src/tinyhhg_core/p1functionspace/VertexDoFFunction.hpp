8447eabaa4c51afb0bb5cbbb16d51ef85e947193
Nils Kohl <nils.kohl@fau.de>
2018-12-04 16:33:16 +0100

    Stokes functions are now also friends of VertexDoFFunction

--- src/tinyhhg_core/p1functionspace/VertexDoFFunction.hpp
+++ src/tinyhhg_core/p1functionspace/VertexDoFFunction.hpp
@@ -192,8 +192,10 @@ class VertexDoFFunction : public Function< VertexDoFFunction< ValueType > >
 
    DoFType boundaryTypeToSkipDuringAdditiveCommunication_;
 
-   /// friend P2Function for usage of enumerate
+   /// friend Stokes and P2Function for usage of enumerate
    friend class P2Function< ValueType >;
+   friend class P1StokesFunction< ValueType >;
+   friend class P2P1TaylorHoodFunction< ValueType >;
 };
 
 inline void projectMean( VertexDoFFunction< real_t >& pressure, VertexDoFFunction< real_t >& tmp, uint_t level )
