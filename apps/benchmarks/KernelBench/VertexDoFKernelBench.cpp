8ab1bf07d4aeb1e7f2a4dc07aba9997bda8e2f5e
Dominik Thoennes <dominik.thoennes@fau.de>
2018-12-17 13:28:42 +0100

    improved benchmark

--- apps/benchmarks/KernelBench/VertexDoFKernelBench.cpp
+++ apps/benchmarks/KernelBench/VertexDoFKernelBench.cpp
@@ -1,19 +1,19 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 #include "core/timing/Timer.h"
 
 #include "tinyhhg_core/LikwidWrapper.hpp"
+#include "tinyhhg_core/misc/dummy.hpp"
 #include "tinyhhg_core/p1functionspace/generatedKernels/apply_2D_macroface_vertexdof_to_vertexdof_add.cpp"
 //#include "tinyhhg_core/p1functionspace/generatedKernels/GeneratedKernels.hpp"
 
-int main( int argc, char* argv[] )
+int main( int, char** )
 {
-   LIKWID_MARKER_INIT
+   walberla::WcTimer timerSpecial;
+   walberla::WcTimer timerAny;
 
-   walberla::WcTimer timer;
-
-   for( size_t level = 2; level < 14; ++level )
+   for( size_t level = 2; level < 15; ++level )
    {
       size_t edgeSize = (size_t) std::pow( 2u, level ) + 1;
       size_t faceSize = ( size_t )( edgeSize * ( edgeSize + 1u ) ) / 2;
@@ -25,22 +25,40 @@ int main( int argc, char* argv[] )
       std::vector< double > stencil( 7 );
       std::generate( stencil.begin(), stencil.end(), std::rand );
 
-      timer.reset();
+      double timeSpecial, timeAny;
 
-      hhg::vertexdof::macroface::generated::apply_2D_macroface_vertexdof_to_vertexdof_add(
-          dst.data(), src.data(), stencil.data(), (int64_t) level );
+      size_t iter = 2;
+      while( timerSpecial.total() < 0.5 )
+      {
+         timerSpecial.reset();
+         for( size_t i = 0; i < iter; ++i )
+         {
+            hhg::vertexdof::macroface::generated::apply_2D_macroface_vertexdof_to_vertexdof_add(
+                dst.data(), src.data(), stencil.data(), (int64_t) level );
+            hhg::misc::dummy( dst.data(), src.data() );
+         }
+         timerSpecial.end();
 
-      timer.end();
-      double time1 = timer.last();
-      timer.reset();
+         timerAny.reset();
+         for( size_t i = 0; i < iter; ++i )
+         {
+            hhg::vertexdof::macroface::generated::apply_2D_macroface_vertexdof_to_vertexdof_add_level_any(
+                dst.data(), src.data(), stencil.data(), (int64_t) level );
+            hhg::misc::dummy( dst.data(), src.data() );
+         }
+         timerAny.end();
 
-      hhg::vertexdof::macroface::generated::apply_2D_macroface_vertexdof_to_vertexdof_add_level_any(
-          dst.data(), src.data(), stencil.data(), (int64_t) level );
+         timeSpecial = timerSpecial.total();
+         timeAny     = timerAny.total();
+         iter *= 2;
+      }
+      iter /= 2;
 
-      timer.end();
-      double time2 = timer.last();
+      timerSpecial.reset();
+      timerAny.reset();
 
-      std::cout << "special version: " << time1 << " any version: " << time2 << " speedup: " << time2 / time1 << std::endl;
-
+      std::cout << "Level: " << level << " special version: " << timeSpecial / (double) iter
+                << " any version: " << timeAny / (double) iter << " speedup (Any/special): " << timeAny / timeSpecial
+                << std::endl;
    }
 }
