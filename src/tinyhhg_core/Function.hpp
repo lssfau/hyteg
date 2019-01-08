7065b23dd853f784b3df5c5f660ddec61787407c
Nils Kohl <nils.kohl@fau.de>
2019-01-08 13:07:35 +0100

    Added typedefs Tag and ValueType to Function class

--- src/tinyhhg_core/Function.hpp
+++ src/tinyhhg_core/Function.hpp
@@ -21,6 +21,9 @@ template< typename FunctionType >
 class Function {
 public:
 
+  typedef typename FunctionTrait< FunctionType >::Tag Tag;
+  typedef typename FunctionTrait< FunctionType >::ValueType ValueType;
+
   Function( std::string name, const std::shared_ptr<PrimitiveStorage> & storage ) : functionName_(std::move(name)), storage_( storage ), minLevel_( 0 ), maxLevel_( 0 ), isDummy_( true ) {}
 
   Function(std::string name, const std::shared_ptr<PrimitiveStorage> & storage, uint_t minLevel, uint_t maxLevel)
