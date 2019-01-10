b3cdf912eb8426f47437453833f6b410bb21ab00
Nils Kohl <nils.kohl@fau.de>
2019-01-10 18:12:06 +0100

    Minor: removed logging

--- src/tinyhhg_core/petsc/PETScSparseMatrix.hpp
+++ src/tinyhhg_core/petsc/PETScSparseMatrix.hpp
@@ -91,7 +91,7 @@ public:
     MatTranspose(mat, MAT_INITIAL_MATRIX, &B);
     MatAYPX(B, -1.0, mat, DIFFERENT_NONZERO_PATTERN);
     MatNorm(B, NORM_INFINITY, &norm);
-    WALBERLA_LOG_DEVEL_ON_ROOT( "PETSC_NORM = " << norm );
+    // WALBERLA_LOG_DEVEL_ON_ROOT( "PETSC_NORM = " << norm );
     MatDestroy( &B );
     return norm < tol;
   }
