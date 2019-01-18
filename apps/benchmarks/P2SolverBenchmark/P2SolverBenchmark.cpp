cd4811a09743e3b6b95bdd5963e8a7b6527b95da
Nils Kohl <nils.kohl@fau.de>
2019-01-18 15:45:13 +0100

    Formatting and option to switch off solver by setting iterations to zero

--- apps/benchmarks/P2SolverBenchmark/P2SolverBenchmark.cpp
+++ apps/benchmarks/P2SolverBenchmark/P2SolverBenchmark.cpp
@@ -1,16 +1,16 @@
 #include "core/Environment.h"
 
 #include "tinyhhg_core/LikwidWrapper.hpp"
+#include "tinyhhg_core/VTKWriter.hpp"
+#include "tinyhhg_core/gridtransferoperators/P2toP2QuadraticProlongation.hpp"
+#include "tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.hpp"
 #include "tinyhhg_core/p2functionspace/P2ConstantOperator.hpp"
 #include "tinyhhg_core/p2functionspace/P2Function.hpp"
 #include "tinyhhg_core/primitivestorage/SetupPrimitiveStorage.hpp"
 #include "tinyhhg_core/solvers/CGSolver.hpp"
-#include "tinyhhg_core/solvers/MinresSolver.hpp"
-#include "tinyhhg_core/solvers/GeometricMultigridSolver.hpp"
 #include "tinyhhg_core/solvers/GaussSeidelSmoother.hpp"
-#include "tinyhhg_core/gridtransferoperators/P2toP2QuadraticProlongation.hpp"
-#include "tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.hpp"
-#include "tinyhhg_core/VTKWriter.hpp"
+#include "tinyhhg_core/solvers/GeometricMultigridSolver.hpp"
+#include "tinyhhg_core/solvers/MinresSolver.hpp"
 
 using walberla::real_c;
 using walberla::real_t;
@@ -26,22 +26,24 @@ int main( int argc, char** argv )
    walberla::Environment env( argc, argv );
    walberla::MPIManager::instance()->useWorldComm();
 
-   auto timingTree = std::make_shared< walberla::WcTimingTree >();
+   auto              timingTree = std::make_shared< walberla::WcTimingTree >();
    walberla::WcTimer timer;
 
    //check if a config was given on command line or load default file otherwise
-   auto cfg = std::make_shared<walberla::config::Config>();
-   if( env.config() == nullptr ) {
+   auto cfg = std::make_shared< walberla::config::Config >();
+   if( env.config() == nullptr )
+   {
       auto defaultFile = "./P2SolverBenchmark.prm";
-      WALBERLA_LOG_PROGRESS_ON_ROOT("No Parameter file given loading default parameter file: " << defaultFile);
+      WALBERLA_LOG_PROGRESS_ON_ROOT( "No Parameter file given loading default parameter file: " << defaultFile );
       cfg->readParameterFile( defaultFile );
-   } else {
+   } else
+   {
       cfg = env.config();
    }
    const walberla::Config::BlockHandle mainConf = cfg->getBlock( "Parameters" );
 
-   const uint_t level              = mainConf.getParameter< uint_t >( "level" );
-   const uint_t minLevel           = mainConf.getParameter< uint_t >( "minLevel" );
+   const uint_t level    = mainConf.getParameter< uint_t >( "level" );
+   const uint_t minLevel = mainConf.getParameter< uint_t >( "minLevel" );
 
    const uint_t cgIterations       = mainConf.getParameter< uint_t >( "cgIterations" );
    const uint_t minresIterations   = mainConf.getParameter< uint_t >( "minresIterations" );
@@ -49,25 +51,25 @@ int main( int argc, char** argv )
    const uint_t preSmoothingSteps  = mainConf.getParameter< uint_t >( "preSmoothingSteps" );
    const uint_t postSmoothingSteps = mainConf.getParameter< uint_t >( "postSmoothingSteps" );
 
-   const std::string meshFile      = mainConf.getParameter< std::string >( "mesh" );
+   const std::string meshFile = mainConf.getParameter< std::string >( "mesh" );
 
    const bool checkError = mainConf.getParameter< bool >( "checkError" );
 
    LIKWID_MARKER_THREADINIT;
 
-   MeshInfo                            meshInfo = MeshInfo::fromGmshFile( meshFile );
-   SetupPrimitiveStorage               setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   MeshInfo              meshInfo = MeshInfo::fromGmshFile( meshFile );
+   SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
    setupStorage.setMeshBoundaryFlagsOnBoundary( 1, 0, true );
    std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage, timingTree );
 
-   auto storageInfo = storage->getGlobalInfo();
-   auto numVertexDoFs = numberOfGlobalDoFs< VertexDoFFunctionTag >( *storage, level );
-   auto numEdgeDoFs   = numberOfGlobalDoFs< EdgeDoFFunctionTag >( *storage, level );
+   auto storageInfo    = storage->getGlobalInfo();
+   auto numVertexDoFs  = numberOfGlobalDoFs< VertexDoFFunctionTag >( *storage, level );
+   auto numEdgeDoFs    = numberOfGlobalDoFs< EdgeDoFFunctionTag >( *storage, level );
    auto numP2DoFsTotal = numberOfGlobalDoFs< P2FunctionTag >( *storage, level );
 
    WALBERLA_LOG_DEVEL_ON_ROOT( "" );
    WALBERLA_LOG_DEVEL_ON_ROOT( "=================================" );
-   WALBERLA_LOG_DEVEL_ON_ROOT( "===== P2 Solver Benchmark =====" );
+   WALBERLA_LOG_DEVEL_ON_ROOT( "====== P2 Solver Benchmark ======" );
    WALBERLA_LOG_DEVEL_ON_ROOT( "=================================" );
    WALBERLA_LOG_DEVEL_ON_ROOT( "" );
 
@@ -80,16 +82,17 @@ int main( int argc, char** argv )
    WALBERLA_LOG_INFO_ON_ROOT( "iterations CG:            " << cgIterations );
    WALBERLA_LOG_INFO_ON_ROOT( "iterations MinRes:        " << minresIterations );
    WALBERLA_LOG_INFO_ON_ROOT( "iterations MG (V-cycles): " << vCycles );
-   WALBERLA_LOG_INFO_ON_ROOT( "MG smoothing (pre/post):  " << "(" << preSmoothingSteps << ", " << postSmoothingSteps << ")" );
+   WALBERLA_LOG_INFO_ON_ROOT( "MG smoothing (pre/post):  "
+                              << "(" << preSmoothingSteps << ", " << postSmoothingSteps << ")" );
 
    WALBERLA_LOG_INFO_ON_ROOT( "=== Starting measurements ===" );
 
-   P2Function< real_t > u    ( "u",     storage, minLevel, level );
-   P2Function< real_t > f    ( "f",     storage, minLevel, level );
-   P2Function< real_t > r    ( "r",     storage, minLevel, level );
+   P2Function< real_t > u( "u", storage, minLevel, level );
+   P2Function< real_t > f( "f", storage, minLevel, level );
+   P2Function< real_t > r( "r", storage, minLevel, level );
    P2Function< real_t > error( "error", storage, minLevel, level );
    P2Function< real_t > exact( "exact", storage, minLevel, level );
-   P2Function< real_t > tmp  ( "tmp",   storage, minLevel, level );
+   P2Function< real_t > tmp( "tmp", storage, minLevel, level );
 
    hhg::P2ConstantLaplaceOperator A( storage, minLevel, level );
    hhg::P2ConstantMassOperator    M( storage, level, level );
@@ -101,13 +104,11 @@ int main( int argc, char** argv )
    vtkOutput.add( error );
    vtkOutput.add( exact );
 
-   std::function< real_t( const hhg::Point3D& ) > exactSolution = []( const hhg::Point3D& x )
-   {
-       return ( 1.0 / 2.0 ) * sin( 2 * x[0] ) * sinh( x[1] );
+   std::function< real_t( const hhg::Point3D& ) > exactSolution = []( const hhg::Point3D& x ) {
+      return ( 1.0 / 2.0 ) * sin( 2 * x[0] ) * sinh( x[1] );
    };
-   std::function< real_t( const hhg::Point3D& ) > rhsFunction = []( const hhg::Point3D& x )
-   {
-       return ( 3.0 / 2.0 ) * sin( 2 * x[0] ) * sinh( x[1] );
+   std::function< real_t( const hhg::Point3D& ) > rhsFunction = []( const hhg::Point3D& x ) {
+      return ( 3.0 / 2.0 ) * sin( 2 * x[0] ) * sinh( x[1] );
    };
    std::function< real_t( const hhg::Point3D& ) > ones = []( const hhg::Point3D& ) { return 1.0; };
 
@@ -116,7 +117,7 @@ int main( int argc, char** argv )
    tmp.interpolate( rhsFunction, level );
    M.apply( tmp, f, level, hhg::All );
 
-   if ( checkError )
+   if( checkError )
    {
       vtkOutput.write( level, 0 );
    }
@@ -125,97 +126,113 @@ int main( int argc, char** argv )
    // CG //
    ////////
 
-   u.interpolate( real_c(0), level, Inner );
-   CGSolver< P2ConstantLaplaceOperator > cgSolver( storage, level, level, cgIterations );
-
-   LIKWID_MARKER_START( "cg solver" );
-   timer.reset();
-   cgSolver.solve( A, u, f, level );
-   timer.end();
-   LIKWID_MARKER_STOP( "cg solver" );
-   WALBERLA_LOG_INFO_ON_ROOT( "cg solver:        " << timer.last() );
-
-   if ( checkError )
+   if( cgIterations > 0 )
    {
-      tmp.interpolate( real_c(0), level );
-      A.apply( u, tmp, level, Inner );
-      r.assign( {1.0, -1.0}, {f, tmp}, level );
-      error.assign( {1.0, -1.0}, {u, exact}, level );
-      const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c(numP2DoFsTotal) );
-      const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c(numP2DoFsTotal) );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
-      vtkOutput.write( level, 1 );
+      u.interpolate( real_c( 0 ), level, Inner );
+      CGSolver< P2ConstantLaplaceOperator > cgSolver( storage, level, level, cgIterations );
+
+      LIKWID_MARKER_START( "cg solver" );
+      timer.reset();
+      cgSolver.solve( A, u, f, level );
+      timer.end();
+      LIKWID_MARKER_STOP( "cg solver" );
+      WALBERLA_LOG_INFO_ON_ROOT( "cg solver:        " << timer.last() );
+
+      if( checkError )
+      {
+         tmp.interpolate( real_c( 0 ), level );
+         A.apply( u, tmp, level, Inner );
+         r.assign( {1.0, -1.0}, {f, tmp}, level );
+         error.assign( {1.0, -1.0}, {u, exact}, level );
+         const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c( numP2DoFsTotal ) );
+         const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c( numP2DoFsTotal ) );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
+         vtkOutput.write( level, 1 );
+      }
    }
 
    ////////////
    // MinRes //
    ////////////
 
-   u.interpolate( real_c(0), level, Inner );
-   MinResSolver< P2ConstantLaplaceOperator > minresSolver( storage, level, level, minresIterations );
-
-   LIKWID_MARKER_START( "minres solver" );
-   timer.reset();
-   minresSolver.solve( A, u, f, level );
-   timer.end();
-   LIKWID_MARKER_STOP( "minres solver" );
-   WALBERLA_LOG_INFO_ON_ROOT( "minres solver:    " << timer.last() );
-
-   if ( checkError )
+   if( minresIterations > 0 )
    {
-      tmp.interpolate( real_c(0), level );
-      A.apply( u, tmp, level, Inner );
-      r.assign( {1.0, -1.0}, {f, tmp}, level );
-      error.assign( {1.0, -1.0}, {u, exact}, level );
-      const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c(numP2DoFsTotal) );
-      const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c(numP2DoFsTotal) );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
-      vtkOutput.write( level, 2 );
+      u.interpolate( real_c( 0 ), level, Inner );
+      MinResSolver< P2ConstantLaplaceOperator > minresSolver( storage, level, level, minresIterations );
+
+      LIKWID_MARKER_START( "minres solver" );
+      timer.reset();
+      minresSolver.solve( A, u, f, level );
+      timer.end();
+      LIKWID_MARKER_STOP( "minres solver" );
+      WALBERLA_LOG_INFO_ON_ROOT( "minres solver:    " << timer.last() );
+
+      if( checkError )
+      {
+         tmp.interpolate( real_c( 0 ), level );
+         A.apply( u, tmp, level, Inner );
+         r.assign( {1.0, -1.0}, {f, tmp}, level );
+         error.assign( {1.0, -1.0}, {u, exact}, level );
+         const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c( numP2DoFsTotal ) );
+         const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c( numP2DoFsTotal ) );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
+         vtkOutput.write( level, 2 );
+      }
    }
 
    ////////
    // MG //
    ////////
 
-   u.interpolate( real_c(0), level, Inner );
-   auto smoother             = std::make_shared< GaussSeidelSmoother< P2ConstantLaplaceOperator > >();
-   auto coarseGridSolver     = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( storage, minLevel, minLevel );
-   auto restrictionOperator  = std::make_shared< P2toP2QuadraticRestriction >();
-   auto prolongationOperator = std::make_shared< P2toP2QuadraticProlongation >();
-   GeometricMultigridSolver< P2ConstantLaplaceOperator > gmgSolver( storage, smoother, coarseGridSolver,
-                                                                    restrictionOperator, prolongationOperator,
-                                                                    minLevel, level, preSmoothingSteps, postSmoothingSteps, 0 );
-
-   LIKWID_MARKER_START( "multigrid solver" );
-   timer.reset();
-   for ( uint_t vCycle = 0; vCycle < vCycles; vCycle++ )
+   if( vCycles > 0 )
    {
-      gmgSolver.solve( A, u, f, level );
-   }
-   timer.end();
-   LIKWID_MARKER_STOP( "multigrid solver" );
-   WALBERLA_LOG_INFO_ON_ROOT( "multigrid solver: " << timer.last() );
+      u.interpolate( real_c( 0 ), level, Inner );
+      auto smoother             = std::make_shared< GaussSeidelSmoother< P2ConstantLaplaceOperator > >();
+      auto coarseGridSolver     = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( storage, minLevel, minLevel );
+      auto restrictionOperator  = std::make_shared< P2toP2QuadraticRestriction >();
+      auto prolongationOperator = std::make_shared< P2toP2QuadraticProlongation >();
+      GeometricMultigridSolver< P2ConstantLaplaceOperator > gmgSolver( storage,
+                                                                       smoother,
+                                                                       coarseGridSolver,
+                                                                       restrictionOperator,
+                                                                       prolongationOperator,
+                                                                       minLevel,
+                                                                       level,
+                                                                       preSmoothingSteps,
+                                                                       postSmoothingSteps,
+                                                                       0 );
 
-   if ( checkError )
-   {
-      tmp.interpolate( real_c(0), level );
-      A.apply( u, tmp, level, Inner );
-      r.assign( {1.0, -1.0}, {f, tmp}, level );
-      error.assign( {1.0, -1.0}, {u, exact}, level );
-      const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c(numP2DoFsTotal) );
-      const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c(numP2DoFsTotal) );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
-      WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
-      vtkOutput.write( level, 3 );
+      LIKWID_MARKER_START( "multigrid solver" );
+      timer.reset();
+      for( uint_t vCycle = 0; vCycle < vCycles; vCycle++ )
+      {
+         gmgSolver.solve( A, u, f, level );
+      }
+      timer.end();
+      LIKWID_MARKER_STOP( "multigrid solver" );
+      WALBERLA_LOG_INFO_ON_ROOT( "multigrid solver: " << timer.last() );
+
+      if( checkError )
+      {
+         tmp.interpolate( real_c( 0 ), level );
+         A.apply( u, tmp, level, Inner );
+         r.assign( {1.0, -1.0}, {f, tmp}, level );
+         error.assign( {1.0, -1.0}, {u, exact}, level );
+         const real_t residualL2 = std::sqrt( r.dotGlobal( r, level ) / real_c( numP2DoFsTotal ) );
+         const real_t errorL2    = std::sqrt( error.dotGlobal( error, level ) / real_c( numP2DoFsTotal ) );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 residual: " << residualL2 );
+         WALBERLA_LOG_INFO_ON_ROOT( "L2 error:    " << errorL2 );
+         vtkOutput.write( level, 3 );
+      }
    }
 
    auto timingTreeReducedWithRemainder = timingTree->getReduced().getCopyWithRemainder();
    WALBERLA_LOG_INFO_ON_ROOT( timingTreeReducedWithRemainder );
 
    nlohmann::json ttjson = nlohmann::json( timingTreeReducedWithRemainder );
-   std::ofstream o("P2SolverBenchmark.json");
+   std::ofstream  o( "P2SolverBenchmark.json" );
    o << ttjson;
    o.close();
 
