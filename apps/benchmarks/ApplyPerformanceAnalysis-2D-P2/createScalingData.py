02dfe8020f26aceda595c041796abc1601c75084
Dominik Thoennes <dominik.thoennes@fau.de>
2019-01-23 21:50:32 +0100

    improve data creating script

--- apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createScalingData.py
+++ apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createScalingData.py
@@ -6,19 +6,13 @@ import collections
 
 
 def main():
-
-    #procs = range(20)
-    totalProcs = range(2,20)
-
-    # if "emmy" in socket.gethostname():
-    #     subprocess.call(["source", "~/script/modules/gcc.emmy.modules"])
+    totalProcs = range(2,41)
 
     f = open('scalingData.txt','w')
-
     for i in totalProcs:
-        subprocess.call(["likwid-mpirun", "-np", str(i), "-g", "MEM_DP", "-m", "-O",
-                         "./ApplyPerformanceAnalysis-2D-P2", "ApplyPerformanceAnalysis-2D-P2.prm",
-                         "-Parameters.level=11"], stdout=f)
+        #if i is 33:
+            continue
+        subprocess.run(["likwid-mpirun", "-np", str(i), "-g", "MEM_DP", "-m", "-O","./ApplyPerformanceAnalysis-2D-P2", "ApplyPerformanceAnalysis-2D-P2.prm", "-Parameters.level=11"], stdout=f)
     f.close()
 
 if __name__ == "__main__":
