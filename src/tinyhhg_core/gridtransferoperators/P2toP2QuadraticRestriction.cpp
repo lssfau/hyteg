a00b3aadf350913afd78ef2299756d2ff3b70760
Nils Kohl <nils.kohl@fau.de>
2019-01-30 14:57:45 +0100

    Replaced old P2 to P2 restriction with new, additive, generated version.
    The old implementation was not implemented correctly at the macro-vertices yet.
    This is fixed now with the additive version.
    
    The old implementation was not removed but only deactivated.

---
+++ src/tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.cpp
@@ -0,0 +1,138 @@
+
+#include "tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.hpp"
+
+#include "tinyhhg_core/gridtransferoperators/generatedKernels/GeneratedKernelsP2MacroFace2D.hpp"
+
+namespace hhg {
+
+using indexing::Index;
+using indexing::IndexIncrement;
+
+void P2toP2QuadraticRestriction::restrictAdditively( const P2Function< real_t >& function,
+                                                     const uint_t&               sourceLevel,
+                                                     const DoFType& ) const
+{
+   const auto storage = function.getStorage();
+
+   const uint_t fineLevel   = sourceLevel;
+   const uint_t coarseLevel = sourceLevel - 1;
+
+   function.communicate< Vertex, Edge >( fineLevel );
+   function.communicate< Edge, Face >( fineLevel );
+
+   for ( const auto& faceIt : function.getStorage()->getFaces() )
+   {
+      const auto face = faceIt.second;
+
+      const auto vertexFineData = face->getData( function.getVertexDoFFunction().getFaceDataID() )->getPointer( fineLevel );
+      const auto edgeFineData   = face->getData( function.getEdgeDoFFunction().getFaceDataID() )->getPointer( fineLevel );
+
+      auto vertexCoarseData = face->getData( function.getVertexDoFFunction().getFaceDataID() )->getPointer( coarseLevel );
+      auto edgeCoarseData   = face->getData( function.getEdgeDoFFunction().getFaceDataID() )->getPointer( coarseLevel );
+
+      const double numNeighborFacesEdge0 =
+          static_cast< double >( storage->getEdge( face->neighborEdges().at( 0 ) )->getNumNeighborFaces() );
+      const double numNeighborFacesEdge1 =
+          static_cast< double >( storage->getEdge( face->neighborEdges().at( 1 ) )->getNumNeighborFaces() );
+      const double numNeighborFacesEdge2 =
+          static_cast< double >( storage->getEdge( face->neighborEdges().at( 2 ) )->getNumNeighborFaces() );
+      const double numNeighborFacesVertex0 =
+          static_cast< double >( storage->getVertex( face->neighborVertices().at( 0 ) )->getNumNeighborFaces() );
+      const double numNeighborFacesVertex1 =
+          static_cast< double >( storage->getVertex( face->neighborVertices().at( 1 ) )->getNumNeighborFaces() );
+      const double numNeighborFacesVertex2 =
+          static_cast< double >( storage->getVertex( face->neighborVertices().at( 2 ) )->getNumNeighborFaces() );
+
+      P2::macroface::generated::restrict_2D_macroface_P2_update_vertexdofs( edgeFineData,
+                                                                            vertexCoarseData,
+                                                                            vertexFineData,
+                                                                            coarseLevel,
+                                                                            numNeighborFacesEdge0,
+                                                                            numNeighborFacesEdge1,
+                                                                            numNeighborFacesEdge2,
+                                                                            numNeighborFacesVertex0,
+                                                                            numNeighborFacesVertex1,
+                                                                            numNeighborFacesVertex2 );
+
+      P2::macroface::generated::restrict_2D_macroface_P2_update_edgedofs( edgeCoarseData,
+                                                                          edgeFineData,
+                                                                          vertexFineData,
+                                                                          coarseLevel,
+                                                                          numNeighborFacesEdge0,
+                                                                          numNeighborFacesEdge1,
+                                                                          numNeighborFacesEdge2 );
+   }
+
+   function.getVertexDoFFunction().communicateAdditively< Face, Edge >( coarseLevel );
+   function.getVertexDoFFunction().communicateAdditively< Face, Vertex >( coarseLevel );
+
+   function.getEdgeDoFFunction().communicateAdditively< Face, Edge >( coarseLevel );
+}
+
+void P2toP2QuadraticRestriction::restrictWithPostCommunication( const hhg::P2Function< walberla::real_t >& function,
+                                                                const uint_t&                              sourceLevel,
+                                                                const hhg::DoFType&                        flag ) const
+{
+   const auto  storage           = function.getStorage();
+   const auto& vertexDoFFunction = function.getVertexDoFFunction();
+   const auto& edgeDoFFunction   = function.getEdgeDoFFunction();
+   const auto  boundaryCondition = function.getBoundaryCondition();
+
+   edgeDoFFunction.template communicate< Vertex, Edge >( sourceLevel );
+   edgeDoFFunction.template communicate< Edge, Face >( sourceLevel );
+
+   for ( const auto& it : storage->getFaces() )
+   {
+      const Face& face = *it.second;
+
+      const DoFType faceBC = boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag() );
+      if ( testFlag( faceBC, flag ) )
+      {
+         P2::macroface::restrict< real_t >(
+             sourceLevel, face, vertexDoFFunction.getFaceDataID(), edgeDoFFunction.getFaceDataID() );
+      }
+   }
+
+   /// sync the vertex dofs which contain the missing edge dofs
+   edgeDoFFunction.template communicate< Face, Edge >( sourceLevel );
+
+   /// remove the temporary updates
+   for ( const auto& it : storage->getFaces() )
+   {
+      const Face& face = *it.second;
+
+      const DoFType faceBC = boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag() );
+      if ( testFlag( faceBC, flag ) )
+      {
+         P2::macroface::postRestrict< real_t >(
+             sourceLevel, face, vertexDoFFunction.getFaceDataID(), edgeDoFFunction.getFaceDataID() );
+      }
+   }
+
+   for ( const auto& it : storage->getEdges() )
+   {
+      const Edge& edge = *it.second;
+
+      const DoFType edgeBC = boundaryCondition.getBoundaryType( edge.getMeshBoundaryFlag() );
+      if ( testFlag( edgeBC, flag ) )
+      {
+         P2::macroedge::restrict< real_t >(
+             sourceLevel, edge, vertexDoFFunction.getEdgeDataID(), edgeDoFFunction.getEdgeDataID() );
+      }
+   }
+
+   //TODO: add real vertex restrict
+   for ( const auto& it : storage->getVertices() )
+   {
+      const Vertex& vertex = *it.second;
+
+      const DoFType vertexBC = boundaryCondition.getBoundaryType( vertex.getMeshBoundaryFlag() );
+      if ( testFlag( vertexBC, flag ) )
+      {
+         P2::macrovertex::restrictInjection< real_t >(
+             sourceLevel, vertex, vertexDoFFunction.getVertexDataID(), edgeDoFFunction.getVertexDataID() );
+      }
+   }
+}
+
+} // namespace hhg
--- src/tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.hpp
+++ src/tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.hpp
@@ -1,80 +1,24 @@
 
 #pragma once
 
-#include "tinyhhg_core/p2functionspace/P2Function.hpp"
 #include "tinyhhg_core/gridtransferoperators/RestrictionOperator.hpp"
+#include "tinyhhg_core/p2functionspace/P2Function.hpp"
 
 namespace hhg {
 
 class P2toP2QuadraticRestriction : public RestrictionOperator< P2Function< real_t > >
 {
-public:
+ public:
+   inline void restrict( const P2Function< real_t >& function, const uint_t& sourceLevel, const DoFType& flag ) const override
+   {
+      restrictAdditively( function, sourceLevel, flag );
+   }
 
-    inline void restrict( const P2Function< real_t > & function, const uint_t & sourceLevel, const DoFType & flag ) const override
-    {
-        const auto storage = function.getStorage();
-        const auto& vertexDoFFunction = function.getVertexDoFFunction();
-        const auto& edgeDoFFunction = function.getEdgeDoFFunction();
-        const auto boundaryCondition = function.getBoundaryCondition();
-
-        edgeDoFFunction.template communicate< Vertex, Edge >( sourceLevel );
-        edgeDoFFunction.template communicate< Edge, Face >( sourceLevel );
-
-        for( const auto& it : storage->getFaces() )
-        {
-            const Face& face = *it.second;
-
-            const DoFType faceBC = boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag() );
-            if( testFlag( faceBC, flag ) )
-            {
-                P2::macroface::restrict< real_t >(
-                sourceLevel, face, vertexDoFFunction.getFaceDataID(), edgeDoFFunction.getFaceDataID() );
-            }
-        }
-
-        /// sync the vertex dofs which contain the missing edge dofs
-        edgeDoFFunction.template communicate< Face, Edge >( sourceLevel );
-
-        /// remove the temporary updates
-        for( const auto& it : storage->getFaces() )
-        {
-            const Face& face = *it.second;
-
-            const DoFType faceBC = boundaryCondition.getBoundaryType( face.getMeshBoundaryFlag() );
-            if( testFlag( faceBC, flag ) )
-            {
-                P2::macroface::postRestrict< real_t >(
-                sourceLevel, face, vertexDoFFunction.getFaceDataID(), edgeDoFFunction.getFaceDataID() );
-            }
-        }
-
-        for( const auto& it : storage->getEdges() )
-        {
-            const Edge& edge = *it.second;
-
-            const DoFType edgeBC = boundaryCondition.getBoundaryType( edge.getMeshBoundaryFlag() );
-            if( testFlag( edgeBC, flag ) )
-            {
-                P2::macroedge::restrict< real_t >(
-                sourceLevel, edge, vertexDoFFunction.getEdgeDataID(), edgeDoFFunction.getEdgeDataID() );
-            }
-        }
-
-        //TODO: add real vertex restrict
-        for( const auto& it : storage->getVertices() )
-        {
-            const Vertex& vertex = *it.second;
-
-            const DoFType vertexBC = boundaryCondition.getBoundaryType( vertex.getMeshBoundaryFlag() );
-            if( testFlag( vertexBC, flag ) )
-            {
-                P2::macrovertex::restrictInjection< real_t >(
-                sourceLevel, vertex, vertexDoFFunction.getVertexDataID(), edgeDoFFunction.getVertexDataID() );
-            }
-        }
-    }
-
-
+ private:
+   void restrictWithPostCommunication( const P2Function< real_t >& function,
+                                       const uint_t&               sourceLevel,
+                                       const DoFType&              flag ) const;
+   void restrictAdditively( const P2Function< real_t >& function, const uint_t& sourceLevel, const DoFType& flag ) const;
 };
 
-}
+} // namespace hhg
