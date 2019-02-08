739590d2927d78f77e2ea35bb0d184f62ae45437
Dominik Thoennes <dominik.thoennes@fau.de>
2019-02-08 14:49:00 +0100

    fixed in level scaling script

--- apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createLevelScalingData.py
+++ apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createLevelScalingData.py
@@ -2,12 +2,13 @@ import subprocess
 
 
 def main():
-    alllevel = range(2, 14)
+    alllevel = range(2, 15)
 
     f = open('levelScalingData.txt', 'w')
     for level in alllevel:
-        subprocess.run(["likwid-perfctr", "-C", 1, "-g", "MEM_DP", "-m", "-O", "./ApplyPerformanceAnalysis-2D-P2",
-                        "ApplyPerformanceAnalysis-2D-P2.prm", "-Parameters.level=" + level], stdout=f)
+        levelstring =  str("-Parameters.level=" + str(level))
+        subprocess.run(["likwid-perfctr", "-C", "1", "-g", "MEM_DP", "-m", "-O", "--stats", "./ApplyPerformanceAnalysis-2D-P2",
+                        "ApplyPerformanceAnalysis-2D-P2.prm", levelstring], stdout=f)
     f.close()
 
 
