8ac40177feedece6b45405cda7a433d12522f58e
Nils Kohl <nils.kohl@fau.de>
2019-02-11 17:40:50 +0100

    Added isDummy() and constant interpolate() member to stokes functions

--- src/tinyhhg_core/composites/P2P1TaylorHoodFunction.hpp
+++ src/tinyhhg_core/composites/P2P1TaylorHoodFunction.hpp
@@ -39,12 +39,22 @@ public:
         , p(_name+"_p", storage, minLevel, maxLevel, BoundaryCondition::createAllInnerBC() )
   {}
 
+  bool isDummy() const { return false; }
+
   void interpolate(const std::function<real_t(const hhg::Point3D&)>& expr, size_t level, DoFType flag = All) const
   {
     u.interpolate(expr, level, flag);
     v.interpolate(expr, level, flag);
     w.interpolate(expr, level, flag);
     p.interpolate(expr, level, flag);
+  }
+
+  void interpolate( const real_t& constant, size_t level, DoFType flag = All ) const
+  {
+     u.interpolate( constant, level, flag );
+     v.interpolate( constant, level, flag );
+     w.interpolate( constant, level, flag );
+     p.interpolate( constant, level, flag );
   }
 
   void swap( const P2P1TaylorHoodFunction< ValueType > & other,
