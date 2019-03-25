d8b88fedab59872e256696c9afec73e887ec89b0
Dominik Thoennes <dominik.thoennes@fau.de>
2019-03-25 22:26:08 +0100

    add command line options to level scaling script

--- apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createLevelScalingData.py
+++ apps/benchmarks/ApplyPerformanceAnalysis-2D-P2/createLevelScalingData.py
@@ -1,16 +1,29 @@
 import subprocess
+import time
+import argparse
 
 
-def main():
-    alllevel = range(2, 15)
+def main(cores, minLevel, maxLevel):
+    datestamp = time.strftime("%y_%m_%d-%I:%M:%S")
+    f = open(datestamp + 'levelScalingData.txt', 'w')
 
-    f = open('levelScalingData.txt', 'w')
-    for level in alllevel:
-        levelstring =  str("-Parameters.level=" + str(level))
-        subprocess.run(["likwid-perfctr", "-C", "1", "-g", "MEM_DP", "-m", "-O", "--stats", "./ApplyPerformanceAnalysis-2D-P2",
-                        "ApplyPerformanceAnalysis-2D-P2.prm", levelstring], stdout=f)
+    if cores is 1:
+        for level in range(minLevel, maxLevel + 1):
+            levelstring =  str("-Parameters.level=" + str(level))
+            subprocess.run(["likwid-perfctr", "-C", "1", "-g", "MEM_DP", "-m", "-O", "--stats", "./ApplyPerformanceAnalysis-2D-P2",
+            "ApplyPerformanceAnalysis-2D-P2.prm", levelstring], stdout=f)
+    else:
+        for level in range(minLevel, maxLevel + 1):
+            levelstring =  str("-Parameters.level=" + str(level))
+            subprocess.run(["likwid-mpirun", "-n", str(cores), "-g", "MEM_DP", "-m", "-O", "./ApplyPerformanceAnalysis-2D-P2",
+                            "ApplyPerformanceAnalysis-2D-P2.prm", levelstring], stdout=f)
     f.close()
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cores", help="number of cores to run with")
+    parser.add_argument("minLevel", default=2, help="minimal scaling Level")
+    parser.add_argument("maxLevel", default=15, help="maximal scaling Level")
+    args = parser.parse_args()
     main()
