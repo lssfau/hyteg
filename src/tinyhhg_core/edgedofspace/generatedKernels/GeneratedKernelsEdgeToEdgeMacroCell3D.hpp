4124086dddd9a471351cc04c385e28341592a8f4
Nils Kohl <nils.kohl@fau.de>
2019-04-02 13:13:39 +0200

    First generated kernels for edge DoFs in 3D (assign)

---
+++ src/tinyhhg_core/edgedofspace/generatedKernels/GeneratedKernelsEdgeToEdgeMacroCell3D.hpp
@@ -0,0 +1,20 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// This file is generated! To fix issues, please fix them in the generator. //
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "core/DataTypes.h"
+
+namespace hhg {
+namespace edgedof {
+namespace macrocell {
+namespace generated {
+
+void assign_3D_macrocell_edgedof_1_rhs_function(double * _data_edgeCellDst_X, double * _data_edgeCellDst_XY, double * _data_edgeCellDst_XYZ, double * _data_edgeCellDst_XZ, double * _data_edgeCellDst_Y, double * _data_edgeCellDst_YZ, double * _data_edgeCellDst_Z, double * _data_edgeCellSrc_X, double * _data_edgeCellSrc_XY, double * _data_edgeCellSrc_XYZ, double * _data_edgeCellSrc_XZ, double * _data_edgeCellSrc_Y, double * _data_edgeCellSrc_YZ, double * _data_edgeCellSrc_Z, double c, int64_t level);
+
+} // namespace generated
+} // namespace macrocell
+} // namespace edgedof
+} // namespace hhg
