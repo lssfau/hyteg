9167f221ea5893cc2201bbe5544cf4266e8c3e54
Nils Kohl <nils.kohl@fau.de>
2019-04-05 15:15:00 +0200

    Generated vertex to edge and edge to vertex macro-cell apply kernels (only add)

---
+++ src/tinyhhg_core/mixedoperators/EdgeDoFToVertexDoFOperator/generatedKernels/GeneratedKernelsEdgeToVertexMacroCell3D.hpp
@@ -0,0 +1,22 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// This file is generated! To fix issues, please fix them in the generator. //
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "core/DataTypes.h"
+
+#include "tinyhhg_core/edgedofspace/EdgeDoFIndexing.hpp"
+
+#include <map>
+
+namespace hhg {
+namespace EdgeDoFToVertexDoF {
+namespace generated {
+
+void apply_3D_macrocell_edgedof_to_vertexdof_add(double * _data_edgeCellSrc_X, double * _data_edgeCellSrc_XY, double * _data_edgeCellSrc_XYZ, double * _data_edgeCellSrc_XZ, double * _data_edgeCellSrc_Y, double * _data_edgeCellSrc_YZ, double * _data_edgeCellSrc_Z, double * _data_vertexCellDst, std::map< hhg::edgedof::EdgeDoFOrientation, std::map< hhg::indexing::IndexIncrement, double > > e2vStencilMap, int64_t level);
+
+} // namespace generated
+} // namespace EdgeDoFToVertexDoF
+} // namespace hhg
---
+++ src/tinyhhg_core/mixedoperators/VertexDoFToEdgeDoFOperator/generatedKernels/GeneratedKernelsVertexToEdgeMacroCell3D.hpp
@@ -0,0 +1,22 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// This file is generated! To fix issues, please fix them in the generator. //
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "core/DataTypes.h"
+
+#include "tinyhhg_core/edgedofspace/EdgeDoFIndexing.hpp"
+
+#include <map>
+
+namespace hhg {
+namespace VertexDoFToEdgeDoF {
+namespace generated {
+
+void apply_3D_macrocell_vertexdof_to_edgedof_add(double * _data_edgeCellDst_X, double * _data_edgeCellDst_XY, double * _data_edgeCellDst_XYZ, double * _data_edgeCellDst_XZ, double * _data_edgeCellDst_Y, double * _data_edgeCellDst_YZ, double * _data_edgeCellDst_Z, double * _data_vertexCellSrc, int64_t level, std::map< hhg::edgedof::EdgeDoFOrientation, std::map< hhg::indexing::IndexIncrement, double > > v2eStencilMap);
+
+} // namespace generated
+} // namespace VertexDoFToEdgeDoF
+} // namespace hhg
