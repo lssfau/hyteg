790f3b2bc3299b7988abc6cd5a12e22148322255
Nils Kohl <nils.kohl@fau.de>
2019-04-16 17:07:47 +0200

    Generated and integrated P2 3D restriction kernels

--- src/tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.cpp
+++ src/tinyhhg_core/gridtransferoperators/P2toP2QuadraticRestriction.cpp
@@ -108,36 +108,36 @@ void P2toP2QuadraticRestriction::restrictAdditively3D( const P2Function< real_t 
       auto vertexCoarseData = cell->getData( function.getVertexDoFFunction().getCellDataID() )->getPointer( coarseLevel );
       auto edgeCoarseData   = cell->getData( function.getEdgeDoFFunction().getCellDataID() )->getPointer( coarseLevel );
 
-//      const double numNeighborCellsFace0 =
-//          static_cast< double >( storage->getFace( cell->neighborFaces().at( 0 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsFace1 =
-//          static_cast< double >( storage->getFace( cell->neighborFaces().at( 1 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsFace2 =
-//          static_cast< double >( storage->getFace( cell->neighborFaces().at( 2 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsFace3 =
-//          static_cast< double >( storage->getFace( cell->neighborFaces().at( 3 ) )->getNumNeighborCells() );
-//
-//      const double numNeighborCellsEdge0 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 0 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsEdge1 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 1 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsEdge2 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 2 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsEdge3 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 3 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsEdge4 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 4 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsEdge5 =
-//          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 5 ) )->getNumNeighborCells() );
-//
-//      const double numNeighborCellsVertex0 =
-//          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 0 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsVertex1 =
-//          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 1 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsVertex2 =
-//          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 2 ) )->getNumNeighborCells() );
-//      const double numNeighborCellsVertex3 =
-//          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 3 ) )->getNumNeighborCells() );
+      const double numNeighborCellsFace0 =
+          static_cast< double >( storage->getFace( cell->neighborFaces().at( 0 ) )->getNumNeighborCells() );
+      const double numNeighborCellsFace1 =
+          static_cast< double >( storage->getFace( cell->neighborFaces().at( 1 ) )->getNumNeighborCells() );
+      const double numNeighborCellsFace2 =
+          static_cast< double >( storage->getFace( cell->neighborFaces().at( 2 ) )->getNumNeighborCells() );
+      const double numNeighborCellsFace3 =
+          static_cast< double >( storage->getFace( cell->neighborFaces().at( 3 ) )->getNumNeighborCells() );
+
+      const double numNeighborCellsEdge0 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 0 ) )->getNumNeighborCells() );
+      const double numNeighborCellsEdge1 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 1 ) )->getNumNeighborCells() );
+      const double numNeighborCellsEdge2 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 2 ) )->getNumNeighborCells() );
+      const double numNeighborCellsEdge3 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 3 ) )->getNumNeighborCells() );
+      const double numNeighborCellsEdge4 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 4 ) )->getNumNeighborCells() );
+      const double numNeighborCellsEdge5 =
+          static_cast< double >( storage->getEdge( cell->neighborEdges().at( 5 ) )->getNumNeighborCells() );
+
+      const double numNeighborCellsVertex0 =
+          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 0 ) )->getNumNeighborCells() );
+      const double numNeighborCellsVertex1 =
+          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 1 ) )->getNumNeighborCells() );
+      const double numNeighborCellsVertex2 =
+          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 2 ) )->getNumNeighborCells() );
+      const double numNeighborCellsVertex3 =
+          static_cast< double >( storage->getVertex( cell->neighborVertices().at( 3 ) )->getNumNeighborCells() );
 
       typedef edgedof::EdgeDoFOrientation eo;
       std::map< eo, uint_t >              firstIdxFine;
@@ -157,7 +157,21 @@ void P2toP2QuadraticRestriction::restrictAdditively3D( const P2Function< real_t 
                                                                             &edgeFineData[firstIdxFine[eo::Z]],
                                                                             vertexCoarseData,
                                                                             vertexFineData,
-                                                                            static_cast< int64_t >( coarseLevel ) );
+                                                                            static_cast< int64_t >( coarseLevel ),
+                                                                            numNeighborCellsEdge0,
+                                                                            numNeighborCellsEdge1,
+                                                                            numNeighborCellsEdge2,
+                                                                            numNeighborCellsEdge3,
+                                                                            numNeighborCellsEdge4,
+                                                                            numNeighborCellsEdge5,
+                                                                            numNeighborCellsFace0,
+                                                                            numNeighborCellsFace1,
+                                                                            numNeighborCellsFace2,
+                                                                            numNeighborCellsFace3,
+                                                                            numNeighborCellsVertex0,
+                                                                            numNeighborCellsVertex1,
+                                                                            numNeighborCellsVertex2,
+                                                                            numNeighborCellsVertex3 );
 
       P2::macrocell::generated::restrict_3D_macrocell_P2_update_edgedofs( &edgeCoarseData[firstIdxCoarse[eo::X]],
                                                                           &edgeCoarseData[firstIdxCoarse[eo::XY]],
@@ -174,10 +188,25 @@ void P2toP2QuadraticRestriction::restrictAdditively3D( const P2Function< real_t 
                                                                           &edgeFineData[firstIdxFine[eo::YZ]],
                                                                           &edgeFineData[firstIdxFine[eo::Z]],
                                                                           vertexFineData,
-                                                                          static_cast< int64_t >( coarseLevel ) );
+                                                                          static_cast< int64_t >( coarseLevel ),
+                                                                          numNeighborCellsEdge0,
+                                                                          numNeighborCellsEdge1,
+                                                                          numNeighborCellsEdge2,
+                                                                          numNeighborCellsEdge3,
+                                                                          numNeighborCellsEdge4,
+                                                                          numNeighborCellsEdge5,
+                                                                          numNeighborCellsFace0,
+                                                                          numNeighborCellsFace1,
+                                                                          numNeighborCellsFace2,
+                                                                          numNeighborCellsFace3 );
    }
 
-   // TODO additive comm
+  function.getVertexDoFFunction().communicateAdditively< Cell, Face >( coarseLevel );
+  function.getVertexDoFFunction().communicateAdditively< Cell, Edge >( coarseLevel );
+  function.getVertexDoFFunction().communicateAdditively< Cell, Vertex >( coarseLevel );
+
+  function.getEdgeDoFFunction().communicateAdditively< Cell, Face >( coarseLevel );
+  function.getEdgeDoFFunction().communicateAdditively< Cell, Edge >( coarseLevel );
 }
 
 void P2toP2QuadraticRestriction::restrictWithPostCommunication( const hhg::P2Function< walberla::real_t >& function,
