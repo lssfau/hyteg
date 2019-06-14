af617361d16c472512242a546638bcc043f634ba
Nils Kohl <nils.kohl@fau.de>
2019-06-14 17:08:28 +0200

    Forgot two generated kernels.

---
+++ src/tinyhhg_core/p1functionspace/generatedKernels/gaussseidel_2D_macroface_vertexdof_to_vertexdof_backwards.hpp
@@ -0,0 +1,24 @@
+
+//////////////////////////////////////////////////////////////////////////////
+// This file is generated! To fix issues, please fix them in the generator. //
+//////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "all.hpp"
+#include "core/DataTypes.h"
+#include "core/Macros.h"
+#include "tinyhhg_core/edgedofspace/EdgeDoFIndexing.hpp"
+#include <map>
+#define RESTRICT WALBERLA_RESTRICT
+
+namespace hhg {
+namespace vertexdof {
+namespace macroface {
+namespace generated {
+
+void gaussseidel_2D_macroface_vertexdof_to_vertexdof_backwards(double * RESTRICT _data_p1FaceDst, double * RESTRICT _data_p1FaceRhs, double const * RESTRICT const _data_p1FaceStencil, int32_t level);
+
+} // namespace generated
+} // namespace macroface
+} // namespace vertexdof
+} // namespace hhg
