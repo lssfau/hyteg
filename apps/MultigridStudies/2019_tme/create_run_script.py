7d85b9a07e7832824d97d5926b97ddc65e85cc9d
Nils Kohl <nils.kohl@fau.de>
2019-07-09 12:09:06 +0200

    FMG parameter settings creation for TME benchmarks

--- apps/MultigridStudies/2019_tme/create_run_script.py
+++ apps/MultigridStudies/2019_tme/create_run_script.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-base_config = """
+##################
+# Omega Sampling #
+##################
+
+def omega_sampling():
+
+    base_config = """
 Parameters
 {
     equation stokes;
@@ -9,9 +15,9 @@ Parameters
     discretization P1;
     meshLayout CRISSCROSS;
     symmetricCuboidMesh true;
-    numCycles 5;
+    numCycles 1;
     cycleType V;
-    fmgInnerCycles 0; // 0 == no fmg
+    fmgInnerCycles 1; // 0 == no fmg
 
     // CRISSCROSS: ~0.4
     // CRISS:    : P1: ? , P2: ~0.72
@@ -39,31 +45,114 @@ Parameters
     outputTimingJSON true;
     outputTimingJSONFile timing.json;
     outputSQL true;
+    outputSQLFile omega_sampling.db;
 }
 """
 
-with open("base_config.prm", "w") as f:
-    f.write(base_config)
+    with open("omega_sampling_base_config.prm", "w") as f:
+        f.write(base_config)
 
-##################
-# Omega Sampling #
-##################
+    discretizations = ["P1", "P2"]
+    num_processes = 8
+    omegas = {"P1": list(np.arange(0.05, 0.5, 0.05)), "P2": list(np.arange(0.05, 0.5, 0.05))}
 
-discretizations = ["P1", "P2"]
-num_processes = 8
-max_level = 5
-omegas = {"P1": list(np.arange(0.05, 0.6, 0.05)), "P2": list(np.arange(0.05, 1.0, 0.05))}
+    with open("run_omega_sampling.sh", "w") as f:
+        f.write("echo\n")
+        f.write("echo OMEGA SAMPLING\n")
+        f.write("echo\n")
+        for discretization in discretizations:
+            for omega in omegas[discretization]:
+                for symm_gs_velocity in ["true", "false"]:
+                    for smooth in range(1, 4):
+                        for max_level in range(4, 6):
+                            cmd = "mpirun --allow-run-as-root -np {} --map-by core --bind-to core --report-bindings ./MultigridStudies 2019_tme/omega_sampling_base_config.prm " \
+                                  "-Parameters.sorRelax={} " \
+                                  "-Parameters.discretization={} " \
+                                  "-Parameters.symmGSVelocity={} " \
+                                  "-Parameters.preSmoothingSteps={} " \
+                                  "-Parameters.postSmoothingSteps={} " \
+                                  "-Parameters.smoothingIncrement={} " \
+                                  "-Parameters.maxLevel={}".format(num_processes, omega, discretization, symm_gs_velocity, smooth, smooth, smooth, max_level)
+                            f.write("echo \"{}\"\n".format(cmd))
+                            f.write(cmd + "\n")
 
-with open("run_omega_sampling.sh", "w") as f:
-    f.write("echo")
-    f.write("echo OMEGA SAMPLING")
-    f.write("echo")
-    for discretization in discretizations:
-        for omega in omegas[discretization]:
-            cmd = "mpirun --allow-run-as-root -np {} --map-by core --bind-to core --report-bindings ./MultigridStudies 2019_tme/base_config.prm " \
-                  "-Parameters.sorRelax={} " \
-                  "-Parameters.discretization={} " \
-                  "-Parameters.maxLevel={}".format(num_processes, omega, discretization, max_level)
-            f.write("echo \"{}\"\n".format(cmd))
-            f.write(cmd + "\n")
 
+
+def fmg_tests():
+
+    base_config = """
+Parameters
+{
+    equation stokes;
+    dim 3;
+    numFacesPerSide 1;
+    discretization P1;
+    meshLayout CRISSCROSS;
+    symmetricCuboidMesh true;
+    numCycles 5;
+    cycleType V;
+    fmgInnerCycles 1; // 0 == no fmg
+
+    // CRISSCROSS: ~0.4
+    // CRISS:    : P1: ? , P2: ~0.72
+    sorRelax 0.3;
+
+    symmGSVelocity true;
+    numGSVelocity 1;
+    symmGSPressure false;
+    numGSPressure 1;
+
+    preSmoothingSteps 2;
+    postSmoothingSteps 2;
+    smoothingIncrement 2;
+    minLevel 2;
+    maxLevel 8; // P1 level, P2 level is automatically reduced by 1
+    skipCyclesForAvgConvRate 0;
+    L2residualTolerance 1e-16;
+    projectPressureAfterRestriction true;
+    calculateDiscretizationError false;
+    coarseGridMaxIterations 100000;
+    coarseGridResidualTolerance 1e-16;
+
+    outputVTK false;
+    outputTiming false;
+    outputTimingJSON true;
+    outputTimingJSONFile timing.json;
+    outputSQL true;
+    outputSQLFile fmg_benchmark.db;
+}
+"""
+
+    with open("fmg_tests_base_config.prm", "w") as f:
+        f.write(base_config)
+
+    discretizations = ["P1", "P2"]
+    num_processes = 8
+    sor_omega = {"P1": 0.3, "P2": 0.3}
+    symm_gs = ["true", "false"]
+    num_pre_post_inc = [(1, 1, 2), (2, 2, 2), (3, 3, 2)]
+    inner_cycles = [1, 2]
+
+    with open("run_fmg.sh", "w") as f:
+        f.write("echo\n")
+        f.write("echo FMG TESTS\n")
+        f.write("echo\n")
+        for discretization in discretizations:
+            for symm in symm_gs:
+                    for smooth in num_pre_post_inc:
+                        for r in inner_cycles:
+                            cmd = "mpirun --allow-run-as-root -np {} --map-by core --bind-to core --report-bindings ./MultigridStudies 2019_tme/fmg_tests_base_config.prm " \
+                                  "-Parameters.preSmoothingSteps={} " \
+                                  "-Parameters.postSmoothingSteps={} " \
+                                  "-Parameters.smoothingIncrement={} " \
+                                  "-Parameters.sorRelax={} " \
+                                  "-Parameters.symmGSVelocity={} " \
+                                  "-Parameters.fmgInnerCycles={} " \
+                                  "-Parameters.discretization={} ".format(num_processes, smooth[0], smooth[1], smooth[2], sor_omega[discretization], symm, r, discretization)
+                            f.write("echo \"{}\"\n".format(cmd))
+                            f.write(cmd + "\n")
+
+
+if __name__ == "__main__":
+    omega_sampling()
+    fmg_tests()
