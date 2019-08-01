233b0bedd7da9d3dfa12a05071eb40099f74df7f
Nils Kohl <nils.kohl@fau.de>
2019-08-01 10:56:23 +0200

    Added test scripts for P2 operator benchmarks

---
+++ apps/benchmarks/P2OperatorBenchmarks/2019_tme/create_run_script.py
@@ -0,0 +1,34 @@
+
+def generate_benchmark_script():
+
+    base_config = """
+Parameters
+{
+    level 8;
+    iterationMinTime 0.0001;
+    chunkSize 5;
+    kernelType APPLY_V_TO_V_REPLACE;
+}
+"""
+
+    with open("run_benchmark_base_config.prm", "w") as f:
+        f.write(base_config)
+
+    num_processes = 8
+    kernel_types = ["APPLY_V_TO_V_REPLACE", "APPLY_V_TO_E_ADD", "APPLY_E_TO_V_ADD", "APPLY_E_TO_E_REPLACE",
+                    "SOR_P1_V", "SOR_P2_V", "SOR_P2_E"]
+
+    with open("run_benchmark.sh", "w") as f:
+        f.write("echo\n")
+        f.write("echo BENCHMARK\n")
+        f.write("echo\n")
+        for kernel_type in kernel_types:
+            cmd = "likwid-mpirun -np {} -nperdomain S:4 -g FLOPS_DP -m ./P2OperatorBenchmarks 2019_tme/run_benchmark_base_config.prm " \
+                  "-Parameters.kernelType={}".format(num_processes, kernel_type)
+            f.write("echo \"{}\"\n".format(cmd))
+            f.write(cmd + "\n")
+
+
+if __name__ == "__main__":
+    generate_benchmark_script()
+
