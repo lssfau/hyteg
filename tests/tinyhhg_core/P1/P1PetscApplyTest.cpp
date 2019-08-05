adb7339ce89c03cc24ac263fbbceabad6d3d8aa5
Nils Kohl <nils.kohl@fau.de>
2019-08-05 11:32:31 +0200

    Minor: epsilon fix

--- tests/tinyhhg_core/P1/P1PetscApplyTest.cpp
+++ tests/tinyhhg_core/P1/P1PetscApplyTest.cpp
@@ -111,7 +111,7 @@ int main(int argc, char* argv[])
   hhg::p1PetscApplyTest( 3, "../../data/meshes/3D/tet_1el.msh",     hhg::Inner, 7.6e-18 );
   hhg::p1PetscApplyTest( 3, "../../data/meshes/3D/pyramid_2el.msh", hhg::Inner, 2.0e-16 );
   hhg::p1PetscApplyTest( 3, "../../data/meshes/3D/pyramid_4el.msh", hhg::Inner, 1.0e-15 );
-  hhg::p1PetscApplyTest( 3, "../../data/meshes/3D/regular_octahedron_8el.msh", hhg::Inner, 4.5e-16 );
+  hhg::p1PetscApplyTest( 3, "../../data/meshes/3D/regular_octahedron_8el.msh", hhg::Inner, 5.0e-16 );
 
   return EXIT_SUCCESS;
 }
