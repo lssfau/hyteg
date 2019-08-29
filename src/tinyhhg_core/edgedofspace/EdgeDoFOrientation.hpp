58ae726bd50373f6b7910ca5f40eded202c8775a
Dominik Thoennes <dominik.thoennes@fau.de>
2019-08-29 17:05:48 +0200

    add COPYING.txt; move EdgeDoFOrientatin into seperate file [skip ci]

---
+++ src/tinyhhg_core/edgedofspace/EdgeDoFOrientation.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "core/DataTypes.h"
+
+namespace hhg {
+namespace edgedof {
+
+/// Mapping of X,Y,Z coordinates to uint_t
+/// located in seperate file to reduce dependencies in generated kernels
+enum class EdgeDoFOrientation : walberla::uint_t
+{
+   X,
+   Y,
+   Z,
+   XY,
+   XZ,
+   YZ,
+   XYZ,
+   INVALID,
+};
+}// namespace edgedof
+}// namespace hhg
