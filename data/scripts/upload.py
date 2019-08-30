ac5fd29fd647f665140c1a0fa8db32a25d82775b
Dominik Thoennes <dominik.thoennes@fau.de>
2019-08-30 13:37:34 +0200

    upload build timing to grafana

---
+++ data/scripts/upload.py
@@ -0,0 +1,41 @@
+import os
+import time
+import math
+import random
+import re
+from influxdb import InfluxDBClient
+
+
+def main():
+    write_user_pw = 'hyteg'
+
+    client = InfluxDBClient('i10grafana.informatik.uni-erlangen.de', 8086,
+                            'terraneo', write_user_pw, 'terraneo')
+
+
+    with open("BuildTiming.txt") as f:
+        s = f.read()
+    hyteg = re.search('hyteg buildtime: (\d*.\d*)', s)
+    tests = re.search('tests buildtime: (\d*.\d*)', s)
+    apps = re.search('apps buildtime: (\d*.\d*)', s)
+
+    print(hyteg)
+
+    json_body = [
+        {
+            'measurement': 'P1_Benchmark',
+            'tags': {
+                'host'     : os.uname()[1],
+                'project'  : 'terraneo',
+            },
+            'time': int(time.time()),
+            'fields': {'hyteg_buildtime': float(hyteg.group(1)),
+                       'tests_buildtime': float(tests.group(1)),
+                       'apps_buildtime': float(apps.group(1))}
+        }
+    ]
+    print(json_body)
+    client.write_points(json_body, time_precision='s')
+
+if __name__ == "__main__":
+    main()
