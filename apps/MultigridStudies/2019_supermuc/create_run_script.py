9ce965e2a235edf8b5fad23cc057e0b318a9e265
Nils Kohl <nils.kohl@fau.de>
2019-11-21 13:23:27 +0100

    Supermuc runs: - adapted coarse grid residual tolerance (1e-10) - better job script naming - P1 and P2 cases - strong scaling

--- apps/MultigridStudies/2019_supermuc/create_run_script.py
+++ apps/MultigridStudies/2019_supermuc/create_run_script.py
@@ -45,8 +45,8 @@ Parameters
     L2residualTolerance 1e-16;
     projectPressureAfterRestriction true;
     calculateDiscretizationError false;
-    coarseGridMaxIterations 2000;
-    coarseGridResidualTolerance 1e-04;
+    coarseGridMaxIterations 20000;
+    coarseGridResidualTolerance 1e-10;
 
     cyclesBeforeDC 0;
     postDCPreSmoothingSteps 3;
@@ -78,6 +78,10 @@ def supermuc_job_file_string(job_name="hyteg_job", wall_clock_limit="1:00:00", p
         elif num_nodes <= 3072:
             return "large"
 
+    constraint = ""
+    if num_nodes <= 792:
+        constraint = "#SBATCH --constraint=[i01|i02|i03|i04|i05|i06|i07|i08]"
+
     base_config = """#!/bin/bash
 # Job Name and Files (also --job-name)
 #SBATCH -J {job_name}
@@ -102,6 +106,7 @@ def supermuc_job_file_string(job_name="hyteg_job", wall_clock_limit="1:00:00", p
 #Number of nodes and MPI tasks per node:
 #SBATCH --nodes={num_nodes}
 #SBATCH --ntasks-per-node=48
+{constraint}
 
 module load slurm_setup
 
@@ -116,76 +121,116 @@ ls -lha
 #Run the program:
 mpiexec -n $SLURM_NTASKS ./MultigridStudies 2019_supermuc/{prm_file}
 
-""".format(job_name=job_name, wall_clock_limit=wall_clock_limit, num_nodes=num_nodes, prm_file=prm_file, partition=partition(num_nodes))
+""".format(job_name=job_name, wall_clock_limit=wall_clock_limit, num_nodes=num_nodes, prm_file=prm_file, partition=partition(num_nodes),
+           constraint=constraint)
     return base_config
 
 
 def supermuc_scaling(cube_scaling=True):
 
-    shell_base_config = {"discretization": "P2", "fmg_r": 1, "max_level": 6,
-                         "num_cycles": 2, "pre": 6, "post": 6}
-
-    ppn = 48
-
-    node_dep_parameters_shell = {
-        1: {"ntan": 3, "nrad":  7, "omega": 0.2},
-        2: {"ntan": 3, "nrad": 13, "omega": 0.2}
-    }
-
-    cube_base_config = {
-        "discretization": "P2",
-        "fmg_r": 0,
-        "max_level": 8,
-        "num_cycles": 10,
-        "pre": 4,
-        "post": 4,
-        "num_gs_velocity": 2,
-        "omega": 0.55
+    cube_base_config_fmg = {
+        "weak": {
+            "P1": {
+                "discretization": "P1",
+                "fmg_r": 1,
+                "max_level": 8,
+                "num_cycles": 3,
+                "pre": 2,
+                "post": 2,
+                "num_gs_velocity": 2,
+                "omega": 0.3
+            },
+            "P2": {
+                "discretization": "P2",
+                "fmg_r": 1,
+                "max_level": 8,
+                "num_cycles": 3,
+                "pre": 3,
+                "post": 3,
+                "num_gs_velocity": 2,
+                "omega": 0.3
+            }
+        },
+        "strong": {
+            "P1": {
+                "discretization": "P1",
+                "fmg_r": 1,
+                "max_level": 6,
+                "num_cycles": 3,
+                "pre": 2,
+                "post": 2,
+                "num_gs_velocity": 2,
+                "omega": 0.3
+            },
+            "P2": {
+                "discretization": "P2",
+                "fmg_r": 1,
+                "max_level": 6,
+                "num_cycles": 3,
+                "pre": 3,
+                "post": 3,
+                "num_gs_velocity": 2,
+                "omega": 0.3
+            }
+        }
     }
 
     node_dep_parameters_cube = {
-        1: {"num_faces_per_side": 1},
-        2: {"num_faces_per_side": 2},
-        6: {"num_faces_per_side": 3},
-        12: {"num_faces_per_side": 4},
-        24: {"num_faces_per_side": 5},
-        48: {"num_faces_per_side": 6},
-        96: {"num_faces_per_side": 8},
-        192: {"num_faces_per_side": 10},
-        384: {"num_faces_per_side": 13},
-        768: {"num_faces_per_side": 16},
-        1536: {"num_faces_per_side": 20},
-        3072: {"num_faces_per_side": 26},
+        "weak": {
+            1: {"num_faces_per_side": 1},
+            2: {"num_faces_per_side": 2},
+            6: {"num_faces_per_side": 3},
+            12: {"num_faces_per_side": 4},
+            24: {"num_faces_per_side": 5},
+            48: {"num_faces_per_side": 6},
+            96: {"num_faces_per_side": 8},
+            192: {"num_faces_per_side": 10},
+            384: {"num_faces_per_side": 13},
+            768: {"num_faces_per_side": 16},
+            1536: {"num_faces_per_side": 20},
+            3072: {"num_faces_per_side": 26},
+        },
+        "strong": {
+            1: {"num_faces_per_side": 6},
+            2: {"num_faces_per_side": 6},
+            6: {"num_faces_per_side": 6},
+            12: {"num_faces_per_side": 6},
+            24: {"num_faces_per_side": 6},
+            48: {"num_faces_per_side": 6},
+            96: {"num_faces_per_side": 6},
+        }
     }
 
-    if cube_scaling:
-        base_config = cube_base_config
-        node_dep_parameters = node_dep_parameters_cube
+    some_id = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
 
-    for num_nodes, prms in node_dep_parameters.items():
-        # some_id = str(uuid4())
-        some_id = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
-        job_name = "mg_studies_{}nodes_{}".format(num_nodes, some_id)
-        prm_file_name = job_name + ".prm"
-        job_file_name = job_name + ".job"
+    for discretization in ["P1", "P2"]:
+        for scaling_type in ["weak", "strong"]:
+            base_config = cube_base_config_fmg[scaling_type][discretization]
+            node_dep_parameters = node_dep_parameters_cube[scaling_type]
 
-        timing_file = job_name + ".json"
-        db_file = job_name + ".db"
+            for num_nodes, prms in node_dep_parameters.items():
+                # some_id = str(uuid4())
+                job_name = "mg_studies_{}_{}_{}nodes_{}".format(scaling_type, discretization, num_nodes, some_id)
+                prm_file_name = job_name + ".prm"
+                job_file_name = job_name + ".job"
 
-        prm_string_prm_dict = {}
-        prm_string_prm_dict.update(base_config)
-        prm_string_prm_dict.update(prms)
-        prm_string_prm_dict["timing_file"] = timing_file
-        prm_string_prm_dict["db_file"] = db_file
+                timing_file = job_name + ".json"
+                db_file = job_name + ".db"
 
-        prm_string = supermuc_scaling_prm_file_string(**prm_string_prm_dict)
-        job_string = supermuc_job_file_string(job_name=job_name, wall_clock_limit="1:00:00",
-                                              num_nodes=num_nodes, prm_file=prm_file_name)
+                prm_string_prm_dict = {}
+                prm_string_prm_dict.update(base_config)
+                prm_string_prm_dict.update(prms)
+                prm_string_prm_dict["timing_file"] = timing_file
+                prm_string_prm_dict["db_file"] = db_file
 
-        with open(prm_file_name, "w") as f:
-            f.write(prm_string)
-        with open(job_file_name, "w") as f:
-            f.write(job_string)
+                prm_string = supermuc_scaling_prm_file_string(**prm_string_prm_dict)
+                job_string = supermuc_job_file_string(job_name=job_name, wall_clock_limit="1:00:00",
+                                                      num_nodes=num_nodes, prm_file=prm_file_name)
+
+                with open(prm_file_name, "w") as f:
+                    f.write(prm_string)
+                with open(job_file_name, "w") as f:
+                    f.write(job_string)
 
 if __name__ == "__main__":
     supermuc_scaling()
