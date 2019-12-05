e5beaaecb38247f7698a8696d42646d4b37d1ac3
Nils Kohl <nils.kohl@fau.de>
2019-12-05 17:15:24 +0100

    Extended agglomeration tests to levels 0 and 1

--- tests/hyteg/adaptivity/AgglomerationConvergenceTest.cpp
+++ tests/hyteg/adaptivity/AgglomerationConvergenceTest.cpp
@@ -44,11 +44,12 @@ using walberla::uint_t;
 namespace hyteg {
 
 void AgglomerationConvergenceTest( const std::string& meshFile,
-                                   const uint_t       level,
-                                   const real_t       targetError,
-                                   const bool         localMPI )
+                                   const uint_t &     minLevel,
+                                   const uint_t &     maxLevel,
+                                   const real_t &     targetError,
+                                   const bool   &     localMPI )
 {
-   WALBERLA_CHECK_GREATER( level, 2 );
+   WALBERLA_CHECK_LESS( minLevel, maxLevel );
 
    const uint_t numIterations = 5;
 
@@ -61,14 +62,14 @@ void AgglomerationConvergenceTest( const std::string& meshFile,
 
    auto storage = std::make_shared< PrimitiveStorage >( setupStorage );
 
-   hyteg::P2ConstantLaplaceOperator L( storage, 2, level );
+   hyteg::P2ConstantLaplaceOperator L( storage, minLevel, maxLevel );
 
-   hyteg::P2Function< real_t > r( "r", storage, 2, level );
-   hyteg::P2Function< real_t > f( "f", storage, 2, level );
-   hyteg::P2Function< real_t > u( "u", storage, 2, level );
-   hyteg::P2Function< real_t > u_exact( "u_exact", storage, 2, level );
-   hyteg::P2Function< real_t > err( "err", storage, 2, level );
-   hyteg::P2Function< real_t > npoints_helper( "npoints_helper", storage, 2, level );
+   hyteg::P2Function< real_t > r( "r", storage, minLevel, maxLevel );
+   hyteg::P2Function< real_t > f( "f", storage, minLevel, maxLevel );
+   hyteg::P2Function< real_t > u( "u", storage, minLevel, maxLevel );
+   hyteg::P2Function< real_t > u_exact( "u_exact", storage, minLevel, maxLevel );
+   hyteg::P2Function< real_t > err( "err", storage, minLevel, maxLevel );
+   hyteg::P2Function< real_t > npoints_helper( "npoints_helper", storage, minLevel, maxLevel );
 
    if ( localMPI )
    {
@@ -79,8 +80,8 @@ void AgglomerationConvergenceTest( const std::string& meshFile,
    std::function< real_t( const hyteg::Point3D& ) > rhs   = []( const hyteg::Point3D& ) { return 0; };
    std::function< real_t( const hyteg::Point3D& ) > ones  = []( const hyteg::Point3D& ) { return 1.0; };
 
-   u.interpolate( exact, level, hyteg::DirichletBoundary );
-   u_exact.interpolate( exact, level );
+   u.interpolate( exact, maxLevel, hyteg::DirichletBoundary );
+   u_exact.interpolate( exact, maxLevel );
 
    hyteg::VTKOutput vtkOutput( "../../output", "AgglomerationConvergenceTest", storage );
    vtkOutput.add( u );
@@ -89,7 +90,7 @@ void AgglomerationConvergenceTest( const std::string& meshFile,
    vtkOutput.add( r );
    vtkOutput.add( err );
    vtkOutput.add( npoints_helper );
-   vtkOutput.write( level, 0 );
+   vtkOutput.write( maxLevel, 0 );
 
    // Setup of the agglomeration based solver.
    // Apart from the coarse grid, everything is performed in parallel.
@@ -103,35 +104,36 @@ void AgglomerationConvergenceTest( const std::string& meshFile,
    auto restriction  = std::make_shared< P2toP2QuadraticRestriction >();
 #if 1
    // pass agglomeration storage to coarse grid solver
-   auto coarseGridSolver = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( agglomerationStorage, 2, 2 );
+   auto coarseGridSolver = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( agglomerationStorage, minLevel, minLevel );
+   coarseGridSolver->setPrintInfo( true );
    // now wrap the solver
    auto coarseGridSolverAgglomeration =
-       std::make_shared< AgglomerationWrapper< P2ConstantLaplaceOperator > >( coarseGridSolver, agglomerationStorage, 2, numberOfSubsetProcesses );
+       std::make_shared< AgglomerationWrapper< P2ConstantLaplaceOperator > >( coarseGridSolver, agglomerationStorage, minLevel, numberOfSubsetProcesses );
 #else
-  auto coarseGridSolverAgglomeration = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( storage, 2, 2 );
+  auto coarseGridSolverAgglomeration = std::make_shared< CGSolver< P2ConstantLaplaceOperator > >( storage, minLevel, minLevel );
 #endif
 
    auto solver = std::make_shared< GeometricMultigridSolver< P2ConstantLaplaceOperator > >(
-       storage, smoother, coarseGridSolverAgglomeration, restriction, prolongation, 2, level );
+       storage, smoother, coarseGridSolverAgglomeration, restriction, prolongation, minLevel, maxLevel );
 
    real_t discr_l2_err, discr_l2_residual;
    for ( uint_t iteration = 0; iteration < numIterations; iteration++ )
    {
-      solver->solve( L, u, f, level );
+      solver->solve( L, u, f, maxLevel );
 
-      err.assign( {1.0, -1.0}, {u, u_exact}, level );
-      npoints_helper.interpolate( ones, level );
+      err.assign( {1.0, -1.0}, {u, u_exact}, maxLevel );
+      npoints_helper.interpolate( ones, maxLevel );
 
-      const real_t npoints = npoints_helper.dotGlobal( npoints_helper, level );
-      discr_l2_err         = std::sqrt( err.dotGlobal( err, level ) / npoints );
+      const real_t npoints = npoints_helper.dotGlobal( npoints_helper, maxLevel );
+      discr_l2_err         = std::sqrt( err.dotGlobal( err, maxLevel ) / npoints );
 
-      L.apply( u, err, level, Inner | NeumannBoundary );
-      r.assign( {1.0, -1.0}, {f, err}, level, Inner | NeumannBoundary );
-      discr_l2_residual = std::sqrt( r.dotGlobal( r, level, Inner | NeumannBoundary ) / npoints );
+      L.apply( u, err, maxLevel, Inner | NeumannBoundary );
+      r.assign( {1.0, -1.0}, {f, err}, maxLevel, Inner | NeumannBoundary );
+      discr_l2_residual = std::sqrt( r.dotGlobal( r, maxLevel, Inner | NeumannBoundary ) / npoints );
 
       WALBERLA_LOG_INFO_ON_ROOT( "residual " << discr_l2_residual << ", error " << discr_l2_err );
 
-      vtkOutput.write( level, iteration + 1 );
+      vtkOutput.write( maxLevel, iteration + 1 );
    }
 
    WALBERLA_LOG_INFO_ON_ROOT( "discrete L2 error = " << discr_l2_err << ", (mesh: " << meshFile << ")" );
@@ -146,10 +148,20 @@ int main( int argc, char* argv[] )
    walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
    walberla::MPIManager::instance()->useWorldComm();
 
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/tri_1el.msh", 3, 1e-7, false );
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/quad_4el.msh", 3, 1e-7, false );
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/annulus_coarse.msh", 3, 3e-7, false );
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/tet_1el.msh", 3, 4e-7, true );
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/pyramid_2el.msh", 3, 3e-6, false );
-   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/regular_octahedron_8el.msh", 3, 1.8e-6, true );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/tri_1el.msh", 0, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/quad_4el.msh", 0, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/annulus_coarse.msh", 0, 3, 3e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/tet_1el.msh", 0, 3, 4e-7, true );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/pyramid_2el.msh", 0, 3, 3e-6, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/regular_octahedron_8el.msh", 0, 3, 1.8e-6, true );
+
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/tri_1el.msh", 1, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/quad_4el.msh", 1, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/tet_1el.msh", 1, 3, 4e-7, true );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/regular_octahedron_8el.msh", 1, 3, 1.8e-6, true );
+
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/tri_1el.msh", 2, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/quad_4el.msh", 2, 3, 1e-7, false );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/tet_1el.msh", 2, 3, 4e-7, true );
+   hyteg::AgglomerationConvergenceTest( "../../data/meshes/3D/regular_octahedron_8el.msh", 2, 3, 1.8e-6, true );
 }
