39eb59b0f209451b7f74a7af643aa7951fb10fa8
Marcus Mohr <marcus.mohr@lmu.de>
2020-01-10 18:41:39 +0100

    Integrates P1BlendingMassOperator into MassOperatorTest
    The commit includes the P1BlendingMassOperator into the test we run
    in the MassOperatorTest. We do this for the PolarCoordsMap and can,
    thus, remove the PolarCoordsMapTest. Additionally we also make a
    test with the CircularMap.

--- tests/hyteg/operators/MassOperatorTest.cpp
+++ tests/hyteg/operators/MassOperatorTest.cpp
@@ -19,23 +19,28 @@
  */
 // test that the product one^T*M*one with mass matrix M and vector of ones gives area of domain
 #include "core/Environment.h"
+#include <core/math/Constants.h>
 
 #include "hyteg/dataexport/VTKOutput.hpp"
 #include "hyteg/elementwiseoperators/P1ElementwiseOperator.hpp"
 #include "hyteg/elementwiseoperators/P2ElementwiseOperator.hpp"
-// #include "hyteg/p1functionspace/P1VariableOperator.hpp"
+#include "hyteg/p1functionspace/P1VariableOperator.hpp"
 #include "hyteg/p2functionspace/P2ConstantOperator.hpp"
 #include "hyteg/p2functionspace/P2Function.hpp"
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
+#include "hyteg/geometry/CircularMap.hpp"
+#include "hyteg/geometry/PolarCoordsMap.hpp"
 
+using walberla::math::pi;
 using walberla::real_t;
 using walberla::uint_t;
+
 using namespace hyteg;
 
 template < typename OperatorType >
-void checkArea( std::shared_ptr< PrimitiveStorage > storage, real_t area, std::string tag )
+void checkArea( std::shared_ptr< PrimitiveStorage > storage, real_t area, std::string tag, const uint_t minLevel = 2, bool outputVTK = false )
 {
-   const uint_t minLevel = 2;
+   // const uint_t minLevel = 2;
    const uint_t maxLevel = storage->hasGlobalCells() ? 3 : 4;
 
    OperatorType massOp( storage, minLevel, maxLevel );
@@ -48,9 +53,29 @@ void checkArea( std::shared_ptr< PrimitiveStorage > storage, real_t area, std::s
       vecOfOnes.interpolate( real_c( 1.0 ), lvl, All );
       massOp.apply( vecOfOnes, aux, lvl, All );
       real_t measure = vecOfOnes.dotGlobal( aux, lvl );
+
+      if( outputVTK ) {
+        VTKOutput vtkOutput( "../../output", tag, storage );
+        vtkOutput.add( vecOfOnes );
+        vtkOutput.add( aux );
+        vtkOutput.write( lvl );
+      }
+
       WALBERLA_LOG_INFO_ON_ROOT( "measure = " << std::scientific << measure << " (" << tag << ")" );
       WALBERLA_CHECK_FLOAT_EQUAL( measure, area );
    }
+}
+
+void setMap( SetupPrimitiveStorage& setupStorage, std::shared_ptr<GeometryMap> map ) {
+  for( auto it : setupStorage.getFaces() ) {
+    setupStorage.setGeometryMap( it.second->getID(), map );
+  }
+  for( auto it : setupStorage.getEdges() ) {
+    setupStorage.setGeometryMap( it.second->getID(), map );
+  }
+  for( auto it : setupStorage.getVertices() ) {
+    setupStorage.setGeometryMap( it.second->getID(), map );
+  }
 }
 
 int main( int argc, char** argv )
@@ -116,6 +141,55 @@ int main( int argc, char** argv )
    checkArea< P2ConstantMassOperator >( storageTSS, volume, "P2ConstantMassOperator" );
    checkArea< P1ElementwiseMassOperator >( storageTSS, volume, "P1ElementwiseMassOperator" );
    checkArea< P2ElementwiseMassOperator >( storageTSS, volume, "P2ElementwiseMassOperator" );
+
+   // -------------------
+   //  2D Blending Tests
+   // -------------------
+
+   // Test with annulus
+   WALBERLA_LOG_INFO_ON_ROOT( "Testing with BLENDING( ANNULUS )" );
+   MeshInfo meshInfoPolar = MeshInfo::meshRectangle( Point2D( { 1.0, 0.0 } ), Point2D( { 2.0, 2.0*pi } ), MeshInfo::CROSS, 1, 6 );
+   SetupPrimitiveStorage               setupStoragePolar( meshInfoPolar, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   setMap( setupStoragePolar, std::make_shared< PolarCoordsMap >() );
+   std::shared_ptr< PrimitiveStorage > storagePolar = std::make_shared< PrimitiveStorage >( setupStoragePolar );
+
+   checkArea< P1BlendingMassOperator >( storagePolar, 3.0 * pi, "P1BlendingMassOperator" );
+
+   // Test with unit square containing circular hole
+   WALBERLA_LOG_INFO_ON_ROOT( "Testing with BLENDING( SQUARE with CIRCULAR HOLE )" );
+   MeshInfo              meshInfoHole = MeshInfo::fromGmshFile( "../../data/meshes/unitsquare_with_circular_hole.msh" );
+   SetupPrimitiveStorage setupStorageHole( meshInfoHole, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+
+   Point3D circleCenter{ { 0.5, 0.5, 0 } };
+   real_t  circleRadius = 0.25;
+
+   for ( const auto& it : setupStorageHole.getFaces() )
+   {
+      Face& face = *( it.second );
+
+      std::vector< PrimitiveID > neighborEdgesOnBoundary = face.neighborEdges();
+      neighborEdgesOnBoundary.erase(
+          std::remove_if( neighborEdgesOnBoundary.begin(),
+                          neighborEdgesOnBoundary.end(),
+                          [&setupStorageHole]( const PrimitiveID& id ) { return !setupStorageHole.onBoundary( id ); } ),
+          neighborEdgesOnBoundary.end() );
+
+      if ( neighborEdgesOnBoundary.size() > 0 )
+      {
+         Edge& edge = *setupStorageHole.getEdge( neighborEdgesOnBoundary[0] );
+
+         if ( ( edge.getCoordinates()[0] - circleCenter ).norm() < 0.4 )
+         {
+            setupStorageHole.setGeometryMap( edge.getID(),
+                                         std::make_shared< CircularMap >( face, setupStorageHole, circleCenter, circleRadius ) );
+            setupStorageHole.setGeometryMap( face.getID(),
+                                         std::make_shared< CircularMap >( face, setupStorageHole, circleCenter, circleRadius ) );
+         }
+      }
+   }
+   std::shared_ptr< PrimitiveStorage > storageHole = std::make_shared< PrimitiveStorage >( setupStorageHole );
+
+   checkArea< P1BlendingMassOperator >( storageHole, 1.0 - pi / 16.0, "P1BlendingMassOperator", 3 );
 
    return EXIT_SUCCESS;
 }
