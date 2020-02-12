06db059e7f4fd5d1fdf120d4815e7125a3a317a3
Marcus Mohr <marcus.mohr@lmu.de>
2020-02-12 17:11:30 +0100

    Adds blending to P2FormLaplace
    The correctness of the incorporation of blending needs to be tested,
    still. Currently we only see that it does not spoil the results of
    the non-blended case (in comparison with the FEniCS form).

--- src/hyteg/forms/form_hyteg_manual/P2FormLaplace.hpp
+++ src/hyteg/forms/form_hyteg_manual/P2FormLaplace.hpp
@@ -54,31 +54,46 @@ class P2Form_laplace : public P2FormHyTeG
 #define QUADWEIGHTS quadrature::D5_weights
 
 // Executing quadrature rule
-#define INTEGRATE2D( i, j )                                                                                        \
-   elMat( i, j ) = 0.0;                                                                                            \
-   for ( uint_t k = 0; k < QUADWEIGHTS.size(); k++ )                                                               \
-   {                                                                                                               \
-      real_t L2      = QUADPOINTS[k][0];                                                                           \
-      real_t L3      = QUADPOINTS[k][1];                                                                           \
-      real_t L1      = 1.0 - L2 - L3;                                                                              \
-      mappedPt[0]    = ( coords[1][0] - coords[0][0] ) * L2 + ( coords[2][0] - coords[0][0] ) * L3 + coords[0][0]; \
-      mappedPt[1]    = ( coords[1][1] - coords[0][1] ) * L2 + ( coords[2][1] - coords[0][1] ) * L3 + coords[0][1]; \
-      real_t detDPsi = std::abs( geometryMap_->evalDetDF( mappedPt ) );                                            \
-      real_t tmp6    = ( ( DetaN##i * tmp1 - DxiN##i * tmp3 ) * ( DetaN##j * tmp1 - DxiN##j * tmp3 ) +             \
-                      ( DetaN##i * tmp2 - DxiN##i * tmp0 ) * ( DetaN##j * tmp2 - DxiN##j * tmp0 ) ) *           \
-                    tmp5;                                                                                          \
-      elMat( i, j ) += QUADWEIGHTS[k] * detJacPhiInv * tmp6;                                                       \
-   }                                                                                                               \
+#define INTEGRATE2D( i, j )                                                                                     \
+   elMat( i, j ) = 0.0;                                                                                         \
+   for ( uint_t k = 0; k < QUADWEIGHTS.size(); k++ )                                                            \
+   {                                                                                                            \
+      real_t L2   = QUADPOINTS[k][0];                                                                           \
+      real_t L3   = QUADPOINTS[k][1];                                                                           \
+      real_t L1   = 1.0 - L2 - L3;                                                                              \
+      mappedPt[0] = ( coords[1][0] - coords[0][0] ) * L2 + ( coords[2][0] - coords[0][0] ) * L3 + coords[0][0]; \
+      mappedPt[1] = ( coords[1][1] - coords[0][1] ) * L2 + ( coords[2][1] - coords[0][1] ) * L3 + coords[0][1]; \
+      Matrix2r DPsi;                                                                                            \
+      geometryMap_->evalDF( mappedPt, DPsi );                                                                   \
+                                                                                                                \
+      real_t tmp4 = DPsi( 0, 1 ) * tmp3 + DPsi( 0, 0 ) * tmp0;                                                  \
+      real_t tmp5 = DPsi( 0, 1 ) * tmp1 + DPsi( 0, 0 ) * tmp2;                                                  \
+      real_t tmp6 = DPsi( 1, 0 ) * tmp2 + DPsi( 1, 1 ) * tmp1;                                                  \
+      real_t tmp7 = DPsi( 1, 0 ) * tmp0 + DPsi( 1, 1 ) * tmp3;                                                  \
+                                                                                                                \
+      real_t aux3 = DPsi( 0, 1 ) * DPsi( 1, 0 ) - DPsi( 1, 1 ) * DPsi( 0, 0 );                                  \
+      real_t aux4 = aux3 * aux3;                                                                                \
+                                                                                                                \
+      real_t detDPsi = std::abs( aux3 );                                                                        \
+                                                                                                                \
+      real_t aux5 = ( ( -DetaN##i * tmp5 + DxiN##i * tmp4 ) * ( -DetaN##j * tmp5 + DxiN##j * tmp4 ) +           \
+                      ( DetaN##i * tmp6 - DxiN##i * tmp7 ) * ( DetaN##j * tmp6 - DxiN##j * tmp7 ) ) /           \
+                    ( aux4 * aux2 );                                                                            \
+                                                                                                                \
+      elMat( i, j ) += QUADWEIGHTS[k] * detJacPhiInv * detDPsi * aux5;                                          \
+   }                                                                                                            \
    elMat( j, i ) = elMat( i, j )
 
       // compute Jacobian determinant of inverse pull-back mapping
-      real_t tmp0         = coords[0][0] - coords[1][0];
-      real_t tmp1         = coords[0][1] - coords[2][1];
-      real_t tmp2         = coords[0][0] - coords[2][0];
-      real_t tmp3         = coords[0][1] - coords[1][1];
-      real_t tmp4         = tmp0 * tmp1 - tmp2 * tmp3;
-      real_t tmp5         = 1.0 / ( tmp4 * tmp4 );
-      real_t detJacPhiInv = std::abs( tmp4 );
+      real_t tmp0 = coords[0][0] - coords[1][0];
+      real_t tmp1 = coords[0][1] - coords[2][1];
+      real_t tmp2 = coords[0][0] - coords[2][0];
+      real_t tmp3 = coords[0][1] - coords[1][1];
+
+      real_t aux1 = tmp0 * tmp1 - tmp2 * tmp3;
+      real_t aux2 = aux1 * aux1;
+
+      real_t detJacPhiInv = std::abs( aux1 );
 
       // Quadrature point mapped to computational triangle
       Point3D mappedPt;
