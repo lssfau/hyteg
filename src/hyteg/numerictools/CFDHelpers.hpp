23fad40fc55cf233c9dacd2b5c49ac0614fd0d55
Nils Kohl <nils.kohl@fau.de>
2020-04-03 19:32:53 +0200

    Added 2D velocity max magnitude helper

--- src/hyteg/numerictools/CFDHelpers.hpp
+++ src/hyteg/numerictools/CFDHelpers.hpp
@@ -19,8 +19,6 @@ real_t velocityMaxMagnitude( const FunctionType& velocityX,
                              const uint_t & level,
                              const DoFType & flag )
 {
-   // Could be done with interpolateExtended() and without tmp variables, however, this version should be much faster.
-
    tmp2.interpolate( 0, level, flag );
 
    tmp1.assign( {1.0}, {velocityX}, level, flag );
@@ -32,6 +30,27 @@ real_t velocityMaxMagnitude( const FunctionType& velocityX,
    tmp2.assign( {1.0, 1.0}, {tmp2, tmp1}, level, flag );
 
    tmp1.assign( {1.0}, {velocityZ}, level, flag );
+   tmp1.multElementwise( {tmp1, tmp1}, level, flag );
+   tmp2.assign( {1.0, 1.0}, {tmp2, tmp1}, level, flag );
+
+   return std::sqrt( tmp2.getMaxMagnitude( level, flag ) );
+}
+
+template < typename FunctionType >
+real_t velocityMaxMagnitude( const FunctionType& velocityX,
+                             const FunctionType& velocityY,
+                             const FunctionType& tmp1,
+                             const FunctionType& tmp2,
+                             const uint_t & level,
+                             const DoFType & flag )
+{
+   tmp2.interpolate( 0, level, flag );
+
+   tmp1.assign( {1.0}, {velocityX}, level, flag );
+   tmp1.multElementwise( {tmp1, tmp1}, level, flag );
+   tmp2.assign( {1.0, 1.0}, {tmp2, tmp1}, level, flag );
+
+   tmp1.assign( {1.0}, {velocityY}, level, flag );
    tmp1.multElementwise( {tmp1, tmp1}, level, flag );
    tmp2.assign( {1.0, 1.0}, {tmp2, tmp1}, level, flag );
 
