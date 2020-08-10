801232ee6df24018ddca8d7ef06ea3dc2906e2a1
Daniel Drzisga <drzisga@ma.tum.de>
2020-08-10 13:55:30 +0200

    fixed P2 normal projection in 3D

--- src/hyteg/edgedofspace/freeslip/EdgeDoFProjectNormal.hpp
+++ src/hyteg/edgedofspace/freeslip/EdgeDoFProjectNormal.hpp
@@ -21,47 +21,62 @@
 
 #include "core/debug/all.h"
 
-#include "hyteg/primitives/Face.hpp"
 #include "hyteg/Levelinfo.hpp"
 #include "hyteg/Macros.hpp"
-#include "hyteg/p1functionspace/VertexDoFMemory.hpp"
-#include "hyteg/p1functionspace/P1Elements.hpp"
 #include "hyteg/indexing/Common.hpp"
-#include "hyteg/polynomial/PolynomialEvaluator.hpp"
-#include "hyteg/p1functionspace/VertexDoFMacroFace.hpp"
+#include "hyteg/p1functionspace/P1Elements.hpp"
 #include "hyteg/p1functionspace/VertexDoFMacroEdge.hpp"
+#include "hyteg/p1functionspace/VertexDoFMacroFace.hpp"
+#include "hyteg/p1functionspace/VertexDoFMemory.hpp"
+#include "hyteg/polynomial/PolynomialEvaluator.hpp"
+#include "hyteg/primitives/Face.hpp"
 
-using walberla::uint_t;
 using walberla::real_c;
+using walberla::uint_t;
 
 namespace hyteg {
 namespace edgedof {
 
-void projectionMatrix3D(const Point3D& normal, Matrix3r& projection)
+void projectionMatrix2D( const Point3D& normal, Matrix2r& projection )
 {
-   projection(0,0) = 1.0 - normal[0] * normal[0];
-   projection(0,1) = - normal[0] * normal[1];
-   projection(0,2) = - normal[0] * normal[2];
-   projection(1,0) = projection(0,1);
-   projection(1,1) = 1.0 - normal[1] * normal[1];
-   projection(1,2) = - normal[1] * normal[2];
-   projection(2,0) = projection(0,2);
-   projection(2,1) = projection(1,2);
-   projection(2,2) = 1.0 - normal[2] * normal[2];
+   projection( 0, 0 ) = 1.0 - normal[0] * normal[0];
+   projection( 0, 1 ) = -normal[0] * normal[1];
+   projection( 1, 0 ) = projection( 0, 1 );
+   projection( 1, 1 ) = 1.0 - normal[1] * normal[1];
+}
+
+void projectionMatrix3D( const Point3D& normal, Matrix3r& projection )
+{
+   projection( 0, 0 ) = 1.0 - normal[0] * normal[0];
+   projection( 0, 1 ) = -normal[0] * normal[1];
+   projection( 0, 2 ) = -normal[0] * normal[2];
+   projection( 1, 0 ) = projection( 0, 1 );
+   projection( 1, 1 ) = 1.0 - normal[1] * normal[1];
+   projection( 1, 2 ) = -normal[1] * normal[2];
+   projection( 2, 0 ) = projection( 0, 2 );
+   projection( 2, 1 ) = projection( 1, 2 );
+   projection( 2, 2 ) = 1.0 - normal[2] * normal[2];
 }
 
 namespace macroface {
 
 template < typename ValueType >
-inline void projectNormal3D(uint_t level, const Face &face, const std::shared_ptr<PrimitiveStorage> &storage, const std::function<void(const Point3D&, Point3D& )>& normal_function, const PrimitiveDataID<FunctionMemory<ValueType>, Face> &dstIdU, const PrimitiveDataID<FunctionMemory<ValueType>, Face> &dstIdV, const PrimitiveDataID<FunctionMemory<ValueType>, Face> &dstIdW) {
+inline void projectNormal3D( uint_t                                                      level,
+                             const Face&                                                 face,
+                             const std::shared_ptr< PrimitiveStorage >&                  storage,
+                             const std::function< void( const Point3D&, Point3D& ) >&    normal_function,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& dstIdU,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& dstIdV,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& dstIdW )
+{
    auto dstU = face.getData( dstIdU )->getPointer( level );
    auto dstV = face.getData( dstIdV )->getPointer( level );
    auto dstW = face.getData( dstIdW )->getPointer( level );
 
-   Point3D normal;
+   Point3D  normal;
    Matrix3r projection;
-   Point3D in;
-   Point3D out;
+   Point3D  in;
+   Point3D  out;
 
    Point3D x;
    Point3D xBlend;
@@ -89,8 +104,8 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
       if ( it.row() != 0 )
       {
          face.getGeometryMap()->evalF( horizontalMicroEdgePosition, xBlend );
-         normal_function(xBlend, normal);
-         projectionMatrix3D(normal, projection);
+         normal_function( xBlend, normal );
+         projectionMatrix3D( normal, projection );
 
          const uint_t idx = edgedof::macroface::horizontalIndex( level, it.col(), it.row() );
 
@@ -98,7 +113,7 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
          in[1] = dstV[idx];
          in[2] = dstW[idx];
 
-         out = projection.mul(in);
+         out = projection.mul( in );
 
          dstU[idx] = out[0];
          dstV[idx] = out[1];
@@ -109,8 +124,8 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
       if ( it.col() != 0 )
       {
          face.getGeometryMap()->evalF( verticalMicroEdgePosition, xBlend );
-         normal_function(xBlend, normal);
-         projectionMatrix3D(normal, projection);
+         normal_function( xBlend, normal );
+         projectionMatrix3D( normal, projection );
 
          const uint_t idx = edgedof::macroface::verticalIndex( level, it.col(), it.row() );
 
@@ -118,7 +133,7 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
          in[1] = dstV[idx];
          in[2] = dstW[idx];
 
-         out = projection.mul(in);
+         out = projection.mul( in );
 
          dstU[idx] = out[0];
          dstV[idx] = out[1];
@@ -129,8 +144,8 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
       if ( it.col() + it.row() != ( hyteg::levelinfo::num_microedges_per_edge( level ) - 1 ) )
       {
          face.getGeometryMap()->evalF( diagonalMicroEdgePosition, xBlend );
-         normal_function(xBlend, normal);
-         projectionMatrix3D(normal, projection);
+         normal_function( xBlend, normal );
+         projectionMatrix3D( normal, projection );
 
          const uint_t idx = edgedof::macroface::diagonalIndex( level, it.col(), it.row() );
 
@@ -138,7 +153,7 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
          in[1] = dstV[idx];
          in[2] = dstW[idx];
 
-         out = projection.mul(in);
+         out = projection.mul( in );
 
          dstU[idx] = out[0];
          dstV[idx] = out[1];
@@ -152,7 +167,13 @@ inline void projectNormal3D(uint_t level, const Face &face, const std::shared_pt
 namespace macroedge {
 
 template < typename ValueType >
-inline void projectNormal2D(uint_t level, const Edge &edge, const std::shared_ptr<PrimitiveStorage> &storage, const std::function<void(const Point3D&, Point3D& )>& normal_function, const PrimitiveDataID<FunctionMemory<ValueType>, Edge> &dstIdU, const PrimitiveDataID<FunctionMemory<ValueType>, Edge> &dstIdV) {
+inline void projectNormal2D( uint_t                                                      level,
+                             const Edge&                                                 edge,
+                             const std::shared_ptr< PrimitiveStorage >&                  storage,
+                             const std::function< void( const Point3D&, Point3D& ) >&    normal_function,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& dstIdU,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& dstIdV )
+{
    auto dstU = edge.getData( dstIdU )->getPointer( level );
    auto dstV = edge.getData( dstIdV )->getPointer( level );
 
@@ -161,10 +182,10 @@ inline void projectNormal2D(uint_t level, const Edge &edge, const std::shared_pt
 
    const Point3D microEdgeOffset = ( rightCoords - leftCoords ) / real_c( 2 * levelinfo::num_microedges_per_edge( level ) );
 
-   Point3D normal;
+   Point3D  normal;
    Matrix2r projection;
-   Point2D in;
-   Point2D out;
+   Point2D  in;
+   Point2D  out;
 
    Point3D xPhy;
 
@@ -173,19 +194,15 @@ inline void projectNormal2D(uint_t level, const Edge &edge, const std::shared_pt
       const Point3D currentCoordinates = leftCoords + microEdgeOffset + real_c( 2 ) * it.col() * microEdgeOffset;
       edge.getGeometryMap()->evalF( currentCoordinates, xPhy );
 
-      normal_function(xPhy, normal);
-
-      projection(0,0) = 1.0 - normal[0] * normal[0];
-      projection(0,1) = - normal[0] * normal[1];
-      projection(1,0) = projection(0,1);
-      projection(1,1) = 1.0 - normal[1] * normal[1];
+      normal_function( xPhy, normal );
+      projectionMatrix2D( normal, projection );
 
       const uint_t idx = edgedof::macroedge::indexFromHorizontalEdge( level, it.col(), stencilDirection::EDGE_HO_C );
 
       in[0] = dstU[idx];
       in[1] = dstV[idx];
 
-      out = projection.mul(in);
+      out = projection.mul( in );
 
       dstU[idx] = out[0];
       dstV[idx] = out[1];
@@ -193,8 +210,14 @@ inline void projectNormal2D(uint_t level, const Edge &edge, const std::shared_pt
 }
 
 template < typename ValueType >
-inline void projectNormal3D(uint_t level, const Edge &edge, const std::shared_ptr<PrimitiveStorage> &storage, const std::function<void(const Point3D&, Point3D& )>& normal_function, const PrimitiveDataID<FunctionMemory<ValueType>, Edge> &dstIdU, const PrimitiveDataID<FunctionMemory<ValueType>, Edge> &dstIdV, const PrimitiveDataID<FunctionMemory<ValueType>, Edge> &dstIdW) {
-
+inline void projectNormal3D( uint_t                                                      level,
+                             const Edge&                                                 edge,
+                             const std::shared_ptr< PrimitiveStorage >&                  storage,
+                             const std::function< void( const Point3D&, Point3D& ) >&    normal_function,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& dstIdU,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& dstIdV,
+                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& dstIdW )
+{
    auto dstU = edge.getData( dstIdU )->getPointer( level );
    auto dstV = edge.getData( dstIdV )->getPointer( level );
    auto dstW = edge.getData( dstIdW )->getPointer( level );
@@ -204,20 +227,20 @@ inline void projectNormal3D(uint_t level, const Edge &edge, const std::shared_pt
 
    const Point3D microEdgeOffset = ( rightCoords - leftCoords ) / real_c( 2 * levelinfo::num_microedges_per_edge( level ) );
 
-   Point3D normal;
-   Matrix2r projection;
-   Point2D in;
-   Point2D out;
+   Point3D  normal;
+   Matrix3r projection;
+   Point3D  in;
+   Point3D  out;
 
    Point3D xPhy;
 
-   for ( const auto & it : vertexdof::macroedge::Iterator( level, 1 ) )
+   for ( const auto& it : edgedof::macroedge::Iterator( level ) )
    {
       const Point3D currentCoordinates = leftCoords + microEdgeOffset + real_c( 2 ) * it.col() * microEdgeOffset;
       edge.getGeometryMap()->evalF( currentCoordinates, xPhy );
 
-      normal_function(xPhy, normal);
-      projectionMatrix3D(normal, projection);
+      normal_function( xPhy, normal );
+      projectionMatrix3D( normal, projection );
 
       const uint_t idx = edgedof::macroedge::indexFromHorizontalEdge( level, it.col(), stencilDirection::EDGE_HO_C );
 
@@ -225,7 +248,7 @@ inline void projectNormal3D(uint_t level, const Edge &edge, const std::shared_pt
       in[1] = dstV[idx];
       in[2] = dstW[idx];
 
-      out = projection.mul(in);
+      out = projection.mul( in );
 
       dstU[idx] = out[0];
       dstV[idx] = out[1];
