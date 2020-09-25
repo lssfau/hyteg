746ead2af25879d5450f08b80502061c9ea10c6e
Marcus Mohr <marcus.mohr@lmu.de>
2020-09-25 17:42:02 +0200

    Splits sources for Horbach benchmark into two files.

--- apps/SphericalHarmonics/bmark.cpp
+++ apps/SphericalHarmonics/bmark.cpp
@@ -68,202 +68,13 @@ using walberla::real_c;
 using walberla::real_t;
 using namespace hyteg;
 
+#include "bmark_tools.hpp"
+
 // radii of spherical shell
 const real_t innerRadius = 1.0;
 const real_t outerRadius = 2.0;
 
 namespace terraneo {
-
-// type to distinguish different scenarios w.r.t. boundary conditions
-typedef enum
-{
-   BC_NOSLIP,
-   BC_FREESLIP,
-   BC_MIXED
-} bcType;
-
-// ===============
-//  SetForceField
-// ===============
-template < typename vecFuncType >
-void setForceField( vecFuncType&                              f,
-                    uint_t                                    level,
-                    uint_t                                    degree,
-                    int                                       order,
-                    std::shared_ptr< SphericalHarmonicsTool > sphTool,
-                    bcType                                    bmCase )
-{
-   // safety check
-   if ( (uint_t) std::abs( order ) > degree )
-   {
-      WALBERLA_ABORT( "Spherical harmonics order must be smaller equal to degree!" );
-   }
-
-   // first we compute the spherical harmonics contribution which is the same
-   // for all three component functions
-   std::function< real_t( const Point3D& ) > sphFunc = [sphTool, degree, order]( const Point3D& x ) {
-      return sphTool->shconvert_eval( degree, order, x[0], x[1], x[2] );
-   };
-
-   typename vecFuncType::template VectorComponentType sph( "spherical harmonic", f[0].getStorage(), level, level );
-   // feFuncType sph( "spherical harmonic", f[0].getStorage(), level, level );
-   sph.interpolate( sphFunc, level, All );
-
-   // set polynomial coefficients for radial component
-   real_t                  elfac = real_c( degree * ( degree + 1 ) );
-   std::array< real_t, 5 > pc;
-
-   pc[0] = ( 12.0 - elfac ) * ( 2.0 - elfac );
-   pc[1] = elfac * ( 6.0 - elfac );
-   pc[2] = elfac * ( elfac - 2.0 );
-   pc[3] = elfac * ( 2.0 - elfac );
-   pc[4] = elfac * ( elfac - 6.0 );
-
-   switch ( bmCase )
-   {
-   case BC_NOSLIP:
-      pc[0] *= 0.25;
-      pc[1] *= 1.50;
-      pc[2] *= 3.25;
-      pc[3] *= 3.00;
-      pc[4] *= 1.00;
-      break;
-
-   case BC_FREESLIP:
-      pc[0] *= 7.0 / 24.0;
-      pc[1] *= 1.875;
-      pc[2] *= 49.0 / 12.0;
-      pc[3] *= 3.5;
-      pc[4] *= 1.0;
-      break;
-
-   case BC_MIXED:
-      pc[0] *= 0.375;
-      pc[1] *= 2.125;
-      pc[2] *= 4.250;
-      pc[3] *= 3.500;
-      pc[4] *= 1.000;
-      break;
-   }
-
-   // x-component
-   std::function< real_t( const Point3D& ) > funcX = [pc]( const Point3D& x ) {
-      real_t rad = sqrt( x[0] * x[0] + x[1] * x[1] + x[2] * x[2] );
-      WALBERLA_ASSERT( rad < 2.0 + 1e-12 );
-      WALBERLA_ASSERT( rad > 1.0 - 1e-12 );
-      real_t iRad  = 1.0 / rad;
-      real_t value = ( pc[0] + ( pc[1] + ( pc[2] + ( pc[3] + pc[4] * iRad ) * iRad ) * iRad ) * iRad );
-
-      return value * x[0] * iRad;
-   };
-
-   f[0].interpolate( funcX, level, All );
-   f[0].multElementwise( {f[0], sph}, level, All );
-
-   // y-component
-   std::function< real_t( const Point3D& ) > funcY = [pc]( const Point3D& x ) {
-      real_t rad = sqrt( x[0] * x[0] + x[1] * x[1] + x[2] * x[2] );
-      WALBERLA_ASSERT( rad < 2.0 + 1e-12 );
-      WALBERLA_ASSERT( rad > 1.0 - 1e-12 );
-      real_t iRad  = 1.0 / rad;
-      real_t value = ( pc[0] + ( pc[1] + ( pc[2] + ( pc[3] + pc[4] * iRad ) * iRad ) * iRad ) * iRad );
-
-      return value * x[1] * iRad;
-   };
-
-   f[1].interpolate( funcY, level, All );
-   f[1].multElementwise( {f[1], sph}, level, All );
-
-   // z-component
-   std::function< real_t( const Point3D& ) > funcZ = [pc]( const Point3D& x ) {
-      real_t rad = sqrt( x[0] * x[0] + x[1] * x[1] + x[2] * x[2] );
-      WALBERLA_ASSERT( rad < 2.0 + 1e-12 );
-      WALBERLA_ASSERT( rad > 1.0 - 1e-12 );
-      real_t iRad  = 1.0 / rad;
-      real_t value = ( pc[0] + ( pc[1] + ( pc[2] + ( pc[3] + pc[4] * iRad ) * iRad ) * iRad ) * iRad );
-
-      return value * x[2] * iRad;
-   };
-
-   f[2].interpolate( funcZ, level, All );
-   f[2].multElementwise( {f[2], sph}, level, All );
-}
-
-// =======================
-//  SetAnalyticalSolution
-// =======================
-template < typename vecFuncType >
-void setAnalyticSolution( vecFuncType&                              u,
-                          uint_t                                    level,
-                          uint_t                                    degree,
-                          int                                       order,
-                          std::shared_ptr< SphericalHarmonicsTool > sphTool,
-                          bcType                                    bmCase )
-{
-   // safety check
-   if ( (uint_t) std::abs( order ) > degree )
-   {
-      WALBERLA_ABORT( "Spherical harmonics order must be smaller equal to degree!" );
-   }
-
-   // set polynomial coefficients for radial component
-   real_t                  elfac = real_c( degree * ( degree + 1 ) );
-   std::array< real_t, 5 > pCoeff;
-
-   switch ( bmCase )
-   {
-   case BC_NOSLIP:
-      pCoeff[0] = 1.00;
-      pCoeff[1] = -3.00;
-      pCoeff[2] = 3.25;
-      pCoeff[3] = -1.50;
-      pCoeff[4] = 0.25;
-      break;
-
-   case BC_FREESLIP:
-      pCoeff[0] = 1.00;
-      pCoeff[1] = -7.0 / 2.0;
-      pCoeff[2] = 49.0 / 12.0;
-      pCoeff[3] = -15.0 / 8.0;
-      pCoeff[4] = 7.0 / 24.0;
-      break;
-
-   case BC_MIXED:
-      pCoeff[0] = 1.00;
-      pCoeff[1] = -7.0 / 2.0;
-      pCoeff[2] = 17.0 / 4.0;
-      pCoeff[3] = -17.0 / 8.0;
-      pCoeff[4] = 3.0 / 8.0;
-      break;
-   }
-
-   // define function for evaluating the different components
-   uint_t                                    component;
-   std::function< real_t( const Point3D& ) > vshFunc = [sphTool, degree, order, &component, pCoeff, elfac]( const Point3D& x ) {
-      real_t value = real_c( 0 );
-      real_t rad   = sqrt( x[0] * x[0] + x[1] * x[1] + x[2] * x[2] );
-
-      // check validity of radial value
-      WALBERLA_ASSERT( rad < 2.0 + 1e-12 );
-      WALBERLA_ASSERT( rad > 1.0 - 1e-12 );
-
-      real_t poly = ( ( ( pCoeff[4] * rad + pCoeff[3] ) * rad + pCoeff[2] ) * rad + pCoeff[1] ) * rad + pCoeff[0];
-
-      real_t polyDeriv = ( ( 4.0 * pCoeff[4] * rad + 3.0 * pCoeff[3] ) * rad + 2.0 * pCoeff[2] ) * rad + pCoeff[1];
-
-      // Add Y_(l,m)^0 contribution
-      value = sphTool->evalVSH( degree, order, x[0], x[1], x[2], 0, component ) * poly * elfac / ( rad * rad );
-
-      // Add Y_(l,m)^1 contribution
-      value += sphTool->evalVSH( degree, order, x[0], x[1], x[2], 1, component ) * polyDeriv * sqrt( elfac ) / rad;
-
-      return value;
-   };
-
-   // interpolate component functions
-   for( component = 0; component < 3; ++component ) u[component].interpolate( vshFunc, level, All );
-
-}
 
 // ===================
 //  runBenchmarkTests
@@ -450,13 +261,13 @@ void runBenchmarkTests( std::shared_ptr< walberla::config::Config > cfg, std::sh
 
    if ( problemCfg.getParameter< bool >( "compute_L2_error" ) )
    {
-      real_t L2error = 0.0;
+      real_t                                      L2error = 0.0;
       typename vf_t::template VectorComponentType aux( "aux", storage, maxLevel, maxLevel );
 
-      for( uint_t idx = 0; idx < 3; ++idx )
+      for ( uint_t idx = 0; idx < 3; ++idx )
       {
-        mass.apply( error[idx], aux, maxLevel, All );
-        L2error += error[idx].dotGlobal( aux, maxLevel );
+         mass.apply( error[idx], aux, maxLevel, All );
+         L2error += error[idx].dotGlobal( aux, maxLevel );
       }
       L2error = std::sqrt( L2error );
       WALBERLA_LOG_INFO_ON_ROOT( "L2 error = " << std::scientific << L2error );
@@ -470,19 +281,19 @@ void runBenchmarkTests( std::shared_ptr< walberla::config::Config > cfg, std::sh
       vf_t   u_fine( "u_fine", storage, fLevel, fLevel );
       setAnalyticSolution( u_fine, fLevel, degree, order, sphTool, bmarkBC );
 
-      typename vf_t::template VectorComponentType aux( "aux", storage, maxLevel, maxLevel+1 );
-      P2toP2QuadraticProlongation embedder;
+      typename vf_t::template VectorComponentType aux( "aux", storage, maxLevel, maxLevel + 1 );
+      P2toP2QuadraticProlongation                 embedder;
 
       // TEST
       // feSol.prolongate( maxLevel, All ); there is no P2Function::prolongate !!!
 
-      for( uint_t idx = 0; idx < 3; ++idx )
+      for ( uint_t idx = 0; idx < 3; ++idx )
       {
-        aux.assign( {1.0}, {feSol.uvw[idx]}, maxLevel );
-        embedder.prolongate( aux, maxLevel, All );
-        aux.assign( {1.0, -1.0}, {u_fine[idx], aux}, fLevel );
-        real_t val = aux.getMaxMagnitude( fLevel );
-        maxError   = maxError < val ? val : maxError;
+         aux.assign( {1.0}, {feSol.uvw[idx]}, maxLevel );
+         embedder.prolongate( aux, maxLevel, All );
+         aux.assign( {1.0, -1.0}, {u_fine[idx], aux}, fLevel );
+         real_t val = aux.getMaxMagnitude( fLevel );
+         maxError   = maxError < val ? val : maxError;
       }
 
       WALBERLA_LOG_INFO_ON_ROOT( "Maximum error = " << std::scientific << maxError );
@@ -494,13 +305,13 @@ void runBenchmarkTests( std::shared_ptr< walberla::config::Config > cfg, std::sh
    if ( problemCfg.getParameter< bool >( "VTKOutput" ) )
    {
       hyteg::VTKOutput vtkOutput( "./output", "bmark", storage );
-      for( uint_t idx = 0; idx < 3; ++idx )
+      for ( uint_t idx = 0; idx < 3; ++idx )
       {
-        vtkOutput.add( forcing[idx] );
-        vtkOutput.add( rhs.uvw[idx] );
-        vtkOutput.add( u_exact[idx] );
-        vtkOutput.add( feSol.uvw[idx] );
-        vtkOutput.add( error[idx] );
+         vtkOutput.add( forcing[idx] );
+         vtkOutput.add( rhs.uvw[idx] );
+         vtkOutput.add( u_exact[idx] );
+         vtkOutput.add( feSol.uvw[idx] );
+         vtkOutput.add( error[idx] );
       }
       vtkOutput.write( maxLevel, 0 );
    }
