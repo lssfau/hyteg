bbfac137f6715bf7c5d91e99b0cfb2b5589fc86f
Nils Kohl <nils.kohl@fau.de>
2020-11-17 16:21:54 +0100

    Fixed typo

--- apps/2020-moc/MOCScaling/load_modules_supermuc.sh
+++ apps/2020-moc/MOCScaling/load_modules_supermuc.sh
@@ -8,7 +8,7 @@ module load devEnv/GCC
 module load cmake
 module load boost
 module load petsc
-moduel load metis/5.1.0-i64-r64
+module load metis/5.1.0-i64-r64
 module load parmetis/4.0.3-impi-i64-r64
 
 export CC=gcc
