158dd47e96f6b3a2239e6e16ef8daae7a6d9f9f5
Nils Kohl <nils.kohl@fau.de>
2021-03-05 12:51:45 +0100

    [WIP] Re-configuring parameter study with improved omega. Added omega sweep parameterizations.

--- apps/2020-tme/run_all_benchmarks.py
+++ apps/2020-tme/run_all_benchmarks.py
@@ -3,39 +3,181 @@ import datetime
 import time
 import os
 import subprocess
+import numpy as np
 
 executables = {
-    1: 'Benchmark_01_Cube', # discr error P2-P1
-    2: 'Benchmark_01_Cube', # discr error P1-P1
-    3: 'Benchmark_01_Cube', # FMG table, and convergence plot, P2-P1
-    4: 'Benchmark_01_Cube', # v-cycles, P2-P1
-    5: 'Benchmark_01_Cube', # FMG table, and convergence plot, P1-P1
-    6: 'Benchmark_02_Y-Pipe',
+
+    # discretization errors
+
+    'discr-cube-p2p1': 'Benchmark_01_Cube',
+    'discr-cube-p1p1': 'Benchmark_01_Cube',
+
+    'discr-pipe-p2p1': 'Benchmark_02_Y-Pipe',
+    'discr-pipe-p1p1': 'Benchmark_02_Y-Pipe',
+
+    # fmg studies
+
+    'fmg-cube-p2p1': 'Benchmark_01_Cube',
+    'fmg-cube-p1p1': 'Benchmark_01_Cube',
+
+    'fmg-pipe-p2p1': 'Benchmark_02_Y-Pipe',
+    'fmg-pipe-p1p1': 'Benchmark_02_Y-Pipe',
 }
 
+for prm in ['omega-cube-p1p1-1-1-2-1symm', 'omega-cube-p1p1-2-2-2-1symm', 'omega-cube-p1p1-3-3-2-1symm', 'omega-cube-p1p1-1-1-2-3fwd', 'omega-cube-p1p1-2-2-2-3fwd', 'omega-cube-p1p1-3-3-2-3fwd',
+            'omega-cube-p2p1-1-1-2-1symm', 'omega-cube-p2p1-2-2-2-1symm', 'omega-cube-p2p1-3-3-2-1symm', 'omega-cube-p2p1-1-1-2-3fwd', 'omega-cube-p2p1-2-2-2-3fwd', 'omega-cube-p2p1-3-3-2-3fwd']:
+    executables[prm] = 'Benchmark_01_Cube'
+
+for prm in ['omega-pipe-p1p1-1-1-2-1symm', 'omega-pipe-p1p1-2-2-2-1symm', 'omega-pipe-p1p1-3-3-2-1symm', 'omega-pipe-p1p1-1-1-2-3fwd', 'omega-pipe-p1p1-2-2-2-3fwd', 'omega-pipe-p1p1-3-3-2-3fwd',
+            'omega-pipe-p2p1-1-1-2-1symm', 'omega-pipe-p2p1-2-2-2-1symm', 'omega-pipe-p2p1-3-3-2-1symm', 'omega-pipe-p2p1-1-1-2-3fwd', 'omega-pipe-p2p1-2-2-2-3fwd', 'omega-pipe-p2p1-3-3-2-3fwd']:
+    executables[prm] = 'Benchmark_02_Y-Pipe'
+
 # omega
+
+# from omega sweep:
+# cube:
+# - P1-P1-stab: 0.1
+# - P2-P1:      0.2
+# pipe:
+# - P1-P1-stab: 0.3
+# - P2-P1:      0.4
+
+# old:
 # P2-P1, level 5, 100 iterations: 0.448872
 # P1-P1, level 6, 100 iterations: 0.570751 -> bad choice, v-cycles diverge, but seems to work for FMG
 # P1-P1, level 6, guess:          0.3      -> seems to work for FMG and v-cycles
 
+omega_base = {
+    'minLevel': 0,
+    'numEdgesPerSide': 1,
+    'vtk': False,
+    'numCycles': 1,
+    'absoluteResidualTolerance': 1e-12,
+    'estimateOmega': False,
+    'coarseGridSolverType': 1,
+    'normCalculationLevelIncrement': 1,
+    'calculateDiscretizationError': False
+}
+
+discr_base =  {'minLevel': 0, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 100, 'absoluteResidualTolerance': 1e-12,
+               'estimateOmega': False, 'preSmooth': 10, 'postSmooth': 10, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 4, 'symmGSVelocity': False,
+               'coarseGridSolverType': 1, 'normCalculationLevelIncrement': 1}
+
 parameterizations_base = {
-    1: {'discretization': 'p2p1', 'minLevel': 0, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 100, 'absoluteResidualTolerance': 1e-12,
-        'estimateOmega': True, 'omegaEstimationIterations': 20,
-        'preSmooth': 10, 'postSmooth': 10, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
-        'coarseGridSolverType': 0, 'normCalculationLevelIncrement': 1},
 
-    2: {'discretization': 'p1p1', 'minLevel': 0, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 100, 'absoluteResidualTolerance': 1e-12,
-        'estimateOmega': False, 'omega': 0.3,
-        'preSmooth': 10, 'postSmooth': 10, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
-        'coarseGridSolverType': 0, 'normCalculationLevelIncrement': 1},
+    # Omega estiamtion cube
 
-    3: {'discretization': 'p2p1', 'minLevel': 0, 'maxLevel': 5, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 1, 'absoluteResidualTolerance': 1e-12,
-        'estimateOmega': False, 'omega': 0.448872, 'omegaEstimationLevel': 6, 'omegaEstimationIterations': 20,
-        'coarseGridSolverType': 0, 'normCalculationLevelIncrement': 1},
+    'omega-cube-p1p1-1-1-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
 
-    4: {'discretization': 'p2p1', 'minLevel': 0, 'maxLevel': 5, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 100, 'absoluteResidualTolerance': 1e-12,
-        'estimateOmega': False, 'omega': 0.448872, 'omegaEstimationLevel': 6, 'omegaEstimationIterations': 20,
-        'coarseGridSolverType': 0, 'normCalculationLevelIncrement': 1, 'fmgInnerIterations': 0},
+    'omega-cube-p1p1-2-2-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-cube-p1p1-3-3-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-cube-p1p1-1-1-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-cube-p1p1-2-2-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-cube-p1p1-3-3-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 5, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-cube-p2p1-1-1-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-cube-p2p1-2-2-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-cube-p2p1-3-3-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-cube-p2p1-1-1-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-cube-p2p1-2-2-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-cube-p2p1-3-3-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 4, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+
+    # Omega estimation y-pipe
+
+    'omega-pipe-p1p1-1-1-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p1p1-2-2-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p1p1-3-3-2-1symm' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p1p1-1-1-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-pipe-p1p1-2-2-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-pipe-p1p1-3-3-2-3fwd' : {**omega_base, **{
+        'discretization': 'p1p1', 'maxLevel': 4, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-pipe-p2p1-1-1-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p2p1-2-2-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p2p1-3-3-2-1symm' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 1, 'symmGSVelocity': True,
+    }},
+
+    'omega-pipe-p2p1-1-1-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 1, 'postSmooth': 1, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-pipe-p2p1-2-2-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 2, 'postSmooth': 2, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    'omega-pipe-p2p1-3-3-2-3fwd' : {**omega_base, **{
+        'discretization': 'p2p1', 'maxLevel': 3, 'preSmooth': 3, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False,
+    }},
+
+    ########################### discr error ###########################
+
+    'discr-cube-p1p1': {**discr_base, **{'discretization': 'p1p1', 'omega': 0.1}},
+    'discr-cube-p2p1': {**discr_base, **{'discretization': 'p2p1', 'omega': 0.2}},
+
+    ########################
+
+    'fmg-cube-p1p1': {'discretization': 'p1p1', 'minLevel': 0, 'maxLevel': 6, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 1, 'absoluteResidualTolerance': 1e-12,
+                      'estimateOmega': False, 'omega': 0.1, 'coarseGridSolverType': 1, 'normCalculationLevelIncrement': 1, 'fmgInnerIterations': 0},
+
+    'fmg-cube-p2p1': {'discretization': 'p2p1', 'minLevel': 0, 'maxLevel': 5, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 1, 'absoluteResidualTolerance': 1e-12,
+                      'estimateOmega': False, 'omega': 0.2, 'coarseGridSolverType': 1, 'normCalculationLevelIncrement': 1, 'fmgInnerIterations': 0},
 
     5: {'discretization': 'p1p1', 'minLevel': 0, 'maxLevel': 6, 'numEdgesPerSide': 1, 'vtk': False, 'numCycles': 1, 'absoluteResidualTolerance': 1e-12,
         'estimateOmega': False, 'omega': 0.570751, 'omegaEstimationLevel': 6, 'omegaEstimationIterations': 20,
@@ -56,20 +198,11 @@ parameterizations_base = {
         'DiscretizationErrorSolver.symmGSVelocity': False,
         'DiscretizationErrorSolver.estimateOmega': False,
         'DiscretizationErrorSolver.omega': 0.2,
-        'DiscretizationErrorSolver.coarseGridSolverType': 0}
+        'DiscretizationErrorSolver.coarseGridSolverType': 0},
 }
 
 parameterizations = {
-    1: [
-        {'maxLevel': 0, 'omegaEstimationLevel': 0},
-        {'maxLevel': 1, 'omegaEstimationLevel': 1},
-        {'maxLevel': 2, 'omegaEstimationLevel': 2},
-        {'maxLevel': 3, 'omegaEstimationLevel': 3},
-        {'maxLevel': 4, 'omegaEstimationLevel': 4},
-        {'maxLevel': 5, 'omegaEstimationLevel': 5},
-        {'maxLevel': 6, 'omegaEstimationLevel': 6},
-    ],
-    2: [
+    'discr-cube-p1p1': [
         {'maxLevel': 0},
         {'maxLevel': 1},
         {'maxLevel': 2},
@@ -77,30 +210,67 @@ parameterizations = {
         {'maxLevel': 4},
         {'maxLevel': 5},
         {'maxLevel': 6},
-        {'maxLevel': 7},
     ],
-    3: [],
-    4: [],
+    'discr-cube-p2p1': [
+        {'maxLevel': 0},
+        {'maxLevel': 1},
+        {'maxLevel': 2},
+        {'maxLevel': 3},
+        {'maxLevel': 4},
+        {'maxLevel': 5},
+    ],
+    'discr-pipe-p1p1': [
+        {'maxLevel': 0},
+        {'maxLevel': 1},
+        {'maxLevel': 2},
+        {'maxLevel': 3},
+        {'maxLevel': 4},
+        {'maxLevel': 5},
+    ],
+    'discr-pipe-p2p1': [
+        {'maxLevel': 0},
+        {'maxLevel': 1},
+        {'maxLevel': 2},
+        {'maxLevel': 3},
+        {'maxLevel': 4},
+    ],
+
+    'fmg-cube-p1p1': [],
+    'fmg-cube-p2p1': [],
     5: [],
 
     6: [{'preSmooth': 1, 'postSmooth': 3, 'incSmooth': 2, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False},
         {'preSmooth': 1, 'postSmooth': 2, 'incSmooth': 1, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False},
-        {'preSmooth': 0, 'postSmooth': 2, 'incSmooth': 1, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False},]
+        {'preSmooth': 0, 'postSmooth': 2, 'incSmooth': 1, 'fmgInnerIterations': 1, 'numGSVelocity': 3, 'symmGSVelocity': False},],
+
 }
+
+for prm in ['omega-cube-p1p1-1-1-2-1symm', 'omega-cube-p1p1-2-2-2-1symm', 'omega-cube-p1p1-3-3-2-1symm', 'omega-cube-p1p1-1-1-2-3fwd', 'omega-cube-p1p1-2-2-2-3fwd', 'omega-cube-p1p1-3-3-2-3fwd',
+            'omega-cube-p2p1-1-1-2-1symm', 'omega-cube-p2p1-2-2-2-1symm', 'omega-cube-p2p1-3-3-2-1symm', 'omega-cube-p2p1-1-1-2-3fwd', 'omega-cube-p2p1-2-2-2-3fwd', 'omega-cube-p2p1-3-3-2-3fwd']:
+    parameterizations[prm] = []
+
+for prm in ['omega-pipe-p1p1-1-1-2-1symm', 'omega-pipe-p1p1-2-2-2-1symm', 'omega-pipe-p1p1-3-3-2-1symm', 'omega-pipe-p1p1-1-1-2-3fwd', 'omega-pipe-p1p1-2-2-2-3fwd', 'omega-pipe-p1p1-3-3-2-3fwd',
+            'omega-pipe-p2p1-1-1-2-1symm', 'omega-pipe-p2p1-2-2-2-1symm', 'omega-pipe-p2p1-3-3-2-1symm', 'omega-pipe-p2p1-1-1-2-3fwd', 'omega-pipe-p2p1-2-2-2-3fwd', 'omega-pipe-p2p1-3-3-2-3fwd']:
+    parameterizations[prm] = []
+
+for omega in np.linspace(0.1, 1.0, 10):
+
+    for prm in ['omega-cube-p1p1-1-1-2-1symm', 'omega-cube-p1p1-2-2-2-1symm', 'omega-cube-p1p1-3-3-2-1symm', 'omega-cube-p1p1-1-1-2-3fwd', 'omega-cube-p1p1-2-2-2-3fwd', 'omega-cube-p1p1-3-3-2-3fwd',
+                'omega-cube-p2p1-1-1-2-1symm', 'omega-cube-p2p1-2-2-2-1symm', 'omega-cube-p2p1-3-3-2-1symm', 'omega-cube-p2p1-1-1-2-3fwd', 'omega-cube-p2p1-2-2-2-3fwd', 'omega-cube-p2p1-3-3-2-3fwd']:
+        parameterizations[prm].append({'omega': omega})
+
+    for prm in ['omega-pipe-p1p1-1-1-2-1symm', 'omega-pipe-p1p1-2-2-2-1symm', 'omega-pipe-p1p1-3-3-2-1symm', 'omega-pipe-p1p1-1-1-2-3fwd', 'omega-pipe-p1p1-2-2-2-3fwd', 'omega-pipe-p1p1-3-3-2-3fwd',
+                'omega-pipe-p2p1-1-1-2-1symm', 'omega-pipe-p2p1-2-2-2-1symm', 'omega-pipe-p2p1-3-3-2-1symm', 'omega-pipe-p2p1-1-1-2-3fwd', 'omega-pipe-p2p1-2-2-2-3fwd', 'omega-pipe-p2p1-3-3-2-3fwd']:
+        parameterizations[prm].append({'omega': omega})
 
 for pre in range(4):
     for post in range(4):
         for inc in range(4):
             for kappa in range(1, 3):
                 for gs, symm in [(1, True), (2, True), (1, False), (2, False), (3, False), (4, False)]:
-                    parameterizations[3].append({'preSmooth': pre, 'postSmooth': post, 'incSmooth': inc, 'fmgInnerIterations': kappa, 'numGSVelocity': gs, 'symmGSVelocity': symm})
-                    parameterizations[5].append({'preSmooth': pre, 'postSmooth': post, 'incSmooth': inc, 'fmgInnerIterations': kappa, 'numGSVelocity': gs, 'symmGSVelocity': symm})
+                    parameterizations['fmg-cube-p1p1'].append({'preSmooth': pre, 'postSmooth': post, 'incSmooth': inc, 'fmgInnerIterations': kappa, 'numGSVelocity': gs, 'symmGSVelocity': symm})
+                    parameterizations['fmg-cube-p2p1'].append({'preSmooth': pre, 'postSmooth': post, 'incSmooth': inc, 'fmgInnerIterations': kappa, 'numGSVelocity': gs, 'symmGSVelocity': symm})
 
-for pre in range(4):
-    for post in range(4):
-        for inc in range(4):
-            for gs, symm in [(1, True), (1, False), (2, False), (3, False)]:
-                parameterizations[4].append({'preSmooth': pre, 'postSmooth': post, 'incSmooth': inc, 'numGSVelocity': gs, 'symmGSVelocity': symm})
 
 def run_all_configs(benchmark_id):
 
@@ -132,9 +302,17 @@ def run_all_configs(benchmark_id):
         with open(out_file, 'w') as f:
             f.write(completed_process.stdout)
 
-# run_all_configs(1)
-# run_all_configs(2)
-# run_all_configs(3)
-# run_all_configs(4)
-# run_all_configs(5)
-run_all_configs(6)
+
+# for prm in ['omega-cube-p1p1-1-1-2-1symm', 'omega-cube-p1p1-2-2-2-1symm', 'omega-cube-p1p1-3-3-2-1symm', 'omega-cube-p1p1-1-1-2-3fwd', 'omega-cube-p1p1-2-2-2-3fwd', 'omega-cube-p1p1-3-3-2-3fwd',
+#             'omega-cube-p2p1-1-1-2-1symm', 'omega-cube-p2p1-2-2-2-1symm', 'omega-cube-p2p1-3-3-2-1symm', 'omega-cube-p2p1-1-1-2-3fwd', 'omega-cube-p2p1-2-2-2-3fwd', 'omega-cube-p2p1-3-3-2-3fwd']:
+#     run_all_configs(prm)
+
+# for prm in ['omega-pipe-p1p1-1-1-2-1symm', 'omega-pipe-p1p1-2-2-2-1symm', 'omega-pipe-p1p1-3-3-2-1symm', 'omega-pipe-p1p1-1-1-2-3fwd', 'omega-pipe-p1p1-2-2-2-3fwd', 'omega-pipe-p1p1-3-3-2-3fwd',
+#             'omega-pipe-p2p1-1-1-2-1symm', 'omega-pipe-p2p1-2-2-2-1symm', 'omega-pipe-p2p1-3-3-2-1symm', 'omega-pipe-p2p1-1-1-2-3fwd', 'omega-pipe-p2p1-2-2-2-3fwd', 'omega-pipe-p2p1-3-3-2-3fwd']:
+#     run_all_configs(prm)
+
+# for prm in ['discr-cube-p1p1', 'discr-cube-p2p1']:
+#     run_all_configs(prm)
+
+for prm in ['fmg-cube-p1p1', 'fmg-cube-p2p1']:
+    run_all_configs(prm)
