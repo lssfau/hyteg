cb6b3310cefde0f1ef33519617558c76da9c602f
Benjamin Mann <benjamin.mann@fau.de>
2021-04-27 09:46:20 +0200

    fix tokamak function

--- data/codegen/divkgrad_solution_generator_3d.py
+++ data/codegen/divkgrad_solution_generator_3d.py
@@ -11,28 +11,62 @@ import random
 #------- coordinates ---------#
 x, y, z = symbols('x y z')
 
-def torusCoords(R, x, y, z):
-    ψ = atan2(y, x)  # angle anlong torus
-    # polar coordinates (φ,r) on crosssection
-    φ = atan2(z, sqrt(x*x + y*y) - R)
-    r = sqrt(R*R + x*x + y*y + z*z - 2*R*sqrt(x*x + y*y))
+def torusCoords(R0,R1,R2, x, y, z):
+    φ = atan2(y, x)  # angle anlong torus
 
-    return (ψ,φ,r)
+    # helper variables
+    ξ = (sqrt(x*x + y*y) - R0)/R1
+    ζ = z/R2
+    # normalized polar coordinates (θ,ϱ) on crosssection
+    θ = atan2(ζ,ξ)
+    ϱ = sqrt(ξ*ξ + ζ*ζ)
+    # ϱ = sqrt((R0**2 + x**2 + y**2 - 2*R0*sqrt(x**2 + y**2))/(R1**2) + (z**2)/(R2**2))
+
+    return (φ,θ,ϱ)
 
 
 #------ sample solutions -------#
 
 def tokamak_jump():
-    R_torus = symbols('R_torus')
-    r_torus = symbols('r_torus')
-    ψ,φ,r = torusCoords(R_torus, x,y,z)
+    # tokamak parameters
+    R0 = symbols('R0')
+    R1 = symbols('R1')
+    R2 = symbols('R2')
+    δ  = symbols('delta')
+
     # parameters to adjust position, width and limits of jump
     d_jump,r_jump,k_min,k_max = symbols('d_jump r_jump k_min k_max')
 
-    u = cos(pi/2*r/r_torus)*sin(pi*z/r_torus)
-    k = k_min + 0.5 * (k_max - k_min) * (1 + tanh(7 / d_jump * (r - r_jump)))
+    # helper variables
+    r = sqrt(x**2 + y**2) - R0
+    rR = r/R1
+    zR = z/R2
 
-    return u,k
+    φ, θ, ϱ = torusCoords(R0, R1, R2, x, y, z)
+
+    # boundary points r_min,r_max s.th. (x,y,z) with sqrt(x^2 + y^2) - R0 ∈ {r_min, r_max} are on ∂Ω
+    r_min = -cos(asin(zR) - asin(δ)*zR)
+    r_max = cos(asin(zR) + asin(δ)*zR)
+
+    u = sin(rR-δ)*(rR - r_min)*(rR - r_max) * zR*zR*sin(pi*zR)
+    # u = sin(pi*x)*sin(pi*y)*sin(pi*z)
+    # u = (rR-δ)*sin(pi*(rR - r_min)/(r_max-r_min)) * sin(pi*zR)
+    # u = (rR-δ)*cos(pi/2*ϱ)*sin(pi*zR)
+    # u = (rR-δ)*cos(pi/2*ϱ)*sin(pi*zR**3)
+    k = k_min + 0.5 * (k_max - k_min) * (1 + tanh(3.5 / d_jump * (ϱ - r_jump)))
+
+    print("// parameters for analytic solution and diffusion coefficient:")
+    print("double %s;\t\t// torus radius" %R0);
+    print("double %s;\t\t// semi-minor half axis of crossection" %R1)
+    print("double %s;\t\t// semi-major half axis of crossection" %R2)
+    print("double %s;\t\t// triangularity parameter" %δ)
+    print("double %s;\t\t// triangularity parameter" %δ)
+    print("double %s;\t\t// location of the jump, relative to radius of crosssection" %r_jump)
+    print("double %s;\t\t// smoothed width of the jump, relative to diameter of crosssection" %d_jump)
+    print("double %s, %s;\t// min/max value of jump coefficient" %(k_min,k_max))
+    print("\n// TODO initialize above variables!\n")
+
+    return u,k,ϱ*ϱ
 
 
 def example():
@@ -49,49 +83,37 @@ def gradient(u):
 def divergence(u):
     return diff(u[0],x) + diff(u[1],y) + diff(u[2],z)
 
-
-
 if __name__ == '__main__':
 
     # exact solution and diffusion coefficient
-    u,k = tokamak_jump()
-
-    print('u =', u)
-    u = simplify(u)
-    print(' = ', u)
-
-    print('k =', k)
-    k = simplify(k)
-    print(' = ', k)
+    u,k,ϱ2 = tokamak_jump()
 
     # Right hand side
     f = -divergence(k * gradient(u))
 
-    print('∇⋅(k∇u) = f =', f)
-    f = simplify(f)
-    print(' = ', f)
+    r2 = symbols('r2_xyz')
+    r2.name = 'r2(x,y,z)'
 
-    print('=========================================')
+    u = u.subs(ϱ2,r2)
+    k = k.subs(ϱ2,r2)
+    f = f.subs(ϱ2,r2)
 
+    # print('r2 = @(x,y,z) max(1e-100,', octave_code(ϱ2), ');')
+    # print('u = @(x,y,z)', octave_code(u), ';')
+    # print('k = @(x,y,z)', octave_code(k), ';')
+    # print('f = @(x,y,z)', octave_code(f), ';')
 
 
     # Generate code
     x.name = 'x[0]'
     y.name = 'x[1]'
     z.name = 'x[2]'
+    r2.name = 'r2(x)'
 
     def function_template(name, body):
-        return 'std::function<real_t(const hyteg::Point3D&)> %s = [](const hyteg::Point3D& x) { return %s; };' % (name, body)
+        return 'std::function<real_t(const hyteg::Point3D&)> %s = [=](const hyteg::Point3D& x) { return %s; };' % (name, body)
 
-    print(function_template('coeff', ccode(simplify(k))))
-    print(function_template('exact', ccode(simplify(u))))
-    print(function_template('rhs', ccode(simplify(f))))
-
-    # tokamak jump c++
-    #
-    # parameters for analytic solution and diffusion coefficient:
-    # double R_torus;       // torus radius
-    # double r_torus;       // radius of torus crosssection
-    # double r_jump;        // location of the jump (r_jump in [0,r_torus])
-    # double k_min, k_max;  // min/max value of coefficient
-    # double d_jump;        // smoothed width of jump, i.e. |coeff(r) - k_min| ≈ 0 for r < (r_jump - d_jump/2) and |coeff(r) - k_max| ≈ 0 for r > (r_jump + d_jump/2)
+    print(function_template('r2', 'std::max(1e-100,  %s)' %ccode(ϱ2)))
+    print(function_template('coeff', ccode(k)))
+    print(function_template('exact', ccode(u)))
+    print(function_template('rhs', ccode(f)))
