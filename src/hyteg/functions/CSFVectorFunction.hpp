b1c3ac34fee628f07ef96102e5687ebc9cd9d88b
Marcus Mohr <marcus.mohr@lmu.de>
2021-04-30 19:21:33 +0200

    Replaces walberla::real_t by valueType in CSFVectorFunction
    There were some class methods that used real_t as type for arguments
    or return value. Now they all use type valueType which is derived
    from the template argument of CSFVectorFunction.

--- src/hyteg/functions/CSFVectorFunction.hpp
+++ src/hyteg/functions/CSFVectorFunction.hpp
@@ -140,7 +140,7 @@ class CSFVectorFunction : public GenericFunction
       }
    }
 
-   void add( real_t scalar, size_t level, DoFType flag = All ) const
+   void add( valueType scalar, size_t level, DoFType flag = All ) const
    {
       for ( uint_t k = 0; k < compFunc_.size(); ++k )
       {
@@ -148,7 +148,7 @@ class CSFVectorFunction : public GenericFunction
       }
    }
 
-   void add( const std::vector< walberla::real_t >                                    scalars,
+   void add( const std::vector< valueType >                                           scalars,
              const std::vector< std::reference_wrapper< const VectorFunctionType > >& functions,
              size_t                                                                   level,
              DoFType                                                                  flag = All ) const
@@ -201,7 +201,7 @@ class CSFVectorFunction : public GenericFunction
       }
    }
 
-   void assign( const std::vector< walberla::real_t >                                    scalars,
+   void assign( const std::vector< valueType >                                           scalars,
                 const std::vector< std::reference_wrapper< const VectorFunctionType > >& functions,
                 size_t                                                                   level,
                 DoFType                                                                  flag = All ) const
@@ -212,7 +212,7 @@ class CSFVectorFunction : public GenericFunction
       }
    }
 
-   walberla::real_t dotLocal( const VectorFunctionType& rhs, const uint_t level, const DoFType flag = All ) const
+   valueType dotLocal( const VectorFunctionType& rhs, const uint_t level, const DoFType flag = All ) const
    {
       valueType sum = valueType( 0 );
       for ( uint_t k = 0; k < compFunc_.size(); ++k )
@@ -222,7 +222,7 @@ class CSFVectorFunction : public GenericFunction
       return sum;
    }
 
-   walberla::real_t dotGlobal( const VectorFunctionType& rhs, const uint_t level, const DoFType flag = All ) const
+   valueType dotGlobal( const VectorFunctionType& rhs, const uint_t level, const DoFType flag = All ) const
    {
       auto sum = dotLocal( rhs, level, flag );
       walberla::mpi::allReduceInplace( sum, walberla::mpi::SUM, walberla::mpi::MPIManager::instance()->comm() );
