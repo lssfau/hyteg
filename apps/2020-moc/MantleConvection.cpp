b413cf797fcdca3e8488e2bc4bbd418ebe3b5884
Nils Kohl <nils.kohl@fau.de>
2021-11-22 12:01:14 +0100

    Removing Stokes solver type from db to get rid of warning.

--- apps/2020-moc/MantleConvection.cpp
+++ apps/2020-moc/MantleConvection.cpp
@@ -425,7 +425,7 @@ void runBenchmark( real_t     cflMax,
    db.setConstantEntry( "num_macro_primitives", uint_c( setupStorage->getNumberOfPrimitives() ) );
    db.setConstantEntry( "diffusivity", diffusivity );
 
-   db.setConstantEntry( "stokes_solver_type", uint_c( solverInfo.stokesSolverType ) );
+   // db.setConstantEntry( "stokes_solver_type", uint_c( solverInfo.stokesSolverType ) );
    db.setConstantEntry( "uzawa_omega", solverInfo.uzawaOmega );
    db.setConstantEntry( "uzawa_inner_smooth", solverInfo.uzawaInnerIterations );
    db.setConstantEntry( "uzawa_pre_smooth", solverInfo.uzawaPreSmooth );
