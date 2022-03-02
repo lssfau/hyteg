8d07c1a3a48438e68bc961f5758c007462a6ec29
Nils Kohl <nils.kohl@fau.de>
2022-03-02 09:38:36 +0100

    Adding some stubs for DG0.

--- src/hyteg/dgfunctionspace/DGBasisLinearLagrange_Example.hpp
+++ src/hyteg/dgfunctionspace/DGBasisLinearLagrange_Example.hpp
@@ -31,35 +31,39 @@
 namespace hyteg {
 namespace dg {
 
-/// \brief Quick reference implementation of linear DG elements with Lagrange basis functions.
+/// \brief Quick reference implementation of const and linear DG elements with Lagrange basis functions.
 class DGBasisLinearLagrange_Example : public DGBasisInfo
 {
  public:
-   virtual uint_t minPolynomialDegree() const { return 1; }
+   virtual uint_t minPolynomialDegree() const { return 0; }
 
    virtual uint_t maxPolynomialDegree() const { return 1; }
 
    virtual uint_t numDoFsPerElement( uint_t degree ) const
    {
-      WALBERLA_CHECK_EQUAL( degree, 1, "Only linear elements supported." );
-      return 3;
+      WALBERLA_CHECK_LESS_EQUAL( degree, 1, "Only const or linear elements supported." );
+      const std::map< uint_t, uint_t > ndofs = { { 0, 1 }, { 1, 3 } };
+      return ndofs.at( degree );
    }
 
    virtual uint_t quadratureDegreeForLinearFunctional() const { return 4; }
 
-   virtual void evaluate( uint_t degree,
-                          // const std::array< Eigen::Matrix< real_t, 2, 1 >, 3 >& coords,
-                          const Eigen::Matrix< real_t, 2, 1 >& pos,
-                          const std::vector< real_t >&         dofs,
-                          real_t&                              value ) const
+   virtual void
+       evaluate( uint_t degree, const Eigen::Matrix< real_t, 2, 1 >& pos, const std::vector< real_t >& dofs, real_t& value ) const
    {
-      WALBERLA_CHECK_EQUAL( degree, 1, "Only degree 1 supported." );
-      WALBERLA_CHECK_EQUAL(
-          dofs.size(), numDoFsPerElement( degree ), "Linear Lagrange basis requires exactly 3 DoFs per element." );
+      WALBERLA_CHECK_EQUAL( dofs.size(), numDoFsPerElement( degree ), "Number of DoFs does not match degree." );
 
-      const auto dof_0 = dofs[0];
-      const auto dof_1 = dofs[1];
-      const auto dof_2 = dofs[2];
+      switch ( degree )
+      {
+      case 0: {
+         value = 1;
+         break;
+      }
+
+      case 1: {
+         const auto dof_0 = dofs[0];
+         const auto dof_1 = dofs[1];
+         const auto dof_2 = dofs[2];
 #if 0
       // affine space
 
@@ -88,14 +92,16 @@ class DGBasisLinearLagrange_Example : public DGBasisInfo
       real_t tmp_10 = tmp_5 * ( p_affine_0_1 - p_affine_1_1 );
       real_t a_0_0  = dof_0 * ( -tmp_10 - tmp_6 - tmp_8 - tmp_9 + 1 ) + dof_1 * ( tmp_6 + tmp_8 ) + dof_2 * ( tmp_10 + tmp_9 );
 #endif
-      // ref space
+         // ref space
 
-      const auto x_ref_0 = pos( 0 );
-      const auto x_ref_1 = pos( 1 );
+         const auto x_ref_0 = pos( 0 );
+         const auto x_ref_1 = pos( 1 );
 
-      real_t a_0_0 = dof_0 * ( -x_ref_0 - x_ref_1 + 1 ) + dof_1 * x_ref_0 + dof_2 * x_ref_1;
+         real_t a_0_0 = dof_0 * ( -x_ref_0 - x_ref_1 + 1 ) + dof_1 * x_ref_0 + dof_2 * x_ref_1;
 
-      value = a_0_0;
+         value = a_0_0;
+      }
+      }
    };
 
    virtual void integrateBasisFunction( uint_t                                                degree,
@@ -103,7 +109,6 @@ class DGBasisLinearLagrange_Example : public DGBasisInfo
                                         const std::function< real_t( const Point3D& ) >&      f,
                                         std::vector< real_t >&                                values )
    {
-      WALBERLA_CHECK_EQUAL( degree, 1, "Only degree 1 supported." );
       values.resize( numDoFsPerElement( degree ) );
 
       const auto p_affine_0_0 = coords[0]( 0 );
@@ -117,60 +122,70 @@ class DGBasisLinearLagrange_Example : public DGBasisInfo
 
       callback_Scalar_Variable_Coefficient_2D_k = f;
 
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id0 = 0;
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id1 = 0;
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id2 = 0;
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id3 = 0;
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id4 = 0;
-      real_t Scalar_Variable_Coefficient_2D_k_out0_id5 = 0;
-      Scalar_Variable_Coefficient_2D_k(
-          0.091576213509770743 * p_affine_0_0 + 0.091576213509770743 * p_affine_1_0 + 0.81684757298045851 * p_affine_2_0,
-          0.091576213509770743 * p_affine_0_1 + 0.091576213509770743 * p_affine_1_1 + 0.81684757298045851 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id0 );
-      Scalar_Variable_Coefficient_2D_k(
-          0.44594849091596489 * p_affine_0_0 + 0.44594849091596489 * p_affine_1_0 + 0.10810301816807022 * p_affine_2_0,
-          0.44594849091596489 * p_affine_0_1 + 0.44594849091596489 * p_affine_1_1 + 0.10810301816807022 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id1 );
-      Scalar_Variable_Coefficient_2D_k(
-          0.091576213509770743 * p_affine_0_0 + 0.81684757298045851 * p_affine_1_0 + 0.091576213509770743 * p_affine_2_0,
-          0.091576213509770743 * p_affine_0_1 + 0.81684757298045851 * p_affine_1_1 + 0.091576213509770743 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id2 );
-      Scalar_Variable_Coefficient_2D_k(
-          0.44594849091596489 * p_affine_0_0 + 0.10810301816807022 * p_affine_1_0 + 0.44594849091596489 * p_affine_2_0,
-          0.44594849091596489 * p_affine_0_1 + 0.10810301816807022 * p_affine_1_1 + 0.44594849091596489 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id3 );
-      Scalar_Variable_Coefficient_2D_k(
-          0.81684757298045851 * p_affine_0_0 + 0.091576213509770743 * p_affine_1_0 + 0.091576213509770743 * p_affine_2_0,
-          0.81684757298045851 * p_affine_0_1 + 0.091576213509770743 * p_affine_1_1 + 0.091576213509770743 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id4 );
-      Scalar_Variable_Coefficient_2D_k(
-          0.10810301816807022 * p_affine_0_0 + 0.44594849091596489 * p_affine_1_0 + 0.44594849091596489 * p_affine_2_0,
-          0.10810301816807022 * p_affine_0_1 + 0.44594849091596489 * p_affine_1_1 + 0.44594849091596489 * p_affine_2_1,
-          &Scalar_Variable_Coefficient_2D_k_out0_id5 );
-      real_t tmp_0 = std::abs( p_affine_0_0 * p_affine_1_1 - p_affine_0_0 * p_affine_2_1 - p_affine_0_1 * p_affine_1_0 +
-                               p_affine_0_1 * p_affine_2_0 + p_affine_1_0 * p_affine_2_1 - p_affine_1_1 * p_affine_2_0 );
-      real_t tmp_1 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id0 * tmp_0;
-      real_t tmp_2 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id1 * tmp_0;
-      real_t tmp_3 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id2 * tmp_0;
-      real_t tmp_4 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id3 * tmp_0;
-      real_t tmp_5 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id4 * tmp_0;
-      real_t tmp_6 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id5 * tmp_0;
-      real_t a_0_0 = 0.091576213509770743 * tmp_1 + 0.44594849091596489 * tmp_2 + 0.091576213509770743 * tmp_3 +
-                     0.44594849091596489 * tmp_4 + 0.81684757298045851 * tmp_5 + 0.10810301816807022 * tmp_6;
-      real_t a_0_1 = 0;
-      real_t a_0_2 = 0;
-      real_t a_1_0 = 0;
-      real_t a_1_1 = 0.091576213509770743 * tmp_1 + 0.44594849091596489 * tmp_2 + 0.81684757298045851 * tmp_3 +
-                     0.10810301816807022 * tmp_4 + 0.091576213509770743 * tmp_5 + 0.44594849091596489 * tmp_6;
-      real_t a_1_2 = 0;
-      real_t a_2_0 = 0;
-      real_t a_2_1 = 0;
-      real_t a_2_2 = 0.81684757298045851 * tmp_1 + 0.10810301816807022 * tmp_2 + 0.091576213509770743 * tmp_3 +
-                     0.44594849091596489 * tmp_4 + 0.091576213509770743 * tmp_5 + 0.44594849091596489 * tmp_6;
+      switch ( degree )
+      {
+      case 0: {
+         WALBERLA_ABORT( "Not implemented." );
+         break;
+      }
+      case 1: {
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id0 = 0;
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id1 = 0;
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id2 = 0;
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id3 = 0;
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id4 = 0;
+         real_t Scalar_Variable_Coefficient_2D_k_out0_id5 = 0;
+         Scalar_Variable_Coefficient_2D_k(
+             0.091576213509770743 * p_affine_0_0 + 0.091576213509770743 * p_affine_1_0 + 0.81684757298045851 * p_affine_2_0,
+             0.091576213509770743 * p_affine_0_1 + 0.091576213509770743 * p_affine_1_1 + 0.81684757298045851 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id0 );
+         Scalar_Variable_Coefficient_2D_k(
+             0.44594849091596489 * p_affine_0_0 + 0.44594849091596489 * p_affine_1_0 + 0.10810301816807022 * p_affine_2_0,
+             0.44594849091596489 * p_affine_0_1 + 0.44594849091596489 * p_affine_1_1 + 0.10810301816807022 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id1 );
+         Scalar_Variable_Coefficient_2D_k(
+             0.091576213509770743 * p_affine_0_0 + 0.81684757298045851 * p_affine_1_0 + 0.091576213509770743 * p_affine_2_0,
+             0.091576213509770743 * p_affine_0_1 + 0.81684757298045851 * p_affine_1_1 + 0.091576213509770743 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id2 );
+         Scalar_Variable_Coefficient_2D_k(
+             0.44594849091596489 * p_affine_0_0 + 0.10810301816807022 * p_affine_1_0 + 0.44594849091596489 * p_affine_2_0,
+             0.44594849091596489 * p_affine_0_1 + 0.10810301816807022 * p_affine_1_1 + 0.44594849091596489 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id3 );
+         Scalar_Variable_Coefficient_2D_k(
+             0.81684757298045851 * p_affine_0_0 + 0.091576213509770743 * p_affine_1_0 + 0.091576213509770743 * p_affine_2_0,
+             0.81684757298045851 * p_affine_0_1 + 0.091576213509770743 * p_affine_1_1 + 0.091576213509770743 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id4 );
+         Scalar_Variable_Coefficient_2D_k(
+             0.10810301816807022 * p_affine_0_0 + 0.44594849091596489 * p_affine_1_0 + 0.44594849091596489 * p_affine_2_0,
+             0.10810301816807022 * p_affine_0_1 + 0.44594849091596489 * p_affine_1_1 + 0.44594849091596489 * p_affine_2_1,
+             &Scalar_Variable_Coefficient_2D_k_out0_id5 );
+         real_t tmp_0 = std::abs( p_affine_0_0 * p_affine_1_1 - p_affine_0_0 * p_affine_2_1 - p_affine_0_1 * p_affine_1_0 +
+                                  p_affine_0_1 * p_affine_2_0 + p_affine_1_0 * p_affine_2_1 - p_affine_1_1 * p_affine_2_0 );
+         real_t tmp_1 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id0 * tmp_0;
+         real_t tmp_2 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id1 * tmp_0;
+         real_t tmp_3 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id2 * tmp_0;
+         real_t tmp_4 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id3 * tmp_0;
+         real_t tmp_5 = 0.054975871827660928 * Scalar_Variable_Coefficient_2D_k_out0_id4 * tmp_0;
+         real_t tmp_6 = 0.11169079483900572 * Scalar_Variable_Coefficient_2D_k_out0_id5 * tmp_0;
+         real_t a_0_0 = 0.091576213509770743 * tmp_1 + 0.44594849091596489 * tmp_2 + 0.091576213509770743 * tmp_3 +
+                        0.44594849091596489 * tmp_4 + 0.81684757298045851 * tmp_5 + 0.10810301816807022 * tmp_6;
+         real_t a_0_1 = 0;
+         real_t a_0_2 = 0;
+         real_t a_1_0 = 0;
+         real_t a_1_1 = 0.091576213509770743 * tmp_1 + 0.44594849091596489 * tmp_2 + 0.81684757298045851 * tmp_3 +
+                        0.10810301816807022 * tmp_4 + 0.091576213509770743 * tmp_5 + 0.44594849091596489 * tmp_6;
+         real_t a_1_2 = 0;
+         real_t a_2_0 = 0;
+         real_t a_2_1 = 0;
+         real_t a_2_2 = 0.81684757298045851 * tmp_1 + 0.10810301816807022 * tmp_2 + 0.091576213509770743 * tmp_3 +
+                        0.44594849091596489 * tmp_4 + 0.091576213509770743 * tmp_5 + 0.44594849091596489 * tmp_6;
 
-      values[0] = a_0_0;
-      values[1] = a_1_1;
-      values[2] = a_2_2;
+         values[0] = a_0_0;
+         values[1] = a_1_1;
+         values[2] = a_2_2;
+         break;
+      }
+      }
    }
 
  private:
