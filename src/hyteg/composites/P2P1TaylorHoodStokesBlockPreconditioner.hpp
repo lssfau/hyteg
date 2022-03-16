1338975aefe6c2bc49e798fc89fcabd773090672
Marcus Mohr <marcus.mohr@lmu.de>
2022-03-16 14:48:31 +0100

    Updating two StokesBlockPreconditioners
    The two preconditioners
    - P1StokesBlockPreconditioner.hpp
    - P2P1TaylorHoodStokesBlockPreconditioner.hpp
    now work with a ConstantVectorLaplaceOperator internally

--- src/hyteg/composites/P2P1TaylorHoodStokesBlockPreconditioner.hpp
+++ src/hyteg/composites/P2P1TaylorHoodStokesBlockPreconditioner.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "hyteg/composites/P2P1TaylorHoodFunction.hpp"
+#include "hyteg/operators/VectorLaplaceOperator.hpp"
 #include "hyteg/p1functionspace/P1ConstantOperator.hpp"
-#include "hyteg/p2functionspace/P2ConstantOperator.hpp"
 
 namespace hyteg {
 
@@ -10,7 +10,7 @@ class P2P1TaylorHoodStokesBlockPreconditioner
 : public Operator< P2P1TaylorHoodFunction< real_t >, P2P1TaylorHoodFunction< real_t > >
 {
  public:
-   typedef P2ConstantLaplaceOperator VelocityOperator_T;
+   typedef P2ConstantVectorLaplaceOperator VelocityOperator_T;
 
    P2P1TaylorHoodStokesBlockPreconditioner( const std::shared_ptr< PrimitiveStorage >& storage, size_t minLevel, size_t maxLevel )
    : Operator( storage, minLevel, maxLevel )
@@ -25,15 +25,11 @@ class P2P1TaylorHoodStokesBlockPreconditioner
                   size_t                                      level,
                   DoFType                                     flag ) const
    {
-      for ( uint_t k = 0; k < src.uvw().getDimension(); ++k )
-      {
-         A.toMatrix( mat, src.uvw()[k], dst.uvw()[k], level, flag );
-      }
-
-      P.toMatrix( mat, src.p(), dst.p(), level, flag );
+     A.toMatrix( mat, src.uvw(), dst.uvw(), level, flag );
+     P.toMatrix( mat, src.p(), dst.p(), level, flag );
    }
 
-   P2ConstantLaplaceOperator A;
+   P2ConstantVectorLaplaceOperator A;
    P1LumpedMassOperator      P;
 
    bool hasGlobalCells_;
