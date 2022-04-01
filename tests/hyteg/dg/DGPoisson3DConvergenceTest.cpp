99bac323d2f59cd1d01e6be88630bee59cb3fa67
Nils Kohl <nils.kohl@fau.de>
2022-04-01 17:29:49 +0200

    Fixing warnings.

--- tests/hyteg/dg/DGPoisson3DConvergenceTest.cpp
+++ tests/hyteg/dg/DGPoisson3DConvergenceTest.cpp
@@ -301,7 +301,7 @@ int main( int argc, char** argv )
 
    {
       WALBERLA_LOG_INFO_ON_ROOT( "### testOnSingleElementHomDirichlet ###" );
-      for ( int degree = 1; degree <= 1; degree++ )
+      for ( uint_t degree = 1; degree <= 1; degree++ )
       {
          uint_t minLevel    = 3;
          auto   l2ConvRate  = std::pow( 2, -( int( degree ) + 1 ) );
