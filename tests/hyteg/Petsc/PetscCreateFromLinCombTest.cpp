1c611a404a2b0aa7ea41e8b5228b43bc73258dee
Fabian Böhm <Fabian.boehm@fau.de>
2022-05-31 11:37:28 +0200

    update CreateFromLinComb test

--- tests/hyteg/Petsc/PetscCreateFromLinCombTest.cpp
+++ tests/hyteg/Petsc/PetscCreateFromLinCombTest.cpp
@@ -22,9 +22,9 @@
 #include "core/Environment.h"
 #include "core/logging/Logging.h"
 
-#include "hyteg/petsc/PETScSparseMatrixProxy.hpp"
-#include "hyteg/petsc/PETScSparseMatrix.hpp"
 #include "hyteg/petsc/PETScManager.hpp"
+#include "hyteg/petsc/PETScSparseMatrix.hpp"
+#include "hyteg/petsc/PETScSparseMatrixProxy.hpp"
 #include "hyteg/primitivestorage/PrimitiveStorage.hpp"
 
 #ifndef HYTEG_BUILD_WITH_PETSC
@@ -43,47 +43,53 @@ int main( int argc, char* argv[] )
 
    // setup 3 matrices
    Mat M1, M2, M3;
-   MatCreate(PETSC_COMM_WORLD, &M1);
-   MatCreate(PETSC_COMM_WORLD, &M2);
-   MatCreate(PETSC_COMM_WORLD, &M3);
-   MatSetSizes(M1, 2, 2, 2, 2);
-   MatSetSizes(M2, 2, 2, 2, 2);
-   MatSetSizes(M3, 2, 2, 2, 2);
-   MatSetUp(M1);
-   MatSetUp(M2);
-   MatSetUp(M3);
-   MatZeroEntries(M1);
-   MatZeroEntries(M2);
-   MatZeroEntries(M3);
+   MatCreate( PETSC_COMM_WORLD, &M1 );
+   MatCreate( PETSC_COMM_WORLD, &M2 );
+   MatCreate( PETSC_COMM_WORLD, &M3 );
+   MatSetSizes( M1, 2, 2, 2, 2 );
+   MatSetSizes( M2, 2, 2, 2, 2 );
+   MatSetSizes( M3, 2, 2, 2, 2 );
+   MatSetUp( M1 );
+   MatSetUp( M2 );
+   MatSetUp( M3 );
+   MatZeroEntries( M1 );
+   MatZeroEntries( M2 );
+   MatZeroEntries( M3 );
 
    // and corresponding hyteg proxy objects
-   PETScSparseMatrixProxy pM1(M1);
-   PETScSparseMatrixProxy pM2(M2);
-   PETScSparseMatrixProxy pM3(M3);
+   PETScSparseMatrixProxy pM1( M1 );
+   PETScSparseMatrixProxy pM2( M2 );
+   PETScSparseMatrixProxy pM3( M3 );
 
    // fill operand matrices
-   pM1.addValue(0, 0, 2);pM1.addValue(0, 1, 2);
-   pM1.addValue(1, 0, 3);pM1.addValue(1, 1, 4); 
+   pM1.addValue( 0, 0, 1 );
+   pM1.addValue( 0, 1, 2 );
+   pM1.addValue( 1, 0, 3 );
+   pM1.addValue( 1, 1, 4 );
 
-  
-   pM2.addValue(0, 0, 2);pM2.addValue(0, 1, 4);
-   pM2.addValue(1, 0, 6);pM2.addValue(1, 1, 8);
+   pM2.addValue( 0, 0, 2 );
+   pM2.addValue( 0, 1, 4 );
+   pM2.addValue( 1, 0, 6 );
+   pM2.addValue( 1, 1, 8 );
 
    // petsc assembly stuff
-   MatAssemblyBegin(M1,MAT_FINAL_ASSEMBLY); 
-   MatAssemblyEnd(M1,MAT_FINAL_ASSEMBLY);
-   MatAssemblyBegin(M2,MAT_FINAL_ASSEMBLY);
-   MatAssemblyEnd(M2,MAT_FINAL_ASSEMBLY);
-   
+   MatAssemblyBegin( M1, MAT_FINAL_ASSEMBLY );
+   MatAssemblyEnd( M1, MAT_FINAL_ASSEMBLY );
+   MatAssemblyBegin( M2, MAT_FINAL_ASSEMBLY );
+   MatAssemblyEnd( M2, MAT_FINAL_ASSEMBLY );
+
    // M3 = 2*M1 - M2
-   pM3.createFromMatrixLinComb({2, -1}, {std::make_shared<PETScSparseMatrixProxy>(pM1), std::make_shared<PETScSparseMatrixProxy>(pM2)});
-   
+   pM3.createFromMatrixLinComb(
+       { 2, -1 }, { std::make_shared< PETScSparseMatrixProxy >( pM1 ), std::make_shared< PETScSparseMatrixProxy >( pM2 ) } );
+
    // check result
    real_t val = 0;
-   for(int i = 0; i < 2; ++i) {
-      for(int j = 0; j < 2; ++j) {
-         MatGetValue(M3,i,j,&val);
-         WALBERLA_CHECK_FLOAT_EQUAL(val, 0.0);
+   for ( int i = 0; i < 2; ++i )
+   {
+      for ( int j = 0; j < 2; ++j )
+      {
+         MatGetValue( M3, i, j, &val );
+         WALBERLA_CHECK_FLOAT_EQUAL( val, 0.0 );
       }
    }
    return EXIT_SUCCESS;
