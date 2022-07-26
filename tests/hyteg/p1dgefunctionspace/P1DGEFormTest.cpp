889fc385c966b7df5f4137c6e4760c07aad0f824
Fabian Böhm <Fabian.boehm@fau.de>
2022-07-26 09:53:47 +0200

    epsilon form test

--- tests/hyteg/p1dgefunctionspace/P1DGEFormTest.cpp
+++ tests/hyteg/p1dgefunctionspace/P1DGEFormTest.cpp
@@ -23,6 +23,7 @@
 #include "core/mpi/MPIManager.h"
 
 #include "hyteg/dgfunctionspace/DGBasisLinearLagrange_Example.hpp"
+#include "hyteg/p1dgefunctionspace/EDGEpsilonForm.hpp"
 #include "hyteg/p1dgefunctionspace/P1DGEOperators.hpp"
 #include "hyteg/petsc/PETScManager.hpp"
 
@@ -319,7 +320,7 @@ void testLaplaceForm()
       WALBERLA_CHECK_FLOAT_EQUAL( elMat( 0, 2 ), expected[1] );
    }
 
-   // checking inner facet integral
+   // checking outer facet integral
    {
       hyteg::dg::DGVectorLaplaceFormEDGP1_0 form;
 
@@ -358,6 +359,156 @@ void testLaplaceForm()
    }
 }
 
+void testEpsilonForm()
+{
+   // we test two triangles with coordinates (p0, p1, p2) and (p0, p2, p3):
+   Eigen::Matrix< real_t, 3, 1 > p0{ 0, 0, 0 };
+   Eigen::Matrix< real_t, 3, 1 > p1{ 1, 0, 0 };
+   Eigen::Matrix< real_t, 3, 1 > p2{ -0.2, 1, 0 };
+   Eigen::Matrix< real_t, 3, 1 > p3{ -1, 0.5, 0 };
+   Eigen::Matrix< real_t, 3, 1 > n1{ -1, -0.2, 0 };
+
+   // offset to have some shift from the origin in the coordinates
+   // Eigen::Matrix< real_t, 3, 1 > offset{ 0.05, -0.7, 0 };
+   Eigen::Matrix< real_t, 3, 1 > offset{ 0., 0., 0. };
+   p0 += offset;
+   p1 += offset;
+   p2 += offset;
+   p3 += offset;
+
+   hyteg::dg::DGBasisLinearLagrange_Example basis;
+
+   Eigen::Matrix< real_t, Eigen::Dynamic, Eigen::Dynamic > elMat;
+   elMat.resize( 3, 1 );
+
+   const real_t sigma = 6.;
+
+   // volume integrals: (epsilon, epsilon) term
+   // edg, edg pairing
+   {
+      hyteg::dg::EDGEpsilonFormEDGEDG form;
+
+      // (2 * \int_T \epsilon(\phi_{edg}) : \epsilon(\phi_{edg}) dx from sympy notebook
+      real_t expected_t1 = 2 * 1.0; // 2*0.96080;
+      real_t expected_t2 = 2 * 0.9; // 2*1.88888888888889;
+
+      elMat.resize( 1, 1 );
+
+      // triangle 1
+      form.integrateVolume2D( { p0, p1, p2 }, basis, basis, 0, 0, elMat );
+      WALBERLA_CHECK_FLOAT_EQUAL( expected_t1, elMat( 0, 0 ) )
+
+      // triangle 2
+      form.integrateVolume2D( { p0, p2, p3 }, basis, basis, 0, 0, elMat );
+      WALBERLA_CHECK_FLOAT_EQUAL( expected_t2, elMat( 0, 0 ) )
+   }
+
+   // edg, p1 pairing
+   {
+      hyteg::dg::EDGEpsilonFormEDGP1_0 form;
+
+      elMat.resize( 3, 1 );
+
+      // triangle 1
+      Eigen::Matrix< real_t, 3, 1 > expected_t1;
+      // clang-format off
+      expected_t1 <<
+      -0.50, 0.50, 0;
+      // clang-format on
+      expected_t1 *= 2; // prefactor of volume integral term
+
+      form.integrateVolume2D( { p0, p1, p2 }, basis, basis, 0, 1, elMat );
+      for ( uint_t i = 0; i < 3; i += 1 )
+         WALBERLA_CHECK_FLOAT_EQUAL( expected_t1( i, 0 ), elMat( i, 0 ) )
+
+      // triangle 2
+      Eigen::Matrix< real_t, 3, 1 > expected_t2;
+      // clang-format off
+      expected_t2 <<
+          0.250, 0.250, -0.50;
+      // clang-format on
+      expected_t2 *= 2; // prefactor of volume integral term
+
+      form.integrateVolume2D( { p0, p2, p3 }, basis, basis, 0, 1, elMat );
+      for ( uint_t i = 0; i < 3; i += 1 )
+         WALBERLA_CHECK_FLOAT_EQUAL( expected_t2( i, 0 ), elMat( i, 0 ) )
+   }
+
+   
+   // edge integrals: consistency and symmetry term
+   {
+      // edg, edg pairing
+      {
+         hyteg::dg::EDGEpsilonFormEDGEDG form;
+         {
+            elMat.resize( 1, 1 );
+
+            // inner  side of facet integral
+            {
+               real_t expected_consistency_symmetry = 0.339934634239519; // consistency, symmetry (penalty does not contain eps)
+               real_t expected_penalty              = 0.24888888888888888;
+               real_t expected = -expected_consistency_symmetry + sigma*expected_penalty;
+               form.integrateFacetInner(2, { p0, p1, p2 }, { p0, p2 }, p1, n1, basis, basis, 0, 0, elMat );
+               WALBERLA_CHECK_FLOAT_EQUAL( elMat( 0, 0 ), expected );
+            }
+
+            // outer/coupling side of facet integral
+            {
+               real_t expected_consistency_symmetry = 0.322937902527543;
+               real_t expected_penalty              = 0.02333333333333332;
+               real_t expected = -expected_consistency_symmetry + sigma*expected_penalty;
+               form.integrateFacetCoupling(2,
+                    { p0, p1, p2 }, { p0, p2, p3 }, { p0, p2 }, p1, p3, n1, basis, basis, 0, 0, elMat );
+
+               WALBERLA_CHECK_FLOAT_EQUAL( elMat( 0, 0 ), expected );
+            }
+         }
+      }
+
+      // edg, p1 pairing
+      {
+         hyteg::dg::EDGEpsilonFormEDGP1_0 form;
+
+         // inner  side of facet integral
+         {
+            elMat.resize( 3, 1 );
+
+            std::vector< real_t > expected_consistency_symmetry = {
+                -0.413360515235255, 0.182204963952382, -0.278746400076406};
+            std::vector< real_t > expected_penalty = { -0.16666666666666674, 0.0, -0.2 };
+            std::vector< real_t > expected         = {
+                        -expected_consistency_symmetry[0] + sigma * expected_penalty[0],
+                        -expected_consistency_symmetry[1] + sigma * expected_penalty[1],
+                        -expected_consistency_symmetry[2] + sigma * expected_penalty[2],
+            };
+
+            form.integrateFacetInner2D(  { p0, p1, p2 },  { p0, p2 }, p1,  n1, basis, basis, 0, 1, elMat );
+
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 0, 0 ), expected[0] );
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 1, 0 ), expected[1] );
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 2, 0 ), expected[2] );
+         }
+
+         // outer/coupling side of facet integral
+         {
+            std::vector< real_t > expected_consistency_symmetry = {
+             0.426278031336357, 0.156029997115939, 0.239653917138861};
+            std::vector< real_t > expected_penalty = { 0.16666666666666666, 0.2, 0.0 };
+            std::vector< real_t > expected         = {
+                        expected_consistency_symmetry[0] - sigma * expected_penalty[0],
+                        expected_consistency_symmetry[1] - sigma * expected_penalty[1],
+                        expected_consistency_symmetry[2] - sigma * expected_penalty[2],
+            };
+
+            form.integrateFacetCoupling( 2, { p0, p1, p2 }, { p0, p2, p3 }, { p2, p0 }, p1, p3, n1, basis, basis, 1, 0, elMat );
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 0, 0 ), expected[0] );
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 1, 0 ), expected[1] );
+            WALBERLA_CHECK_FLOAT_EQUAL( elMat( 2, 0 ), expected[2] );
+         }
+      }
+   }
+
+}
 } // namespace hyteg
 
 int main( int argc, char* argv[] )
@@ -366,8 +517,9 @@ int main( int argc, char* argv[] )
    walberla::MPIManager::instance()->useWorldComm();
    hyteg::PETScManager petscManager( &argc, &argv );
 
-   hyteg::testDivForm();
-   hyteg::testLaplaceForm();
+   //hyteg::testDivForm();
+   //hyteg::testLaplaceForm();
+   hyteg::testEpsilonForm();
 
    return 0;
 }
