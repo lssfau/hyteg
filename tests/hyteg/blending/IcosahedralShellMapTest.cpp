b3376a05b2a4988f0cf1469f44f665f56ecd4d59
Marcus Mohr <marcus.mohr@lmu.de>
2022-07-28 16:26:12 +0200

    Adds demo code for problem reported in issue #183 to test

--- tests/hyteg/blending/IcosahedralShellMapTest.cpp
+++ tests/hyteg/blending/IcosahedralShellMapTest.cpp
@@ -18,52 +18,70 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "hyteg/geometry/IcosahedralShellMap.hpp"
+
 #include <core/Environment.h>
 
 #include "hyteg/mesh/MeshInfo.hpp"
+#include "hyteg/p1functionspace/VertexDoFIndexing.hpp"
+#include "hyteg/p1functionspace/VertexDoFMacroCell.hpp"
 #include "hyteg/primitivestorage/PrimitiveStorage.hpp"
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
 #include "hyteg/primitivestorage/Visualization.hpp"
-#include "hyteg/geometry/IcosahedralShellMap.hpp"
-#include "hyteg/p1functionspace/VertexDoFIndexing.hpp"
-#include "hyteg/p1functionspace/VertexDoFMacroCell.hpp"
 
 namespace hyteg {
 
 void testInverse()
 {
-   const uint_t level = 4;
-   auto meshInfo = MeshInfo::meshSphericalShell( 5, 2, 0.5, 1.0 );
-   auto setupStorage = std::make_shared< SetupPrimitiveStorage >( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   const uint_t level    = 4;
+   auto         meshInfo = MeshInfo::meshSphericalShell( 5, 2, 0.5, 1.0 );
+   auto         setupStorage =
+       std::make_shared< SetupPrimitiveStorage >( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
    auto storage = std::make_shared< PrimitiveStorage >( *setupStorage );
 
    writeDomainPartitioningVTK( storage, "../../output", "IcosahedralShellMapTestDomain" );
 
-   for ( const auto & it : storage->getCells() )
+   for ( const auto& it : storage->getCells() )
    {
-      auto cell = it.second;
+      auto                cell = it.second;
       IcosahedralShellMap geometryMap( *cell, *setupStorage );
-      for ( const auto & idx : vertexdof::macrocell::Iterator( level ) )
+      for ( const auto& idx : vertexdof::macrocell::Iterator( level ) )
       {
          const auto position = vertexdof::macrocell::coordinateFromIndex( level, *cell, idx );
-         Point3D mappedPosition;
-         Point3D invMappedPosition;
+         Point3D    mappedPosition;
+         Point3D    invMappedPosition;
          geometryMap.evalF( position, mappedPosition );
          geometryMap.evalFinv( mappedPosition, invMappedPosition );
-         auto error = (position - invMappedPosition).norm();
+         auto error = ( position - invMappedPosition ).norm();
          WALBERLA_CHECK_LESS( error, 1e-14 );
       }
    }
 }
 
+void checkIssue183()
+{
+   MeshInfo meshInfo = MeshInfo::emptyMeshInfo();
+   meshInfo          = MeshInfo::meshSphericalShell( 3, 3, real_c(200.f), real_c(400.f) );
+
+   auto setupStorage = std::make_shared< SetupPrimitiveStorage >(
+       meshInfo, walberla::uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+
+   setupStorage->setMeshBoundaryFlagsOnBoundary( 1, 0, true );
+
+   IcosahedralShellMap::setMap( *setupStorage );
+
+   std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( *setupStorage, 1 );
 }
 
-int main( int argc, char ** argv )
+} // namespace hyteg
+
+int main( int argc, char** argv )
 {
    walberla::Environment env( argc, argv );
    walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
    walberla::MPIManager::instance()->useWorldComm();
 
    hyteg::testInverse();
+   hyteg::checkIssue183();
    return 0;
 }
