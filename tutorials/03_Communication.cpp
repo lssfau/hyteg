3e16c65b9a0e2a9b2a476ba839b591a4f6c519f0
Nils Kohl <nils.kohl@fau.de>
2022-08-22 14:18:03 +0200

    Fixing communication tutorial.

--- tutorials/03_Communication.cpp
+++ tutorials/03_Communication.cpp
@@ -20,6 +20,7 @@
 #include "hyteg/primitives/Primitive.hpp"
 #include "hyteg/communication/PackInfo.hpp"
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
+#include "hyteg/primitivestorage/PrimitiveStorage.hpp"
 #include "hyteg/primitivestorage/loadbalancing/SimpleBalancer.hpp"
 #include "hyteg/communication/BufferedCommunication.hpp"
 #include "core/Environment.h"
@@ -132,8 +133,8 @@ namespace hyteg {
 /// [TestData]
 struct TestData
 {
-  uint_t ownID;
-  std::vector< uint_t > neighborIDs;
+  PrimitiveID ownID;
+  std::vector< PrimitiveID > neighborIDs;
 };
 
 struct TestDataHandling : OnlyInitializeDataHandling< TestData, Primitive >
@@ -170,7 +171,7 @@ public:
     WALBERLA_LOG_INFO( "Unpacking data on edge..." );
 
     TestData * data = receiver->getData( dataID_ );
-    uint_t vertexData;
+    PrimitiveID vertexData;
     buffer >> vertexData;
 
     // Adding the received ID to the neighbors..
