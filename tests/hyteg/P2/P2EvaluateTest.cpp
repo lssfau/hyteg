938af4bde390830d89184c276ff4a11064a66f56
Marcus Mohr <marcus.mohr@lmu.de>
2022-10-26 17:53:53 +0200

    Extend P2EvaluateTest
    We add another free-function testEvaluateWithBlending() which checks
    that P2::evaluate() gives correct results when blending is applied and
    we feed it with physical coordinates.
    
    Results look fine for AffineMap2D, but not very accurate for AnnulusMap.

--- tests/hyteg/P2/P2EvaluateTest.cpp
+++ tests/hyteg/P2/P2EvaluateTest.cpp
@@ -17,19 +17,25 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <hyteg/communication/Syncing.hpp>
-#include <hyteg/p1functionspace/VertexDoFMacroEdge.hpp>
-#include <hyteg/p1functionspace/VertexDoFMacroFace.hpp>
-
 #include "core/Environment.h"
+#include "core/math/Constants.h"
 #include "core/math/Random.h"
 
+#include "hyteg/communication/Syncing.hpp"
 #include "hyteg/dataexport/VTKOutput.hpp"
 #include "hyteg/functions/FunctionIterator.hpp"
+#include "hyteg/geometry/AffineMap2D.hpp"
+#include "hyteg/geometry/AnnulusMap.hpp"
+#include "hyteg/geometry/IcosahedralShellMap.hpp"
+#include "hyteg/p1functionspace/VertexDoFMacroEdge.hpp"
+#include "hyteg/p1functionspace/VertexDoFMacroFace.hpp"
 #include "hyteg/p2functionspace/P2Function.hpp"
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
 
+using walberla::real_c;
 using walberla::real_t;
+using walberla::math::pi;
+
 using namespace hyteg;
 
 void test2D()
@@ -56,9 +62,9 @@ void test2D()
    // IMPORTANT
    communication::syncP2FunctionBetweenPrimitives( x, maxLevel );
 
-   Point3D coordinates( {0.0, 0.5, 0.0} );
+   Point3D coordinates( { 0.0, 0.5, 0.0 } );
    real_t  eval;
-   auto success = x.evaluate( coordinates, maxLevel, eval );
+   auto    success = x.evaluate( coordinates, maxLevel, eval );
    WALBERLA_CHECK( success );
    Point3D gradient;
    x.evaluateGradient( coordinates, maxLevel, gradient );
@@ -68,7 +74,7 @@ void test2D()
 
    coordinates[0] = 2.0;
    coordinates[1] = 0.0;
-   success = x.evaluate( coordinates, maxLevel, eval );
+   success        = x.evaluate( coordinates, maxLevel, eval );
    WALBERLA_CHECK( success );
    x.evaluateGradient( coordinates, maxLevel, gradient );
    WALBERLA_CHECK_FLOAT_EQUAL( eval, testFunc( coordinates ) );
@@ -77,7 +83,7 @@ void test2D()
 
    coordinates[0] = 2.0;
    coordinates[1] = 1.0;
-   success = x.evaluate( coordinates, maxLevel, eval );
+   success        = x.evaluate( coordinates, maxLevel, eval );
    WALBERLA_CHECK( success );
    x.evaluateGradient( coordinates, maxLevel, gradient );
    WALBERLA_CHECK_FLOAT_EQUAL( eval, testFunc( coordinates ) );
@@ -86,7 +92,7 @@ void test2D()
 
    coordinates[0] = 0.0;
    coordinates[1] = 1.0;
-   success = x.evaluate( coordinates, maxLevel, eval );
+   success        = x.evaluate( coordinates, maxLevel, eval );
    WALBERLA_CHECK( success );
    x.evaluateGradient( coordinates, maxLevel, gradient );
    WALBERLA_CHECK_FLOAT_EQUAL( eval, testFunc( coordinates ) );
@@ -95,7 +101,7 @@ void test2D()
 
    coordinates[0] = 0.5;
    coordinates[1] = 0.5;
-   success = x.evaluate( coordinates, maxLevel, eval );
+   success        = x.evaluate( coordinates, maxLevel, eval );
    WALBERLA_CHECK( success );
    x.evaluateGradient( coordinates, maxLevel, gradient );
    WALBERLA_CHECK_FLOAT_EQUAL( eval, testFunc( coordinates ) );
@@ -123,7 +129,7 @@ void test2D()
 
 void test3D()
 {
-   MeshInfo              meshInfo = MeshInfo::meshSymmetricCuboid( Point3D( {-1, -1, -1} ), Point3D( {1, 1, 1} ), 1, 1, 1 );
+   MeshInfo              meshInfo = MeshInfo::meshSymmetricCuboid( Point3D( { -1, -1, -1 } ), Point3D( { 1, 1, 1 } ), 1, 1, 1 );
    SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
    std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
 
@@ -154,7 +160,7 @@ void test3D()
          coordinates[2] = walberla::math::realRandom( 0.0, 1.0 );
 
          real_t eval;
-         auto success = x.evaluate( coordinates, level, eval );
+         auto   success = x.evaluate( coordinates, level, eval );
          WALBERLA_CHECK( success );
          WALBERLA_CHECK_FLOAT_EQUAL( eval, testFunc( coordinates ), "Test3D: wrong value at " << coordinates << "." );
 
@@ -165,8 +171,8 @@ void test3D()
 
 void test3DReversibility()
 {
-   MeshInfo                            meshInfo = MeshInfo::meshCuboid( Point3D( {-1, -1, -1} ), Point3D( {1, 1, 1} ), 1, 1, 1 );
-   SetupPrimitiveStorage               setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   MeshInfo              meshInfo = MeshInfo::meshCuboid( Point3D( { -1, -1, -1 } ), Point3D( { 1, 1, 1 } ), 1, 1, 1 );
+   SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
    std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
 
    const size_t minLevel = 2;
@@ -194,34 +200,32 @@ void test3DReversibility()
       for ( auto it : FunctionIterator< vertexdof::VertexDoFFunction< real_t > >( y.getVertexDoFFunction(), level ) )
       {
          real_t evaluation;
-         auto success = x.evaluate( it.coordinates(), level, evaluation );
+         auto   success = x.evaluate( it.coordinates(), level, evaluation );
          WALBERLA_CHECK( success );
-         auto nodeValue = testFunc( it.coordinates() );
+         auto nodeValue  = testFunc( it.coordinates() );
          auto localError = std::abs( evaluation - nodeValue );
          if ( localError > 1e-06 )
-            WALBERLA_LOG_INFO_ON_ROOT( "error: " << localError << ", " << it);
+            WALBERLA_LOG_INFO_ON_ROOT( "error: " << localError << ", " << it );
          success = x.evaluate( it.coordinates(), level, evaluation );
          WALBERLA_CHECK( success );
          it.value() = evaluation;
-
-
       }
 
       for ( auto it : FunctionIterator< EdgeDoFFunction< real_t > >( y.getEdgeDoFFunction(), level ) )
       {
          real_t evaluation;
-         auto success = x.evaluate( it.coordinates(), level, evaluation );
+         auto   success = x.evaluate( it.coordinates(), level, evaluation );
          WALBERLA_CHECK( success );
-         auto nodeValue = testFunc( it.coordinates() );
+         auto nodeValue  = testFunc( it.coordinates() );
          auto localError = std::abs( evaluation - nodeValue );
          if ( localError > 1e-06 )
-            WALBERLA_LOG_INFO_ON_ROOT( "error: " << localError << ", " << it);
+            WALBERLA_LOG_INFO_ON_ROOT( "error: " << localError << ", " << it );
          success = x.evaluate( it.coordinates(), level, evaluation );
          WALBERLA_CHECK( success );
          it.value() = evaluation;
       }
 
-      error.assign( {1.0, -1.0}, {x, y}, level, All );
+      error.assign( { 1.0, -1.0 }, { x, y }, level, All );
       const auto errorL2 =
           std::sqrt( error.dotGlobal( error, level, All ) / real_c( numberOfGlobalDoFs< P2FunctionTag >( *storage, level ) ) );
       WALBERLA_LOG_DEVEL_ON_ROOT( "Level " << level << ": L2 error = " << errorL2 );
@@ -243,6 +247,100 @@ void test3DReversibility()
    }
 }
 
+void testEvaluateWithBlending( uint_t numSamples, uint_t mapType )
+{
+   std::vector< Point3D > samples;
+   samples.reserve( numSamples );
+
+   std::shared_ptr< PrimitiveStorage > storage;
+   walberla::math::seedRandomGenerator( 12345678 );
+
+   real_t tolerance = real_c( -1 );
+
+   // annulus map
+   if ( mapType == 0 )
+   {
+      WALBERLA_LOG_INFO_ON_ROOT( "Testing with AnnulusMap" );
+      real_t rMin = real_c( 1 );
+      real_t rMax = real_c( 2 );
+
+      MeshInfo              meshInfo = MeshInfo::meshAnnulus( rMin, rMax, MeshInfo::CRISS, 12, 3 );
+      SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+      AnnulusMap::setMap( setupStorage );
+
+      for ( uint_t k = 0; k < numSamples; ++k )
+      {
+         real_t radius = walberla::math::realRandom( rMin, rMax );
+         real_t angle  = walberla::math::realRandom( real_c( 0 ), real_c( 2 ) * pi );
+         samples.push_back( Point3D( { radius * std::cos( angle ), radius * std::sin( angle ), real_c( 0 ) } ) );
+      }
+
+      storage = std::make_shared< PrimitiveStorage >( setupStorage );
+
+      tolerance = real_c( 1e-2 );
+   }
+
+   // 2D affine map
+   else
+   {
+      WALBERLA_LOG_INFO_ON_ROOT( "Testing with AffineMap2D" );
+      MeshInfo              meshInfo = MeshInfo::fromGmshFile( "../../data/meshes/quad_16el.msh" );
+      SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+
+      Point2D  shift( { real_c( 4 ), real_c( -1.0 / 3.0 ) } );
+      real_t   angle = pi / real_c( 180.0 * 25.0 );
+      Matrix2r mat;
+      mat( 0, 0 ) = +std::cos( angle );
+      mat( 0, 1 ) = -std::sin( angle );
+      mat( 1, 0 ) = +std::sin( angle );
+      mat( 1, 1 ) = +std::cos( angle );
+
+      AffineMap2D::setMap( setupStorage, mat, shift );
+      for ( uint_t k = 0; k < numSamples; ++k )
+      {
+         real_t xPos = walberla::math::realRandom( real_c( 0 ), real_c( 1 ) );
+         real_t yPos = walberla::math::realRandom( real_c( 0 ), real_c( 1 ) );
+         real_t x    = mat( 0, 0 ) * xPos + mat( 0, 1 ) * yPos + shift[0];
+         real_t y    = mat( 1, 0 ) * xPos + mat( 1, 1 ) * yPos + shift[1];
+         samples.push_back( Point3D( { x, y, real_c( 0 ) } ) );
+      }
+
+      storage = std::make_shared< PrimitiveStorage >( setupStorage );
+
+      tolerance = real_c( 1e-12 );
+   }
+
+   const size_t minLevel = 2;
+   const size_t maxLevel = 4;
+
+   auto poly2 = []( const Point3D& x ) {
+      // return real_c( 1 ) + real_c( 5 ) * x[0] + real_c( 3 ) * x[1] - real_c( 2 ) * x[0] * x[0] + real_c( 0.5 ) * x[0] * x[1] + real_c( 7 ) * x[1] * x[1];
+      return real_c( 1 ) + real_c( 0.5 ) * x[0] - real_c( 2 ) * x[1];
+      // return real_c( 1 );
+   };
+
+   P2Function< real_t > func( "func", storage, minLevel, maxLevel );
+
+   for ( uint_t level = minLevel; level <= maxLevel; level++ )
+   {
+      func.interpolate( poly2, level );
+      real_t testValue = real_c( 0 );
+      real_t ctrlValue = real_c( 0 );
+      communication::syncFunctionBetweenPrimitives( func, level );
+      for ( uint_t idx = 0; idx < numSamples; ++idx )
+      {
+         ctrlValue          = poly2( samples[idx] );
+         bool   coordsFound = func.evaluate( samples[idx], level, testValue, real_c( 1e-12 ) );
+         real_t error       = std::abs( testValue - ctrlValue );
+         WALBERLA_LOG_INFO_ON_ROOT( "sampling Point: " << std::scientific << std::showpos << samples[idx] << ", coordsFound = "
+                                                       << ( coordsFound ? "TRUE" : "FALSE" ) << ", ctrl = " << ctrlValue
+                                                       << ", test = " << testValue << ", error = " << error );
+         WALBERLA_CHECK( coordsFound );
+         WALBERLA_CHECK_LESS( error, tolerance );
+      }
+   }
+}
+
 int main( int argc, char** argv )
 {
    walberla::debug::enterTestMode();
@@ -252,5 +350,8 @@ int main( int argc, char** argv )
    test2D();
    test3D();
    test3DReversibility();
+   testEvaluateWithBlending( 10, 1 );
+   testEvaluateWithBlending( 10, 0 );
+
    return EXIT_SUCCESS;
 }
