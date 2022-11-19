4a91f86c280ab72e3e9e6b1ad6cfb4f9f8f3e8f7
wagnandr <wagnandr@in.tum.de>
2022-11-19 18:12:18 +0100

    adds P1ToDGOperator

---
+++ tests/hyteg/mixedoperators/P1ToDGOperatorTest.cpp
@@ -0,0 +1,88 @@
+/*
+* Copyright (c) 2022 Andreas Wagner.
+*
+* This file is part of HyTeG
+* (see https://i10git.cs.fau.de/hyteg/hyteg).
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "hyteg/mixedoperators/P1ToDGOperator.hpp"
+
+#include <hyteg/dg1functionspace/DG1Operator.hpp>
+#include <hyteg/dgfunctionspace/DGMassForm_Example.hpp>
+
+#include "core/Environment.h"
+#include "core/math/Constants.h"
+
+#include "hyteg/dataexport/VTKOutput.hpp"
+#include "hyteg/primitivestorage/PrimitiveStorage.hpp"
+#include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
+
+using namespace hyteg;
+using walberla::real_t;
+using walberla::math::pi;
+
+void checkP1ToDG1ByIntegral(const uint_t dim)
+{
+   std::string mesh_file;
+   if (dim == 2)
+      mesh_file = "../../data/meshes/quad_4el.msh";
+   else
+      mesh_file = "../../data/meshes/3D/cube_6el.msh";
+
+   MeshInfo meshInfo = MeshInfo::fromGmshFile( mesh_file );
+
+   SetupPrimitiveStorage setup( meshInfo, walberla::uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   const auto            storage = std::make_shared< PrimitiveStorage >( setup, 1 );
+
+   const uint_t level = 2;
+   const auto   form  = std::make_shared< InterpolationForm >();
+
+   P1Function< real_t >  src( "src", storage, level, level );
+   DG1Function< real_t > dst( "dst", storage, level, level );
+   DG1Function< real_t > Mdst( "Mdst", storage, level, level );
+
+   P1ToDGOperator< InterpolationForm > op( storage, level, level, form );
+
+   src.interpolate( []( auto p ) { return p[0] - 0.1 * p[1] + 0.2 * p[2]; }, level, All );
+
+   op.apply( src, *dst.getDGFunction(), level, All, hyteg::Replace );
+
+   auto massForm    = std::make_shared< DGMassForm_Example >();
+   DG1Operator M( storage, level, level, massForm );
+
+   M.apply( dst, Mdst, level, All, Replace );
+   const real_t integralValue = Mdst.sumGlobal(level, All);
+   const real_t expectedIntegralValue = 0.5 - 0.1 * 0.5 + (dim==3 ? 0.2 * 0.5 : 0.);
+
+   WALBERLA_CHECK_FLOAT_EQUAL( integralValue, expectedIntegralValue, "integral values must match" );
+
+   // VTK
+   // VTKOutput vtkOutput( "../../output", "P1ToDGOperatorTest", storage );
+   // vtkOutput.add( src );
+   // vtkOutput.add( dst );
+   // vtkOutput.write( level, 0 );
+}
+
+int main( int argc, char** argv )
+{
+   walberla::mpi::Environment MPIenv( argc, argv );
+   walberla::MPIManager::instance()->useWorldComm();
+
+   checkP1ToDG1ByIntegral(2);
+   checkP1ToDG1ByIntegral(3);
+
+   return EXIT_SUCCESS;
+}
