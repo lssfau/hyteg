d6dd12d350271ed5dd6e914a291d4e483f3e1397
Marcus Mohr <marcus.mohr@lmu.de>
2022-12-15 17:58:21 +0100

    Add N1curlFenicsForm class and n1Curl_test_mass form for testing
    Commit extends the FFC build files to allow generation of a mass form for
    3D for Nedelec H(curl) elements (of the first kind). It also adds the
    resulting form. The latter, is integrated into the HyTeG code structure
    by adding a corresponding class and included into the HyTeGVsFenicsFormTest
    to compare it to the N1E1FormMass form.

---
+++ src/hyteg/forms/form_fenics_base/N1curlFenicsForm.hpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022 Marcus Mohr.
+ *
+ * This file is part of HyTeG
+ * (see https://i10git.cs.fau.de/hyteg/hyteg).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "hyteg/fenics/fenics.hpp"
+#include "hyteg/fenics/ufc_traits.hpp"
+#include "hyteg/forms/P1Form.hpp"
+
+#ifdef _MSC_VER
+#pragma warning( push, 0 )
+#endif
+
+// N1curl
+#include "hyteg/forms/form_fenics_generated/n1Curl_tet_mass.h"
+
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
+
+namespace hyteg {
+
+template < class UFCOperator2D, class UFCOperator3D = fenics::UndefinedAssembly >
+class N1curlFenicsForm : public Form
+{
+ public:
+
+   void integrateAll( const std::array< Point3D, 4 >& coords, Matrix6r& elMat ) const
+   {
+      real_t fenicsCoords[12];
+      fenicsCoords[0] = coords[0][0];
+      fenicsCoords[1] = coords[0][1];
+      fenicsCoords[2] = coords[0][2];
+
+      fenicsCoords[3] = coords[1][0];
+      fenicsCoords[4] = coords[1][1];
+      fenicsCoords[5] = coords[1][2];
+
+      fenicsCoords[6] = coords[2][0];
+      fenicsCoords[7] = coords[2][1];
+      fenicsCoords[8] = coords[2][2];
+
+      fenicsCoords[9]  = coords[3][0];
+      fenicsCoords[10] = coords[3][1];
+      fenicsCoords[11] = coords[3][2];
+
+      UFCOperator3D gen;
+      gen.tabulate_tensor( elMat.data(), nullptr, fenicsCoords, 0 );
+   }
+
+   bool assemble2D() const override
+   {
+      WALBERLA_ABORT( "N1curlFenicsForm not implemented for 2D!" );
+      return false;
+   }
+
+   bool assemble3D() const override { return !std::is_same< UFCOperator3D, hyteg::fenics::NoAssemble >::value; }
+
+   bool assembly2DDefined() const override
+   {
+      WALBERLA_ABORT( "N1curlFenicsForm not implemented for 2D!" );
+      return false;
+   }
+
+   bool assembly3DDefined() const override { return !std::is_same< UFCOperator3D, hyteg::fenics::UndefinedAssembly >::value; }
+
+   inline void setGeometryMap( const std::shared_ptr< GeometryMap > ) const {}
+};
+
+} // namespace hyteg
