73acf97a9b88ef915e5695b58c498f9942214ffb
Marcus Mohr <marcus.mohr@lmu.de>
2023-03-22 12:45:14 +0100

    Rename Level to level and add missing ValueType cast for literal

--- src/hyteg/p2functionspace/P2TransferOperators.hpp
+++ src/hyteg/p2functionspace/P2TransferOperators.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Daniel Drzisga, Dominik Thoennes, Nils Kohl.
+ * Copyright (c) 2017-2023 Daniel Drzisga, Dominik Thoennes, Nils Kohl, Marcus Mohr.
  *
  * This file is part of HyTeG
  * (see https://i10git.cs.fau.de/hyteg/hyteg).
@@ -38,58 +38,58 @@ using walberla::uint_t;
 namespace macroface {
 
 template < typename ValueType >
-inline void prolongateP1ToP2( const uint_t&                                               Level,
+inline void prolongateP1ToP2( const uint_t&                                               level,
                               const Face&                                                 face,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p2VertexDoFMemoryID,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p2EdgeDoFMemoryID,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p1VertexDoFMemoryID )
 {
-   auto p2Vertices = face.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   auto p2Edges    = face.getData( p2EdgeDoFMemoryID )->getPointer( Level );
+   auto p2Vertices = face.getData( p2VertexDoFMemoryID )->getPointer( level );
+   auto p2Edges    = face.getData( p2EdgeDoFMemoryID )->getPointer( level );
 
-   const auto p1Vertices = face.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   const auto p1Vertices = face.getData( p1VertexDoFMemoryID )->getPointer( level );
 
-   for ( const auto& it : vertexdof::macroface::Iterator( Level, 0 ) )
+   for ( const auto& it : vertexdof::macroface::Iterator( level, 0 ) )
    {
-      const uint_t idx = vertexdof::macroface::indexFromVertex( Level, it.x(), it.y(), stencilDirection::VERTEX_C );
+      const uint_t idx = vertexdof::macroface::indexFromVertex( level, it.x(), it.y(), stencilDirection::VERTEX_C );
       p2Vertices[idx]  = p1Vertices[idx];
    }
 
-   for ( const auto& it : edgedof::macroface::Iterator( Level, 0 ) )
+   for ( const auto& it : edgedof::macroface::Iterator( level, 0 ) )
    {
-      p2Edges[edgedof::macroface::indexFromHorizontalEdge( Level, it.x(), it.y(), stencilDirection::EDGE_HO_C )] =
+      p2Edges[edgedof::macroface::indexFromHorizontalEdge( level, it.x(), it.y(), stencilDirection::EDGE_HO_C )] =
           ValueType( 0.5 ) *
-          ( p1Vertices[vertexdof::macroface::indexFromHorizontalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_W )] +
-            p1Vertices[vertexdof::macroface::indexFromHorizontalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_E )] );
+          ( p1Vertices[vertexdof::macroface::indexFromHorizontalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_W )] +
+            p1Vertices[vertexdof::macroface::indexFromHorizontalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_E )] );
 
-      p2Edges[edgedof::macroface::indexFromDiagonalEdge( Level, it.x(), it.y(), stencilDirection::EDGE_DI_C )] =
+      p2Edges[edgedof::macroface::indexFromDiagonalEdge( level, it.x(), it.y(), stencilDirection::EDGE_DI_C )] =
           ValueType( 0.5 ) *
-          ( p1Vertices[vertexdof::macroface::indexFromDiagonalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_NW )] +
-            p1Vertices[vertexdof::macroface::indexFromDiagonalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_SE )] );
+          ( p1Vertices[vertexdof::macroface::indexFromDiagonalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_NW )] +
+            p1Vertices[vertexdof::macroface::indexFromDiagonalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_SE )] );
 
-      p2Edges[edgedof::macroface::indexFromVerticalEdge( Level, it.x(), it.y(), stencilDirection::EDGE_VE_C )] =
+      p2Edges[edgedof::macroface::indexFromVerticalEdge( level, it.x(), it.y(), stencilDirection::EDGE_VE_C )] =
           ValueType( 0.5 ) *
-          ( p1Vertices[vertexdof::macroface::indexFromVerticalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_N )] +
-            p1Vertices[vertexdof::macroface::indexFromVerticalEdge( Level, it.x(), it.y(), stencilDirection::VERTEX_S )] );
+          ( p1Vertices[vertexdof::macroface::indexFromVerticalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_N )] +
+            p1Vertices[vertexdof::macroface::indexFromVerticalEdge( level, it.x(), it.y(), stencilDirection::VERTEX_S )] );
    }
 }
 
 template < typename ValueType >
-inline void restrictP2ToP1( const uint_t&                                               Level,
+inline void restrictP2ToP1( const uint_t&                                               level,
                             const Face&                                                 face,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p2VertexDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p2EdgeDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Face >& p1VertexDoFMemoryID )
 {
-   const auto p2Vertices = face.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   const auto p2Edges    = face.getData( p2EdgeDoFMemoryID )->getPointer( Level );
+   const auto p2Vertices = face.getData( p2VertexDoFMemoryID )->getPointer( level );
+   const auto p2Edges    = face.getData( p2EdgeDoFMemoryID )->getPointer( level );
 
-   auto p1Vertices = face.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   auto p1Vertices = face.getData( p1VertexDoFMemoryID )->getPointer( level );
 
-   for ( const auto& it : vertexdof::macroface::Iterator( Level, 1 ) )
+   for ( const auto& it : vertexdof::macroface::Iterator( level, 1 ) )
    {
       ValueType    tmp;
-      const uint_t idx = vertexdof::macroface::indexFromVertex( Level, it.x(), it.y(), stencilDirection::VERTEX_C );
+      const uint_t idx = vertexdof::macroface::indexFromVertex( level, it.x(), it.y(), stencilDirection::VERTEX_C );
       tmp              = p2Vertices[idx];
 
       const std::array< stencilDirection, 6 > directVertexDoFNeighbors = { {
@@ -103,7 +103,7 @@ inline void restrictP2ToP1( const uint_t&                                       
 
       for ( const auto& neighbor : directVertexDoFNeighbors )
       {
-         const uint_t neighborEdgeIdx = edgedof::macroface::indexFromVertex( Level, it.x(), it.y(), neighbor );
+         const uint_t neighborEdgeIdx = edgedof::macroface::indexFromVertex( level, it.x(), it.y(), neighbor );
          tmp += ValueType( 0.5 ) * p2Edges[neighborEdgeIdx];
       }
 
@@ -116,25 +116,25 @@ inline void restrictP2ToP1( const uint_t&                                       
 namespace macroedge {
 
 template < typename ValueType >
-inline void prolongateP1ToP2( const uint_t&                                               Level,
+inline void prolongateP1ToP2( const uint_t&                                               level,
                               const Edge&                                                 edge,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p2VertexDoFMemoryID,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p2EdgeDoFMemoryID,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p1VertexDoFMemoryID )
 {
-   auto p2Vertices = edge.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   auto p2Edges    = edge.getData( p2EdgeDoFMemoryID )->getPointer( Level );
+   auto p2Vertices = edge.getData( p2VertexDoFMemoryID )->getPointer( level );
+   auto p2Edges    = edge.getData( p2EdgeDoFMemoryID )->getPointer( level );
 
-   const auto p1Vertices = edge.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   const auto p1Vertices = edge.getData( p1VertexDoFMemoryID )->getPointer( level );
 
-   for ( const auto& it : edgedof::macroedge::Iterator( Level, 0 ) )
+   for ( const auto& it : edgedof::macroedge::Iterator( level, 0 ) )
    {
-      p2Edges[edgedof::macroedge::indexFromHorizontalEdge( Level, it.x(), stencilDirection::EDGE_HO_C )] = 0;
+      p2Edges[edgedof::macroedge::indexFromHorizontalEdge( level, it.x(), stencilDirection::EDGE_HO_C )] = ValueType( 0 );
    }
 
-   for ( const auto& it : vertexdof::macroedge::Iterator( Level, 1 ) )
+   for ( const auto& it : vertexdof::macroedge::Iterator( level, 1 ) )
    {
-      const uint_t    idx               = vertexdof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::VERTEX_C );
+      const uint_t    idx               = vertexdof::macroedge::indexFromVertex( level, it.x(), stencilDirection::VERTEX_C );
       const ValueType p1VertexValue     = p1Vertices[idx];
       const ValueType p1VertexValueHalf = ValueType( 0.5 ) * p1VertexValue;
 
@@ -142,40 +142,40 @@ inline void prolongateP1ToP2( const uint_t&                                     
 
       for ( const auto& neighbor : edgedof::macroedge::neighborsOnEdgeFromVertex )
       {
-         const uint_t neighborEdgeIdx = edgedof::macroedge::indexFromVertex( Level, it.x(), neighbor );
+         const uint_t neighborEdgeIdx = edgedof::macroedge::indexFromVertex( level, it.x(), neighbor );
          p2Edges[neighborEdgeIdx] += p1VertexValueHalf;
       }
    }
 }
 
 template < typename ValueType >
-inline void restrictP2ToP1( const uint_t&                                               Level,
+inline void restrictP2ToP1( const uint_t&                                               level,
                             const Edge&                                                 edge,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p2VertexDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p2EdgeDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Edge >& p1VertexDoFMemoryID )
 {
-   const auto p2Vertices = edge.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   const auto p2Edges    = edge.getData( p2EdgeDoFMemoryID )->getPointer( Level );
+   const auto p2Vertices = edge.getData( p2VertexDoFMemoryID )->getPointer( level );
+   const auto p2Edges    = edge.getData( p2EdgeDoFMemoryID )->getPointer( level );
 
-   auto p1Vertices = edge.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   auto p1Vertices = edge.getData( p1VertexDoFMemoryID )->getPointer( level );
 
-   for ( const auto& it : vertexdof::macroedge::Iterator( Level, 1 ) )
+   for ( const auto& it : vertexdof::macroedge::Iterator( level, 1 ) )
    {
       ValueType    tmp;
-      const uint_t idx = vertexdof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::VERTEX_C );
+      const uint_t idx = vertexdof::macroedge::indexFromVertex( level, it.x(), stencilDirection::VERTEX_C );
       tmp              = p2Vertices[idx];
 
-      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_HO_W )];
-      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_HO_E )];
+      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_HO_W )];
+      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_HO_E )];
 
-      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_VE_S )];
-      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_DI_SE )];
+      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_VE_S )];
+      tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_DI_SE )];
 
       if ( edge.getNumNeighborFaces() == 2 )
       {
-         tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_VE_N )];
-         tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( Level, it.x(), stencilDirection::EDGE_DI_NW )];
+         tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_VE_N )];
+         tmp += ValueType( 0.5 ) * p2Edges[edgedof::macroedge::indexFromVertex( level, it.x(), stencilDirection::EDGE_DI_NW )];
       }
 
       p1Vertices[idx] = tmp;
@@ -187,7 +187,7 @@ inline void restrictP2ToP1( const uint_t&                                       
 namespace macrovertex {
 
 template < typename ValueType >
-inline void prolongateP1ToP2( const uint_t&                                                 Level,
+inline void prolongateP1ToP2( const uint_t&                                                 level,
                               const Vertex&                                                 vertex,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Vertex >& p2VertexDoFMemoryID,
                               const PrimitiveDataID< FunctionMemory< ValueType >, Vertex >& p2EdgeDoFMemoryID,
@@ -195,23 +195,23 @@ inline void prolongateP1ToP2( const uint_t&                                     
 {
    WALBERLA_UNUSED( p2EdgeDoFMemoryID );
 
-   auto       p2Vertices = vertex.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   const auto p1Vertices = vertex.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   auto       p2Vertices = vertex.getData( p2VertexDoFMemoryID )->getPointer( level );
+   const auto p1Vertices = vertex.getData( p1VertexDoFMemoryID )->getPointer( level );
 
    p2Vertices[0] = p1Vertices[0];
 }
 
 template < typename ValueType >
-inline void restrictP2ToP1( const uint_t&                                                 Level,
+inline void restrictP2ToP1( const uint_t&                                                 level,
                             const Vertex&                                                 vertex,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Vertex >& p2VertexDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Vertex >& p2EdgeDoFMemoryID,
                             const PrimitiveDataID< FunctionMemory< ValueType >, Vertex >& p1VertexDoFMemoryID )
 {
-   const auto p2Vertices = vertex.getData( p2VertexDoFMemoryID )->getPointer( Level );
-   const auto p2Edges    = vertex.getData( p2EdgeDoFMemoryID )->getPointer( Level );
+   const auto p2Vertices = vertex.getData( p2VertexDoFMemoryID )->getPointer( level );
+   const auto p2Edges    = vertex.getData( p2EdgeDoFMemoryID )->getPointer( level );
 
-   auto p1Vertices = vertex.getData( p1VertexDoFMemoryID )->getPointer( Level );
+   auto p1Vertices = vertex.getData( p1VertexDoFMemoryID )->getPointer( level );
 
    p1Vertices[0] = p2Vertices[0];
 
