lssfau/hyteg

commit 0998ae4645aa8ce37dcdbe1c331eeed377ffa29b

Add safeguards to P2Function w.r.t. issue #205
The following methods will now abort if they encounter macro-cells:
- restrictInjection()
- prolongateP1ToP2()
- restrictP2ToP1()
Additionally the commit extends P2P1TransferTest to run the embedding
test also in 3D, which currently needs to fail then.

src/hyteg/p2functionspace/P2Function.cpp
@@ -379,6 +379,11 @@ void P2Function< ValueType >::prolongateP1ToP2( const hyteg::P1Function< ValueTy
 {
    // Note: 'this' is the dst function - therefore we test this' boundary conditions
 
+   if ( !this->getStorage()->hasGlobalCells() )
+   {
+      WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
+   }
+
    this->startTiming( "Prolongate P1 -> P2" );
 
    p1Function.template startCommunication< Vertex, Edge >( level );
@@ -435,6 +440,11 @@ void P2Function< ValueType >::restrictP2ToP1( const P1Function< ValueType >& p1F
                                               const uint_t&                  level,
                                               const DoFType&                 flag ) const
 {
+   if ( !this->getStorage()->hasGlobalCells() )
+   {
+      WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
+   }
+
    this->startTiming( "Restrict P2 -> P1" );
 
    vertexDoFFunction_.template startCommunication< Edge, Vertex >( level );
@@ -506,6 +516,12 @@ void P2Function< ValueType >::restrictP2ToP1( const P1Function< ValueType >& p1F
 template < typename ValueType >
 void P2Function< ValueType >::restrictInjection( uint_t sourceLevel, DoFType flag ) const
 {
+
+   if ( !this->getStorage()->hasGlobalCells() )
+   {
+      WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
+   }
+
    for ( const auto& it : this->getStorage()->getFaces() )
    {
       const Face& face = *it.second;

tests/hyteg/P2/P2P1TransferTest.cpp
@@ -33,91 +33,101 @@ namespace hyteg {
 
 static void testP2P1Transfer()
 {
-  const uint_t level = 4;
+   const uint_t level = 4;
 
-  MeshInfo mesh  = MeshInfo::fromGmshFile( "../../data/meshes/tri_1el.msh" );
-  SetupPrimitiveStorage setupStorage( mesh, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
-  std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
+   MeshInfo                            mesh = MeshInfo::fromGmshFile( "../../data/meshes/tri_1el.msh" );
+   SetupPrimitiveStorage               setupStorage( mesh, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
 
-  auto p1Function = std::make_shared< P1Function< real_t > >( "p1Function", storage, level, level );
-  auto p2Function = std::make_shared< P2Function< real_t > >( "p2Function", storage, level, level );
+   auto p1Function = std::make_shared< P1Function< real_t > >( "p1Function", storage, level, level );
+   auto p2Function = std::make_shared< P2Function< real_t > >( "p2Function", storage, level, level );
 
-  VTKOutput vtkOutput("../../output", "P2P1TransferTest", storage);
-  vtkOutput.add( *p1Function );
-  vtkOutput.add( *p2Function );
+   VTKOutput vtkOutput( "../../output", "P2P1TransferTest", storage );
+   vtkOutput.add( *p1Function );
+   vtkOutput.add( *p2Function );
 
-  // To test the transfer we
-  // 1. set one vertex unknown in the middle of a P1 macro-face to testValue (rest 0.0)
-  // 2. we prolongate to P2 and should get testValue at the corresponding vertex unknown and 0.5 * testValue at all (six, directly) neighboring edge unknowns
-  // 3. we restrict again and should get a 2.5 * testValue at the corresponding vertex unknown and 0.25 * testValue at all neighboring vertex unknowns
+   // To test the transfer we
+   // 1. set one vertex unknown in the middle of a P1 macro-face to testValue (rest 0.0)
+   // 2. we prolongate to P2 and should get testValue at the corresponding vertex unknown and 0.5 * testValue at all (six, directly) neighboring edge unknowns
+   // 3. we restrict again and should get a 2.5 * testValue at the corresponding vertex unknown and 0.25 * testValue at all neighboring vertex unknowns
 
-  // Step 1:
+   // Step 1:
 
-  std::function< real_t( const Point3D & )> zeros = []( const Point3D & ) { return 0; };
-  p1Function->interpolate( zeros, level );
-  p2Function->interpolate( zeros, level );
+   std::function< real_t( const Point3D& ) > zeros = []( const Point3D& ) { return 0; };
+   p1Function->interpolate( zeros, level );
+   p2Function->interpolate( zeros, level );
 
-  const uint_t x = 4;
-  const uint_t y = 4;
-  const real_t testValue = 1.0;
+   const uint_t x         = 4;
+   const uint_t y         = 4;
+   const real_t testValue = 1.0;
 
-  std::vector< PrimitiveID > faceIDs;
-  storage->getFaceIDs( faceIDs );
+   std::vector< PrimitiveID > faceIDs;
+   storage->getFaceIDs( faceIDs );
 
-  WALBERLA_CHECK_EQUAL( storage->getNumberOfLocalFaces(), 1 );
-  WALBERLA_CHECK_EQUAL( faceIDs.size(),                   1 );
+   WALBERLA_CHECK_EQUAL( storage->getNumberOfLocalFaces(), 1 );
+   WALBERLA_CHECK_EQUAL( faceIDs.size(), 1 );
 
-  const auto p1FaceDataID = p1Function->getFaceDataID();
-        auto p1FaceData   = storage->getFace( faceIDs[ 0 ] )->getData( p1FaceDataID )->getPointer( level );
+   const auto p1FaceDataID = p1Function->getFaceDataID();
+   auto       p1FaceData   = storage->getFace( faceIDs[0] )->getData( p1FaceDataID )->getPointer( level );
 
-  const uint_t idx = vertexdof::macroface::indexFromVertex( level, x, y, stencilDirection::VERTEX_C );
+   const uint_t idx = vertexdof::macroface::indexFromVertex( level, x, y, stencilDirection::VERTEX_C );
 
-  p1FaceData[ idx ] = testValue;
+   p1FaceData[idx] = testValue;
 
-  vtkOutput.write( level, 1 );
+   vtkOutput.write( level, 1 );
 
-  // Step 2:
+   // Step 2:
 
-  p2Function->prolongateP1ToP2( *p1Function, level );
+   p2Function->prolongateP1ToP2( *p1Function, level );
 
-  const auto p2VertexDoFFaceDataID = p2Function->getVertexDoFFunction().getFaceDataID();
-  const auto p2EdgeDoFFaceDataID   = p2Function->getEdgeDoFFunction().getFaceDataID();
+   const auto p2VertexDoFFaceDataID = p2Function->getVertexDoFFunction().getFaceDataID();
+   const auto p2EdgeDoFFaceDataID   = p2Function->getEdgeDoFFunction().getFaceDataID();
 
-        auto p2VertexDoFFaceData   = storage->getFace( faceIDs[ 0 ] )->getData( p2VertexDoFFaceDataID )->getPointer( level );
-        auto p2EdgeDoFFaceData     = storage->getFace( faceIDs[ 0 ] )->getData( p2EdgeDoFFaceDataID )->getPointer( level );
+   auto p2VertexDoFFaceData = storage->getFace( faceIDs[0] )->getData( p2VertexDoFFaceDataID )->getPointer( level );
+   auto p2EdgeDoFFaceData   = storage->getFace( faceIDs[0] )->getData( p2EdgeDoFFaceDataID )->getPointer( level );
 
-  WALBERLA_CHECK_FLOAT_EQUAL( p2VertexDoFFaceData[ idx ], testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL( p2VertexDoFFaceData[idx], testValue )
 
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_W )], 0.5 * testValue )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_E )], 0.5 * testValue )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_NW )], 0.5 * testValue )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_SE )], 0.5 * testValue )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_N )], 0.5 * testValue )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_S )], 0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_W )],
+                               0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_E )],
+                               0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_NW )], 0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_SE )], 0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_N )],
+                               0.5 * testValue )
+   WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_S )],
+                               0.5 * testValue )
 
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_NW )], 0.0 )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_SE )], 0.0 )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_SW )], 0.0 )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_NE )], 0.0 )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_NW )], 0.0 )
-  WALBERLA_CHECK_FLOAT_EQUAL( p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_SE )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_NW )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_HO_SE )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_SW )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_DI_NE )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_NW )], 0.0 )
+   WALBERLA_CHECK_FLOAT_EQUAL(
+       p2EdgeDoFFaceData[edgedof::macroface::indexFromVertex( level, x, y, stencilDirection::EDGE_VE_SE )], 0.0 )
 
-  vtkOutput.write( level, 2 );
+   vtkOutput.write( level, 2 );
 
-  // Step 3:
+   // Step 3:
 
-  p2Function->restrictP2ToP1( *p1Function, level );
+   p2Function->restrictP2ToP1( *p1Function, level );
 
-  WALBERLA_CHECK_FLOAT_EQUAL( p1FaceData[ idx ], 2.5 * testValue );
+   WALBERLA_CHECK_FLOAT_EQUAL( p1FaceData[idx], 2.5 * testValue );
 
-  for ( const auto & neighbor : vertexdof::macroface::neighborsWithoutCenter )
-  {
-    WALBERLA_CHECK_FLOAT_EQUAL( p1FaceData[vertexdof::macroface::indexFromVertex( level, x, y,
-                                                                                  neighbor )], 0.25 * testValue );
-  }
+   for ( const auto& neighbor : vertexdof::macroface::neighborsWithoutCenter )
+   {
+      WALBERLA_CHECK_FLOAT_EQUAL( p1FaceData[vertexdof::macroface::indexFromVertex( level, x, y, neighbor )], 0.25 * testValue );
+   }
 
-  vtkOutput.write( level, 3 );
-
+   vtkOutput.write( level, 3 );
 }
 
 } // namespace hyteg
@@ -132,14 +142,8 @@ void logSectionHeader( const char* header )
    WALBERLA_LOG_INFO_ON_ROOT( separator << "\n " << hdr << "\n" << separator );
 }
 
-void run2dP1ToP2EmbeddingTest()
+void run_P1ToP2EmbeddingTest( const std::shared_ptr< PrimitiveStorage >& storage, uint_t level )
 {
-   uint_t level = 3;
-
-   MeshInfo meshInfo = MeshInfo::meshRectangle( Point2D( 0.0, -1.0 ), Point2D( 2.0, 3.0 ), MeshInfo::CRISS, 1, 2 );
-   SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
-   auto                  storage = std::make_shared< PrimitiveStorage >( setupStorage );
-
    // auto linearPolynomial = []( const Point3D& x ) { return real_c( 3 ) * x[0] + real_c( 0.5 ) * x[1] - real_c( 1 ); };
    auto linearPolynomial = []( const Point3D& ) { return real_c( 1 ); };
 
@@ -173,12 +177,29 @@ void run2dP1ToP2EmbeddingTest()
    WALBERLA_CHECK_LESS( errorMeasure, tol );
 }
 
+void run2D_P1ToP2EmbeddingTest( uint_t level )
+{
+   MeshInfo              meshInfo = MeshInfo::meshRectangle( Point2D( 0.0, -1.0 ), Point2D( 2.0, 3.0 ), MeshInfo::CRISS, 1, 2 );
+   SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   auto                  storage = std::make_shared< PrimitiveStorage >( setupStorage );
+
+   run_P1ToP2EmbeddingTest( storage, level );
+}
+
+void run3D_P1ToP2EmbeddingTest( uint_t level )
+{
+   MeshInfo              meshInfo = MeshInfo::fromGmshFile( "../../data/meshes/3D/regular_octahedron_8el.msh" );
+   SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   auto                  storage = std::make_shared< PrimitiveStorage >( setupStorage );
+
+   run_P1ToP2EmbeddingTest( storage, level );
+}
 
 int main( int argc, char* argv[] )
 {
    walberla::debug::enterTestMode();
 
-   walberla::Environment walberlaEnv(argc, argv);
+   walberla::Environment walberlaEnv( argc, argv );
    walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
    walberla::MPIManager::instance()->useWorldComm();
 
@@ -186,7 +207,10 @@ int main( int argc, char* argv[] )
    hyteg::testP2P1Transfer();
 
    logSectionHeader( "Testing embedding in 2D" );
-   run2dP1ToP2EmbeddingTest();
+   run2D_P1ToP2EmbeddingTest( 3 );
+
+   logSectionHeader( "Testing embedding in 3D" );
+   run3D_P1ToP2EmbeddingTest( 2 );
 
    return EXIT_SUCCESS;
 }

commit 80c7cc3e0db420228071f7419e7c817d38ed881b

Fix logic error in safe-guards introduced in [0998ae4]

src/hyteg/p2functionspace/P2Function.cpp
@@ -379,7 +379,7 @@ void P2Function< ValueType >::prolongateP1ToP2( const hyteg::P1Function< ValueTy
 {
    // Note: 'this' is the dst function - therefore we test this' boundary conditions
 
-   if ( !this->getStorage()->hasGlobalCells() )
+   if ( this->getStorage()->hasGlobalCells() )
    {
       WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
    }
@@ -440,7 +440,7 @@ void P2Function< ValueType >::restrictP2ToP1( const P1Function< ValueType >& p1F
                                               const uint_t&                  level,
                                               const DoFType&                 flag ) const
 {
-   if ( !this->getStorage()->hasGlobalCells() )
+   if ( this->getStorage()->hasGlobalCells() )
    {
       WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
    }
@@ -517,7 +517,7 @@ template < typename ValueType >
 void P2Function< ValueType >::restrictInjection( uint_t sourceLevel, DoFType flag ) const
 {
 
-   if ( !this->getStorage()->hasGlobalCells() )
+   if ( this->getStorage()->hasGlobalCells() )
    {
       WALBERLA_ABORT( "See issue #205 at https://i10git.cs.fau.de/hyteg/hyteg/-/issues/205" );
    }