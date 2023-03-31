0c2d42547ebffd8d1ebdb32e47afad67e5154ef2
Marcus Mohr <marcus.mohr@lmu.de>
2023-03-31 19:04:25 +0200

    Enable building with ADIOS2 [skip ci]
    Commit adds a new flag HYTEG_BUILD_WITH_ADIOS2 to our CMake infrastructure.
    If set to yes/on CMake will try to find ADIOS2's CXX and for parallel builds
    also its MPI component and also define the HYTEG_BUILD_WITH_ADIOS2.
    
    Additionally commit adds a BasicADIOSTest to check that compiling and linking
    works in both serial and parallel mode.

--- tests/hyteg/dataexport/BasicADIOSTest.cpp
+++ tests/hyteg/dataexport/BasicADIOSTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Dominik Thoennes, Marcus Mohr, Nils Kohl.
+ * Copyright (c) 2023 Marcus Mohr.
  *
  * This file is part of HyTeG
  * (see https://i10git.cs.fau.de/hyteg/hyteg).
@@ -17,28 +17,27 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#pragma once
+#include <adios2.h>
 
-#cmakedefine HYTEG_BUILD_WITH_ADIOS2
-#cmakedefine HYTEG_BUILD_WITH_PETSC
-#cmakedefine HYTEG_BUILD_WITH_TRILINOS
-#cmakedefine HYTEG_USE_GENERATED_KERNELS
-#cmakedefine HYTEG_TERRANEO_MODULE
+#include "core/DataTypes.h"
+#include "core/Environment.h"
+#include "core/debug/CheckFunctions.h"
+#include "core/debug/TestSubsystem.h"
+#include "core/timing/all.h"
 
-#ifdef HYTEG_USE_GENERATED_KERNELS
-namespace hyteg {
-namespace globalDefines {
-constexpr bool useGeneratedKernels = true;
-} // namespace globalDefines
-} // namespace hyteg
+int main( int argc, char* argv[] )
+{
+   walberla::debug::enterTestMode();
+
+   walberla::Environment walberlaEnv( argc, argv );
+   walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
+   walberla::MPIManager::instance()->useWorldComm();
+
+#ifdef WALBERLA_BUILD_WITH_MPI
+   adios2::ADIOS adios( walberla::MPIManager::instance()->comm() );
 #else
-namespace hyteg {
-namespace globalDefines {
-constexpr bool useGeneratedKernels = false;
-} // namespace globalDefines
-} // namespace hyteg
+   adios2::ADIOS adios;
 #endif
 
-// clang-format off
-#define HYTEG_ARCH_ENDIANESS ${CMAKE_CXX_BYTE_ORDER}
-// clang-format on
+   return EXIT_SUCCESS;
+}
