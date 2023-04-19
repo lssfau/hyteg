04508d6c15c2298722cb9e39b2eefbe2020a7100
Marcus Mohr <marcus.mohr@lmu.de>
2023-04-19 18:34:02 +0200

    Start with a BasicBlendingMapTest
    Currently the test will check that the new query functions isIdentity()
    and isAffine() are working correctly. This is done for most, but not all
    maps, yet, as generation of a map object can be tedious, because required
    constructor arguments vary a lot.

---
+++ tests/hyteg/blending/BasicBlendingMapTest.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2017-2023 Marcus Mohr.
+ *
+ * This file is part of HyTeG
+ * (see https://i10git.cs.fau.de/hyteg/hyteg).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <cfenv>
+#include <core/Environment.h>
+#include <core/math/Constants.h>
+#include <core/timing/Timer.h>
+
+#include "hyteg/PrimitiveID.hpp"
+#include "hyteg/dataexport/VTKOutput.hpp"
+#include "hyteg/elementwiseoperators/P1ElementwiseOperator.hpp"
+#include "hyteg/elementwiseoperators/P2ElementwiseOperator.hpp"
+#include "hyteg/geometry/AffineMap2D.hpp"
+#include "hyteg/geometry/AffineMap3D.hpp"
+#include "hyteg/geometry/AnnulusMap.hpp"
+#include "hyteg/geometry/CircularMap.hpp"
+#include "hyteg/geometry/IcosahedralShellMap.hpp"
+#include "hyteg/geometry/IdentityMap.hpp"
+#include "hyteg/geometry/PolarCoordsMap.hpp"
+#include "hyteg/geometry/ThinShellMap.hpp"
+#include "hyteg/geometry/TokamakMap.hpp"
+
+using walberla::real_t;
+using walberla::uint_c;
+using walberla::uint_t;
+using walberla::math::pi;
+
+using namespace hyteg;
+
+bool blendingMapIsIdentity( const GeometryMap& map )
+{
+   return dynamic_cast< const IdentityMap* >( &map ) != nullptr;
+}
+
+bool blendingMapIsAffine( const GeometryMap& map )
+{
+   return ( dynamic_cast< const IdentityMap* >( &map ) != nullptr ) ||
+             ( dynamic_cast< const AffineMap2D* >( &map ) != nullptr ) ||
+             ( dynamic_cast< const AffineMap3D* >( &map ) != nullptr );
+}
+
+struct MapProperties
+{
+   bool              isIdentity;
+   bool              isAffine;
+   GeometryMap::Type type;
+};
+
+auto genMap( const std::string& variant )
+{
+   std::shared_ptr< GeometryMap > map;
+
+   if ( variant == "IdentityMap" )
+   {
+      map = std::make_shared< IdentityMap >();
+   }
+
+   else if ( variant == "PolarCoordsMap" )
+   {
+      map = std::make_shared< PolarCoordsMap >();
+   }
+
+   else if ( variant == "AffineMap2D" )
+   {
+      map = std::make_shared< AffineMap2D >( Matrix2r(), Point2D() );
+   }
+
+   else if ( variant == "AffineMap3D" )
+   {
+      map = std::make_shared< AffineMap3D >( Matrix3r(), Point3D() );
+   }
+
+   else if ( variant == "AnnulusMap" )
+   {
+      MeshInfo              meshInfo = MeshInfo::meshAnnulus( real_c( 1 ), real_c( 2 ), MeshInfo::CRISS, 6, 1 );
+      SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+      Face                  face = *( setupStorage.getFaces().begin()->second );
+      map                        = std::make_shared< AnnulusMap >( face );
+   }
+
+   else if ( variant == "CircularMap" )
+   {
+      MeshInfo meshInfo =
+          MeshInfo::singleTriangle( { real_c( 0 ), real_c( 0 ) }, { real_c( -1 ), real_c( 0 ) }, { real_c( 1 ), real_c( 0 ) } );
+      SetupPrimitiveStorage setupStorage( meshInfo, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+      Face                  face = *( setupStorage.getFaces().begin()->second );
+      Point3D               center{ real_c( 0 ), real_c( 0 ), real_c( 0 ) };
+      map = std::make_shared< CircularMap >( face, setupStorage, center, real_c( 0.5 ) );
+   }
+
+   else
+   {
+      WALBERLA_ABORT( "Map variant '" << variant << "' not supported!" );
+   }
+
+   return map;
+}
+
+int main( int argc, char* argv[] )
+{
+   // basic setup
+   walberla::Environment walberlaEnv( argc, argv );
+   walberla::logging::Logging::instance()->setLogLevel( walberla::logging::Logging::PROGRESS );
+   walberla::MPIManager::instance()->useWorldComm();
+
+   // generate a list of maps to check
+   std::vector< std::string > mapVariants{
+       "IdentityMap", "PolarCoordsMap", "AffineMap2D", "AffineMap3D", "AnnulusMap", "CircularMap" };
+
+   // test property query methods
+   for ( const auto& variant : mapVariants )
+   {
+      WALBERLA_LOG_INFO_ON_ROOT( "*** Testing " << variant << " ***" );
+      auto candidate = genMap( variant );
+      WALBERLA_LOG_INFO_ON_ROOT( "isIdentity = " << std::boolalpha << candidate->isIdentity() );
+      WALBERLA_LOG_INFO_ON_ROOT( "isAffine   = " << std::boolalpha << candidate->isAffine() );
+      WALBERLA_CHECK_EQUAL( candidate->isIdentity(), blendingMapIsIdentity( *candidate ) );
+      WALBERLA_CHECK_EQUAL( candidate->isAffine(), blendingMapIsAffine( *candidate ) );
+   }
+
+   // test (de)serialisation
+   // TODO
+
+   return 0;
+}
