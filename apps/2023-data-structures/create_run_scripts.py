5e2fdc591e72fbffc6ba5bbc97dbc3eaa337cdec
Daniel Bauer <daniel.j.bauer@fau.de>
2023-07-27 12:14:07 +0200

    update create run scripts script

--- apps/2023-data-structures/create_run_scripts.py
+++ apps/2023-data-structures/create_run_scripts.py
@@ -1,23 +1,29 @@
 import time
 
+class Mesh:
+    def __init__(self, toroidal_resolution, poloidal_resolution, tube_layer_radii, n1e1_spectral_radius, p1_spectral_radius):
+        self.toroidal_resolution  = toroidal_resolution
+        self.poloidal_resolution  = poloidal_resolution
+        self.tube_layer_radii     = tube_layer_radii
+        self.n1e1_spectral_radius = n1e1_spectral_radius
+        self.p1_spectral_radius   = p1_spectral_radius
+
 # those are constant for now
 pre_smooth = 1
 post_smooth = 1
-fmg_v_cycles = 4
-n1e1SpectralRadius = 3.08346
-p1SpectralRadius   = 2.00736
-max_level = 7
+fmg_v_cycles = 5
 ppn = 128
 
+mesh0005 = Mesh( 34,  6, [               0.4], 3.08346, 2.00736)
+mesh0256 = Mesh(136, 10, [0.1, 0.2, 0.3, 0.4], 3.2032 , 2.01869)
 
-def create_file(datestamp, nodes, coarse_refinements, thin_torus):
-    base_name = '_'.join(['curlcurl', datestamp, f'nodes_{nodes}_coarse_{coarse_refinements}_thin_{thin_torus}'])
-
-    default_toroidal_resolution = 34
-    default_tube_radius = 0.4
+def create_file(datestamp, mesh, nodes, max_level):
+    base_name = '_'.join(['curlcurl', datestamp, f'nodes_{nodes:04}_lvl_{max_level}'])
 
     prm_file = base_name + ".prm"
     job_file = base_name + ".job"
+
+    radii_string = "\n    ".join(f"{i} {r};" for i, r in enumerate(mesh.tube_layer_radii))
 
     prm_file_string = f"""
 Parameters
@@ -36,16 +42,20 @@ Parameters
   postSmooth    {post_smooth};
   numVCyclesFMG {fmg_v_cycles};
 
-  n1e1SpectralRadius {n1e1SpectralRadius};
-  p1SpectralRadius   {p1SpectralRadius};
+  n1e1SpectralRadius {mesh.n1e1_spectral_radius};
+  p1SpectralRadius   {mesh.p1_spectral_radius};
 
-  coarseGridRefinements {coarse_refinements};
+  coarseGridRefinements 0;
 
-  toroidalResolution {default_toroidal_resolution * 2 ** thin_torus};
-  poloidalResolution 6;
-  tubeLayerRadius {default_tube_radius / 2 ** thin_torus};
+  toroidalResolution {mesh.toroidal_resolution};
+  poloidalResolution {mesh.poloidal_resolution};
+  tubeLayerRadii {{
+    {radii_string}
+  }}
 
   vtk false;
+  printSetupStorage false;
+  printPrimitiveStorage true;
   timingJSON true;
 
   baseName {base_name};
@@ -57,7 +67,7 @@ Parameters
 
 #PBS -N {base_name}
 #PBS -l select={nodes}:node_type=rome:mpiprocs={ppn}
-#PBS -l walltime=00:12:00
+#PBS -l walltime=00:24:00
 
 module load petsc
 
@@ -73,15 +83,11 @@ mpirun -n {nodes * ppn} ./curlCurlConvergence {prm_file} > {base_name + ".out"} 
         f.write(job_file_string)
 
 
-# 612 tets for coarse refinement 0
-# 5 nodes * 128 ppn = 640 cores
-
 datestamp = time.strftime('%y_%m_%d-%H_%M_%S')
 
-nodes = [5, 40, 320, 2560]
-coarse_refinements = [0, 1, 2, 3]
-thin_torus = [0, 1]
+nodes = [256, int(256/8), int(256/8**2)]
+meshes = 3 * [mesh0256]
+max_levels = [7, 6, 5]
 
-for n, c in zip(nodes, coarse_refinements):
-    for t in thin_torus:
-        create_file(datestamp, n, c, t)
+for n, m, l in zip(nodes, meshes, max_levels):
+    create_file(datestamp, m, n, l)
