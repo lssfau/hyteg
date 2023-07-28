ad829b9031e8773f194c332319d73ed4da3feb9f
Daniel Bauer <daniel.j.bauer@fau.de>
2023-07-28 13:48:22 +0200

    update torus visualization app

--- apps/2023-data-structures/torusVisualization.cpp
+++ apps/2023-data-structures/torusVisualization.cpp
@@ -22,7 +22,7 @@
 #include "core/mpi/Environment.h"
 
 #include "hyteg/dataexport/VTKOutput/VTKOutput.hpp"
-#include "hyteg/geometry/TokamakMap.hpp"
+#include "hyteg/geometry/TorusMap.hpp"
 #include "hyteg/mesh/MeshInfo.hpp"
 #include "hyteg/n1e1functionspace/N1E1VectorFunction.hpp"
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
@@ -33,52 +33,71 @@ using walberla::real_t;
 using walberla::uint_c;
 using walberla::uint_t;
 
-int main( int argc, char** argv )
+std::shared_ptr< PrimitiveStorage >
+    createStorage( const uint_t toroidalResolution, const uint_t poloidalResolution, const std::vector< real_t > tubeLayerRadii )
 {
-   walberla::mpi::Environment MPIenv( argc, argv );
-   walberla::MPIManager::instance()->useWorldComm();
+   const real_t radiusOriginToCenterOfTube = 2;
+   const real_t torodialStartAngle         = 0.0;
+   const real_t polodialStartAngle         = 0.0;
 
-   const uint_t level      = 3;
-   const uint_t resolution = uint_c( std::pow( level, 2 ) ) + 1;
-
-   const uint_t                toroidalResolution         = 34;
-   const uint_t                poloidalResolution         = 6;
-   const real_t                radiusOriginToCenterOfTube = 2;
-   const std::vector< real_t > tubeLayerRadii             = { 0.4 };
-   const real_t                torodialStartAngle         = 0.0;
-   const real_t                polodialStartAngle         = 0.0;
-   const real_t                delta                      = 0;
-   const real_t                r1                         = tubeLayerRadii.back();
-   const real_t                r2                         = tubeLayerRadii.back();
-
-   const MeshInfo        torus = MeshInfo::meshTorus( toroidalResolution,
+   const MeshInfo torus = MeshInfo::meshTorus( toroidalResolution,
                                                poloidalResolution,
                                                radiusOriginToCenterOfTube,
                                                tubeLayerRadii,
                                                torodialStartAngle,
                                                polodialStartAngle );
+
    SetupPrimitiveStorage setupStorage( torus, uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
 
-   TokamakMap::setMap( setupStorage,
-                       toroidalResolution,
-                       poloidalResolution,
-                       radiusOriginToCenterOfTube,
-                       tubeLayerRadii,
-                       torodialStartAngle,
-                       polodialStartAngle,
-                       delta,
-                       r1,
-                       r2 );
-   std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
+   TorusMap::setMap( setupStorage,
+                     toroidalResolution,
+                     poloidalResolution,
+                     radiusOriginToCenterOfTube,
+                     tubeLayerRadii,
+                     torodialStartAngle,
+                     polodialStartAngle );
+   return std::make_shared< PrimitiveStorage >( setupStorage );
+}
+
+int main( int argc, char** argv )
+{
+   walberla::mpi::Environment MPIenv( argc, argv );
+   walberla::MPIManager::instance()->useWorldComm();
 
    // edges
-   writeBlendedCoarseMeshVTK( storage, "output", "torusVisualization", resolution );
+   {
+      std::shared_ptr< PrimitiveStorage > storage = createStorage( 136, 10, { 0.1, 0.2, 0.3, 0.4 } );
+
+      const uint_t resolution = 3;
+      writeBlendedCoarseMeshVTK( storage, "output", "torusVisualization", resolution );
+   }
 
    // faces
-   n1e1::N1E1VectorFunction< real_t > u( "u", storage, level, level );
-   VTKOutput                          vtk( "output", "torusVisualization", storage );
-   vtk.add( u );
-   vtk.write( level );
+   {
+      std::shared_ptr< PrimitiveStorage > storage = createStorage( 34, 6, { 0.4 } );
+      const uint_t                        level   = 5;
+
+      const real_t R = 2;
+      const real_t r = 0.4;
+
+      const auto analyticalSol = [R, r]( const Point3D& xVec ) {
+         const real_t x    = xVec[0];
+         const real_t y    = xVec[1];
+         const real_t z    = xVec[2];
+         const real_t tmp0 = std::sqrt( std::pow( x, 2 ) + std::pow( y, 2 ) );
+         const real_t tmp1 = ( -std::pow( r, 2 ) + std::pow( z, 2 ) + std::pow( -R + tmp0, 2 ) ) / tmp0;
+         const real_t u0   = tmp1 * y;
+         const real_t u1   = -tmp1 * x;
+         const real_t u2   = 0;
+         return Point3D{ u0, u1, u2 };
+      };
+      n1e1::N1E1VectorFunction< real_t > sol( "sol", storage, level, level );
+      sol.interpolate( analyticalSol, level );
+
+      VTKOutput vtk( "output", "torusVisualization", storage );
+      vtk.add( sol );
+      vtk.write( level );
+   }
 
    return EXIT_SUCCESS;
 }
