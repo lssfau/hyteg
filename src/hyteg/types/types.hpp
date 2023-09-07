0714939141c1415118e9c3a18347d7fa48dbd4e1
Marcus Mohr <marcus.mohr@lmu.de>
2023-09-07 15:27:24 +0200

    Run clang-format on types.hpp

--- src/hyteg/types/types.hpp
+++ src/hyteg/types/types.hpp
@@ -28,56 +28,61 @@ using idx_t = int64_t;
 
 enum UpdateType
 {
-  Replace = 0,
-  Add = 1
+   Replace = 0,
+   Add     = 1
 };
 
-enum DoFType:size_t
+enum DoFType : size_t
 {
-  None = 0,
-  All = 1+2+4+8,
-  Boundary = 2+4+8,
-  Inner = 1,
-  DirichletBoundary = 2,
-  NeumannBoundary = 4,
-  FreeslipBoundary = 8
+   None              = 0,
+   All               = 1 + 2 + 4 + 8,
+   Boundary          = 2 + 4 + 8,
+   Inner             = 1,
+   DirichletBoundary = 2,
+   NeumannBoundary   = 4,
+   FreeslipBoundary  = 8
 };
 
-inline DoFType operator|(DoFType a, DoFType b){
-  return DoFType(static_cast<size_t>(a) | static_cast<size_t>(b));
+inline DoFType operator|( DoFType a, DoFType b )
+{
+   return DoFType( static_cast< size_t >( a ) | static_cast< size_t >( b ) );
 }
 
-inline DoFType operator&(DoFType a, DoFType b){
-  return DoFType(static_cast<size_t>(a) & static_cast<size_t>(b));
+inline DoFType operator&( DoFType a, DoFType b )
+{
+   return DoFType( static_cast< size_t >( a ) & static_cast< size_t >( b ) );
 }
 
-inline DoFType operator^(DoFType a, DoFType b){
-  return DoFType(static_cast<size_t>(a) ^ static_cast<size_t>(b));
+inline DoFType operator^( DoFType a, DoFType b )
+{
+   return DoFType( static_cast< size_t >( a ) ^ static_cast< size_t >( b ) );
 }
 
-inline std::ostream& operator<<(std::ostream &os, const DoFType type){
-  switch(type) {
-    case NeumannBoundary   :
+inline std::ostream& operator<<( std::ostream& os, const DoFType type )
+{
+   switch ( type )
+   {
+   case NeumannBoundary:
       return os << "NeumannBoundary";
-    case DirichletBoundary :
+   case DirichletBoundary:
       return os << "DirichletBoundary";
-    default:
+   default:
       return os << "Inner";
-  }
+   }
 }
 
-
-inline bool testFlag(DoFType a, DoFType b)
+inline bool testFlag( DoFType a, DoFType b )
 {
-  return (a & b) != 0;
+   return ( a & b ) != 0;
 }
 
-enum class MemoryType {
-  Base,
-  P1Stencil,
-  P1Function,
-  P1BubbleFunction,
-  P1BubbleStencil
+enum class MemoryType
+{
+   Base,
+   P1Stencil,
+   P1Function,
+   P1BubbleFunction,
+   P1BubbleStencil
 };
 
 enum class CycleType
@@ -87,4 +92,3 @@ enum class CycleType
 };
 
 } // namespace hyteg
-
