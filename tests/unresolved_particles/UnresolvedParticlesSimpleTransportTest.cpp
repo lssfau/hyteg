6a47cfdc733a993db8d98953c93150e51913055f
Nils Kohl <nils.kohl@fau.de>
2023-09-19 14:59:06 +0200

    Improved unresolved particle advection test.

--- tests/unresolved_particles/UnresolvedParticlesSimpleTransportTest.cpp
+++ tests/unresolved_particles/UnresolvedParticlesSimpleTransportTest.cpp
@@ -34,14 +34,19 @@ using namespace walberla::unresolved_particles;
 using walberla::unresolved_particles::Vec3;
 using namespace hyteg;
 
+/// This tests simply advects some particles through a "tube" and later counts if all of them arrived.
 void simpleTransportTest()
 {
+   bool vtk = false;
+
    real_t cubeSize             = 10;
-   uint_t tunnelStretch        = 5;
-   real_t particleInitCubeSize = cubeSize / 2;
-   uint_t timesteps            = 1000;
-   real_t dt                   = 1e-2;
-   uint_t level                = 3;
+   uint_t tunnelStretch        = 2;
+   real_t particleInitCubeSize = cubeSize / 3;
+
+   uint_t timesteps =
+       300; // this was determined experimentally (it`s just the number of time steps it takes such that all particles wander over)
+   real_t dt    = 2e-2;
+   uint_t level = 2;
 
    MeshInfo meshInfo = MeshInfo::meshCuboid(
        Point3D( 0, 0, 0 ), Point3D( real_c( tunnelStretch ) * cubeSize, cubeSize, cubeSize ), tunnelStretch, 1, 1 );
@@ -53,7 +58,7 @@ void simpleTransportTest()
 
    hyteg::unresolved_particles::UnresolvedParticles unresolvedParticles( storage );
 
-   uint_t numParticles = 100;
+   uint_t numParticles = 50;
    for ( uint_t i = 0; i < numParticles; i++ )
    {
       Point3D pos( ( walberla::math::realRandom() - 0.5 ) * particleInitCubeSize,
@@ -64,29 +69,58 @@ void simpleTransportTest()
       auto particleCreated = unresolvedParticles.createParticle( pos );
       if ( particleCreated )
       {
-         real_t massAndSize = std::clamp( walberla::math::realRandom(), 0.5, 1.0 );
-
          auto particle = particleCreated.value();
-
-         particle->setInteractionRadius( massAndSize );
-         particle->setInvMass( 1 / massAndSize );
+         particle->setInteractionRadius( 0.1 );
+         particle->setInvMass( 0.1 );
       }
    }
 
-   unresolvedParticles.initVTK();
+   if ( vtk )
+   {
+      unresolvedParticles.initVTK();
+   }
 
-   P1VectorFunction< real_t > forceField( "forceField", storage, 3, 3 );
+   P1VectorFunction< real_t > forceField( "forceField", storage, level, level );
    forceField[0].interpolate( 5.0, level );
+
+   auto countParticles = [&]( real_t ballCenterX, real_t ballRadius ) -> uint_t {
+      uint_t countedParticles = 0;
+
+      for ( auto p : *unresolvedParticles.getParticleStorage() )
+      {
+         if ( ( p.getPosition() - Vec3( ballCenterX, cubeSize / 2, cubeSize / 2 ) ).length() < ballRadius )
+         {
+            countedParticles++;
+         }
+      }
+
+      return countedParticles;
+   };
 
    for ( uint_t ts = 0; ts < timesteps; ts++ )
    {
-      WALBERLA_LOG_DEVEL_ON_ROOT( "Timestep: " << ts );
+      auto particlesStart = countParticles( cubeSize / 2, cubeSize / 2 );
+      auto particlesEnd   = countParticles( cubeSize + cubeSize / 2, cubeSize / 2 );
+
+      particlesStart = walberla::mpi::reduce( particlesStart, walberla::mpi::SUM );
+      particlesEnd   = walberla::mpi::reduce( particlesEnd, walberla::mpi::SUM );
+
+      WALBERLA_LOG_DEVEL_ON_ROOT( "Timestep: " << ts << " particles start: " << particlesStart
+                                               << " particles end: " << particlesEnd );
 
       hyteg::unresolved_particles::applyForceField( unresolvedParticles, forceField, level );
       hyteg::unresolved_particles::explicitEulerStep( unresolvedParticles, dt );
 
-      unresolvedParticles.writeVTK();
+      if ( vtk )
+      {
+         unresolvedParticles.writeVTK();
+      }
    }
+
+   auto particlesFinal = countParticles( cubeSize + cubeSize / 2, cubeSize / 2 );
+   particlesFinal      = walberla::mpi::allReduce( particlesFinal, walberla::mpi::SUM );
+
+   WALBERLA_CHECK_EQUAL( particlesFinal, numParticles );
 }
 
 int main( int argc, char** argv )
