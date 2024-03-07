433450625051975a6e956d3c1deeeae49e8d9ddc
Nils Kohl <nils.kohl@fau.de>
2024-03-07 14:42:45 +0100

    Integrating P2toP2QuadraticVectorProlongation.hpp and P2toP2QuadraticVectorRestriction.hpp from state at 0a3cdd49a6a15782b46d5acfae8305ce88a1a4cf. Credits to Andreas Burkhart.
    Just the vector versions for prolongation and restriction of P2Functions.
    
    Co-authored-by: Andreas Burkhart <burk@ma.tum.de>

---
+++ src/hyteg/gridtransferoperators/P2toP2QuadraticVectorProlongation.hpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2023 Andreas Burkhart.
+ *
+ * This file is part of HyTeG
+ * (see https://i10git.cs.fau.de/hyteg/hyteg).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "hyteg/composites/P2P1TaylorHoodFunction.hpp"
+#include "hyteg/gridtransferoperators/P1toP1LinearProlongation.hpp"
+#include "hyteg/gridtransferoperators/P2toP2QuadraticProlongation.hpp"
+#include "hyteg/gridtransferoperators/ProlongationOperator.hpp"
+#include "hyteg/p2functionspace/P2ProjectNormalOperator.hpp"
+
+namespace hyteg {
+
+class P2toP2QuadraticVectorProlongation : public ProlongationOperator< P2VectorFunction< real_t > >
+{
+ public:
+   void prolongate( const P2VectorFunction< real_t >& function, const uint_t& sourceLevel, const DoFType& flag ) const override
+   {
+      for ( uint_t k = 0; k < function.getDimension(); k++ )
+      {
+         quadraticProlongationOperator_.prolongate( function[k], sourceLevel, flag );
+      }
+   }
+
+   void prolongateAndAdd( const P2VectorFunction< real_t >& function,
+                          const uint_t&                     sourceLevel,
+                          const DoFType&                    flag ) const override
+   {
+      for ( uint_t k = 0; k < function.getDimension(); k++ )
+      {
+         quadraticProlongationOperator_.prolongateAndAdd( function[k], sourceLevel, flag );
+      }
+   }
+
+ private:
+   P2toP2QuadraticProlongation quadraticProlongationOperator_;
+};
+
+class P2toP2QuadraticVectorProlongationWithProjection : public P2toP2QuadraticVectorProlongation
+{
+ public:
+   P2toP2QuadraticVectorProlongationWithProjection( std::shared_ptr< P2VectorFunction< real_t > > temp,
+                                                    std::shared_ptr< P2ProjectNormalOperator >    projection )
+   : P2toP2QuadraticVectorProlongation()
+   , temp_( temp )
+   , projection_( projection )
+   {}
+
+   void prolongate( const P2VectorFunction< real_t >& function, const uint_t& sourceLevel, const DoFType& flag ) const override
+   {
+      P2toP2QuadraticVectorProlongation::prolongate( function, sourceLevel, flag );
+      projection_->project( function, sourceLevel + 1, FreeslipBoundary );
+   }
+
+   // prolongateAndAdd has a different implementation than prolongate and seemingly needs internal projections
+   // as a quick fix we are using prolongate on a temporary function and add it manually
+   void prolongateAndAdd( const P2VectorFunction< real_t >& function,
+                          const uint_t&                     sourceLevel,
+                          const DoFType&                    flag ) const override
+   {
+      temp_->assign( { 1.0 }, { function }, sourceLevel, All );
+      prolongate( *temp_, sourceLevel, flag );
+      function.assign( { 1.0, 1.0 }, { function, *temp_ }, sourceLevel + 1, flag );
+   }
+
+ private:
+   std::shared_ptr< P2VectorFunction< real_t > > temp_;
+   std::shared_ptr< P2ProjectNormalOperator >    projection_;
+};
+} // namespace hyteg
