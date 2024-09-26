7f250785304e63172816ced97502d364d4663d40
Ponsuganth Ilangovan Ponkumar Ilango <pponkumar@geophysik.uni-muenchen.de>
2024-09-26 11:12:58 +0200

    Add some averaging and grid transfer [skip ci]

--- src/hyteg/p0functionspace/P0Function.hpp
+++ src/hyteg/p0functionspace/P0Function.hpp
@@ -25,6 +25,7 @@
 #include "hyteg/functions/FunctionProperties.hpp"
 #include "hyteg/p1functionspace/VertexDoFMacroFace.hpp"
 #include "hyteg/volumedofspace/VolumeDoFFunction.hpp"
+#include "hyteg/p1functionspace/P1Function.hpp"
 
 namespace hyteg {
 
@@ -238,7 +239,64 @@ class P0Function : public Function< P0Function< ValueType > >
       WALBERLA_LOG_WARNING_ON_ROOT( "P0Function::interpolate() 'interpolates' values at the centroid." );
       if ( this->storage_->hasGlobalCells() )
       {
-         WALBERLA_ABORT( "Not implemented" );
+         for ( auto& it : this->getStorage()->getCells() )
+         {
+            const auto  cellID = it.first;
+            const auto& cell   = *it.second;
+
+            WALBERLA_CHECK_EQUAL( getDGFunction()->polynomialDegree( cellID ), 0 );
+            WALBERLA_CHECK_EQUAL( getDGFunction()->basis()->numDoFsPerElement( 3, 0 ), 1 );
+
+            std::vector< ValueType* > dofs;
+            dofs.reserve( srcFunctions.size() + 1 );
+
+            std::vector< volumedofspace::indexing::VolumeDoFMemoryLayout > memLayouts;
+            memLayouts.reserve( srcFunctions.size() + 1 );
+
+            std::vector< ValueType > srcValues;
+            srcValues.reserve( srcFunctions.size() );
+
+            dofs.push_back( getDGFunction()->volumeDoFFunction()->dofMemory( cellID, level ) );
+            memLayouts.push_back( getDGFunction()->volumeDoFFunction()->memoryLayout() );
+
+            for ( const auto src : srcFunctions )
+            {
+               dofs.push_back( src.get().getDGFunction()->volumeDoFFunction()->dofMemory( cellID, level ) );
+               memLayouts.push_back( src.get().getDGFunction()->volumeDoFFunction()->memoryLayout() );
+            }
+
+            for ( auto cellType : celldof::allCellTypes )
+            {
+               for ( const auto& idxIt : celldof::macrocell::Iterator( level, cellType ) )
+               {
+                  const std::array< indexing::Index, 4 > vertexIndices =
+                      celldof::macrocell::getMicroVerticesFromMicroCell( idxIt, cellType );
+                  std::array< Point3D, 4 > elementVertices;
+                  for ( uint_t i = 0; i < 4; i++ )
+                  {
+                     const auto elementVertex = vertexdof::macrocell::coordinateFromIndex( level, cell, vertexIndices[i] );
+                     elementVertices[i]( 0 )  = elementVertex[0];
+                     elementVertices[i]( 1 )  = elementVertex[1];
+                     elementVertices[i]( 2 )  = elementVertex[2];
+                  }
+
+                  const Point3D centroid =
+                      ( elementVertices[0] + elementVertices[1] + elementVertices[2] + elementVertices[3] ) / real_c( 4 );
+
+                  for ( size_t k = 0; k < srcFunctions.size(); ++k )
+                  {
+                     srcValues[k] = dofs[k + 1][volumedofspace::indexing::index(
+                         idxIt.x(), idxIt.y(), idxIt.z(), cellType, 0, 1, level, memLayouts[k + 1] )];
+                  }
+
+                  const auto val = expression( Point3D( centroid( 0 ), centroid( 1 ), centroid( 2 ) ), srcValues );
+
+                  dofs[0]
+                      [volumedofspace::indexing::index( idxIt.x(), idxIt.y(), idxIt.z(), cellType, 0, 1, level, memLayouts[0] )] =
+                          ValueType( val );
+               }
+            }
+         }
       }
       else
       {
@@ -300,6 +358,150 @@ class P0Function : public Function< P0Function< ValueType > >
          }
       }
    };
+
+   void transferToLowerLevel( uint_t level )
+   {
+      for ( auto& it : this->storage_->getCells() )
+      {
+         PrimitiveID cellID = it.first;
+         Cell&       cell   = *( it.second );
+
+         uint_t coarseLevel = level - 1;
+         uint_t fineLevel   = level;
+
+         const auto coarseDofMemory = this->getDGFunction()->volumeDoFFunction()->dofMemory( cellID, coarseLevel );
+         const auto fineDofMemory   = this->getDGFunction()->volumeDoFFunction()->dofMemory( cellID, fineLevel );
+
+         for ( auto coarseCellType : celldof::allCellTypes )
+         {
+            for ( const auto& itSrc : celldof::macrocell::Iterator( coarseLevel, coarseCellType ) )
+            {
+               const indexing::Index& coarseElementIdx = itSrc;
+
+               std::vector< hyteg::indexing::Index > fineElementIndices;
+               std::vector< celldof::CellType >      fineCellTypes;
+
+               real_t fineAverage = 0.0;
+
+               volumedofspace::indexing::getFineMicroElementsFromCoarseMicroElement(
+                   coarseElementIdx, coarseCellType, fineElementIndices, fineCellTypes );
+
+               WALBERLA_CHECK_EQUAL( fineElementIndices.size(), fineCellTypes.size() );
+
+               for ( uint_t fineIdx = 0; fineIdx < fineElementIndices.size(); fineIdx += 1 )
+               {
+                  auto fineElementIdx = fineElementIndices[fineIdx];
+                  auto fineCellType   = fineCellTypes[fineIdx];
+
+                  fineAverage +=
+                      fineDofMemory[volumedofspace::indexing::index( fineElementIdx.x(),
+                                                                     fineElementIdx.y(),
+                                                                     fineElementIdx.z(),
+                                                                     fineCellType,
+                                                                     0u,
+                                                                     1u,
+                                                                     fineLevel,
+                                                                     volumedofspace::indexing::VolumeDoFMemoryLayout::SoA )];
+               }
+
+               // WALBERLA_LOG_INFO_ON_ROOT( "fineElementIndices.size() = " << fineElementIndices.size() );
+
+               coarseDofMemory[volumedofspace::indexing::index( coarseElementIdx.x(),
+                                                                coarseElementIdx.y(),
+                                                                coarseElementIdx.z(),
+                                                                coarseCellType,
+                                                                0u,
+                                                                1u,
+                                                                coarseLevel,
+                                                                volumedofspace::indexing::VolumeDoFMemoryLayout::SoA )] =
+                   fineAverage / fineElementIndices.size();
+            }
+         }
+      }
+   }
+
+   void transferToAllLowerLevels( uint_t sourceLevel )
+   {
+      for ( uint_t level = sourceLevel; level > this->getMinLevel(); level-- )
+      {
+         transferToLowerLevel( level );
+      }
+   }
+
+   inline real_t evaluateSampledAverage( std::array< Point3D, 4 > microTets, std::array< real_t, 4 > valueTets )
+   {
+      // WALBERLA_CHECK( nStrides > 1u, "nStrides must be greater than 1u" );
+
+      Point3D microTet0 = microTets[0];
+      Point3D microTet1 = microTets[1];
+      Point3D microTet2 = microTets[2];
+      Point3D microTet3 = microTets[3];
+
+      real_t valueTet0 = valueTets[0];
+      real_t valueTet1 = valueTets[1];
+      real_t valueTet2 = valueTets[2];
+      real_t valueTet3 = valueTets[3];
+
+      Point3D coordinates = (microTet0 + microTet1 + microTet2 + microTet3) / 4.0;
+
+      auto xLocal = vertexdof::macrocell::detail::transformToLocalTet( microTet0, microTet1, microTet2, microTet3, coordinates );
+
+      auto value = valueTet0 * ( real_c( 1.0 ) - xLocal[0] - xLocal[1] - xLocal[2] ) + valueTet1 * xLocal[0] +
+                  valueTet2 * xLocal[1] + valueTet3 * xLocal[2];
+
+      return (value + valueTet0 + valueTet1 + valueTet2 + valueTet3) / 5.0;
+   }
+
+   void averageFromP1( P1Function< real_t > src, uint_t level )
+   {
+      for ( auto it : this->storage_->getCells() )
+      {
+         PrimitiveID cellId = it.first;
+         Cell&       cell   = *( it.second );
+
+         const auto p0DofMemory = this->getDGFunction()->volumeDoFFunction()->dofMemory( cellId, level );
+
+         auto       p1FuncId   = src.getCellDataID();
+         const auto p1FuncData = cell.getData( p1FuncId )->getPointer( level );
+
+         for ( auto cellType : celldof::allCellTypes )
+         {
+            for ( const auto& idxIt : celldof::macrocell::Iterator( level, cellType ) )
+            {
+               uint_t p0DofIdx = volumedofspace::indexing::index(
+                  idxIt.x(), idxIt.y(), idxIt.z(), cellType, 0, 1, level, volumedofspace::indexing::VolumeDoFMemoryLayout::SoA );
+
+               const std::array< indexing::Index, 4 > vertexIndices =
+                  celldof::macrocell::getMicroVerticesFromMicroCell( idxIt, cellType );
+
+               auto microTet0 = vertexdof::macrocell::coordinateFromIndex( level, cell, vertexIndices[0] );
+               auto microTet1 = vertexdof::macrocell::coordinateFromIndex( level, cell, vertexIndices[1] );
+               auto microTet2 = vertexdof::macrocell::coordinateFromIndex( level, cell, vertexIndices[2] );
+               auto microTet3 = vertexdof::macrocell::coordinateFromIndex( level, cell, vertexIndices[3] );
+
+               auto valueTet0 = p1FuncData[vertexdof::macrocell::index(
+                  level, vertexIndices[0].x(), vertexIndices[0].y(), vertexIndices[0].z() )];
+               auto valueTet1 = p1FuncData[vertexdof::macrocell::index(
+                  level, vertexIndices[1].x(), vertexIndices[1].y(), vertexIndices[1].z() )];
+               auto valueTet2 = p1FuncData[vertexdof::macrocell::index(
+                  level, vertexIndices[2].x(), vertexIndices[2].y(), vertexIndices[2].z() )];
+               auto valueTet3 = p1FuncData[vertexdof::macrocell::index(
+                  level, vertexIndices[3].x(), vertexIndices[3].y(), vertexIndices[3].z() )];
+
+               real_t sampledAverage = evaluateSampledAverage(
+                  { microTet0, microTet1, microTet2, microTet3 }, { valueTet0, valueTet1, valueTet2, valueTet3 } );
+
+               // WALBERLA_LOG_INFO_ON_ROOT( "valueTet0 = " << valueTet0 );
+               // WALBERLA_LOG_INFO_ON_ROOT( "valueTet1 = " << valueTet1 );
+               // WALBERLA_LOG_INFO_ON_ROOT( "valueTet2 = " << valueTet2 );
+               // WALBERLA_LOG_INFO_ON_ROOT( "valueTet3 = " << valueTet3 );
+               // WALBERLA_LOG_INFO_ON_ROOT( "sampledAverage = " << sampledAverage );
+
+               p0DofMemory[p0DofIdx] = sampledAverage;
+            }
+         }
+      }
+   }
 
    /// Set all function DoFs to zero including the ones in the halos
    void setToZero( const uint_t level ) const override final { dgFunction_->setToZero( level ); };
