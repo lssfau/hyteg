52dde43d91830bb674c994a58843479b89533a9c
Benjamin Mann <benjamin.mann@fau.de>
2024-12-10 16:14:43 +0100

    use compression to store basis

--- src/hyteg/polynomial/elementwise/polynomial.hpp
+++ src/hyteg/polynomial/elementwise/polynomial.hpp
@@ -40,17 +40,78 @@ static constexpr uint_t binom< n, n > = 1;
 template < uint_t D, uint_t Q >
 static constexpr uint_t dimP = binom< D + Q, D >;
 
+// store x^i*y^j*z^k by exponents i,j,k compressed into 16 bit
+struct Compression
+{
+   // todo using basis_t = uint16_t;
+   using basis_t = int;
+
+   // return compressed basis function phi = x^i*y^j*z^k
+   static constexpr inline basis_t compress( const basis_t i, const basis_t j, const basis_t k ) { return I * i | J * j | K * k; }
+   // extract exponents i,j,k from compressed basis function phi
+   static constexpr inline std::array< basis_t, 3 > expand( const basis_t phi ) { return { i( phi ), j( phi ), k( phi ) }; }
+
+   // extract exponent i of x^i*y^j*z^k
+   static constexpr inline basis_t i( const basis_t phi ) { return phi & basis_t( J - 1 ); }
+   // extract exponent j of x^i*y^j*z^k
+   static constexpr inline basis_t j( const basis_t phi ) { return ( phi & basis_t( K - 1 ) ) >> 5; }
+   // extract exponent k of x^i*y^j*z^k
+   static constexpr inline basis_t k( const basis_t phi ) { return phi >> 10; }
+   // extract the Nth exponent i,j,or k, i.e., N=0,1,2
+   template < uint8_t N >
+   static constexpr inline basis_t ijk( const basis_t phi )
+   {
+      static_assert( N < 3 );
+      if constexpr ( N == 0 )
+      {
+         return i( phi );
+      }
+      else if constexpr ( N == 1 )
+      {
+         return j( phi );
+      }
+      else
+      {
+         return k( phi );
+      }
+   }
+
+   static constexpr inline double eval( const basis_t phi, const std::array< double, 3 >& x )
+   {
+      const auto ijk = expand( phi );
+
+      double val = 1.0;
+
+      for ( uint8_t d = 0; d < 3; ++d )
+      {
+         // compute x[d]^i[d]
+         for ( uint8_t p = 0; p < ijk[d]; ++p )
+         {
+            val *= x[d];
+         }
+      }
+
+      return val;
+   }
+
+   static constexpr basis_t I = 1;
+   static constexpr basis_t J = 1 << 5;
+   static constexpr basis_t K = 1 << 10;
+};
+
 // monomial basis of polynomial space, given by the exponents of the monomials
 template < uint8_t D, uint8_t Q >
-struct Basis : public std::array< std::array< uint8_t, 3 >, dimP< D, Q > >
+struct Basis : public std::array< Compression::basis_t, dimP< D, Q > >
 {
+   using basis_t = Compression::basis_t;
+
    constexpr Basis()
-   : std::array< std::array< uint8_t, 3 >, dimP< D, Q > >{}
+   : std::array< basis_t, dimP< D, Q > >{}
    {
       // 1d basis
-      for ( uint8_t i = 0; i <= Q; ++i )
+      for ( basis_t i = 0; i <= Q; ++i )
       {
-         ( *this )[i] = { i, 0, 0 };
+         ( *this )[i] = Compression::compress( i, 0, 0 );
       }
       if constexpr ( D == 1 )
       {
@@ -58,11 +119,11 @@ struct Basis : public std::array< std::array< uint8_t, 3 >, dimP< D, Q > >
       }
       // extend to 2d
       auto ij = dimP< 1, Q >;
-      for ( uint8_t i = 0; i < Q; ++i )
+      for ( basis_t i = 0; i < Q; ++i )
       {
-         for ( uint8_t j = 1; i + j <= Q; ++j )
+         for ( basis_t j = 1; i + j <= Q; ++j )
          {
-            ( *this )[ij++] = { i, j, 0 };
+            ( *this )[ij++] = Compression::compress( i, j, 0 );
          }
       }
       if constexpr ( D == 2 )
@@ -71,47 +132,52 @@ struct Basis : public std::array< std::array< uint8_t, 3 >, dimP< D, Q > >
       }
       // extend to 3d
       auto ijk = dimP< 2, Q >;
-      for ( uint8_t i = 0; i < Q; ++i )
+      for ( basis_t i = 0; i < Q; ++i )
       {
-         for ( uint8_t k = 1; i + k <= Q; ++k )
+         for ( basis_t k = 1; i + k <= Q; ++k )
          {
-            ( *this )[ijk++] = { i, 0, k };
+            ( *this )[ijk++] = Compression::compress( i, 0, k );
          }
       }
-      for ( uint8_t i = 0; i < Q; ++i )
+      for ( basis_t i = 0; i < Q; ++i )
       {
-         for ( uint8_t j = 1; i + j < Q; ++j )
+         for ( basis_t j = 1; i + j < Q; ++j )
          {
-            for ( uint8_t k = 1; i + j + k <= Q; ++k )
+            for ( basis_t k = 1; i + j + k <= Q; ++k )
             {
-               ( *this )[ijk++] = { i, j, k };
+               ( *this )[ijk++] = Compression::compress( i, j, k );
             }
          }
       }
    }
 
-   // evaluate the i-th basis function at x
-   constexpr double eval( uint_t i, const std::array< double, 3 >& x ) const
-   {
-      double val = 1.0;
-      for ( uint8_t d = 0; d < D; ++d )
-      {
-         for ( uint8_t p = 0; p < ( *this )[i][d]; ++p )
-         {
-            val *= x[d];
-         }
-      }
-      return val;
-   }
+   // evaluate the n-th basis function at x
+   constexpr inline double eval( uint_t n, const std::array< double, 3 >& x ) const { return Compression::eval( ( *this )[n] ); }
+
+   // return the n-th basis function as array of exponents [i,j,k]
+   constexpr inline std::array< basis_t, 3 > operator()( uint_t n ) const { return Compression::expand( ( *this )[n] ); }
 };
 
-static constexpr uint8_t d = 2;
-static constexpr uint8_t q = 2;
+// static constexpr auto phi = Compression::compress( 2, 1, 3 );
+// static constexpr auto ijk = Compression::expand( phi );
+// static constexpr auto px  = Compression::eval( phi, { 2.5, 1.5, 5.0 } );
+// static constexpr auto y   = 2.5 * 2.5 * 1.5 * 5 * 5 * 5;
 
-static constexpr auto basis = Basis< d, q >();
-static constexpr auto n     = basis.size();
-static constexpr auto b0    = basis[0];
-static constexpr auto b5    = basis[5];
+// static constexpr uint8_t d = 3;
+// static constexpr uint8_t q = 2;
+
+// static constexpr auto basis = Basis< d, q >();
+// static constexpr auto n     = basis.size();
+// static constexpr auto b0    = basis( 0 );
+// static constexpr auto b1 = basis( 1 );
+// static constexpr auto b2 = basis( 2 );
+// static constexpr auto b3 = basis( 3 );
+// static constexpr auto b4 = basis( 4 );
+// static constexpr auto b5 = basis( 5 );
+// static constexpr auto b6 = basis( 6 );
+// static constexpr auto b7 = basis( 7 );
+// static constexpr auto b8 = basis( 8 );
+// static constexpr auto b9 = basis( 9 );
 
 /* Coordinate system for polynomial evaluation.
  ! Different from the coordinates of PDE domain !
@@ -202,7 +268,7 @@ class Polynomial
       }
    }
 
-//  private:
+   //  private:
    static constexpr Basis< D, Q > basis;
 
    std::array< double, nc > c; // coefficients
