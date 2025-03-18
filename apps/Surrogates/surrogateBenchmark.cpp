ce46fa9d6b06238a5238ac1c715339533b4c4612
Benjamin Mann <benjamin.mann@fau.de>
2025-03-18 16:19:21 +0100

    test with mpi

--- apps/Surrogates/surrogateBenchmark.cpp
+++ apps/Surrogates/surrogateBenchmark.cpp
@@ -32,9 +32,43 @@
 using walberla::real_t;
 using namespace hyteg;
 
+// we want roughly one element per process
+template < uint_t DIM >
+std::array< uint_t, DIM > compute_domain_size( uint_t n_procs )
+{
+   uint_t N = n_procs / ( ( DIM == 2 ) ? 2 : 6 ); // number of cubes/quads
+
+   std::vector< uint_t > primeFactors;
+   for ( uint_t i = 2; i * i <= N; ++i )
+   {
+      while ( N % i == 0 )
+      {
+         primeFactors.push_back( i );
+         N /= i;
+      }
+   }
+   if ( N > 1 )
+   {
+      primeFactors.push_back( N );
+   }
+
+   std::array< uint_t, DIM > n;
+   n.fill( 1 );
+
+   for ( auto factor : primeFactors )
+   {
+      auto min_index = std::distance( n.begin(), std::min_element( n.begin(), n.end() ) );
+      n[min_index] *= factor;
+   }
+
+   return n;
+}
+
 void benchmark( const std::shared_ptr< PrimitiveStorage >& storage, const uint8_t q_max, const uint_t level, const uint_t iter )
 {
-   uint8_t dim = storage->hasGlobalCells() ? 3 : 2;
+   uint8_t dim  = storage->hasGlobalCells() ? 3 : 2;
+   uint_t  n_el = ( dim == 2 ) ? storage->getNumberOfGlobalFaces() : storage->getNumberOfGlobalCells();
+   WALBERLA_LOG_INFO_ON_ROOT( walberla::format( "number of elements: %d", n_el ) );
 
    // setup pde coefficient k ∈ P_q
    hyteg::surrogate::polynomial::Polynomial< real_t, 3 > k_poly( q_max + 1 );
@@ -109,19 +143,23 @@ int main( int argc, char* argv[] )
    walberla::MPIManager::instance()->initializeMPI( &argc, &argv );
    walberla::MPIManager::instance()->useWorldComm();
 
+   auto n_procs = walberla::uint_c( walberla::mpi::MPIManager::instance()->numProcesses() );
+
    // ----------------------------
    //  Prepare setup for 2D tests
    // ----------------------------
-   MeshInfo              meshInfo = MeshInfo::meshRectangle( Point2D( 0.0, 0.0 ), Point2D( 1.0, 1.0 ), MeshInfo::CRISS, 1, 1 );
-   SetupPrimitiveStorage setupStorage( meshInfo, walberla::uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   auto     n2       = compute_domain_size< 2 >( n_procs );
+   MeshInfo meshInfo = MeshInfo::meshRectangle( Point2D( 0.0, 0.0 ), Point2D( 1.0, 1.0 ), MeshInfo::CRISS, n2[0], n2[1] );
+   SetupPrimitiveStorage setupStorage( meshInfo, n_procs );
    loadbalancing::roundRobin( setupStorage );
    std::shared_ptr< PrimitiveStorage > storage = std::make_shared< PrimitiveStorage >( setupStorage );
 
    // ----------------------------
    //  Prepare setup for 3D tests
    // ----------------------------
-   MeshInfo              meshInfo3d = MeshInfo::meshCuboid( Point3D( 0.0, 0.0, 0.0 ), Point3D( 1.0, 1.0, 1.0 ), 1, 1, 1 );
-   SetupPrimitiveStorage setupStorage3d( meshInfo3d, walberla::uint_c( walberla::mpi::MPIManager::instance()->numProcesses() ) );
+   auto     n3         = compute_domain_size< 3 >( n_procs );
+   MeshInfo meshInfo3d = MeshInfo::meshCuboid( Point3D( 0.0, 0.0, 0.0 ), Point3D( 1.0, 1.0, 1.0 ), n3[0], n3[1], n3[2] );
+   SetupPrimitiveStorage setupStorage3d( meshInfo3d, n_procs );
    loadbalancing::roundRobin( setupStorage3d );
    std::shared_ptr< PrimitiveStorage > storage3d = std::make_shared< PrimitiveStorage >( setupStorage3d );
 
@@ -131,7 +169,7 @@ int main( int argc, char* argv[] )
    uint_t  lvl2  = 10;
    uint_t  lvl3  = 7;
    uint8_t q_max = 3;
-   uint_t iter = 10;
+   uint_t  iter  = 5;
    WALBERLA_LOG_INFO_ON_ROOT( "" );
    WALBERLA_LOG_INFO_ON_ROOT( walberla::format( "2d, level=%d", lvl2 ) );
    benchmark( storage, q_max, lvl2, iter );
