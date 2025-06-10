80e6fe19a3f5472cbc7d9e01436432e28f206cfb
Marcus Mohr <marcus.mohr@lmu.de>
2025-06-10 15:37:02 +0200

    Places expressions inside a function generator function

--- tests/hyteg/composites/CCRStokes2DTest.cpp
+++ tests/hyteg/composites/CCRStokes2DTest.cpp
@@ -218,100 +218,6 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
    // The problem is posed in the unit square with no-slip boundary conditions
    // for velocity.
 
-   std::function< real_t( const hyteg::Point3D& ) > ux_analytic = []( const hyteg::Point3D& coords ) {
-      real_t x = coords[0];
-      real_t y = coords[1];
-
-      real_t tmp1 = ( real_c( 1 ) - x );
-      real_t tmp2 = tmp1 * tmp1;
-      real_t tmp3 = tmp2 * tmp2;
-
-      return x * x * tmp3 * y * y * ( real_c( 1 ) - y ) * ( real_c( 3 ) - real_c( 5 ) * y );
-   };
-
-   std::function< real_t( const hyteg::Point3D& ) > uy_analytic = []( const hyteg::Point3D& coords ) {
-      real_t x = coords[0];
-      real_t y = coords[1];
-
-      real_t tmp1 = ( real_c( 1 ) - x );
-      real_t tmp2 = tmp1 * tmp1 * tmp1;
-      real_t tmp3 = ( real_c( 1 ) - y ) * ( real_c( 1 ) - y );
-      real_t y3   = y * y * y;
-      return -real_c( 2 ) * x * tmp2 * ( real_c( 1 ) - real_c( 3 ) * x ) * y3 * tmp3;
-   };
-
-   std::function< real_t( const hyteg::Point3D& ) > p_analytic = []( const hyteg::Point3D& coords ) {
-      real_t x = coords[0];
-      real_t y = coords[1];
-
-      real_t x2 = x * x;
-      real_t y3 = y * y * y;
-
-      return pi * pi * ( x * y3 * std::cos( real_c( 2 ) * pi * x2 * y ) - x2 * y * std::sin( real_c( 2 ) * pi * x * y ) ) +
-             real_c( 1.0 / 8.0 );
-   };
-
-   std::function< real_t( const hyteg::Point3D& ) > rhsMomentumX = []( const hyteg::Point3D& coords ) {
-      real_t x = coords[0];
-      real_t y = coords[1];
-
-      real_t t1  = x * x;
-      real_t t4  = 0.2e1 * y * t1 * pi;
-      real_t t5  = std::cos( t4 );
-      real_t t6  = pi * pi;
-      real_t t8  = y * y;
-      real_t t9  = y * t8;
-      real_t t11 = std::sin( t4 );
-      real_t t12 = pi * t6;
-      real_t t14 = t8 * t8;
-      real_t t20 = 0.2e1 * pi * x * y;
-      real_t t21 = std::cos( t20 );
-      real_t t26 = std::sin( t20 );
-      real_t t32 = std::pow( -0.1e1 + x, 0.2e1 );
-      real_t t33 = 0.4e1 / 0.5e1 * y;
-      real_t t35 = t1 * t1;
-      real_t retVal =
-          t9 * t6 * t5 - 0.4e1 * t14 * t1 * t12 * t11 - 0.2e1 * t8 * t1 * t12 * t21 - 0.2e1 * x * y * t6 * t26 -
-          0.60000e5 *
-              ( t35 * ( t8 - t33 + 0.1e1 / 0.10e2 ) + x * t1 * ( -0.2e1 * t8 + 0.8e1 / 0.5e1 * y - 0.1e1 / 0.5e1 ) +
-                t1 * ( 0.5e1 / 0.2e1 * t14 - 0.4e1 * t9 + 0.5e1 / 0.2e1 * t8 - t33 + 0.1e1 / 0.10e2 ) +
-                x * ( -0.5e1 / 0.3e1 * t14 + 0.8e1 / 0.3e1 * t9 - t8 ) + ( -0.3e1 / 0.5e1 + y ) * ( -0.1e1 + y ) * t8 / 0.6e1 ) *
-              t32;
-
-      return retVal;
-   };
-
-   std::function< real_t( const hyteg::Point3D& ) > rhsMomentumY = []( const hyteg::Point3D& coords ) {
-      real_t x = coords[0];
-      real_t y = coords[1];
-
-      real_t t1     = x * x;
-      real_t t4     = 0.2e1 * y * t1 * pi;
-      real_t t5     = std::cos( t4 );
-      real_t t6     = pi * pi;
-      real_t t8     = y * y;
-      real_t t12    = std::sin( t4 );
-      real_t t13    = pi * t6;
-      real_t t15    = x * t1;
-      real_t t16    = y * t8;
-      real_t t22    = 0.2e1 * pi * x * y;
-      real_t t23    = std::cos( t22 );
-      real_t t28    = std::sin( t22 );
-      real_t t31    = -0.1e1 + x;
-      real_t t34    = t8 * t8;
-      real_t t40    = t1 * t1;
-      real_t t46    = t31 * t31;
-      real_t t48    = ( -0.1e1 / 0.3e1 + x ) * t46;
-      real_t retVal = 0.3e1 * t8 * x * t6 * t5 - 0.2e1 * t16 * t15 * t13 * t12 - 0.2e1 * y * t15 * t13 * t23 - t1 * t6 * t28 +
-                      0.120000e6 *
-                          ( t34 * ( t1 - x + 0.1e1 / 0.5e1 ) + t16 * ( -0.2e1 * t1 + 0.2e1 * x - 0.2e1 / 0.5e1 ) +
-                            t8 * ( t40 - 0.7e1 / 0.3e1 * t15 + 0.8e1 / 0.3e1 * t1 - 0.4e1 / 0.3e1 * x + 0.1e1 / 0.5e1 ) -
-                            0.6e1 / 0.5e1 * x * y * t48 + 0.3e1 / 0.10e2 * x * t48 ) *
-                          y * t31;
-
-      return retVal;
-   };
-
    // prepare domain and mesh
    MeshInfo meshInfo = MeshInfo::meshRectangle(
        Point2D( real_c( 0 ), real_c( 0 ) ), Point2D( real_c( 1 ), real_c( 1 ) ), hyteg::MeshInfo::CRISSCROSS, 2, 2 );
@@ -322,11 +228,11 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
    uint_t                      level = 4;
    CCRStokesFunction< real_t > sol_analytic( "Analytic Solution", storage, level, level );
 
-   sol_analytic.uvw().interpolate( { ux_analytic, uy_analytic }, level );
-   sol_analytic.p().interpolate( p_analytic, level );
+   sol_analytic.uvw().interpolate( { getAnalyticalExpression( ExpressionType::UX ), getAnalyticalExpression( ExpressionType::UY ) }, level );
+   sol_analytic.p().interpolate( getAnalyticalExpression( ExpressionType::P ), level );
 
    CCRStokesFunction< real_t > rhs_analytic( "Analytic RHS", storage, level, level );
-   rhs_analytic.uvw().interpolate( { rhsMomentumX, rhsMomentumY }, level );
+   rhs_analytic.uvw().interpolate( { getAnalyticalExpression( ExpressionType::RHS_X ), getAnalyticalExpression( ExpressionType::RHS_Y ) }, level );
 
    // setup FE problem
    CCRStokesOperator           stokesOperator( storage, level, level );
