0b67b96a2eb6684c11fe852efeee4bbd310576d9
Marcus Mohr <marcus.mohr@lmu.de>
2025-06-10 15:27:19 +0200

    Further work on exmple D3 from book by John.
    Use Maple to derive the rhs of the momentum equation. Problem runs through
    and results look good qualitatively, but we are a factor of 1.000 off in
    both velocity and pressure.

--- tests/hyteg/composites/CCRStokes2DTest.cpp
+++ tests/hyteg/composites/CCRStokes2DTest.cpp
@@ -34,6 +34,8 @@
 #include "hyteg/primitivestorage/SetupPrimitiveStorage.hpp"
 #include "hyteg/primitivestorage/Visualization.hpp"
 
+#include "mixed_operator/VectorMassOperator.hpp"
+
 // #ifndef HYTEG_BUILD_WITH_PETSC
 // WALBERLA_ABORT( "This test only works with PETSc enabled. Please enable it via -DHYTEG_BUILD_WITH_PETSC=ON" )
 // #endif
@@ -64,6 +66,141 @@ void objectInstantionTest()
    CCRStokesOperator           oper( storage, level, level );
 }
 
+enum class ExpressionType
+{
+   UX,
+   UY,
+   P,
+   RHS_X,
+   RHS_Y
+};
+
+std::function< real_t( const hyteg::Point3D& ) > getAnalyticalExpression( ExpressionType exType )
+{
+   // The test uses example D.3 for Steady-State flow problems
+   // from John, "Finite Element Methods for Incompressible Flow Problems"
+   //             _
+   //            |  x^2 (1-x)^4 y^2 (1-y) (3-5y)
+   // u = 1000 * |
+   //            |_ -2x (1-x)^3 (1-3x) y^3 (1-y)^2
+   //
+   //
+   // p = pi^2 ( x y^3 cos( 2 pi x^2 y ) - x^2 y sin( 2 pi x y ) + 1/8
+   //
+   // The problem is posed in the unit square with no-slip boundary conditions
+   // for velocity.
+   std::function< real_t( const hyteg::Point3D& ) > expression;
+   switch ( exType )
+   {
+   case ( ExpressionType::UX ):
+      expression = []( const hyteg::Point3D& coords ) {
+         real_t x = coords[0];
+         real_t y = coords[1];
+
+         real_t tmp1 = ( real_c( 1 ) - x );
+         real_t tmp2 = tmp1 * tmp1;
+         real_t tmp3 = tmp2 * tmp2;
+
+         return x * x * tmp3 * y * y * ( real_c( 1 ) - y ) * ( real_c( 3 ) - real_c( 5 ) * y );
+      };
+      break;
+
+   case ( ExpressionType::UY ):
+      expression = []( const hyteg::Point3D& coords ) {
+         real_t x = coords[0];
+         real_t y = coords[1];
+
+         real_t tmp1 = ( real_c( 1 ) - x );
+         real_t tmp2 = tmp1 * tmp1 * tmp1;
+         real_t tmp3 = ( real_c( 1 ) - y ) * ( real_c( 1 ) - y );
+         real_t y3   = y * y * y;
+         return -real_c( 2 ) * x * tmp2 * ( real_c( 1 ) - real_c( 3 ) * x ) * y3 * tmp3;
+      };
+      break;
+
+   case ( ExpressionType::P ):
+      expression = []( const hyteg::Point3D& coords ) {
+         real_t x = coords[0];
+         real_t y = coords[1];
+
+         real_t x2 = x * x;
+         real_t y3 = y * y * y;
+
+         return pi * pi * ( x * y3 * std::cos( real_c( 2 ) * pi * x2 * y ) - x2 * y * std::sin( real_c( 2 ) * pi * x * y ) ) +
+                real_c( 1.0 / 8.0 );
+      };
+      break;
+
+   case ( ExpressionType::RHS_X ):
+      expression = []( const hyteg::Point3D& coords ) {
+         real_t x = coords[0];
+         real_t y = coords[1];
+
+         real_t t1  = x * x;
+         real_t t4  = 0.2e1 * y * t1 * pi;
+         real_t t5  = std::cos( t4 );
+         real_t t6  = pi * pi;
+         real_t t8  = y * y;
+         real_t t9  = y * t8;
+         real_t t11 = std::sin( t4 );
+         real_t t12 = pi * t6;
+         real_t t14 = t8 * t8;
+         real_t t20 = 0.2e1 * pi * x * y;
+         real_t t21 = std::cos( t20 );
+         real_t t26 = std::sin( t20 );
+         real_t t32 = std::pow( -0.1e1 + x, 0.2e1 );
+         real_t t33 = 0.4e1 / 0.5e1 * y;
+         real_t t35 = t1 * t1;
+         real_t retVal =
+             t9 * t6 * t5 - 0.4e1 * t14 * t1 * t12 * t11 - 0.2e1 * t8 * t1 * t12 * t21 - 0.2e1 * x * y * t6 * t26 -
+             0.60000e5 *
+                 ( t35 * ( t8 - t33 + 0.1e1 / 0.10e2 ) + x * t1 * ( -0.2e1 * t8 + 0.8e1 / 0.5e1 * y - 0.1e1 / 0.5e1 ) +
+                   t1 * ( 0.5e1 / 0.2e1 * t14 - 0.4e1 * t9 + 0.5e1 / 0.2e1 * t8 - t33 + 0.1e1 / 0.10e2 ) +
+                   x * ( -0.5e1 / 0.3e1 * t14 + 0.8e1 / 0.3e1 * t9 - t8 ) +
+                   ( -0.3e1 / 0.5e1 + y ) * ( -0.1e1 + y ) * t8 / 0.6e1 ) *
+                 t32;
+
+         return retVal;
+      };
+      break;
+
+   case ( ExpressionType::RHS_Y ):
+      expression = []( const hyteg::Point3D& coords ) {
+         real_t x = coords[0];
+         real_t y = coords[1];
+
+         real_t t1     = x * x;
+         real_t t4     = 0.2e1 * y * t1 * pi;
+         real_t t5     = std::cos( t4 );
+         real_t t6     = pi * pi;
+         real_t t8     = y * y;
+         real_t t12    = std::sin( t4 );
+         real_t t13    = pi * t6;
+         real_t t15    = x * t1;
+         real_t t16    = y * t8;
+         real_t t22    = 0.2e1 * pi * x * y;
+         real_t t23    = std::cos( t22 );
+         real_t t28    = std::sin( t22 );
+         real_t t31    = -0.1e1 + x;
+         real_t t34    = t8 * t8;
+         real_t t40    = t1 * t1;
+         real_t t46    = t31 * t31;
+         real_t t48    = ( -0.1e1 / 0.3e1 + x ) * t46;
+         real_t retVal = 0.3e1 * t8 * x * t6 * t5 - 0.2e1 * t16 * t15 * t13 * t12 - 0.2e1 * y * t15 * t13 * t23 - t1 * t6 * t28 +
+                         0.120000e6 *
+                             ( t34 * ( t1 - x + 0.1e1 / 0.5e1 ) + t16 * ( -0.2e1 * t1 + 0.2e1 * x - 0.2e1 / 0.5e1 ) +
+                               t8 * ( t40 - 0.7e1 / 0.3e1 * t15 + 0.8e1 / 0.3e1 * t1 - 0.4e1 / 0.3e1 * x + 0.1e1 / 0.5e1 ) -
+                               0.6e1 / 0.5e1 * x * y * t48 + 0.3e1 / 0.10e2 * x * t48 ) *
+                             y * t31;
+
+         return retVal;
+      };
+      break;
+   }
+
+   return expression;
+}
+
 void manufacturedSolutionTest( bool doVTKOutput = false )
 {
    WALBERLA_LOG_INFO_ON_ROOT( "Running \"manufacturedSolutionTest\"" );
@@ -73,7 +210,7 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
    //             _
    //            |  x^2 (1-x)^4 y^2 (1-y) (3-5y)
    // u = 1000 * |
-   //            |_ -2x (1-x)^3 (^-3x) y^3 (1-y)^2
+   //            |_ -2x (1-x)^3 (1-3x) y^3 (1-y)^2
    //
    //
    // p = pi^2 ( x y^3 cos( 2 pi x^2 y ) - x^2 y sin( 2 pi x y ) + 1/8
@@ -114,6 +251,67 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
              real_c( 1.0 / 8.0 );
    };
 
+   std::function< real_t( const hyteg::Point3D& ) > rhsMomentumX = []( const hyteg::Point3D& coords ) {
+      real_t x = coords[0];
+      real_t y = coords[1];
+
+      real_t t1  = x * x;
+      real_t t4  = 0.2e1 * y * t1 * pi;
+      real_t t5  = std::cos( t4 );
+      real_t t6  = pi * pi;
+      real_t t8  = y * y;
+      real_t t9  = y * t8;
+      real_t t11 = std::sin( t4 );
+      real_t t12 = pi * t6;
+      real_t t14 = t8 * t8;
+      real_t t20 = 0.2e1 * pi * x * y;
+      real_t t21 = std::cos( t20 );
+      real_t t26 = std::sin( t20 );
+      real_t t32 = std::pow( -0.1e1 + x, 0.2e1 );
+      real_t t33 = 0.4e1 / 0.5e1 * y;
+      real_t t35 = t1 * t1;
+      real_t retVal =
+          t9 * t6 * t5 - 0.4e1 * t14 * t1 * t12 * t11 - 0.2e1 * t8 * t1 * t12 * t21 - 0.2e1 * x * y * t6 * t26 -
+          0.60000e5 *
+              ( t35 * ( t8 - t33 + 0.1e1 / 0.10e2 ) + x * t1 * ( -0.2e1 * t8 + 0.8e1 / 0.5e1 * y - 0.1e1 / 0.5e1 ) +
+                t1 * ( 0.5e1 / 0.2e1 * t14 - 0.4e1 * t9 + 0.5e1 / 0.2e1 * t8 - t33 + 0.1e1 / 0.10e2 ) +
+                x * ( -0.5e1 / 0.3e1 * t14 + 0.8e1 / 0.3e1 * t9 - t8 ) + ( -0.3e1 / 0.5e1 + y ) * ( -0.1e1 + y ) * t8 / 0.6e1 ) *
+              t32;
+
+      return retVal;
+   };
+
+   std::function< real_t( const hyteg::Point3D& ) > rhsMomentumY = []( const hyteg::Point3D& coords ) {
+      real_t x = coords[0];
+      real_t y = coords[1];
+
+      real_t t1     = x * x;
+      real_t t4     = 0.2e1 * y * t1 * pi;
+      real_t t5     = std::cos( t4 );
+      real_t t6     = pi * pi;
+      real_t t8     = y * y;
+      real_t t12    = std::sin( t4 );
+      real_t t13    = pi * t6;
+      real_t t15    = x * t1;
+      real_t t16    = y * t8;
+      real_t t22    = 0.2e1 * pi * x * y;
+      real_t t23    = std::cos( t22 );
+      real_t t28    = std::sin( t22 );
+      real_t t31    = -0.1e1 + x;
+      real_t t34    = t8 * t8;
+      real_t t40    = t1 * t1;
+      real_t t46    = t31 * t31;
+      real_t t48    = ( -0.1e1 / 0.3e1 + x ) * t46;
+      real_t retVal = 0.3e1 * t8 * x * t6 * t5 - 0.2e1 * t16 * t15 * t13 * t12 - 0.2e1 * y * t15 * t13 * t23 - t1 * t6 * t28 +
+                      0.120000e6 *
+                          ( t34 * ( t1 - x + 0.1e1 / 0.5e1 ) + t16 * ( -0.2e1 * t1 + 0.2e1 * x - 0.2e1 / 0.5e1 ) +
+                            t8 * ( t40 - 0.7e1 / 0.3e1 * t15 + 0.8e1 / 0.3e1 * t1 - 0.4e1 / 0.3e1 * x + 0.1e1 / 0.5e1 ) -
+                            0.6e1 / 0.5e1 * x * y * t48 + 0.3e1 / 0.10e2 * x * t48 ) *
+                          y * t31;
+
+      return retVal;
+   };
+
    // prepare domain and mesh
    MeshInfo meshInfo = MeshInfo::meshRectangle(
        Point2D( real_c( 0 ), real_c( 0 ) ), Point2D( real_c( 1 ), real_c( 1 ) ), hyteg::MeshInfo::CRISSCROSS, 2, 2 );
@@ -127,6 +325,9 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
    sol_analytic.uvw().interpolate( { ux_analytic, uy_analytic }, level );
    sol_analytic.p().interpolate( p_analytic, level );
 
+   CCRStokesFunction< real_t > rhs_analytic( "Analytic RHS", storage, level, level );
+   rhs_analytic.uvw().interpolate( { rhsMomentumX, rhsMomentumY }, level );
+
    // setup FE problem
    CCRStokesOperator           stokesOperator( storage, level, level );
    CCRStokesFunction< real_t > discrete_rhs( "Discrete RHS", storage, level, level );
@@ -134,15 +335,22 @@ void manufacturedSolutionTest( bool doVTKOutput = false )
 
    // note: problem has no-slip boundary conditions, so nothing to do for this
 
+   P2PlusBubbleElementwiseVectorMassOperator massOperator( storage, level, level );
+   massOperator.apply( rhs_analytic.uvw(), discrete_rhs.uvw(), level, All );
+
    EigenSparseDirectSolver< CCRStokesOperator > EigenLU( storage, level );
    // EigenLU.setReassembleMatrix( true );
    EigenLU.solve( stokesOperator, discrete_sol, discrete_rhs, level );
+
+   // PETScLUSolver< CCRStokesOperator > petscLU( storage, level );
+   // petscLU.solve( stokesOperator, discrete_sol, discrete_rhs, level );
 
    // export data for post-processing
    if ( doVTKOutput )
    {
       VTKOutput vtkOutput( ".", "ccr", storage );
       vtkOutput.add( sol_analytic );
+      vtkOutput.add( rhs_analytic );
       vtkOutput.add( discrete_sol );
       vtkOutput.add( discrete_rhs );
       vtkOutput.write( level );
